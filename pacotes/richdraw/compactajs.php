6ed3d3407c74ad6d9f12063e4056483743c97907
Edmar Moretti <edmar.moretti@gmail.com>
2007-11-01 15:49:32 +0000

    incluida a biblioteca richdraw para criação de figuras geométricas via SVG ou VML

---
+++ pacotes/richdraw/compactajs.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Title: Compacta js e css do diretório pacotes/richdraw
+
+Compacta os arquivos js utilizados pelo I3Geo.
+
+Deve ser executado sempre que forem feitas alterações nos arquivos javascript existentes em classesjs ou nos arquivos de estilo existentes em css.
+
+About: Licença
+
+I3Geo Interface Integrada de Ferramentas de Geoprocessamento para Internet
+
+Direitos Autorais Reservados (c) 2006 Ministério do Meio Ambiente Brasil
+Desenvolvedor: Edmar Moretti edmar.moretti@mma.gov.br
+
+Este programa é software livre; você pode redistribuí-lo
+e/ou modificá-lo sob os termos da Licença Pública Geral
+GNU conforme publicada pela Free Software Foundation;
+tanto a versão 2 da Licença.
+Este programa é distribuído na expectativa de que seja útil,
+porém, SEM NENHUMA GARANTIA; nem mesmo a garantia implícita
+de COMERCIABILIDADE OU ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA.
+Consulte a Licença Pública Geral do GNU para mais detalhes.
+Você deve ter recebido uma cópia da Licença Pública Geral do
+GNU junto com este programa; se não, escreva para a
+Free Software Foundation, Inc., no endereço
+59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+
+File: compactajs.php
+
+19/7/2007
+
+*/
+//
+//compacta os arquivos do i3geo
+//gera um arquivo compactado para cada um
+//
+//packer("prototype.js","prototype_compacto.js","High ASCII");
+packer("richdraw.js","richdraw_compacto.js","High ASCII");
+packer("svgrenderer.js","svgrenderer_compacto.js","High ASCII");
+packer("vmlrenderer.js","vmlrenderer_compacto.js","High ASCII");
+//packer("prototype.js","prototype_compacto.js","High ASCII");
+
+$s = inicia("prototype.js");
+$abre = fopen("prototype_compacto.js", "wt");
+$escreve = fwrite ($abre,$s);
+$fecha = fclose ($abre);
+
+//
+//gera um único js
+//
+$jsfiles = array(
+"richdraw_compacto.js",
+"svgrenderer_compacto.js",
+"vmlrenderer_compacto.js"
+);
+$buffer = "";
+//junta todos os js em um unico
+foreach ($jsfiles as $f)
+{
+	$abre = fopen($f, "r");
+	while (!feof($abre))
+	{$buffer .= fgets($abre);}
+	fclose($abre);
+	$buffer .= "\n";
+}
+$abre = fopen("richdraw_tudo_compacto.js", "wt");
+$escreve = fwrite ($abre,$buffer);
+$fecha = fclose ($abre);
+chmod("richdraw_tudo_compacto.js",0777);
+function packer($src,$out,$tipo="None")
+{
+//packer
+//$src = 'temp.js';
+//$out = 'i3geo_tudo_compacto.js';
+require_once '../../classesjs/packer/class.JavaScriptPacker.php';
+$script = file_get_contents($src);
+$t1 = microtime(true);
+$packer = new JavaScriptPacker($script, $tipo, true, false);
+$packed = $packer->pack();
+$t2 = microtime(true);
+$time = sprintf('%.4f', ($t2 - $t1) );
+echo 'script ', $src, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
+file_put_contents($out, $packed);
+chmod($out,0777);
+}
+function inicia($arquivo)
+{
+	$abre = fopen($arquivo, "r");
+	while (!feof($abre))
+	{
+		$buffer = fgets($abre);
+		$maparray[] = $buffer."kkkk";
+	}
+	fclose($abre);
+	$c = compress(implode("",$maparray));
+	$c = str_replace("kkkk","\n",$c);
+	$c = str_replace("kkk","",$c);
+	$c = str_replace(";\n",";",$c);
+	$c = str_replace("{\n","{",$c);
+	$c = str_replace("\n}","}",$c);
+	$c = str_replace(")\n",")",$c);
+	$c = str_replace(" \n","",$c);
+	$c = str_replace("\n}","}\n",$c);
+	return $c;
+}
+function compress($code)
+{ // Remove multiline comment
+$mlcomment = '/\/\*(?!-)[\x00-\xff]*?\*\//';
+$code = preg_replace($mlcomment,"",$code);
+
+// Remove single line comment
+$slcomment = '/[^:]\/\/.*/';
+$code = preg_replace($slcomment,"",$code);
+
+// Remove extra spaces
+$extra_space = '/\s+/';
+$code = preg_replace($extra_space," ",$code);
+
+// Remove spaces that can be removed
+$removable_space = '/\s?([\{\};\=\(\)\\\/\+\*-])\s?/';
+$code = preg_replace('/\s?([\{\};\=\(\)\/\+\*-])\s?/',"\\1",$code);
+return $code;
+}
+
+?>
---
+++ pacotes/richdraw/svgrenderer.js
@@ -0,0 +1,271 @@
+/*----------------------------------------------------------------------------
+ SVGRENDERER 1.0
+ SVG Renderer For RichDraw
+ -----------------------------------------------------------------------------
+ Created by Mark Finkle (mark.finkle@gmail.com)
+ Implementation of SVG based renderer.
+ -----------------------------------------------------------------------------
+ Copyright (c) 2006 Mark Finkle
+
+ This program is  free software;  you can redistribute  it and/or  modify it
+ under the terms of the MIT License.
+
+ Permission  is hereby granted,  free of charge, to  any person  obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the  Software without restriction,  including without limitation
+ the  rights to use, copy, modify,  merge, publish, distribute,  sublicense,
+ and/or  sell copies  of the  Software, and to  permit persons to  whom  the
+ Software is  furnished  to do  so, subject  to  the  following  conditions:
+ The above copyright notice and this  permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS",  WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED,  INCLUDING BUT NOT LIMITED TO  THE WARRANTIES  OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR  COPYRIGHT  HOLDERS BE  LIABLE FOR  ANY CLAIM,  DAMAGES OR OTHER
+ LIABILITY, WHETHER  IN AN  ACTION OF CONTRACT, TORT OR  OTHERWISE,  ARISING
+ FROM,  OUT OF OR  IN  CONNECTION  WITH  THE  SOFTWARE OR THE  USE OR  OTHER
+ DEALINGS IN THE SOFTWARE.
+ -----------------------------------------------------------------------------
+ Dependencies:
+ History:
+ 2006-04-05 | Created
+ --------------------------------------------------------------------------*/
+
+
+function SVGRenderer() {
+	this.base = AbstractRenderer;
+	this.svgRoot = null;
+}
+
+
+SVGRenderer.prototype = new AbstractRenderer;
+
+
+SVGRenderer.prototype.init = function(elem) {
+  this.container = elem;
+
+  this.container.style.MozUserSelect = 'none';
+    
+  var svgNamespace = 'http://www.w3.org/2000/svg';
+  this.svgRoot = this.container.ownerDocument.createElementNS(svgNamespace, "svg");
+  this.container.appendChild(this.svgRoot);
+};
+
+
+SVGRenderer.prototype.bounds = function(shape) {
+  var rect = new Object();
+  var box = shape.getBBox();
+  rect['x'] = box.x;
+  rect['y'] = box.y;
+  rect['width'] =  box.width;
+  rect['height'] = box.height;
+  return rect;
+};
+
+
+SVGRenderer.prototype.create = function(shape, fillColor, lineColor, lineWidth, left, top, width, height) {
+  var svgNamespace = 'http://www.w3.org/2000/svg';
+  var svg;
+
+  if (shape == 'rect') {
+    svg = this.container.ownerDocument.createElementNS(svgNamespace, 'rect');
+    svg.setAttributeNS(null, 'x', left + 'px');
+    svg.setAttributeNS(null, 'y', top + 'px');
+    svg.setAttributeNS(null, 'width', width + 'px');
+    svg.setAttributeNS(null, 'height', height + 'px');
+  }
+  else if (shape == 'ellipse') {
+    svg = this.container.ownerDocument.createElementNS(svgNamespace, 'ellipse');
+    svg.setAttributeNS(null, 'cx', (left + width / 2) + 'px');
+    svg.setAttributeNS(null, 'cy', (top + height / 2) + 'px');
+    svg.setAttributeNS(null, 'rx', (width / 2) + 'px');
+    svg.setAttributeNS(null, 'ry', (height / 2) + 'px');
+  }
+  else if (shape == 'roundrect') {
+    svg = this.container.ownerDocument.createElementNS(svgNamespace, 'rect');
+    svg.setAttributeNS(null, 'x', left + 'px');
+    svg.setAttributeNS(null, 'y', top + 'px');
+    svg.setAttributeNS(null, 'rx', '20px');
+    svg.setAttributeNS(null, 'ry', '20px');
+    svg.setAttributeNS(null, 'width', width + 'px');
+    svg.setAttributeNS(null, 'height', height + 'px');
+  }
+  else if (shape == 'line') {
+    svg = this.container.ownerDocument.createElementNS(svgNamespace, 'line');
+    svg.setAttributeNS(null, 'x1', left + 'px');
+    svg.setAttributeNS(null, 'y1', top + 'px');
+    svg.setAttributeNS(null, 'x2', left + 'px');
+    svg.setAttributeNS(null, 'y2', top + 'px');
+  }
+
+  svg.style.position = 'absolute';
+
+  if (fillColor.length == 0)
+    fillColor = 'none';
+  svg.setAttributeNS(null, 'fill', fillColor);
+
+  if (lineColor.length == 0)
+    lineColor = 'none';
+  svg.setAttributeNS(null, 'stroke', lineColor);
+  svg.setAttributeNS(null, 'stroke-width', lineWidth);
+      
+  this.svgRoot.appendChild(svg);
+  
+  return svg;
+};
+
+
+SVGRenderer.prototype.remove = function(shape) {
+  shape.parentNode.removeChild(shape);
+};
+
+
+SVGRenderer.prototype.move = function(shape, left, top) {
+  if (shape.tagName == 'line') {
+    var deltaX = shape.getBBox().width;
+    var deltaY = shape.getBBox().height;
+    shape.setAttributeNS(null, 'x1', left);
+    shape.setAttributeNS(null, 'y1', top);
+    shape.setAttributeNS(null, 'x2', left + deltaX);
+    shape.setAttributeNS(null, 'y2', top + deltaY);
+  }
+  else if (shape.tagName == 'ellipse') {
+    shape.setAttributeNS(null, 'cx', left + (shape.getBBox().width / 2));
+    shape.setAttributeNS(null, 'cy', top + (shape.getBBox().height / 2));
+  }
+  else {
+    shape.setAttributeNS(null, 'x', left);
+    shape.setAttributeNS(null, 'y', top);
+  }
+};
+
+
+SVGRenderer.prototype.track = function(shape) {
+  // TODO
+};
+
+
+SVGRenderer.prototype.resize = function(shape, fromX, fromY, toX, toY) {
+  var deltaX = toX - fromX;
+  var deltaY = toY - fromY;
+
+  if (shape.tagName == 'line') {
+    shape.setAttributeNS(null, 'x2', toX);
+    shape.setAttributeNS(null, 'y2', toY);
+  }
+  else if (shape.tagName == 'ellipse') {
+    if (deltaX < 0) {
+      shape.setAttributeNS(null, 'cx', (fromX + deltaX / 2) + 'px');
+      shape.setAttributeNS(null, 'rx', (-deltaX / 2) + 'px');
+    }
+    else {
+      shape.setAttributeNS(null, 'cx', (fromX + deltaX / 2) + 'px');
+      shape.setAttributeNS(null, 'rx', (deltaX / 2) + 'px');
+    }
+  
+    if (deltaY < 0) {
+      shape.setAttributeNS(null, 'cy', (fromY + deltaY / 2) + 'px');
+      shape.setAttributeNS(null, 'ry', (-deltaY / 2) + 'px');
+    }
+    else {
+      shape.setAttributeNS(null, 'cy', (fromY + deltaY / 2) + 'px');
+      shape.setAttributeNS(null, 'ry', (deltaY / 2) + 'px');
+    }
+  }
+  else { 
+    if (deltaX < 0) {
+      shape.setAttributeNS(null, 'x', toX + 'px');
+      shape.setAttributeNS(null, 'width', -deltaX + 'px');
+    }
+    else {
+      shape.setAttributeNS(null, 'width', deltaX + 'px');
+    }
+  
+    if (deltaY < 0) {
+      shape.setAttributeNS(null, 'y', toY + 'px');
+      shape.setAttributeNS(null, 'height', -deltaY + 'px');
+    }
+    else {
+      shape.setAttributeNS(null, 'height', deltaY + 'px');
+    }
+  }
+};
+
+
+SVGRenderer.prototype.editCommand = function(shape, cmd, value)
+{
+  if (shape != null) {
+    if (cmd == 'fillcolor') {
+      if (value != '')
+        shape.setAttributeNS(null, 'fill', value);
+      else
+        shape.setAttributeNS(null, 'fill', 'none');
+    }
+    else if (cmd == 'linecolor') {
+      if (value != '')
+        shape.setAttributeNS(null, 'stroke', value);
+      else
+        shape.setAttributeNS(null, 'stroke', 'none');
+    }
+    else if (cmd == 'linewidth') {
+      shape.setAttributeNS(null, 'stroke-width', parseInt(value) + 'px');
+    }
+  }
+};
+
+
+SVGRenderer.prototype.queryCommand = function(shape, cmd)
+{
+  var result = '';
+  
+  if (shape != null) {
+    if (cmd == 'fillcolor') {
+      result = shape.getAttributeNS(null, 'fill');
+      if (result == 'none')
+        result = '';
+    }
+    else if (cmd == 'linecolor') {
+      result = shape.getAttributeNS(null, 'stroke');
+      if (result == 'none')
+        result = '';
+    }
+    else if (cmd == 'linewidth') {
+      result = shape.getAttributeNS(null, 'stroke');
+      if (result == 'none')
+        result = '';
+      else
+        result = shape.getAttributeNS(null, 'stroke-width');
+    }
+  }
+  
+  return result;
+};
+
+
+SVGRenderer.prototype.showTracker = function(shape) {
+  var box = shape.getBBox();
+
+  var tracker = document.getElementById('tracker');
+  if (tracker) {
+    this.remove(tracker);
+  }
+
+  var svgNamespace = 'http://www.w3.org/2000/svg';
+
+  tracker = document.createElementNS(svgNamespace, 'rect');
+  tracker.setAttributeNS(null, 'id', 'tracker');
+  tracker.setAttributeNS(null, 'x', box.x - 10);
+  tracker.setAttributeNS(null, 'y', box.y - 10);
+  tracker.setAttributeNS(null, 'width', box.width + 20);
+  tracker.setAttributeNS(null, 'height', box.height + 20);
+  tracker.setAttributeNS(null, 'fill', 'none');
+  tracker.setAttributeNS(null, 'stroke', 'blue');
+  tracker.setAttributeNS(null, 'stroke-width', '1');
+  this.svgRoot.appendChild(tracker);
+};
+
+
+SVGRenderer.prototype.getMarkup = function() {
+  return this.container.innerHTML;
+};
