9afb55a5ebcda6878fc153c3268fb4495a79d609
Edmar Moretti <edmar.moretti@gmail.com>
2008-12-22 14:01:42 +0000

    Incluída opção de navegação em flash na nuvem de tags

--- ferramentas/nuvemtags/index.js
+++ ferramentas/nuvemtags/index.js
@@ -34,13 +34,22 @@ YAHOO.example.init = function ()
 aguarde("block")
 parametrosURL()
 inicio = 0;
+var tagsFlash;
+var palavra = "";
+var temp = (window.location.href).split("palavra=");
+if (temp[1])
+{palavra = (temp[1].split("&"))[0];}
+if(palavra != "")
+procurar(palavra)
 function montaNuvem(r)
 {
 	retorno = r;
 	if(retorno.data)
 	{
 		var tags ="Clique na TAG para localizar temas relacionados<br><br><span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='javascript:inicio = inicio+2;montaNuvem(retorno);' style='cursor:pointer;vertical-align:middle;color:navy;font-size:'12'pt;'>menos</span><span>&nbsp;</span>"
-		tags +="<span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='javascript:inicio = inicio-2;montaNuvem(retorno);' style='cursor:pointer;vertical-align:middle;color:navy;font-size:'12'pt;'>mais</span><br><br>"
+		tags +="<span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='javascript:inicio = inicio-2;montaNuvem(retorno);' style='cursor:pointer;vertical-align:middle;color:navy;font-size:'12'pt;'>mais</span><span>&nbsp;</span>"
+		tags +="<span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='javascript:flash();' style='cursor:pointer;vertical-align:middle;color:navy;font-size:'12'pt;'>animar</span><br><br>"
+
 		if((inicio < 0) || (inicio > retorno.data.length)){return;}
 		for (i=0;i<retorno.data.length;i++)
 		{
@@ -57,11 +66,11 @@ function montaNuvem(r)
 						for (r=0;r<retorno.data[i].noticias.length;r++)
 						{
 							linkrss += "<span><a href='"+retorno.data[i].noticias[r].link+"' target=blanck ><img style=cursor:pointer src='../../imagens/mais.png' title='"+retorno.data[i].noticias[r].titulo+"'/></a></span>" ;
-						}
-						
+						}		
 					}
-
-					tags += "<span> </span> <span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='procurar(this)' style='cursor:pointer;vertical-align:middle;color:rgb("+cor+");font-size:"+h+"pt;'>"+retorno.data[i].tag+"</span>"+linkrss
+					tags += "<span> </span> <span onmouseout='this.style.textDecoration=\"none\"' onmouseover='this.style.textDecoration=\"underline\"' onclick='procurar(\""+retorno.data[i].tag+"\")' style='cursor:pointer;vertical-align:middle;color:rgb("+cor+");font-size:"+h+"pt;'>"+retorno.data[i].tag+"</span>"+linkrss
+					if(h > 10)
+					tagsFlash += "<a href='index.htm?palavra="+retorno.data[i].tag+"' target='_top' style='font-size:"+(h - 15)+"px;color:red;' >"+retorno.data[i].tag+"</a>"
 				}
 			}
 		}
@@ -81,10 +90,10 @@ cp.call(p,"listaTags",montaNuvem);
 
 function procurar(texto)
 {
-	if(window.parent.document.getElementById("buscatema"))
+	if(window.parent.document.getElementById("i3geo_buscatema"))
 	{
-		window.parent.document.getElementById("buscatema").value = texto.innerHTML
-		window.parent.procurartemas()
+		window.parent.document.getElementById("i3geo_buscatema").value = texto
+		window.parent.i3GEO.arvoreDeTemas.buscaTema(texto);
 	}
 }
 
@@ -99,4 +108,18 @@ function buscarss()
 	//cp.set_debug(2)
 	var p = g_locaplic+"/classesphp/mapa_controle.php?funcao=listaTags&g_sid="+g_sid+"&rss="+rss+"&nrss="+$i("nrss").value;
 	cp.call(p,"listaTags",montaNuvem);	
+}
+function flash()
+{
+	$i("resultado").style.top = "-100px";
+	$i("resultado").style.left = "-100px";
+	var so = new SWFObject("../../pacotes/wpcumulus/tagcloud.swf", "tagcloud", "600", "600", "9", "#ffffff");
+	// uncomment next line to enable transparency
+	//so.addParam("wmode", "transparent");
+	so.addVariable("tcolor", "0x333333");
+	so.addVariable("mode", "tags");
+	so.addVariable("distr", "true");
+	so.addVariable("tspeed", "100");
+	so.addVariable("tagcloud", "<tags>"+tagsFlash+"</tags>");
+	so.write("resultado");
 }
