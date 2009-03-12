e114bd6cbeb2a5df54d07d856f373aab01753144
Edmar Moretti <edmar.moretti@gmail.com>
2009-03-12 14:36:42 +0000

    inclusão nas funcoes das ferramentas do cpaint2.inc.js ao inves da versão compactada

--- classesjs/compactajs.php
+++ classesjs/compactajs.php
@@ -166,7 +166,7 @@ $jsfiles = array(
 "../pacotes/yui252/build/tabview/tabview-min.js",
 "../pacotes/yui252/build/animation/animation-min.js",
 "../pacotes/yui252/build/autocomplete/autocomplete-min.js",
-"../pacotes/cpaint/cpaint2.inc.compressed.js"
+"../pacotes/cpaint/cpaint2.inc.js"
 );
 $buffer = "\$i = function(id){return document.getElementById(id);}\n";
 salvatudojs($jsfiles,$buffer,"../ferramentas/i3geo_tudo_compacto.js","js");
