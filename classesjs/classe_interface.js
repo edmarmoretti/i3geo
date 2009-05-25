b5067d101dd900a6300e1df9b07f054c36b324f5
Edmar Moretti <edmar.moretti@gmail.com>
2009-05-25 17:33:05 +0000

    Corrigida função que possibilita iniciar o mapa com um visual diferente escolhido (a função de troca de visual não estava sendo executada na inicialização) Incluídos dois novos exemplos de configuração de visual.

--- classesjs/classe_interface.js
+++ classesjs/classe_interface.js
@@ -279,6 +279,8 @@ i3GEO.interface = {
 			}
 			if(i3GEO.interface.ATIVAMENUCONTEXTO)
 			i3GEO.interface.padrao.ativaMenuContexto();
+			if(i3GEO.configura.visual != "default")
+			i3GEO.gadgets.visual.troca(i3GEO.configura.visual);
 		}
 	},
 	/*
