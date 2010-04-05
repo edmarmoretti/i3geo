60398593d577745fb24ddefeb018253f27030f4f
Edmar Moretti <edmar.moretti@gmail.com>
2010-04-05 15:03:22 +0000

    Correção na abertura da ferramenta analisaGeometrias

--- ferramentas/analisageometrias/index.js.php
+++ ferramentas/analisageometrias/index.js.php
@@ -170,7 +170,7 @@ i3GEOF.analisaGeometrias = {
 			i3GEOF.analisaGeometrias.ativaFoco();
 		};
 		minimiza = function(){
-			i3GEO.janela.minimiza("i3GEOF.agrupaElementos");
+			i3GEO.janela.minimiza("i3GEOF.analisaGeometrias");
 		};
 		//cria a janela flutuante
 		titulo = "An&aacute;lise de geometrias <a class=ajuda_usuario target=_blank href='" + i3GEO.configura.locaplic + "/ajuda_usuario.php?idcategoria=3&idajuda=23' >&nbsp;&nbsp;&nbsp;</a>";
@@ -232,6 +232,7 @@ i3GEOF.analisaGeometrias = {
 			{i3GEO.atualiza(retorno);};
 			i3GEO.janela.abreAguarde("i3GEO.atualiza",$trad("o1"));
 			i3GEO.php.selecaopt(retorna,i3GEO.temaAtivo,objposicaocursor.ddx+" "+objposicaocursor.ddy,$i("i3GEOanalisageometriastipoOperacao").value,5);		
+		}
 	},
 	/*
 	Function: comboTemas
