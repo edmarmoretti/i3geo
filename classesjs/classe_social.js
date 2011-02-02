69ac034f405ac8da24cba98b3d6dda4eafe50349
Edmar Moretti <edmar.moretti@gmail.com>
2011-02-02 11:29:36 +0000

    Inclusão do botão Facebook na função de compartilhamento e complementação da documentação

--- classesjs/classe_social.js
+++ classesjs/classe_social.js
@@ -39,7 +39,7 @@ Opções de compartilhamento e uso de redes sociais.
 i3GEO.social = {
 	curtirFacebook: function(url,tipo){
 		if(tipo == "comtotal")
-		{return "<iframe src='http://www.facebook.com/plugins/like.php?href="+url+"&amp;layout=button_count&amp;show_faces=false&amp;width=200&amp;action=like&amp;colorscheme=light&amp;height=21' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:200px; height:21px;' allowTransparency='true'></iframe>";}
+		{return "<iframe src='http://www.facebook.com/plugins/like.php?href="+url+"&amp;layout=button_count&amp;show_faces=false&amp;width=160&amp;action=like&amp;colorscheme=light&amp;height=21' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:160px; height:21px;' allowTransparency='true'></iframe>";}
 		if(tipo == "semtotal")
 		{return "<iframe src='http://www.facebook.com/plugins/like.php?href="+url+"&amp;layout=button_count&amp;show_faces=false&amp;width=55&amp;action=like&amp;colorscheme=light&amp;height=21' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:55px; height:21px;' allowTransparency='true'></iframe>";}
 	},
@@ -49,7 +49,33 @@ i3GEO.social = {
 		if(tipo == "semtotal")
 		{return '<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.html?count=none&via=i3geo&url='+url+'" style="width:55px; height:21px;"></iframe>';}
 	},
-	compartilhar: function(id,urlcf,urlpt,tipo){
+	/*
+	Function: compartilhar
+	
+	Cria os botões de publicar no Twitter e curtir/compartilhar no Facebook
+	
+	Gera o HTML contendo as tags IMG com os links
+	
+	Parametro:
+	
+	id {string} - id do elemento HTML que receberá os links
+	
+	urlcf {string} - url para o Facebook
+	
+	urlpt {string} - url para o Twitter
+	
+	tipo {string} - tipo de botão comtotal|semtotal
+	
+	
+	locaplic {string} - url onde está o i3Geo. Se não for especificada, será obtida de i3GEO.configura.locaplic
+	
+	Return:
+	
+	HTML com os botões
+	*/
+	compartilhar: function(id,urlcf,urlpt,tipo,locaplic){
+		if(!locaplic)
+		{locaplic = i3GEO.configura.locaplic;}		
 		if(!tipo)
 		{tipo = "comtotal";}
 		var onde = $i(id),
@@ -63,6 +89,7 @@ i3GEO.social = {
 				tabela += "<td>"+i3GEO.social.publicarTwitter(urlpt,tipo)+"</td>";
 			}
 			if(urlcf !== ""){
+				tabela += "<td><img style='cursor:pointer' src='"+locaplic+"/imagens/facebook.gif' onclick='javascript:window.open(\"http://www.facebook.com/sharer.php?u="+urlcf+"\")' title='Compartilhar'/></td>";
 				tabela += "<td>"+i3GEO.social.curtirFacebook(urlcf,tipo)+"</td>";
 			}
 			tabela += "</tr></table>";
@@ -84,15 +111,19 @@ i3GEO.social = {
 	
 	link {string} - link que será marcado
 	
+	locaplic {string} - url onde está o i3Geo. Se não for especificada, será obtida de i3GEO.configura.locaplic
+	
 	Return:
 	
 	HTML com os botões
 	*/
-	bookmark: function(link){
-		ins = "<img src='"+i3GEO.configura.locaplic+"/imagens/delicious.gif' onclick='javascript:window.open(\"http://del.icio.us/post?url="+link+"\")' title='Delicious'/> ";
-		ins += "<img src='"+i3GEO.configura.locaplic+"/imagens/digg.gif' onclick='javascript:window.open(\"http://digg.com/submit/post?url="+link+"\")' title='Digg'/> ";
-		ins += "<img src='"+i3GEO.configura.locaplic+"/imagens/facebook.gif' onclick='javascript:window.open(\"http://www.facebook.com/sharer.php?u="+link+"\")' title='Facebook'/> ";
-		ins += "<img src='"+i3GEO.configura.locaplic+"/imagens/stumbleupon.gif' onclick='javascript:window.open(\"http://www.stumbleupon.com/submit?url="+link+"\")' title='StumbleUpon'/>";
+	bookmark: function(link,locaplic){
+		if(!locaplic)
+		{locaplic = i3GEO.configura.locaplic;}
+		var ins = "<img style='cursor:pointer' src='"+locaplic+"/imagens/delicious.gif' onclick='javascript:window.open(\"http://del.icio.us/post?url="+link+"\")' title='Delicious'/> ";
+		ins += "<img style='cursor:pointer' src='"+locaplic+"/imagens/digg.gif' onclick='javascript:window.open(\"http://digg.com/submit/post?url="+link+"\")' title='Digg'/> ";
+		ins += "<img style='cursor:pointer' src='"+locaplic+"/imagens/facebook.gif' onclick='javascript:window.open(\"http://www.facebook.com/sharer.php?u="+link+"\")' title='Facebook'/> ";
+		ins += "<img style='cursor:pointer' src='"+locaplic+"/imagens/stumbleupon.gif' onclick='javascript:window.open(\"http://www.stumbleupon.com/submit?url="+link+"\")' title='StumbleUpon'/>";
 		return ins;
 	}
 };
