a68a5259f99b5c605018564663ed9ce65b8cf3cd
Edmar Moretti <edmar.moretti@gmail.com>
2011-02-28 00:00:37 +0000

    Fechamento da versão beta 2

--- versao.php
+++ versao.php
@@ -2,5 +2,5 @@
 /*
 Utilizado como um include em ms_configura.php
 */
-$mensagemInicia = 'Vers&atilde;o 4.5 Beta 2 - Revis&atilde;o SVN ';
+$mensagemInicia = 'Vers&atilde;o 4.5 Beta 2 - Revis&atilde;o SVN 1930';
 ?>
