3c72bda503871f198ac0cdacdeec5ecb05549946
Edmar Moretti <edmar.moretti@gmail.com>
2011-04-13 18:31:14 +0000

    Correção nos arquivos de exemplo

--- versao.php
+++ versao.php
@@ -2,5 +2,5 @@
 /*
 Utilizado como um include em ms_configura.php
 */
-$mensagemInicia = 'Vers&atilde;o 4.5 RC 1 - Revis&atilde;o SVN 1994';
+$mensagemInicia = 'Vers&atilde;o 4.5 - Revis&atilde;o SVN 1994';
 ?>
