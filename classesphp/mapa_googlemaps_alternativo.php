45f89db276132b4738ac98ac57701cc3d65dd58e
Edmar Moretti <edmar.moretti@gmail.com>
2013-03-12 17:53:08 +0000

    Atualização do leiame e inclusão de arquivos de renderização alternativos

---
+++ classesphp/mapa_googlemaps_alternativo.php
@@ -0,0 +1,444 @@
+<?php
+/*
+Title: mapa_googlemaps.php
+
+Faz o processamento de um mapfile segundo as necessidades do i3geo, como por exemplo, fazendo a substitui&ccedil;&atilde;o
+das vari&aacute;veis de conex&atilde;o com banco e outras opera&ccedil;&otilde;es espec&iacute;ficas do i3Geo.
+
+&Eacute; utilizado especificamente nas interfaces que utilizam a biblioteca Googlemaps.
+
+Precisa do codigo da "section" PHP aberta pelo i3Geo ou o codigo para acesso especial indicado no par&acirc;metro telaR
+(veja a ferramenta TELAREMOTA).
+
+Parametros:
+
+g_sid {string} - codigo da "section" PHP
+
+telaR {string} - (opcional) utilizado para autorizar o uso do mapfile aberto (deve estar registrado em $fingerprint (vari&aacute;vel de se&ccedil;&atilde;o)
+
+tipolayer {fundo|} - (opcional) indica que a imagem a ser produzida comp&otilde;e o fundo do mapa
+
+BBOX {xmin xmax ymin ymax} - extens&atilde;o geogr&aacute;fica a ser utilizada no desenho do mapa
+
+WIDTH {numeric} - largura do mapa
+
+HEIGHT {numeric} - altura do mapa
+
+layer {string} - codigo do layer existente no mapa que ser&aacute; desenhado (ignorado quando telaR for definido)
+
+DESLIGACACHE {sim|nao} - for&ccedil;a a n&atilde;o usar o cache de imagens qd definido como "sim", do contr&aacute;rio, o uso ou n&atilde;o do cache ser&aacute; definido automaticamente
+
+TIPOIMAGEM {cinza|sepiaclara|sepianormal|negativo|detectaBordas|embassa|gaussian_blur|selective_blur|mean_removal|pixelate
+} - filtro de imagem que ser&aacute; aplicado na imagem
+
+
+Licenca:
+
+GPL2
+
+i3Geo Interface Integrada de Ferramentas de Geoprocessamento para Internet
+
+Direitos Autorais Reservados (c) 2006 Minist&eacute;rio do Meio Ambiente Brasil
+Desenvolvedor: Edmar Moretti edmar.moretti@gmail.com
+
+Este programa &eacute; software livre; voc&ecirc; pode redistribu&iacute;-lo
+e/ou modific&aacute;-lo sob os termos da Licen&ccedil;a P&uacute;blica Geral
+GNU conforme publicada pela Free Software Foundation;
+
+Este programa &eacute; distribu&iacute;do na expectativa de que seja &uacute;til,
+por&eacute;m, SEM NENHUMA GARANTIA; nem mesmo a garantia impl&iacute;cita
+de COMERCIABILIDADE OU ADEQUA&Ccedil;&Atilde;O A UMA FINALIDADE ESPEC&Iacute;FICA.
+Consulte a Licen&ccedil;a P&uacute;blica Geral do GNU para mais detalhes.
+Voc&ecirc; deve ter recebido uma copia da Licen&ccedil;a P&uacute;blica Geral do
+GNU junto com este programa; se n&atilde;o, escreva para a
+Free Software Foundation, Inc., no endere&ccedil;o
+59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+
+Arquivo:
+
+i3geo/classesphp/mapa_googlemaps.php
+
+*/
+//error_reporting(E_ALL);
+error_reporting(0);
+clearstatcache();
+//verifica&ccedil;&atilde;o de seguran&ccedil;a
+$_SESSION = array();
+session_name("i3GeoPHP");
+if(@$_GET["g_sid"]){
+	session_id($_GET["g_sid"]);
+}
+else{
+	ilegal();
+}
+session_start();
+if(@$_SESSION["fingerprint"]){
+	$f = explode(",",$_SESSION["fingerprint"]);
+	if (md5('I3GEOSEC' . $_SERVER['HTTP_USER_AGENT'] . session_id()) != $f[0] && !in_array($_GET["telaR"],$f) ){
+		ilegal();
+	}
+}
+else{
+	exit;
+}
+if(!isset($_SESSION["map_file"])){
+	exit;
+}
+//
+$map_fileX = $_SESSION["map_file"];
+$postgis_mapa = $_SESSION["postgis_mapa"];
+$cachedir = $_SESSION["cachedir"];
+//
+//converte a requisi&ccedil;&atilde;o do tile em coordenadas geo
+//http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#tile_numbers_to_lon.2Flat_2
+//
+$x = $_GET["X"];
+$y = $_GET["Y"];
+$z = $_GET["Z"];
+
+$qyfile = dirname($map_fileX)."/".$_GET["layer"].".php";
+$qy = file_exists($qyfile);
+
+if($qy == false && $_GET["cache"] == "sim" && $_GET["DESLIGACACHE"] != "sim"){
+	carregaCacheImagem($_SESSION["cachedir"],$_SESSION["map_file"],$_GET["tms"]);
+}
+
+$n = pow(2,$z);
+$lon1 = $x / $n * 360.0 - 180.0;
+$lat2 = rad2deg(atan(sinh(pi() * (1 - 2 * $y / $n))));
+$x++;
+$y++;
+$lon2 = $x / $n * 360.0 - 180.0;
+$lat1 = rad2deg(atan(sinh(pi() * (1 - 2 * $y / $n))));
+$x--;
+$y--;
+
+$projInObj = ms_newprojectionobj("proj=latlong,a=6378137,b=6378137");
+$projOutObj = ms_newprojectionobj("proj=merc,a=6378137,b=6378137,lat_ts=0.0,lon_0=0.0,x_0=0.0,y_0=0,k=1.0,units=m");
+
+$poPoint1 = ms_newpointobj();
+$poPoint1->setXY($lon1, $lat1);
+$poPoint1->project($projInObj, $projOutObj);
+$poPoint2 = ms_newpointobj();
+$poPoint2->setXY($lon2, $lat2);
+$poPoint2->project($projInObj, $projOutObj);
+$_GET["BBOX"] = $poPoint1->x." ".$poPoint1->y." ".$poPoint2->x." ".$poPoint2->y;
+$_GET["mapext"] = str_replace(","," ",$_GET["BBOX"]);
+
+$mapa = ms_newMapObj($map_fileX);
+$ret = $mapa->extent;
+
+$cache = false;
+if(!isset($_GET["telaR"])){
+	//no caso de projecoes remotas, o mapfile nao e alterado
+	$numlayers = $mapa->numlayers;
+	for ($i=0;$i < $numlayers;++$i){
+		$l = $mapa->getlayer($i);
+		$layerName = $l->name;
+		$l->set("status",MS_OFF);
+		if($layerName == $_GET["layer"] || $l->group == $_GET["layer"] && $l->group != ""){
+			$l->set("template","none.htm");
+			$l->set("status",MS_DEFAULT);
+			if ($l->getmetadata("classesnome") != ""){
+				if(!function_exists("autoClasses")){
+					include_once("funcoes_gerais.php");
+				}
+				autoClasses($l,$mapa);
+			}
+			if(!empty($postgis_mapa)){
+				if($l->connectiontype == MS_POSTGIS){
+					$lcon = $l->connection;
+					if (($lcon == " ") || ($lcon == "") || (in_array($lcon,array_keys($postgis_mapa)))){
+						if(($lcon == " ") || ($lcon == "")){
+							$l->set("connection",$postgis_mapa);
+						}
+						else{
+							$l->set("connection",$postgis_mapa[$lcon]);
+						}
+					}
+				}
+			}
+			if($l->getProjection() == "" ){
+				$l->setProjection("proj=latlong,a=6378137,b=6378137");
+			}
+		}
+		if($layerName == $_GET["layer"]){
+			if(strtolower($l->getmetadata("cache")) == "sim"){
+				$cache = true;
+			}
+		}
+	}
+}
+else{
+	$mapa->setProjection("proj=merc,a=6378137,b=6378137,lat_ts=0.0,lon_0=0.0,x_0=0.0,y_0=0,k=1.0,units=m");
+	$numlayers = $mapa->numlayers;
+	for ($i=0;$i < $numlayers;++$i){
+		$l = $mapa->getlayer($i);
+		if($l->getProjection() == "" )
+		{$l->setProjection("proj=latlong,a=6378137,b=6378137");}
+	}
+}
+if($_GET["layer"] == "")
+{$cache = true;}
+
+if(($_GET == false) || ($qy) || (strtolower($_GET["DESLIGACACHE"]) == "sim"))
+{$cache = false;}
+elseif(trim($_GET["TIPOIMAGEM"]) != "" && trim($_GET["TIPOIMAGEM"]) != "nenhum")
+{$cache = false;}
+
+if($cache == true){
+	carregaCacheImagem();
+}
+$mapa->setsize(256,256);
+$mapext = explode(" ",$_GET["mapext"]);
+$mapa->setExtent($mapext[0],$mapext[1],$mapext[2],$mapext[3]);
+
+$o = $mapa->outputformat;
+$o->set("imagemode",MS_IMAGEMODE_RGBA);
+
+if(!isset($_GET["telaR"])){
+	$legenda = $mapa->legend;
+	$legenda->set("status",MS_OFF);
+	$escala = $mapa->scalebar;
+	$escala->set("status",MS_OFF);
+}
+//
+//se o layer nao for do tipo fundo
+//
+if($_GET["tipolayer"] != "fundo")
+{$o->set("transparent",MS_TRUE);}
+if(trim($_GET["TIPOIMAGEM"]) != "" && trim($_GET["TIPOIMAGEM"]) != "nenhum")
+{$o->setOption("QUANTIZE_FORCE","OFF");}
+if($qy != true){
+	$img = $mapa->draw();
+}
+else{
+	$handle = fopen ($qyfile, "r");
+	$conteudo = fread ($handle, filesize ($qyfile));
+	fclose ($handle);
+	$shp = unserialize($conteudo);
+	$l = $mapa->getLayerByname($_GET["layer"]);
+	$indxlayer = $l->index;
+	if ($l->connectiontype !== MS_POSTGIS){
+		foreach ($shp as $indx)
+		{$mapa->querybyindex($indxlayer,-1,$indx,MS_TRUE);}
+		$qm = $mapa->querymap;
+		$qm->set("width",255);
+		$qm->set("height",255);
+		$img = $mapa->drawQuery();
+	}
+	else{
+		$img = $mapa->draw();
+		$c = $mapa->querymap->color;
+		$numclasses = $l->numclasses;
+		if ($numclasses > 0)
+		{
+			$classe0 = $l->getClass(0);
+			$classe0->setexpression("");
+			$classe0->set("name"," ");
+			for ($i=1; $i < $numclasses; ++$i)
+			{
+				$classe = $l->getClass($i);
+				$classe->set("status",MS_DELETE);
+			}
+		}
+		$cor = $classe0->getstyle(0)->color;
+		$cor->setrgb($c->red,$c->green,$c->blue);
+		$cor = $classe0->getstyle(0)->outlinecolor;
+		$cor->setrgb($c->red,$c->green,$c->blue);
+		$v = versaoMS();
+		if($v["principal"] == 6){
+			$l->open();
+			foreach ($shp as $indx){
+				$shape = $l->getShape(new resultObj($indx));
+				$shape->draw($mapa,$l,$img);
+			}
+			$l->close();
+		}
+		else{
+			$l->open();
+			foreach ($shp as $indx){
+				$shape = $l->getfeature($indx,-1);
+				$shape->draw($mapa,$l,$img);
+			}
+			$l->close();
+		}
+	}
+}
+if (!function_exists('imagepng')){
+	$s = PHP_SHLIB_SUFFIX;
+	@dl( 'php_gd.'.$s );
+	if (!function_exists('imagepng'))
+	{@dl( 'php_gd2.'.$s );}
+	if (!function_exists('imagepng'))
+	{$_GET["TIPOIMAGEM"] = "";}
+}
+if(trim($_GET["TIPOIMAGEM"]) != "" && trim($_GET["TIPOIMAGEM"]) != "nenhum"){
+	if($img->imagepath == "")
+	{echo "Erro IMAGEPATH vazio";exit;}
+	$nomer = ($img->imagepath)."filtroimgtemp".nomeRand().".png";
+	$img->saveImage($nomer);
+	filtraImg($nomer,trim($_GET["TIPOIMAGEM"]));
+	$img = imagecreatefrompng($nomer);
+	imagealphablending($img, false);
+	imagesavealpha($img, true);
+	ob_clean();
+	echo header("Content-type: image/png \n\n");
+	imagepng($img);
+}
+else{
+	if($cache == true){
+		$nomer = salvaCacheImagem();
+		header('Content-Length: '.filesize($nomer));
+		header('Content-Type: image/png');
+		header('Cache-Control: max-age=3600, must-revalidate');
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+24*60*60) . ' GMT');
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($nomer)).' GMT', true, 200);
+		fpassthru(fopen($nomer, 'rb'));
+	}
+	else{
+		$nomer = ($img->imagepath)."temp".nomeRand().".png";
+		$img->saveImage($nomer);
+		$img = imagecreatefrompng($nomer);
+		imagealphablending($img, false);
+		imagesavealpha($img, true);
+		ob_clean();
+		echo header("Content-type: image/png \n\n");
+		imagepng($img);
+		imagedestroy($img);
+	}
+	exit;
+}
+//$cachedir e definido no ms_configura.php
+function salvaCacheImagem(){
+	global $img,$cachedir,$x,$y,$z,$map_fileX;
+	$layer = $_GET["layer"];
+	if($layer == "")
+	{$layer = "fundo";}
+	if($cachedir == ""){
+		$cachedir = dirname(dirname($map_fileX))."/cache";
+	}
+	$c = $cachedir."/googlemaps/$layer/$z/$x";
+	if(!file_exists($c."/$y.png")){
+		mkdir($cachedir."/googlemaps/$layer/$z/$x",0777,true);
+		$img->saveImage($c."/$y.png");
+		chmod($c."/$y.png",0777);
+	}
+	return $nome;
+}
+function carregaCacheImagem(){
+	global $img,$cachedir,$x,$y,$z,$map_fileX;
+	$layer = $_GET["layer"];
+	if($layer == "")
+	{$layer = "fundo";}
+	if($cachedir == ""){
+		$cachedir = dirname(dirname($map_fileX))."/cache";
+	}
+	$c = $cachedir."/googlemaps/$layer/$z/$x";
+	if(file_exists($c."/$y.png")){
+		header('Content-Length: '.filesize($c."/$y.png"));
+		header('Content-Type: image/png');
+		fpassthru(fopen($c."/$y.png", 'rb'));
+		exit;
+	}
+}
+/*
+function salvaCacheImagem($cachedir,$bbox,$layer,$map,$w,$h){
+	global $img,$map_size;
+	//layers que s&atilde;o sempre iguais
+	//error_reporting(E_ALL);
+	if($layer == "copyright" || $layer == "")
+	{$bbox = "";}
+	if($layer == "")
+	{$layer = "fundo";}
+	if($cachedir == "")
+	{$cachedir = dirname(dirname($map))."/cache/googlemaps/".$layer;}
+	else
+	{$cachedir = $cachedir."/googlemaps/".$layer;}
+	@mkdir($cachedir,0777);
+	$nome = $cachedir."/".$w.$h.$bbox.".png";
+	if(!file_exists($nome))
+	{$img->saveImage($nome);}
+	return $nome;
+}
+function carregaCacheImagem($cachedir,$bbox,$layer,$map,$w,$h){
+	if($layer == "copyright" || $layer == "")
+	{$bbox = "";}
+	if($layer == "")
+	{$layer = "fundo";}
+	$nome = $w.$h.$bbox.".png";
+	if($cachedir == "")
+	{$nome = dirname(dirname($map))."/cache/googlemaps/".$layer."/".$nome;}
+	else
+	{$nome = $cachedir."/googlemaps/".$layer."/".$nome;}
+	if(file_exists($nome)){
+		header('Content-Length: '.filesize($nome));
+		header('Content-Type: image/png');
+		fpassthru(fopen($nome, 'rb'));
+		exit;
+	}
+}
+*/
+function nomeRand($n=10)
+{
+	$nomes = "";
+	$a = 'azertyuiopqsdfghjklmwxcvbnABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$max = 51;
+	for($i=0; $i < $n; ++$i)
+	{$nomes .= $a{mt_rand(0, $max)};}
+	return $nomes;
+}
+function filtraImg($nomer,$tipoimagem){
+	include_once("classe_imagem.php");
+	$tiposImagem = explode(" ",$tipoimagem);
+	foreach ($tiposImagem as $tipoimagem){
+		$m = new Imagem($nomer);
+		if ($tipoimagem == "cinza")
+		{imagepng($m->cinzaNormal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "sepiaclara")
+		{imagepng($m->sepiaClara(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "sepianormal")
+		{imagepng($m->sepiaNormal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "negativo")
+		{imagepng($m->negativo(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "detectaBordas")
+		{imagepng($m->detectaBordas(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "embassa")
+		{imagepng($m->embassa(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "gaussian_blur")
+		{imagepng($m->gaussian_blur(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "selective_blur")
+		{imagepng($m->selective_blur(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "mean_removal")
+		{imagepng($m->mean_removal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "pixelate")
+		{imagepng($m->pixelate(),str_replace("\\","/",$nomer));}
+	}
+}
+function ilegal(){
+	$img = imagecreatefrompng("../imagens/ilegal.png");
+	imagealphablending($img, false);
+	imagesavealpha($img, true);
+	ob_clean();
+	echo header("Content-type: image/png \n\n");
+	imagepng($img);
+	exit;
+}
+function versaoMS()
+{
+	$v = "5.0.0";
+	$vs = explode(" ",ms_GetVersion());
+	$cvs = count($vs);
+	for ($i=0;$i<$cvs;++$i)
+	{
+		if(trim(strtolower($vs[$i])) == "version")
+		{
+			$v = $vs[$i+1];
+		}
+	}
+	$versao["completa"] = $v;
+	$v = explode(".",$v);
+	$versao["principal"] = $v[0];
+	return $versao;
+}
+?>
---
+++ classesphp/mapa_openlayers_alternativo.php
@@ -0,0 +1,425 @@
+<?php
+/*
+Title: mapa_openlayers.php
+
+Faz o processamento de um mapfile segundo as necessidades do i3geo, como por exemplo, fazendo a substitui&ccedil;&atilde;o
+das vari&aacute;veis de conex&atilde;o com banco e outras opera&ccedil;&otilde;es espec&iacute;ficas do i3Geo.
+
+&Eacute; utilizado especificamente nas interfaces que utilizam a biblioteca OpenLayers em LAYERS do tipo WMS.
+
+Precisa do codigo da "section" PHP aberta pelo i3Geo ou o codigo para acesso especial indicado no par&acirc;metro telaR
+(veja a ferramenta TELAREMOTA).
+
+Parametros:
+
+g_sid {string} - codigo da "section" PHP
+
+telaR {string} - (opcional) utilizado para autorizar o uso do mapfile aberto (deve estar registrado em $fingerprint (vari&aacute;vel de se&ccedil;&atilde;o)
+
+tipolayer {fundo|} - (opcional) indica que a imagem a ser produzida comp&otilde;e o fundo do mapa
+
+BBOX {xmin xmax ymin ymax} - extens&atilde;o geogr&aacute;fica a ser utilizada no desenho do mapa
+
+WIDTH {numeric} - largura do mapa
+
+HEIGHT {numeric} - altura do mapa
+
+layer {string} - codigo do layer existente no mapa que ser&aacute; desenhado (ignorado quando telaR for definido)
+
+DESLIGACACHE {sim|nao} - for&ccedil;a a n&atilde;o usar o cache de imagens qd definido como "sim", do contr&aacute;rio, o uso ou n&atilde;o do cache ser&aacute; definido automaticamente
+
+TIPOIMAGEM {cinza|sepiaclara|sepianormal|negativo|detectaBordas|embassa|gaussian_blur|selective_blur|mean_removal|pixelate
+} - filtro de imagem que ser&aacute; aplicado na imagem
+
+Licenca:
+
+GPL2
+
+i3Geo Interface Integrada de Ferramentas de Geoprocessamento para Internet
+
+Direitos Autorais Reservados (c) 2006 Minist&eacute;rio do Meio Ambiente Brasil
+Desenvolvedor: Edmar Moretti edmar.moretti@gmail.com
+
+Este programa &eacute; software livre; voc&ecirc; pode redistribu&iacute;-lo
+e/ou modific&aacute;-lo sob os termos da Licen&ccedil;a P&uacute;blica Geral
+GNU conforme publicada pela Free Software Foundation;
+
+Este programa &eacute; distribu&iacute;do na expectativa de que seja &uacute;til,
+por&eacute;m, SEM NENHUMA GARANTIA; nem mesmo a garantia impl&iacute;cita
+de COMERCIABILIDADE OU ADEQUA&Ccedil;&Atilde;O A UMA FINALIDADE ESPEC&Iacute;FICA.
+Consulte a Licen&ccedil;a P&uacute;blica Geral do GNU para mais detalhes.
+Voc&ecirc; deve ter recebido uma cópia da Licen&ccedil;a P&uacute;blica Geral do
+GNU junto com este programa; se n&atilde;o, escreva para a
+Free Software Foundation, Inc., no endere&ccedil;o
+59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+
+Arquivo:
+
+i3geo/classesphp/mapa_openlayers.php
+
+*/
+error_reporting(0);
+//carrega dados da se&ccedil;&atilde;o, verifica seguran&ccedil;a
+inicializa();
+//
+//calcula a extensao geografica com base no x,y,z
+//nos casos do modo notile, a requisicao e feita como se fosse um wms
+//quando for do tipo tms $_GET["tms"] contem os parametros do tile
+if(isset($_GET["tms"])){
+	$_GET["WIDTH"] = 256;
+	$_GET["HEIGHT"] = 256;
+	$temp = explode("/",$_GET["tms"]);
+	$z = $temp[2];
+	$x = $temp[3];
+	$y = str_replace(".png","",$temp[4]);
+
+	$n = pow(2,$z+1);
+	$lon1 = $x / $n * 360.0 - 180.0;
+	$lon2 = ($x+1) / $n * 360.0 - 180.0;
+	$n = pow(2,$z);
+	$lat1 = $y / $n * 180.0 - 90.0;
+	$lat2 = ($y+1) / $n * 180.0 - 90.0;
+	$_GET["BBOX"] = $lon1." ".$lat1." ".$lon2." ".$lat2;
+}
+$map_fileX = $_SESSION["map_file"];
+//
+//resolve o problema da sele&ccedil;&atilde;o na vers&atilde;o nova do mapserver
+//
+$qyfile = dirname($map_fileX)."/".$_GET["layer"].".php";
+$qy = file_exists($qyfile);
+
+if($qy == false && $_GET["cache"] == "sim" && $_GET["DESLIGACACHE"] != "sim"){
+	carregaCacheImagem($_SESSION["cachedir"],$_SESSION["map_file"],$_GET["tms"]);
+}
+//
+//map_fileX e para o caso register_globals = On no PHP.INI
+
+if(isset($_GET["tipolayer"]) && $_GET["tipolayer"] == "fundo"){
+	$map_fileX = str_replace(".map","fundo.map",$map_fileX);
+}
+$postgis_mapa = $_SESSION["postgis_mapa"];
+$cachedir = $_SESSION["cachedir"];
+if(isset($_GET["BBOX"])){
+	$_GET["mapext"] = str_replace(","," ",$_GET["BBOX"]);
+	$_GET["map_size"] = $_GET["WIDTH"]." ".$_GET["HEIGHT"];
+}
+$_GET["TIPOIMAGEM"] = trim($_GET["TIPOIMAGEM"]);
+$mapa = ms_newMapObj($map_fileX);
+
+//
+//processa os layers do mapfile
+//
+if(!isset($_GET["telaR"])){//no caso de projecoes remotas, o mapfile nao e alterado
+	$numlayers = $mapa->numlayers;
+	$cache = false;
+	for($i = 0;$i < $numlayers;++$i){
+		$l = $mapa->getLayer($i);
+		$layerName = $l->name;
+		if($layerName != $_GET["layer"]){
+			$l->set("status",MS_OFF);
+		}
+		if($layerName == $_GET["layer"] || $l->group == $_GET["layer"] && $l->group != ""){
+			if ($l->getmetadata("classesnome") != ""){
+				if(!function_exists("autoClasses"))
+				{include_once("funcoes_gerais.php");}
+				autoClasses($l,$mapa);
+			}
+			$l->set("status",MS_DEFAULT);
+			$l->set("template","none.htm");
+			if (!empty($postgis_mapa)){
+				if ($l->connectiontype == MS_POSTGIS){
+					$lcon = $l->connection;
+					if (($lcon == " ") || ($lcon == "") || (in_array($lcon,array_keys($postgis_mapa)))){
+						if(($lcon == " ") || ($lcon == ""))
+						{$l->set("connection",$postgis_mapa);}
+						else
+						{$l->set("connection",$postgis_mapa[$lcon]);}
+					}
+				}
+			}
+		}
+		if($layerName == $_GET["layer"]){
+			if(strtolower($l->getmetadata("cache")) == "sim"){
+				$cache = true;
+				$nomecache = $l->getmetadata("nomeoriginal");
+				if($nomecache == ""){
+					$nomecache = $layerName;
+				}
+			}
+		}
+		if($_GET["REQUEST"] == "GetFeatureInfo" || $_GET["REQUEST"] == "getfeature"){
+			$l->setmetadata("gml_include_items","all");
+			$l->setmetadata("WMS_INCLUDE_ITEMS","all");
+			$l->setmetadata("WFS_INCLUDE_ITEMS","all");
+			$l->setmetadata("ows_enable_request","*");
+			$l->set("dump",MS_TRUE);
+			$l->setmetadata("ows_srs","AUTO");
+		}
+	}
+}
+if (!function_exists('imagepng'))
+{$_GET["TIPOIMAGEM"] = "";}
+
+if($_GET["layer"] == "")
+{$cache = true;}
+
+if(($_GET == false) || ($qy) || (strtolower($_GET["DESLIGACACHE"]) == "sim")){
+	$cache = false;
+}
+elseif($_GET["TIPOIMAGEM"] != "" && $_GET["TIPOIMAGEM"] != "nenhum")
+{$cache = false;}
+
+if($cache == true && $_GET["cache"] != "nao"){
+	//carregaCacheImagem($cachedir,$_GET["BBOX"],$nomecache,$map_fileX,$_GET["WIDTH"],$_GET["HEIGHT"]);
+	carregaCacheImagem($cachedir,$map,$tms);
+}
+$map_size = explode(" ",$_GET["map_size"]);
+$mapa->setsize($map_size[0],$map_size[1]);
+if(isset($_GET["mapext"])){
+	$mapext = explode(" ",$_GET["mapext"]);
+	$mapa->setExtent($mapext[0],$mapext[1],$mapext[2],$mapext[3]);
+}
+//
+//qd a cahamda e para um WMS, redireciona para ogc.php
+//
+if($_GET["REQUEST"] == "GetFeatureInfo" || $_GET["request"] == "getfeature"){
+	$req = ms_newowsrequestobj();
+	$_GET = array_merge($_GET,$_POST);
+	foreach ($_GET as $k=>$v){
+		$req->setParameter($k, $v);
+	}
+	$proto = "http" . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "") . "://";
+	$server = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+	$or = $proto.$server.$_SERVER['PHP_SELF'];
+	$mapa->setmetadata("wfs_onlineresource",$or."?".$_SERVER["QUERY_STRING"]);
+
+	ms_ioinstallstdouttobuffer();
+	$mapa->owsdispatch($req);
+	$contenttype = ms_iostripstdoutbuffercontenttype();
+	header("Content-type: $contenttype");
+	ms_iogetStdoutBufferBytes();
+	ms_ioresethandlers();
+	exit;
+}
+
+$o = $mapa->outputformat;
+$o->set("imagemode",MS_IMAGEMODE_RGBA);
+if(!isset($_GET["telaR"])){
+	$legenda = $mapa->legend;
+	$legenda->set("status",MS_OFF);
+	$escala = $mapa->scalebar;
+	$escala->set("status",MS_OFF);
+}
+//
+//se o layer n&atilde;o for do tipo fundo
+//
+if($_GET["tipolayer"] != "fundo")
+{$o->set("transparent",MS_TRUE);}
+if($qy != true)
+{$img = $mapa->draw();}
+else{
+	$handle = fopen ($qyfile, "r");
+	$conteudo = fread ($handle, filesize ($qyfile));
+	fclose ($handle);
+	$shp = unserialize($conteudo);
+	$l = $mapa->getLayerByname($_GET["layer"]);
+	if ($l->connectiontype != MS_POSTGIS){
+		$indxlayer = $l->index;
+		foreach ($shp as $indx)
+		{$mapa->querybyindex($indxlayer,-1,$indx,MS_TRUE);}
+		$qm = $mapa->querymap;
+		$qm->set("width",$map_size[0]);
+		$qm->set("height",$map_size[1]);
+		$img = $mapa->drawQuery();
+	}
+	else{
+		$img = $mapa->draw();
+		$c = $mapa->querymap->color;
+		$numclasses = $l->numclasses;
+		if ($numclasses > 0)
+		{
+			$classe0 = $l->getClass(0);
+			$classe0->setexpression("");
+			$classe0->set("name"," ");
+			for ($i=1; $i < $numclasses; ++$i)
+			{
+				$classe = $l->getClass($i);
+				$classe->set("status",MS_DELETE);
+			}
+		}
+		$cor = $classe0->getstyle(0)->color;
+		$cor->setrgb($c->red,$c->green,$c->blue);
+		$cor = $classe0->getstyle(0)->outlinecolor;
+		$cor->setrgb($c->red,$c->green,$c->blue);
+		$status = $l->open();
+		$status = $l->whichShapes($mapa->extent);
+		while ($shape = $l->nextShape())
+		{
+		  if(in_array($shape->index,$shp))
+		  $shape->draw($mapa,$l,$img);
+		}
+		$l->close();
+	}
+	$cache = false;
+}
+if($_GET["TIPOIMAGEM"] != "" && $_GET["TIPOIMAGEM"] != "nenhum"){
+	if($img->imagepath == "")
+	{echo "Erro IMAGEPATH vazio";exit;}
+	$nomer = ($img->imagepath)."filtroimgtemp".nomeRand().".png";
+	$img->saveImage($nomer);
+	filtraImg($nomer,$_GET["TIPOIMAGEM"]);
+	$img = imagecreatefrompng($nomer);
+	imagealphablending($img, false);
+	imagesavealpha($img, true);
+	ob_clean();
+	echo header("Content-type: image/png \n\n");
+	imagepng($img);
+	imagedestroy($img);
+}
+else{
+	if($cache == true && $_GET["cache"] != "nao"){
+		//$nomer = salvaCacheImagem($cachedir,$_GET["BBOX"],$nomecache,$map_fileX,$_GET["WIDTH"],$_GET["HEIGHT"]);
+		$nomer = salvaCacheImagem($cachedir,$map_fileX,$_GET["tms"]);
+		header('Content-Length: '.filesize($nomer));
+		header('Content-Type: image/png');
+		header('Cache-Control: max-age=3600, must-revalidate');
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+24*60*60) . ' GMT');
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($nomer)).' GMT', true, 200);
+		fpassthru(fopen($nomer, 'rb'));
+	}
+	else{
+		if($img->imagepath == "")
+		{echo "Erro IMAGEPATH vazio";exit;}
+		$nomer = ($img->imagepath)."temp".nomeRand().".png";
+		$img->saveImage($nomer);
+		$img = imagecreatefrompng($nomer);
+		imagealphablending($img, false);
+		imagesavealpha($img, true);
+		ob_clean();
+		echo header("Content-type: image/png \n\n");
+		imagepng($img);
+		imagedestroy($img);
+	}
+}
+function salvaCacheImagem($cachedir,$map,$tms){
+	global $img;
+	if($cachedir == ""){
+		$nome = dirname(dirname($map))."/cache".$tms;
+	}
+	else{
+		$nome = $cachedir.$tms;
+	}
+	if(!file_exists($nome)){
+		@mkdir(dirname($nome),0777,true);
+		$img->saveImage($nome);
+		chmod($nome,0777);
+	}
+	return $nome;
+}
+function carregaCacheImagem($cachedir,$map,$tms){
+	if($cachedir == ""){
+		$nome = dirname(dirname($map))."/cache".$tms;
+	}
+	else{
+		$nome = $cachedir.$tms;
+	}
+	if(file_exists($nome)){
+		header('Content-Length: '.filesize($nome));
+		header('Content-Type: image/png');
+		header('Cache-Control: max-age=3600, must-revalidate');
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+24*60*60) . ' GMT');
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($nome)).' GMT', true, 200);
+		$etag = md5_file($nome);
+		header('Etag: '.$etag);
+		fpassthru(fopen($nome, 'rb'));
+		exit;
+	}
+}
+function XcarregaCacheImagem($cachedir,$bbox,$layer,$map,$w,$h){
+	if($layer == "copyright" || $layer == "")
+	{$bbox = "";}
+	if($layer == "")
+	{$layer = "fundo";}
+	$nome = $w.$h.$bbox.".png";
+	if($cachedir == "")
+	{$nome = dirname(dirname($map))."/cache/".$layer."/".$nome;}
+	else
+	{$nome = $cachedir."/".$layer."/".$nome;}
+	if(file_exists($nome)){
+		header('Content-Length: '.filesize($nome));
+		header('Content-Type: image/png');
+		header('Cache-Control: max-age=3600, must-revalidate');
+		header('Expires: ' . gmdate('D, d M Y H:i:s', time()+24*60*60) . ' GMT');
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($nome)).' GMT', true, 200);
+		$etag = md5_file($nome);
+		header('Etag: '.$etag);
+		fpassthru(fopen($nome, 'rb'));
+		exit;
+	}
+}
+function nomeRand($n=10)
+{
+	$nomes = "";
+	$a = 'azertyuiopqsdfghjklmwxcvbnABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$max = 51;
+	for($i=0; $i < $n; ++$i)
+	{$nomes .= $a{mt_rand(0, $max)};}
+	return $nomes;
+}
+function filtraImg($nomer,$tipoimagem){
+	include_once("classe_imagem.php");
+	$tiposImagem = explode(" ",$tipoimagem);
+	foreach ($tiposImagem as $tipoimagem){
+		$m = new Imagem($nomer);
+		if ($tipoimagem == "cinza")
+		{imagepng($m->cinzaNormal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "sepiaclara")
+		{imagepng($m->sepiaClara(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "sepianormal")
+		{imagepng($m->sepiaNormal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "negativo")
+		{imagepng($m->negativo(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "detectaBordas")
+		{imagepng($m->detectaBordas(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "embassa")
+		{imagepng($m->embassa(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "gaussian_blur")
+		{imagepng($m->gaussian_blur(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "selective_blur")
+		{imagepng($m->selective_blur(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "mean_removal")
+		{imagepng($m->mean_removal(),str_replace("\\","/",$nomer));}
+		if ($tipoimagem == "pixelate")
+		{imagepng($m->pixelate(),str_replace("\\","/",$nomer));}
+	}
+}
+function inicializa(){
+	clearstatcache();
+	session_name("i3GeoPHP");
+	if(@$_GET["g_sid"]){
+		session_id($_GET["g_sid"]);
+	}
+	else{
+		ilegal();
+	}
+	session_start();
+	if(@$_SESSION["fingerprint"]){
+		$f = explode(",",$_SESSION["fingerprint"]);
+		if (md5('I3GEOSEC' . $_SERVER['HTTP_USER_AGENT'] . session_id()) != $f[0] && !in_array($_GET["telaR"],$f) )
+		{ilegal();}
+	}
+	else
+	{exit;}
+	if(!isset($_SESSION["map_file"]))
+	{exit;}
+}
+function ilegal(){
+	$img = imagecreatefrompng("../imagens/ilegal.png");
+	imagealphablending($img, false);
+	imagesavealpha($img, true);
+	ob_clean();
+	echo header("Content-type: image/png \n\n");
+	imagepng($img);
+	exit;
+}
+?>
