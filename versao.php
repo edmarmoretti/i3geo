ad1945d78ddcb335640c8e9b3b3893f3c7a15090
Edmar Moretti <edmar.moretti@gmail.com>
2014-04-01 03:37:56 +0000

    fechamento da versao 5.0

--- versao.php
+++ versao.php
@@ -6,7 +6,7 @@ Utilizado como um include em ms_configura.php
  * TODO v5 numero da versao esta correto?
  * TODO v5 guia de instalacao esta correto?
  * TODO v5 guia de migracao esta correto?
- * TODO v5 gerar repositorio no SVN com o nome i3geo47
+ * TODO v5 gerar repositorio no SVN com o nome i3geo5
  */
 $mensagemInicia = 'Vers&atilde;o 5.0';
 ?>
