ac4a994cae972e43adf7c86eb37167ce1230a5a4
Murilo Caixeta <caixetaunb@gmail.com>
2014-09-16 14:24:29 +0000

    Substituição dos IDs para os textos das ferramenta por palavras-chave

--- ferramentas/confluence/dicionario.js
+++ ferramentas/confluence/dicionario.js
@@ -1,18 +1,18 @@
 //+$trad(1,i3GEOF.confluence.dicionario)+
 i3GEOF.confluence.dicionario = {
-1: [{
-pt:"Navegue pelo mapa para ver o resultado! Clique no link para abrir a p&aacute;gina sobre o ponto.",
-en:"",
-es:""
-}],
-2: [{
-pt:"Nenhuma coordenada encontrada.",
-en:"",
-es:""
-}],
-3: [{
-pt:"Talvez vc precise afastar mais o zoom ou deslocar o mapa.",
-en:"",
-es:""
-}]
+	'ajuda' : [{
+		pt : "Navegue pelo mapa para ver o resultado! Clique no link para abrir a p&aacute;gina sobre o ponto.",
+		en : "",
+		es : ""
+	} ],
+	'msgCoordenada' : [ {
+		pt : "Nenhuma coordenada encontrada.",
+		en : "",
+		es : ""
+	} ],
+	'msgZoom' : [ {
+		pt : "Talvez vc precise afastar mais o zoom ou deslocar o mapa.",
+		en : "",
+		es : ""
+	} ]
 };
