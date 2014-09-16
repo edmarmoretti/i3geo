ce45ec48e3fd30b031b7be795c479ad3fc93d19f
Murilo Caixeta <caixetaunb@gmail.com>
2014-09-16 14:23:17 +0000

    Substituição dos IDs para os textos das ferramenta por palavras-chave

--- ferramentas/convertemapakml/dicionario.js
+++ ferramentas/convertemapakml/dicionario.js
@@ -1,28 +1,28 @@
 //+$trad(1,i3GEOF.convertemapaKml.dicionario)+
 i3GEOF.converteMapaKml.dicionario = {
-1: [{
-pt:"Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar o mapa atual em softwares que aceitam o formato kml,",
-en:"",
-es:""
-}],
-2: [{
-pt:"O endere&ccedil;o de acesso &eacute; tempor&aacute;rio, ficando dispon&iacute;vel por determinado per&iacute;odo de tempo conforme definido pelo administrador do i3Geo.",
-en:"",
-es:""
-}],
-3: [{
-pt:"clique para mais detalhes sobre como usar o link kml no Google Earth.",
-en:"",
-es:""
-}],
-4: [{
-pt:"Kml baseado em um servico WMS:",
-en:"",
-es:""
-}],
-5: [{
-pt:"Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no GoogleEarth (incluindo dados vetoriais)",
-en:"",
-es:""
-}]
+	'ajuda' : [ {
+		pt : "Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar o mapa atual em softwares que aceitam o formato kml,",
+		en : "",
+		es : ""
+	} ],
+	'ajuda2' : [ {
+		pt : "O endere&ccedil;o de acesso &eacute; tempor&aacute;rio, ficando dispon&iacute;vel por determinado per&iacute;odo de tempo conforme definido pelo administrador do i3Geo.",
+		en : "",
+		es : ""
+	} ],
+	'ajuda3' : [ {
+		pt : "para mais detalhes sobre como usar o link kml no Google Earth.",
+		en : "",
+		es : ""
+	} ],
+	'wms' : [ {
+		pt : "Kml baseado em um servico WMS:",
+		en : "",
+		es : ""
+	} ],
+	'ajuda4' : [ {
+		pt : "Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no GoogleEarth (incluindo dados vetoriais)",
+		en : "",
+		es : ""
+	} ]
 };
