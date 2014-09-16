67d0a7f7ebdaa35bf76bb672ec890af62590f864
Edmar Moretti <edmar.moretti@gmail.com>
2014-09-16 00:01:38 +0000

    Ajuste nos estilos

--- ferramentas/graficointerativo1/index.js
+++ ferramentas/graficointerativo1/index.js
@@ -761,13 +761,13 @@ i3GEOF.graficointerativo1 =
 						var temp = retorno.dados.replace(idjanela + "i3GEOgraficointerativo1ComboXid",idjanela + "i3GEOgraficointerativo1ComboYid");
 
 						$i(idjanela + "i3GEOgraficointerativo1ComboY").innerHTML =
-							"<div class=styled-select style='width:145px;float:left;'>" + temp + "&nbsp;</div><div class=styled-select style='width:145px;float: right;' ><input title='" + $trad(33, i3GEOF.graficointerativo1.dicionario)
+							"<div>" + temp + "&nbsp;<div class=styled-select style='width:145px;float: right;' ><input title='" + $trad(33, i3GEOF.graficointerativo1.dicionario)
 								+ "'  type=text id='" + idjanela
 								+ "i3GEOgraficointerativo1ComboYidTitulo' value='' /></div>" + "<div class='styled-select' style='width:80px;float: right;' ><input id='" + idjanela
 								+ "i3GEOgraficointerativo1ComboYidcor' title='cor' type='text' value='' /></div>"
 								+ "<img alt='aquarela.gif' style=position:relative;top:3px;left:3px;cursor:pointer src='"
 								+ i3GEO.configura.locaplic + "/imagens/aquarela.gif' onclick='i3GEOF.graficointerativo1.corj(\"" + idjanela
-								+ "i3GEOgraficointerativo1ComboYidcor\")' /><br>";
+								+ "i3GEOgraficointerativo1ComboYidcor\")' /></div><br>";
 
 						//$i(idjanela + "i3GEOgraficointerativo1ComboXid").id = idjanela + "i3GEOgraficointerativo1ComboYid";
 						//para escolher o item de X
@@ -784,7 +784,7 @@ i3GEOF.graficointerativo1 =
 									var no = document.createElement("div"), id = "CorG" + parseInt(Math.random() * 100000, 10), novoselect;
 									no.innerHTML = ""
 										+ retorno.dados 
-										+ "</div><div class=styled-select style='width:145px;float: right;' ><input title='" + $trad(33, i3GEOF.graficointerativo1.dicionario)
+										+ "<div class=styled-select style='width:145px;float: right;' ><input title='" + $trad(33, i3GEOF.graficointerativo1.dicionario)
 										+ "' type=text value='' /></div>" + "&nbsp;<div class=styled-select style='width:80px;float: right;' ><input id='" + id
 										+ "' type=text value='' title='cor' /></div>"
 										+ "<img alt='aquarela.gif' style=position:relative;top:3px;left:3px;cursor:pointer src='"
