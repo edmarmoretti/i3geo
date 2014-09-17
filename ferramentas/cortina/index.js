7ec15a3221587dfc44784b2e85bbf8c76d3e45ad
Edmar Moretti <edmar.moretti@gmail.com>
2014-09-17 14:53:10 +0000

    Ajustes nos estilos

--- ferramentas/cortina/index.js
+++ ferramentas/cortina/index.js
@@ -120,10 +120,10 @@ i3GEOF.cortina = {
 	String com o c&oacute;digo html
 	*/
 	html:function(){
-		var ins = "<div id='i3GEOcortinaTemasDiv' style='text-align:left;font-size:11px'></div>";
+		var ins = "<div style=margin-left:5px ><div id='i3GEOcortinaTemasDiv' class=styled-select style=width:200px; ></div>";
 		ins += '<div id="slider-bg" class="yui-h-slider" style="background: url('+i3GEO.configura.locaplic+'/pacotes/yui290/build/slider/assets/bg-h.gif) no-repeat 5px 0;height: 28px;width: 210px;" tabindex="-1" title="Slider">' +
 			'<div id="slider-thumb" class="yui-slider-thumb"><img src="'+i3GEO.configura.locaplic+'/pacotes/yui290/build/slider/assets/thumb-n.gif"></div>' +
-			'</div>';
+			'</div></div>';
 
 		return ins;
 	},
