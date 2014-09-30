6b1b40fd76af6826086a211e0553dca54582e561
Edmar Moretti <edmar.moretti@gmail.com>
2014-09-30 21:58:44 +0000

    -

--- ms_configura.php
+++ ms_configura.php
@@ -418,7 +418,7 @@ Tipo:
 $postgis_mapa = array(
 		"teste"=>"user=postgres password=postgres dbname=postgis host=localhost port=5432 options='-c client_encoding=LATIN1'",
 		"postgres"=>"user=postgres password=postgres dbname=postgres host=localhost port=5432",
-		"i3geosaude"=>"user=postgres password=postgres dbname=i3geosaude host=localhost port=5432"
+		"i3geosaude"=>"user=postgres password=postgres dbname=i3geosaude host=localhost port=5432 options='-c client_encoding=LATIN1'"
 );
 /*
 Variable: utilizacgi
