240708e08e65c23c792e8fb6f0180d3dc8b59a1c
Murilo Caixeta <caixetaunb@gmail.com>
2014-10-30 11:06:56 +0000

    Implementação do Mustache

--- ferramentas/animacao/index.js
+++ ferramentas/animacao/index.js
@@ -66,25 +66,6 @@ i3GEOF.animacao = {
 		return dicionario;
 	},
 	/*
-	Function: iniciaDicionario
-
-	Carrega o dicion&aacute;rio e chama a fun&ccedil;&atilde;o que inicia a ferramenta
-
-	O Javascript &eacute; carregado com o id i3GEOF.nomedaferramenta.dicionario_script
-	*/
-	iniciaDicionario: function(){
-		if(typeof(i3GEOF.animacao.dicionario) === 'undefined'){
-			i3GEO.util.scriptTag(
-				i3GEO.configura.locaplic+"/ferramentas/animacao/dicionario.js",
-				"i3GEOF.animacao.iniciaJanelaFlutuante()",
-				"i3GEOF.animacao.dicionario_script"
-			);
-		}
-		else{
-			i3GEOF.animacao.iniciaJanelaFlutuante();
-		}
-	},
-	/*
 	Function: inicia
 
 	Inicia a ferramenta. &Eacute; chamado por criaJanelaFlutuante
