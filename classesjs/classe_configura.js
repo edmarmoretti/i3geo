a30c12e7aa9c2159880ca31a95ebf361e3f99bb5
Edmar Moretti <edmar.moretti@gmail.com>
2014-11-20 15:43:08 +0000

    correção dos links do menu suspenso

--- classesjs/classe_configura.js
+++ classesjs/classe_configura.js
@@ -90,65 +90,65 @@ i3GEO.configura =
 		},
 		/*
 		 * Propriedade: guardaExtensao
-		 * 
+		 *
 		 * Indica se a extensao geografica do mapa sera armazenada como um cookie
-		 * 
+		 *
 		 * Se for true a extensao geografica e armazenada sempre que o evento de navegacoa no mapa for disparado
-		 * 
+		 *
 		 * Tipo: {boolean}
-		 * 
+		 *
 		 * Default: {true}
 		 */
 		guardaExtensao : true,
 		/*
 		 * Propriedade: grupoLayers
-		 * 
+		 *
 		 * Lista de grupos e seus respectivos layers, para montagem da &aacute;rvore de camadas.
-		 * 
+		 *
 		 * Se essa propriedade estiver definida, as camadas ser&atilde;o agrupadas na &aacute;rvore de camadas conforme os grupos definidos.
-		 * 
+		 *
 		 * Layers que n&atilde;o constarem nessa propriedade ser&atilde;o inclu&iacute;dos no grupo "outros"
-		 * 
+		 *
 		 * Ao definir grupos, a &aacute;rvore n&atilde;o conter&aacute; as op&ccedil;&otilde;es de mudan&ccedil;a da prdem de desenho das
 		 * camadas ( veja http://localhost/i3geo/exemplos/legenda2.htm )
-		 * 
+		 *
 		 * Por exemplo i3GEO.configura.grupoLayers = [ {nome:"Grupo 1",icone:true,dinamico:true,expandido:true,layers:["zee","estadosl"]},
 		 * {nome:"Grupo 2",icone:false,dinamico:true,expandido:false,layers:["mundo"]} ];
-		 * 
+		 *
 		 * Onde "icone" indica se o &iacute;cone de ligar/desligar todos os temas do grupo ser&aacute; mostrado, "dinamico" significa que o
 		 * n&oacute; pode ser expandido ou n&atilde;o, e "expandido" significa que o n&oacute; inicia aberto se a &aacute;rvore for
 		 * din&acirc;mica
-		 * 
+		 *
 		 * Type: {JSON}
-		 * 
+		 *
 		 * Default: ""
 		 */
 		grupoLayers : "",
 		/*
 		 * Propriedade: oMenuData
-		 * 
+		 *
 		 * Itens inclu&iacute;dos no menu suspenso. Define os par&acirc;metros para o gadget menu suspenso
-		 * 
+		 *
 		 * Mais informa&ccedil;&otilde;es em <classe_gadgets.js> fun&ccedil;&atilde;o <mostraMenuSuspenso>
-		 * 
+		 *
 		 * Exemplo:
-		 * 
+		 *
 		 * oMenuData:{
-		 * 
+		 *
 		 * menu:[
-		 * 
+		 *
 		 * {nome:$trad("s1"),id:"ajudas"}
 		 *  ],
-		 * 
+		 *
 		 * submenus:{
-		 * 
+		 *
 		 * "ajudas": [
 		 *  { text: $trad("u1"), url: "http://www.softwarepublico.gov.br/spb/ver-comunidade?community_id=1444332" },
 		 *  { text: $trad("u2"), url: "javascript:i3GEO.ajuda.abreDoc()", target: "_blank" }
 		 *  ]
 		 *  }
 		 *  }
-		 * 
+		 *
 		 * Tipo: {object}
 		 */
 		oMenuData : {
@@ -441,22 +441,17 @@ i3GEO.configura =
 									{
 										id : "omenudataInterface2",
 										text : "OpenLayers",
-										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/openlayers.htm?'+i3GEO.configura.sid"
+										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/black_ol.htm?'+i3GEO.configura.sid"
 									},
 									{
 										id : "omenudataInterface2a",
 										text : "OpenLayers OSM",
-										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/osm.htm?'+i3GEO.configura.sid"
-									},
-									{
-										id : "omenudataInterface10",
-										text : "OpenLayers tablet",
-										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/openlayers_t.htm?'+i3GEO.configura.sid"
+										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/black_osm.htm?'+i3GEO.configura.sid"
 									},
 									{
 										id : "omenudataInterface4",
 										text : "Google Maps",
-										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/googlemaps.phtml?'+i3GEO.configura.sid"
+										url : "javascript:window.location = i3GEO.configura.locaplic+'/interface/black_gm.phtml?'+i3GEO.configura.sid"
 									},
 									{
 										id : "omenudataInterface5",
@@ -695,247 +690,247 @@ i3GEO.configura =
 		},
 		/*
 		 * Propriedade: tipoimagem
-		 * 
+		 *
 		 * Indica o tipo de filtro de imagem que est&aacute; ativo. O filtro ativo &eacute; aplicado sobre a imagem toda a vez que o mapa
 		 * &eacute; refeito.
-		 * 
+		 *
 		 * Veja <classe_imagem.php> para obter os tipos poss&iacute;veis
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {"nenhum"}
 		 */
 		tipoimagem : "nenhum",
 		/*
 		 * Propriedade: ajustaDocType
-		 * 
+		 *
 		 * Ajusta ou n&atilde;o a declara&ccedil;&atilde;o DOCTYPE do documento HTML. O ajuste &eacute; necess&aacute;rio para que algumas
 		 * op&ccedil;&otilde;es funcionem adequadamente. Caso vc deseje usar um DOCTYPE espec&iacute;fico, utilize false. O ajuste do
 		 * DOCTYPE n&atilde;o funciona no navegador IE.
-		 * 
+		 *
 		 * Tipo: {boolean}
-		 * 
+		 *
 		 * Default: {true}
 		 */
 		ajustaDocType : true,
 		/*
 		 * Propriedade: tipotip
-		 * 
+		 *
 		 * Tipo de tip que &eacute; mostrado na fun&ccedil;&atilde;o de identifica&ccedil;&atilde;o quando o usu&aacute;rio estaciona o
 		 * mouse sobre o mapa
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Valores:
-		 * 
+		 *
 		 * completo|simples|balao
-		 * 
+		 *
 		 * Default: {"balao"}
 		 */
 		tipotip : "balao",
 		/*
 		 * Propriedade: alturatip
-		 * 
+		 *
 		 * Altura em pixel do tip que &eacute; mostrado na fun&ccedil;&atilde;o de identifica&ccedil;&atilde;o quando o usu&aacute;rio
 		 * estaciona o mouse sobre o mapa
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {"200px"}
 		 */
 		alturatip : "100px",
 		/*
 		 * Propriedade: larguratip
-		 * 
+		 *
 		 * Largura em pixel do tip que &eacute; mostrado na fun&ccedil;&atilde;o de identifica&ccedil;&atilde;o quando o usu&aacute;rio
 		 * estaciona o mouse sobre o mapa
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {"200px"}
 		 */
 		larguratip : "200px",
 		/*
 		 * Propriedade: funcaoTip
-		 * 
+		 *
 		 * Fun&ccedil;&atilde;o que ser&aacute; executada na opera&ccedil;&atilde;o de identifica&ccedil;&atilde;o quando o usu&aacute;rio
 		 * estaciona o mouse sobre o mapa
-		 * 
+		 *
 		 * Tipo: {String}
-		 * 
+		 *
 		 * Default: {"i3GEO.mapa.dialogo.verificaTipDefault()"}
 		 */
 		funcaoTip : "i3GEO.mapa.dialogo.verificaTipDefault()",
 		/*
 		 * Propriedade: funcaoIdentifica
-		 * 
+		 *
 		 * Fun&ccedil;&atilde;o que ser&aacute; executada na opera&ccedil;&atilde;o de identifica&ccedil;&atilde;o quando o usu&aacute;rio
 		 * clica no mapa
-		 * 
+		 *
 		 * Tipo: {String}
-		 * 
+		 *
 		 * Default: {"i3GEO.mapa.dialogo.cliqueIdentificaDefault()"}
 		 */
 		funcaoIdentifica : "i3GEO.mapa.dialogo.cliqueIdentificaDefault()",
 		/*
 		 * Propriedade: diminuixM
-		 * 
+		 *
 		 * Diminui a largura do mapa em pixels no caso do navegador ser o IE. Valores definidos em pixel.
-		 * 
+		 *
 		 * Tipo: {numeric}
-		 * 
+		 *
 		 * Default: {13}
 		 */
 		diminuixM : 0,
 		/*
 		 * Propriedade: diminuixN
-		 * 
+		 *
 		 * Diminui a largura do mapa em pixels no caso do navegador ser o FF. Valores definidos em pixel.
-		 * 
+		 *
 		 * Tipo: {numeric}
-		 * 
+		 *
 		 * Default: {11}
 		 */
 		diminuixN : 0,
 		/*
 		 * Propriedade: diminuiyM
-		 * 
+		 *
 		 * Diminui a altura do mapa em pixels no caso do navegador ser o IE. Valores definidos em pixel.
-		 * 
+		 *
 		 * Tipo: {numeric}
-		 * 
+		 *
 		 * Default: {106}
 		 */
 		diminuiyM : 70,
 		/*
 		 * Propriedade: diminuiyN
-		 * 
+		 *
 		 * Diminui a altura do mapa em pixels no caso do navegador ser o FF. Valores definidos em pixel.
-		 * 
+		 *
 		 * Tipo: {numeric}
-		 * 
+		 *
 		 * Default: {103}
 		 */
 		diminuiyN : 70,
 		/*
 		 * Propriedade: autotamanho
-		 * 
+		 *
 		 * Calcula o tamanho do mapa conforme o tamanho da janela do navegador (false) ou calcula o tamanho do mapa conforme o tamanho da
 		 * tela do monitor (true)
-		 * 
+		 *
 		 * Tipo: {boolean}
-		 * 
+		 *
 		 * Default: {false}
 		 */
 		autotamanho : false,
 		/*
 		 * Vari&aacute;vel que define o nome do map_file que possu&iacute; o layer para uso na fun&ccedil;&atilde;o 3d. Pode ser utilizado o
 		 * caminho completo, se n&atilde;o, busca no diret&oacute;rio aplicmap.
-		 * 
+		 *
 		 * O mapfile deve conter um layer para c&aacute;lculo dos valores de Z para compor o modelo do relevo sobre o qual o mapa
 		 * ser&aacute; desenhado.
-		 * 
+		 *
 		 * Por padr&atilde;o, o i3geo utiliza o mapfile aplicmpa/3dmap.map
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {""}
 		 */
 		map3d : "",
 		/*
 		 * Propriedade: embedLegenda
-		 * 
+		 *
 		 * Indica se a legenda deve ser incluida no corpo do mapa.
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Valores: sim|nao
-		 * 
+		 *
 		 * Default: {nao}
 		 */
 		embedLegenda : "nao",
 		/*
 		 * Propriedade: templateLegenda
-		 * 
+		 *
 		 * Template HTML que ser&aacute; utilizado na gera&ccedil;&atilde;o da legenda HTML.
-		 * 
+		 *
 		 * A sintaxe utilizada na montagem do template &eacute; baseado na sintaxe do pr&oacute;prio Mapserver. O HTML pode ser armazenado
 		 * em i3geo/aplicmap ou em um outro endere&ccedil;o no servidor. O template serve para definir o layout da legenda que &eacute;
 		 * mostrada quando a guia legenda &eacute; ativada. Se for definido como "", &eacute; utilizado o template
 		 * i3geo/aplicmap/legenda.htm.
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {""}
 		 */
 		templateLegenda : "legenda6.htm",
 		/*
 		 * Propriedade: mashuppar
-		 * 
+		 *
 		 * Define os par&acirc;metros que devem ser aplicados no modo mashup
-		 * 
+		 *
 		 * O modo mashup possibilita que o i3Geo seja embutido dentro de uma p&aacute;gina HTML. Nesse caso, o mapa n&atilde;o &eacute;
 		 * criado no modo convencional, que utiliza o programa i3geo/ms_criamapa.php A variavel mashuppar deve conter os par&acirc;metros
 		 * que s&atilde;o utilizados pelo programa ms_criamapa
-		 * 
+		 *
 		 * Exemplo:
-		 * 
+		 *
 		 * i3GEO.configura.mashuppar = "&pontos=-54 -12&temasa=biomas&layers=biomas"
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {""}
 		 */
 		mashuppar : "",
 		/*
 		 * C&oacute;digo da se&ccedil;&atilde;o aberta pelo i3Geo no servidor.
-		 * 
+		 *
 		 * O c&oacute;digo &eacute; gerado na inicializa&ccedil;&atilde;o do i3Geo pelo programa ms_criamapa.php
-		 * 
+		 *
 		 * Tipo: {String}
 		 */
 		sid : "",
 		/*
 		 * Localiza&ccedil;&atilde;o da instala&ccedil;&atilde;o do i3geo (URI)
-		 * 
+		 *
 		 * Por default, &eacute; definida na inicializa&ccedil;&atilde;o do i3Geo
-		 * 
+		 *
 		 * Tipo: {string}
 		 */
 		locaplic : "",
 		/*
 		 * Propriedade: mapaRefDisplay
-		 * 
+		 *
 		 * Indica se o mapa de refer&ecirc;ncia dever&aacute; ser aberto quando o i3Geo for inicializado.
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Default: {"block"}
-		 * 
+		 *
 		 * Valores: block|none
 		 */
 		mapaRefDisplay : "block",
 		/*
 		 * Tipo de visual que ser&aacute; utilizado no mapa.
-		 * 
+		 *
 		 * A lista de visuais existentes &eacute; obtida na inicializa&ccedil;&atilde;o do i3geo.
-		 * 
+		 *
 		 * Veja o diret&oacute;rio i3geo/imagens/visual
-		 * 
+		 *
 		 * Tipo: {String}
-		 * 
+		 *
 		 * Default: {default}
 		 */
 		visual : "default",
 		/*
 		 * Propriedade: cursores
-		 * 
+		 *
 		 * Imagens utilizadas para os cursores do mouse mostrados no mapa
-		 * 
+		 *
 		 * A manipula&ccedil;&atilde;o dos cursores &eacute; feita com i3GEO.util.mudaCursor
-		 * 
+		 *
 		 * &Eacute; poss&iacute;vel utilizar tamb&eacute;m um dos tipos default, pointer, crosshair, help, move, text
-		 * 
+		 *
 		 * Tipo: {JSON}
 		 */
 		cursores : {
@@ -978,9 +973,9 @@ i3GEO.configura =
 		},
 		/*
 		 * Propriedade: listaDePropriedadesDoMapa
-		 * 
+		 *
 		 * Lista com as fun&ccedil;&otilde;es que s&atilde;o inclu&iacute;das no item "Propriedades do mapa"
-		 * 
+		 *
 		 * Tipo: {JSON}
 		 */
 		listaDePropriedadesDoMapa : {
@@ -1026,81 +1021,81 @@ i3GEO.configura =
 		},
 		/*
 		 * Propriedade: tempoAplicar
-		 * 
+		 *
 		 * Tempo em milisegundos que ser&aacute; esperado at&eacute; que o mapa seja desenhado automaticamente.
-		 * 
+		 *
 		 * Utilizado no bot&atilde;o Aplicar, quando o usu&aacute;rio liga/desliga ou adiciona umtema
-		 * 
+		 *
 		 * Tipo: {Numeric}
-		 * 
+		 *
 		 * Default: {4000}
 		 */
 		tempoAplicar : 4000,
 		/*
 		 * Propriedade: tempoMouseParado
-		 * 
+		 *
 		 * Tempo em milisegundos que ser&aacute; esperado para detectar que o mouse est&aacute; parado.
-		 * 
+		 *
 		 * Controla o lapso de tempo utilizado para disparar as fun&ccedil;&otilde;es que ocorrem quando o mouse est&aacute; parado sobre o
 		 * mapa
-		 * 
+		 *
 		 * Tipo: {Numeric}
-		 * 
+		 *
 		 * Default: {3500}
 		 */
 		tempoMouseParado : 1800,
 		/*
 		 * Propriedade: iniciaJanelaMensagens
-		 * 
+		 *
 		 * Inicia o i3geo com a janela de mensagens aberta ou fechada.
-		 * 
+		 *
 		 * Se o cookie g_janelaMen estiver definido, essa vari&aacute;vel n&atilde;o ter&aacute; efeito
-		 * 
+		 *
 		 * Tipo: {Boolean}
-		 * 
+		 *
 		 * Default: {true}
 		 */
 		iniciaJanelaMensagens : false,
 		/*
 		 * Propriedade: mostraRosaDosVentos
-		 * 
+		 *
 		 * Mostra ou n&atilde;o a rosa dos ventos sob o mouse quando estiver parado.
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Valores: {sim|nao}
-		 * 
+		 *
 		 * Default: {"nao"}
 		 */
 		mostraRosaDosVentos : "nao",
 		/*
 		 * Propriedade: liberaGuias
-		 * 
+		 *
 		 * Indica se as guias ser&atilde;o montadas em uma janela flutuante sobre o mapa
-		 * 
+		 *
 		 * Tipo: {string}
-		 * 
+		 *
 		 * Valores: {sim|nao}
-		 * 
+		 *
 		 * Default: {nao}
 		 */
 		liberaGuias : "nao",
 		/*
 		 * Propriedade: funcoesBotoes
-		 * 
+		 *
 		 * Objeto com a lista de funcionalidades que ser&atilde;o adicionadas aos bot&otilde;es existentes no mapa.
-		 * 
+		 *
 		 * Essa lista pode ser modificada antes da inicializa&ccedil;&atilde;o do mapa.
-		 * 
+		 *
 		 * As funcionalidades apenas s&atilde;o inclu&iacute;das se o elemento HTML indicado em iddiv existir. Por isso, caso uma
 		 * fun&ccedil;&atilde;o n&atilde;o seja desejada, basta excluir o div do HTML utilizado no mapa.
-		 * 
+		 *
 		 * A lista de fun&ccedil;&otilde;es &eacute; inclu&iacute;da em i3GEO.configura.funcoesBotoes.botoes
-		 * 
+		 *
 		 * O elemento 'titulo' &eacute; usado na barra de bot&otilde;es do tipo olho de peixe
-		 * 
+		 *
 		 * Tipo: {Object}
-		 * 
+		 *
 		 */
 		funcoesBotoes : {
 			"botoes" : [
