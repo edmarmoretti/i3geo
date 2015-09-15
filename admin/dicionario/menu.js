78bd4a6502ab800db6b72901bdef7f4de7014a6c
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-15 12:34:10 +0000

    Adaptação para dicionário

--- admin/dicionario/menu.js
+++ admin/dicionario/menu.js
@@ -15,5 +15,250 @@ i3GEOadmin.menu.dicionario = {
 		pt : "Controle de acesso e usu&aacute;rios",
 		en : "",
 		es : ""
+	} ],
+	'testaInstal' : [ {
+		pt : "Testar a instala&ccedil;&atilde;o do i3geo (testainstal.php)",
+		en : "",
+		es : ""
+	} ],
+	'removeInicia' : [ {
+		pt : "Remover a tela inicial de apresenta&ccedil;&atilde;o do i3Geo",
+		en : "",
+		es : ""
+	} ],
+	'controleOperac' : [ {
+		pt : "Controle de opera&ccedil;&otilde;es",
+		en : "",
+		es : ""
+	} ],
+	'cadastroUsuario' : [ {
+		pt : "Cadastro de usu&aacute;rios e rela&ccedil;&atilde;o com pap&eacute;is",
+		en : "",
+		es : ""
+	} ],
+	'cadastroGrupos' : [ {
+		pt : "Cadastro de grupos e rela&ccedil;&atilde;o com usu&aacute;rios",
+		en : "",
+		es : ""
+	} ],
+	'metadadosEstatisticos' : [ {
+		pt : "Metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+	'documentacao' : [ {
+		pt : "Documenta&ccedil;&atilde;o (baseada no i3GeoSa&uacute;de)",
+		en : "",
+		es : ""
+	} ],
+	'cadastroVariav' : [ {
+		pt : "Cadastro principal de vari&aacute;veis",
+		en : "",
+		es : ""
+	} ],
+	'cadastroMapas' : [ {
+		pt : "Cadastro de mapas para publica&ccedil;&atilde;o",
+		en : "",
+		es : ""
+	} ],
+	'cadastroMedidas' : [ {
+		pt : "Cadastro de unidades de medida",
+		en : "",
+		es : ""
+	} ],
+	'cadastroPeriodos' : [ {
+		pt : "Cadastro de tipos de per&iacute;odos de tempo",
+		en : "",
+		es : ""
+	} ],
+	'cadastroConexoes' : [ {
+		pt : "Cadastro de conex&otilde;es com banco de dados",
+		en : "",
+		es : ""
+	} ],
+    'cadastroTabelas' : [ {
+		pt : "Cadastro de tabelas com regi&otilde;es geogr&aacute;ficas",
+		en : "",
+		es : ""
+	} ],
+	'cadastroFonte' : [ {
+		pt : "Cadastro de fontes de informa&ccedil;&atilde;o",
+		en : "",
+		es : ""
+	} ],
+	'gerenciaBd' : [ {
+		pt : "Gerenciador de banco de dados, upload de SHP e CSV",
+		en : "",
+		es : ""
+	} ],
+	'xmlWms' : [ {
+		pt : "Ver xml com a lista de servi&ccedil;os WMS originados do sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+	'verRss' : [ {
+		pt : "Ver RSS com a lista de mapas cadastrados",
+		en : "",
+		es : ""
+	} ],
+	'enviarArquivo' : [ {
+		pt : "Enviar arquivo para o servidor",
+		en : "",
+		es : ""
+	} ],
+	'enviarShp' : [ {
+		pt : "Upload de arquivo Shape file",
+		en : "",
+		es : ""
+	} ],
+	'uploadSimbolo' : [ {
+		pt : "Upload de imagem para uso como s&iacute;mbolo",
+		en : "",
+		es : ""
+	} ],
+	'editorMapfile' : [ {
+		pt : "Edi&ccedil;&atilde;o de mapfiles",
+		en : "",
+		es : ""
+	} ],
+	'editorMenus' : [ {
+		pt : "Edi&ccedil;&atilde;o da lista de menus",
+		en : "",
+		es : ""
+	} ],
+	'arvoreTemas' : [ {
+		pt : "&Aacute;rvore de temas",
+		en : "",
+		es : ""
+	} ],
+	'rssArvore' : [ {
+		pt : "Ver RSS com os grupos, subgrupos e temas",
+		en : "",
+		es : ""
+	} ],
+	'rssComentarios' : [ {
+		pt : "Ver RSS com os coment&aacute;rios sobre os temas",
+		en : "",
+		es : ""
+	} ],
+	'linkMapas' : [ {
+		pt : "Links para mapas",
+		en : "",
+		es : ""
+	} ],
+    'editaMapfile' : [ {
+		pt : "Editor de mapfiles",
+		en : "",
+		es : ""
+	} ],
+	'editaLinks' : [ {
+		pt : "Edi&ccedil;&atilde;o dos links",
+		en : "",
+		es : ""
+	} ],
+	'xmlMapas' : [ {
+		pt : "XML com a lista de mapas",
+		en : "",
+		es : ""
+	} ],
+	'editaMenus' : [ {
+		pt : "Edi&ccedil;&atilde;o dos menus de adi&ccedil;&atilde;o de temas",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro principal de vari&aacute;veis",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro de mapas para publica&ccedil;&atilde;o",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro de unidades de medida",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro de tipos de per&iacute;odos de tempo",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro de conex&otilde;es com banco de dados",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "Cadastro de tabelas com regi&otilde;es geogr&aacute;ficas",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Cadastro de fontes de informa&ccedil;&atilde;o",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Gerenciador de banco de dados, upload de SHP e CSV",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Ver xml com a lista de servi&ccedil;os WMS originados do sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Ver RSS com a lista de mapas cadastrados",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Enviar arquivo para o servidor",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Upload de arquivo Shape file",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Upload de imagem para uso como s&iacute;mbolo",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Edi&ccedil;&atilde;o de mapfiles",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Edi&ccedil;&atilde;o da lista de menus",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "&Aacute;rvore de temas",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Ver RSS com os grupos, subgrupos e temas",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Ver RSS com os coment&aacute;rios sobre os temas",
+		en : "",
+		es : ""
+	} ],
+	'' : [ {
+		pt : "Links para mapas",
+		en : "",
+		es : ""
 	} ]
 };
