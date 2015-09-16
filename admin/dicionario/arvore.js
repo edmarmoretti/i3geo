8cf46baba8ff1604c770aaaec463cf566ebdba02
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-16 14:45:32 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/arvore.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.arvore = {};
+i3GEOadmin.arvore.dicionario = {
+	'arvoreTemas' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - &Aacute;rvore de temas ",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite editar &aacute;rvore de temas que ser&aacute;utilizada pelo i3Geo.<p>A &aacute;rvore &eacute; composta de menus, grupos, sub-grupos e temas. Vcdeve criar os menus, a lista de grupos e a lista de sub-grupos. Aocriar um menu, esse &eacute; mostrado imediatamente na &aacute;rvore.<p>Menus ou nós da &aacute;rvore marcados como n&atilde;o publicados s&atilde;omostrados em vermelho e aparecem apenas para os administradores.<p>Em cada nó da &aacute;rvore podem ser associados os temas, mesmo naraiz de um dos n&iacute;veis. Os temas s&atilde;o arquivos mapfiles armazenadosem i3geo/temas e registrados no sistema de administra&ccedil;&atilde;o.<p>A cria&ccedil;&atilde;o de mapfiles e a vincula&ccedil;&atilde;o tema-mapfile &eacute; feitapor meio do editor de mapfiles, e consiste na determina&ccedil;&atilde;o de umnome para cada mapfile. Esse nome &eacute; listado ao ser adicionado umtema na &aacute;rvore.",
+		en : "",
+		es : ""
+	} ],
+    'txtCria' : [ {
+		pt : "<p class=paragrafo>Utilize os bot&otilde;es para criar novos nomes de menus, grupos ou sub-grupos, formando listas de itens.<br>Os itens criados podem ser reaproveitados em v&aacute;rios n&oacute;s da &aacute;rvore.</p>",
+		en : "",
+		es : ""
+	} ],
+    'editaGrupos' : [ {
+		pt : "Editar lista de grupos",
+		en : "",
+		es : ""
+	} ],
+    'editaSubgrupos' : [ {
+		pt : "Editar lista de sub-grupos",
+		en : "",
+		es : ""
+	} ],
+    'editaMenus' : [ {
+		pt : "Editar lista de menus",
+		en : "",
+		es : ""
+	} ],
+    'arvoreCatalogo' : [ {
+		pt : "&Aacute;rvore do cat&aacute;logo:",
+		en : "",
+		es : ""
+	} ]
+};
