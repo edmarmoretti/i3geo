e86dda1d78fd95445f55da6210b5826226763d27
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-18 12:56:03 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/atlas.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.atlas = {};
+i3GEOadmin.atlas.dicionario = {
+	'adminAtlas' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - Atlas ",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjudaAtlas' : [ {
+		pt : "<p>O cadastro de Atlas &eacute; utilizado na interface 'Atlas' do i3geo. Essa interface pode ser vista em http://localhost/i3geo/atlas<p>Cada Atlas possu&iacute; uma lista de pranchas e cada prancha uma lista de temas e outros par&acirc;metros.<p>A montagem de cada Atlas &eacute; feito definindo-se os par&acirc;metros de cada n&iacute;vel hier&aacute;rquico, editando-se uma &aacute;rvore cuja raiz &eacute; cada Atlas, seguido pelas pranchas e temas.<p>Para cada Atlas e cada prancha pode ser definido um texto explicativo que ser&aacute; aberto ao iniciar o Atlas ou a prancha.",
+		en : "",
+		es : ""
+	} ],
+    'criaAtlas' : [ {
+		pt : "Criar um novo atlas",
+		en : "",
+		es : ""
+	} ],
+    'atlasExistente' : [ {
+		pt : "Atlas existentes:",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "Editar lista de sub-grupos",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "Editar lista de menus",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "&Aacute;rvore do cat&aacute;logo:",
+		en : "",
+		es : ""
+	} ]
+};
