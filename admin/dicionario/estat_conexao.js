b247001b58aa058211988c9ee896a0c646813935
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-22 13:25:24 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/estat_conexao.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.estat_conexao = {};
+i3GEOadmin.estat_conexao.dicionario = {
+	'estatConexao' : [ {
+		pt : "Metadados estat&iacute;sticos - cadastro de conex&otilde;es com banco	de dados",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite editar a lista de conex&otilde;es que s&atilde;o mostradas nos formul&aacute;rios do sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+    'adicionaConexao' : [ {
+		pt : "Adicionar uma nova conex&atilde;o",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
---
+++ admin/dicionario/estat_editor.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.estat_editor = {};
+i3GEOadmin.estat_editor.dicionario = {
+	'estatEditor' : [ {
+		pt : "Metadados estat&iacute;sticos - editor de tabelas	de dados",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite editar as tabelas que cont&eacute;m os dados utilizadas pelo sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+    'msg' : [ {
+		pt : "Aten&ccedil;&atilde;o: para poder editar o banco de dados, o usu&aacute;rio da conex&atilde;o escolhida deve ter direitos de edi&ccedil;&atilde;o.	Apenas as tabelas e os dados existentes no esquema i3geo_metaestat podem ser editados",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
