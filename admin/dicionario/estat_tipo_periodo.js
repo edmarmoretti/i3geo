e64fecf1cef9605577392cb0808c82453c4556eb
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-23 13:59:50 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/estat_tipo_periodo.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.estat_tipo_periodo = {};
+i3GEOadmin.estat_tipo_periodo.dicionario = {
+	'estatTipoPeriodo' : [ {
+		pt : "Metadados estat&iacute;sticos - Unidades de medida",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite editar a lista de per&iacute;odos de tempo que s&atilde;o mostradas nos	formul&aacute;rios do sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+    'adicionaRegistro' : [ {
+		pt : "Adicionar um novo registro",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
