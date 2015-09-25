b04ef93aa4b06dcc2d03aa10bd5afcc625acb7ac
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-25 18:35:04 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/estat_uploaddados.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.estat_uploaddados = {};
+i3GEOadmin.estat_uploaddados.dicionario = {
+	'estatUpload' : [ {
+		pt : "Metadados estat&iacute;sticos - upload de dados",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite fazero upload de dados utilizadas pelo sistema de metadados estat&iacute;sticos",
+		en : "",
+		es : ""
+	} ],
+    'txtUpload' : [ {
+		pt : "<p><br> Aten&ccedil;&atilde;o: para poder editar o banco de dados, o usu&aacute;rio da conex&atilde;o escolhida deve ter direitos de	edi&ccedil;&atilde;o. Apenas vari&aacute;veis existentes no esquema i3geo_metaestat podem receber dados dessa forma.<br>O arquivo deve ser do tipo texto e a primeira linha deve conter os nomes das colunas<br>",
+		en : "",
+		es : ""
+	} ],
+    'uploadValores' : [ {
+		pt : "Upload de valores (arquivo CSV)",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
