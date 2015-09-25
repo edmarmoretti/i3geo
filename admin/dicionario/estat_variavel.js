79b4dd089d30e847ebd3b9c57323963cc238605e
Murilo Caixeta <caixetaunb@gmail.com>
2015-09-25 20:10:57 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/estat_variavel.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.estat_variavel = {};
+i3GEOadmin.estat_variavel.dicionario = {
+	'estatVariavel' : [ {
+		pt : "Metadados estat&iacute;sticos do i3geo - Vari&aacute;veis",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Esse formul&aacute;rio permite editar as var&iacute;&aacute;veis e suas medidas, definindo-se as fontes dos dados, tipos de classifica&ccedil;&atilde;o, forma de representa&ccedil;&atilde;o etc. As v&aacute;ri&aacute;veis s&atilde;o fen&ocirc;menos que possuem medidas estat&iacute;sticas. Os par&acirc;metros definem como	as vari&aacute;veis s&atilde;o acessadas para a montagem dos cartogramas.",
+		en : "",
+		es : ""
+	} ],
+    'adicionaVariavel' : [ {
+		pt : "Adicionar vari&aacute;vel",
+		en : "",
+		es : ""
+	} ],
+    'relatorio' : [ {
+		pt : "Relat&oacute;rio",
+		en : "",
+		es : ""
+	} ],
+    'obs' : [ {
+		pt : "Observação: caso algum mapa esteja aberto (em outra aba por exemplo) não se esqueça de recarregar o navegador após alterar alguma classificação. Isso é necessário para que o mapa reflita as alterações feitas, anulando eventuais arquivos em cache.</p>",
+		en : "",
+		es : ""
+	} ],
+    'listaVariaveis' : [ {
+		pt : "<p>Vari&aacute;veis cadastradas:",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
