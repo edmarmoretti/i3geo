07ead502188c42fe14464664cc526ab6c8f0de57
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-02 14:21:47 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/gruposusuarios.js
@@ -0,0 +1,44 @@
+//+$trad(1,i3GEOadmin.editormapfile.dicionario)+
+i3GEOadmin.gruposusuarios = {};
+i3GEOadmin.gruposusuarios.dicionario = {
+	'gruposusuarios' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - Grupos de usu&aacute;rios",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Cada usu&aacute;rio pode pertencer a um ou mais grupos. Os grupos s&atilde;o utilizados na defini&ccedil;&atilde;o das permiss&otilde;es de usu&aacute;rios para acesso a dados ou mapas",
+		en : "",
+		es : ""
+	} ],
+    'adicionaGrupo' : [ {
+		pt : "Adicionar grupo",
+		en : "",
+		es : ""
+	} ],
+    'grupoCadastrado' : [ {
+		pt : "Grupos cadastrados:",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
