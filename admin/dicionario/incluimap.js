db17d56ba87d46475a460b7b2056b33813ac2663
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-05 14:05:19 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/incluimap.js
@@ -0,0 +1,29 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.incluimap = {};
+i3GEOadmin.incluimap.dicionario = {
+	'incluiMap' : [ {
+		pt : "<p>As vers&otilde;es novas do Mapserver exigem que os arquivos mapfile iniciem com a palavra 'MAP'</p><p>Os mapfiles utilizados pelo i3geo, at&eacute; a vers&atilde;o 4.1, e armazenados no diretório i3Geo/temas n&atilde;o iniciam com 'MAP'. Para ajustar os mapfiles voc&ecirc; pode edit&aacute;-los individulamente ou rodar o programa i3geo/admin/php/incluiMAP.php<p>Antes de fazer isso, fa&ccedil;a um backup do seu diretório i3geo/temas<p>",
+		en : "",
+		es : ""
+	} ],
+    'link' : [ {
+		pt : "Clique para ajustar os mapfiles - inclus&atilde;o da palavra 'MAP' no in&iacute;cio dos arquivos;	substitui&ccedil;&atilde;o de v&aacute;rios elementos depreciados",
+		en : "",
+		es : ""
+	} ],
+    'lista' : [ {
+		pt : "Lista de substitui&ccedil;&otilde;es:",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
