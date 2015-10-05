02fac6f6499b50c7d7ea8ea0dd3e0bb8c13210da
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-05 14:15:21 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/mapas.js
@@ -0,0 +1,29 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+//i3GEOadmin.mapas = {};
+i3GEOadmin.mapas.dicionario = {
+	'mapas' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - Links para mapas",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Vc pode registrar qualquer link para mapas. A op&ccedil;&atilde;o 'editar' abre um formul&aacute;rio para a cria&ccedil;&atilde;o do link conforme os par&acirc;metros utilizados pelo i3Geo. Voc&ecirc; pode desprezar todos os campos do formul&aacute;rio e incluir apenas o link direto desejado. Pode ser armazenado um mapfile completo com todas as configura&ccedil;&otilde;es do mapa",
+		en : "",
+		es : ""
+	} ],
+    'ajudaNovo' : [ {
+		pt : "Adicionar um novo",
+		en : "",
+		es : ""
+	} ],
+    'linksExistentes' : [ {
+		pt : "Links existentes:",
+		en : "",
+		es : ""
+	} ]
+};
