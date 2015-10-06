21ff17e68bfcdf33fb2f34447bd51f3e9615dacd
Ayala <ayala@sustentap.com.py>
2015-10-06 19:33:17 +0000

    traducción

--- ferramentas/agrupaelementos/dicionario.js
+++ ferramentas/agrupaelementos/dicionario.js
@@ -3,51 +3,51 @@ i3GEOF.agrupaElementos.dicionario = {
 	'descricao' : [ {
 		pt : "Essa ferramenta transforma v&aacute;rios elementos selecionados de um tema em um s&oacute; gerando um pol&iacute;gono para cada grupo (pol&iacute;gonos convexos).",
 		en : "",
-		es : ""
+		es : "Esta herramienta transforma varios elementos seleccionados de un tema en un &uacute;nico generando un pol&iacute;gono para cada grupo (pol&iacute;gonos convexos)."
 	} ],
 	'descricao2' : [ {
 		pt : "Para definir quais elementos devem ser unidos uns com os outros &eacute; preciso escolher um item da tabela de atributos do tema. Os elementos que possu&iacute;rem o mesmo valor ser&atilde;o considerados no mesmo grupo. Caso n&atilde;o tenha sido escolhido nenhum item, todas os elementos ser&atilde;o agrupados em um s&oacute;.",
 		en : "",
-		es : ""
+		es : "Para definir cuales elementos deben ser unidos unos con otros es, necesario escojer un &iacute;tem de la tabla de atributos del tema. Los elementos que poseen el mismo valor ser&aacute;n considerados en el mismo grupo. En caso que ning&uacute;n item haya sido escogido, todos los elementos ser&aacute;n agrupados en uno &uacute;nico"
 	} ],
 	'descricao3' : [ {
 		pt : "O resultado final ser&aacute; um novo tema com elementos diferentes dos originais e cuja tabela de atributos conter&aacute; apenas o item escolhido.",
 		en : "",
-		es : ""
+		es : "El resultado final ser&aacute; un nuevo tema con elementos diferentes de los originales y cuya tabla de atributos contendr&aacute; apenas el %iacute;tem escogido"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema que cont&eacute;m os elementos (pelo menos um elemento deve estar selecionado)",
 		en : "",
-		es : ""
+		es : "Tema que contiene los elementos (por lo menos un elemento debe estar seleccionado) "
 	} ],
 	'selecionaAtributo' : [ {
 		pt : "Escolha o item da tabela de atributos que ser&aacute; utilizado para agregar. (opcional)",
 		en : "",
-		es : ""
+		es : "Elija el &iacute;tem de la tabla de atributos que ser&aacute; utilizada para adicionar. (opcional)"
 	} ],
 	'adicionaAgrupamento' : [ {
 		pt : "O tema com o agrupamento ser&aacute; adicionado ao mapa atual.",
 		en : "",
-		es : ""
+		es : "El tema con el que el agrupamiento ser&aacute; adicionado al mapa actual"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n tard&oacute; mucho tiempo"
 	} ],
 	'erroTema' : [ {
 		pt : "Tema invalido",
 		en : "",
-		es : ""
+		es : "Tema invalido"
 	} ],
 	'erroItem' : [ {
 		pt : "erro ao ler os itens do tema de origem",
 		en : "",
-		es : ""
+		es : "error al leer los &iacute;tems del tema de origen"
 	} ],
 	'criaAgrupamento' : [ {
 		pt : "Criar agrupamento",
 		en : "",
-		es : ""
+		es : "Crear agrupamiento"
 	} ]
 };
