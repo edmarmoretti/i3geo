a458ed8ed55aab880aa4518d9b322af53c330a25
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-06 14:33:22 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/ogcws.js
@@ -0,0 +1,29 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.ogcws = {};
+i3GEOadmin.ogcws.dicionario = {
+	'ogcws' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - Web Services OGC",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Este formul&aacute;rio permite alterar as configura&ccedil;&otilde;es de contato do gerador de Web Services<p>As mudan&ccedil;as inclu&iacute;das aqui afetam o programa i3geo/ogc.php e s&atilde;o armazenadas em	i3geo/aplicmpa/ogcws.map",
+		en : "",
+		es : ""
+	} ],
+    'parametros' : [ {
+		pt : "<p>Par&acirc;metros obtidos do mapfile:</p>",
+		en : "",
+		es : ""
+	} ],
+    '' : [ {
+		pt : "",
+		en : "",
+		es : ""
+	} ]
+};
