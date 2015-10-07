67b9317b3e53214fe3e00da1da4ffe7b8719be0a
Ayala <ayala@sustentap.com.py>
2015-10-07 20:29:42 +0000

    traducción

--- ferramentas/animacao/dicionario.js
+++ ferramentas/animacao/dicionario.js
@@ -3,26 +3,26 @@ i3GEOF.animacao.dicionario = {
 	'inicia' : [ {
 		pt : "inicia",
 		en : "",
-		es : ""
+		es : "incia"
 	} ],
 	'rebobina' : [ {
 		pt : "rebobina",
 		en : "",
-		es : ""
+		es : "rebobina"
 	} ],
 	'rapido' : [ {
 		pt : "mais rapido",
 		en : "",
-		es : ""
+		es : "m&aacute;s r&aacute;pido"
 	} ],
 	'lento' : [ {
 		pt : "mais lento",
 		en : "",
-		es : ""
+		es : "m&aacute;s lento"
 	} ],
 	'ajuda' : [ {
 		pt : "Marque as camadas que ser&atilde;o inclu&iacute;das na anima&ccedil;&atilde;o e depois clique no bot&atilde;o <i>inicia</i>",
 		en : "",
-		es : ""
+		es : "Marque las capas que ser&aacute;n incluidas en la animaci&oacute;n y despu&eacute;s haga clic en el bot;oacute;n inicio"
 	} ]
 };
