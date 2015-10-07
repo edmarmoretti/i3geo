74b282262f84839e28eaa048aec603d5e9f4f194
Ayala <ayala@sustentap.com.py>
2015-10-07 20:28:00 +0000

    traducción

--- ferramentas/analisageometrias/dicionario.js
+++ ferramentas/analisageometrias/dicionario.js
@@ -3,177 +3,177 @@ i3GEOF.analisaGeometrias.dicionario = {
 		'adiciona': [{
 			pt:"Adiciona",
 			en:"",
-			es:""
+			es:"Adiciona"
 		}],
 		'retira': [{
 			pt:"Retira",
 			en:"",
-			es:""
+			es:"Retira"
 		}],
 		'inverte': [{
 			pt:"Inverte",
 			en:"",
-			es:""
+			es:"Invierte"
 		}],
 		'limpa': [{
 			pt:"Limpa",
 			en:"",
-			es:""
+			es:"Limpia"
 		}],
 		'captura': [{
 			pt:"Capturar",
 			en:"",
-			es:""
+			es:"Capturar"
 		}],
 		'lista': [{
 			pt:"Listar e escolher",
 			en:"",
-			es:""
+			es:"Listar y elegir"
 		}],
 		'analisa': [{
 			pt:"Analisar",
 			en:"",
-			es:""
+			es:"Analizar"
 		}],
 		'escolheTema': [{
 			pt:"Escolha o tema que receber&aacute; a sele&ccedil;&atilde;o",
 			en:"",
-			es:""
+			es:"Elegir el tema que recibir&aacute; la selecci&oacute;n"
 		}],
 		'tipoSelecao': [{
 			pt:"Tipo de sele&ccedil;&atilde;o",
 			en:"",
-			es:""
+			es:"Tipo de selecci&oacute;n"
 		}],
 		'capturaGeom': [{
 			pt:"Capturar as geometrias selecionadas",
 			en:"",
-			es:""
+			es:"Capturar las geometr&iacute;as seleccionadas"
 		}],
 		'ajuda': [{
 			pt:"Ap&oacute;s escolher o tema, clique no mapa para selecionar os elementos desejados, caso vc j&aacute; n&atilde;o tenha feito isso ou caso deseje alterar a sele&ccedil;&atilde;o. Conclu&iacute;da a sele&ccedil;&atilde;o, clique no bot&atilde;o de captura para obter as geometrias. <br>As geometrias capturadas podem ser vistas na segunda guia.",
 			en:"",
-			es:""
+			es:"Luego de elegir el tema, haga clic en el mapa para seleccionar los elementos deseados, en caso de que usted  a&uacute;n no lo haya hecho o desee alterar la selecci&oacute;n. Concluida la selecci&oacute;n,  haga clic en el bot&oacute;n de captura para obtener las geometr&iacute;as. <br> Las geometr&iacute;as capturadas pueden ser vistas en la segunda gu&iacute;a."
 		}],
 		'ajuda2': [{
 			pt:"Marque as geometrias para aplicar os processos desejados.",
 			en:"",
-			es:""
+			es:"Marque las geometr&iacute;as para aplicar los procesos deseados"
 		}],
 		'recalculaGeom': [{
 			pt:"Recalcula a &aacute;rea e o per&iacute;metro das novas geometrias",
 			en:"",
-			es:""
+			es:"Recalcula las &aacute;reas y los per&iacue;metros de las nuevas geometr&iacute;as "
 		}],
 		'operacaoGeom': [{
 			pt:"Opera&ccedil;&otilde;es que obt&eacute;m descri&ccedil;&otilde;es sobre a geometria, retornando valores que s&atilde;o adicionados aos itens da geometria",
 			en:"",
-			es:""
+			es:"Operaciones que optienen descripciones sobre la geometr&iacute;a, retornando valores que son adicionados a los items de la geometr&iacute;a "
 		}],
 		'area': [{
 			pt:"&Aacute;rea",
 			en:"",
-			es:""
+			es:"&Aacute;rea"
 		}],
 		'perimetro': [{
 			pt:"Per&iacute;metro",
 			en:"",
-			es:""
+			es:"Per&iacute;metro"
 		}],
 		'aplicar': [{
 			pt:"Aplicar",
 			en:"",
-			es:""
+			es:"Aplicar"
 		}],
 		'operacaoGeom2': [{
 			pt:"Opera&ccedil;&otilde;es que criam novas geometrias a partir de duas ou mais geometrias",
 			en:"",
-			es:""
+			es:"Operaciones que crean nuevas geometr&iacute;as a partir de dos o m&aacute;s geometr&iacute;as"
 		}],
 		'uniao': [{
 			pt:"Uni&atilde;o",
 			en:"",
-			es:""
+			es:"Uni&oacute;n"
 		}],
 		'interseccao': [{
 			pt:"Intersec&ccedil;&atilde;o",
 			en:"",
-			es:""
+			es:"Intersecci&oacute;n"
 		}],
 		'diferenca': [{
 			pt:"Diferen&ccedil;a",
 			en:"",
-			es:""
+			es:"Diferencia"
 		}],
 		'diferencaInversa': [{
 			pt:"Diferen&ccedil;a inversa",
 			en:"",
-			es:""
+			es:"Diferencia inversa"
 		}],
 		'convexo': [{
 			pt:"Convexo",
 			en:"",
-			es:""
+			es:"Convexo"
 		}],
 		'operacaoGeom3': [{
 			pt:"Opera&ccedil;&otilde;es que atuam sobre uma &uacute;nica geometria",
 			en:"",
-			es:""
+			es:"Operaciones que act&uacute;an sobre una &uacute;nica geometr&iacute;a"
 		}],
 		'convexo': [{
 			pt:"Convexo",
 			en:"",
-			es:""
+			es:"Convexo"
 		}],
 		'entorno': [{
 			pt:"Entorno",
 			en:"",
-			es:""
+			es:"Entorno"
 		}],
 		'ajuda3': [{
 			pt:"As opera&ccedil;&otilde;es de an&aacute;lise s&atilde;o executadas sobre as geometrias selecionadas.",
 			en:"",
-			es:""
+			es:"Las operaciones de an&aacute;lisis son ejecutadas sobre las geometr&iacute;as seleccionadas"
 		}],
 		'ajuda4': [{
 			pt:"Para maiores informa&ccedil;&otilde;es, veja",
 			en:"",
-			es:""
+			es:"Para mayores informaciones, vea"
 		}],
 		'ajuda5': [{
 			pt:"Algumas fun&ccedil;&otilde;es para operarem corretamente sobre temas pontuais, precisam que antes seja feita a uni&atilde;o dos pontos em uma nova geometria.",
 			en:"",
-			es:""
+			es:"Algunas funciones para que operen correctamente sobre temas puntuales, necesitan que antes sea hecha la uni&oacute;n entre dos puntos en una nueva geometr&iacute;a.  "
 		}],
 		'nomeGeom': [{
 			pt:"Ecolha um nome para a geometria",
 			en:"",
-			es:""
+			es:"Elija un nombre para la geometr&iacute;a"
 		}],
 		'exclui': [{
 			pt:"Excluir marcados",
 			en:"",
-			es:""
+			es:"Excluir marcados"
 		}],
 		'verMapa': [{
 			pt:"Ver no mapa",
 			en:"",
-			es:""
+			es:"Ver en el mapa"
 		}],
 		'naoGeom': [{
 			pt:"Nenhuma geometria dispon&iacute;vel",
 			en:"",
-			es:""
+			es:"Ninguna geometr&iacute;a disponible"
 		}],
 		'aguarde': [{
 			pt:"Aguarde...",
 			en:"",
-			es:""
+			es:"Aguarde..."
 		}],
 		'naoGeom2': [{
 			pt:"Nenhuma geometria foi marcada",
 			en:"",
-			es:""
+			es:"Ninguna geometr&iacute;a fue marcada"
 		}],
 		36: [{
 			pt:"",
