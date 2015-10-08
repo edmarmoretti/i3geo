51654c923f1d9efcd4ff88dad41f5426de5fc485
Ayala <ayala@sustentap.com.py>
2015-10-08 16:08:13 +0000

    traducción

--- ferramentas/buffer/dicionario.js
+++ ferramentas/buffer/dicionario.js
@@ -3,56 +3,56 @@ i3GEOF.buffer.dicionario = {
 	'descricao' : [ {
 		pt : "O entorno, ou buffer, &eacute; um pol&iacute;gono que circunda um elemento geogr&aacute;fico em uma dist&acirc;ncia fixa.",
 		en : "",
-		es : ""
+		es : "El entorno, o el buffer, es un pol&iacute;gono que circunda un elemento geogr&aacute;fico con una distancia fija."
 	} ],
 	'descricao2' : [ {
 		pt : "Para gerar o entorno, voc&ecirc; precisa selecionar alguns elementos de um tema. Utilize para isso a op&ccedil;&atilde;o de sele&ccedil;&atilde;o ou a tabela de atributos do tema desejado.",
 		en : "",
-		es : ""
+		es : "Para generar el entorno, usted debe seleccionar algunos elementos de un tema. Utilice para esto la opci&oacute;n de selecci&oacute;n o la tabla de atributos del tema deseado."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado",
 		en : "",
-		es : ""
+		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
 	} ],
 	'entorno' : [ {
 		pt : "Dist&acirc;ncia do entorno em metros:",
 		en : "",
-		es : ""
+		es : "Distancia del entorno en metros"
 	} ],
 	'agregaElementos' : [ {
 		pt : "Considerar os elementos selecionados como se fossem um s&oacute;?",
 		en : "",
-		es : ""
+		es : "Considerar los elementos seleccionados como si fueran uno solo"
 	} ],
 	'adicionaTemaEntorno' : [ {
 		pt : "O tema com o entorno ser&aacute; adicionado ao mapa atual",
 		en : "",
-		es : ""
+		es : "El tema con el entorno ser&aacute; adicionado al mapa actual"
 	} ],
 	'criaEntorno' : [ {
 		pt : "Criar entorno",
 		en : "",
-		es : ""
+		es : "Crear entorno"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ],
 	'erroDistancia' : [ {
 		pt : "Distancia invalida",
 		en : "",
-		es : ""
+		es : "Distancia invalida"
 	} ],
 	'entornoAtributo' : [ {
 		pt : "Ou um item da tabela de atributos que cont&eacute;m as dist&acirc;ncias:",
 		en : "",
-		es : ""
+		es : "O un item de la tabla de atributos que contiene las distancias"
 	} ],
 	'multiplica' : [ {
 		pt : "Multiplicado por:",
 		en : "",
-		es : ""
+		es : "Multiplicado por"
 	} ]
 };
