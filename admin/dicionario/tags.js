b8861102245a76691f9e3f95144ab11e47eea62c
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-08 13:47:16 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/tags.js
@@ -0,0 +1,29 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.tags = {};
+i3GEOadmin.tags.dicionario = {
+	'tags' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - tags",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Tags s&atilde;o como palavras-chave que podem ser adicionadas aos temas para a realiza&ccedil;&atilde;o de buscas.",
+		en : "",
+		es : ""
+	} ],
+    'adicionaTag' : [ {
+		pt : "Adicionar um novo tag",
+		en : "",
+		es : ""
+	} ],
+    'tagsExistentes' : [ {
+		pt : "<p>Tags existentes: (clique na c&eacute;lula para editar e salve a linha editada)</p>",
+		en : "",
+		es : ""
+	} ]
+};
---
+++ admin/dicionario/usuarios.js
@@ -0,0 +1,34 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.usuarios = {};
+i3GEOadmin.usuarios.dicionario = {
+	'usuarios' : [ {
+		pt : "Administra&ccedil;&atilde;o do i3geo - Usu&aacute;rios e pap&eacute;is operacionais",
+		en : "",
+		es : ""
+	} ],
+	'txtUsuarios' : [ {
+		pt : "<p>Os pap&eacute;is s&atilde;o utilizados para controlar as opera&ccedil;&otilde;es administrativas que o usu&aacute;rio poder&aacute; executar. Usu&aacute;rios podem tamb&eacute;m pertencer a determinados grupos. Os grupos s&atilde;o utilizados para controlar o acesso &agrave;s camadas nos mapas.</p><p>Utilize o editor de grupos para definir de quais grupos os usu&aacute;rios fazem parte.</p>",
+		en : "",
+		es : ""
+	} ],
+    'ajuda' : [ {
+		pt : "Ajuda",
+		en : "",
+		es : ""
+	} ],
+    'txtAjuda' : [ {
+		pt : "<p>Cada usu&aacute;rio pode possuir pap&eacute;is. Os pap&eacute;is s&atilde;o utilizados para definir as opera&ccedil;&otilde;es que podem ser executadas.<p>Apenas administradores podem acessar esses dados.",
+		en : "",
+		es : ""
+	} ],
+    'adicionaUsuario' : [ {
+		pt : "Adicionar usu&aacute;rio",
+		en : "",
+		es : ""
+	} ],
+    'usuariosCadastrados' : [ {
+		pt : "Usu&aacute;rios cadastrados:",
+		en : "",
+		es : ""
+	} ]
+};
