f853ffd6f571e550794f0da001caa1211205fbe1
Ayala <ayala@sustentap.com.py>
2015-10-08 16:08:49 +0000

    traducción

--- ferramentas/bufferpt/dicionario.js
+++ ferramentas/bufferpt/dicionario.js
@@ -3,21 +3,21 @@ i3GEOF.bufferpt.dicionario = {
 	'entorno' : [ {
 		pt : "Dist&acirc;ncia em metros ao redor do ponto",
 		en : "",
-		es : ""
+		es : "Distancia en metros alrededor de un punto"
 	} ],
 	'criaEntorno' : [ {
 		pt : "Criar",
 		en : "",
-		es : ""
+		es : "Crear"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ],
 	'erroDistancia' : [ {
 		pt : "Dist&acirc;ncia inv&aacute;lida",
 		en : "",
-		es : ""
+		es : "Distancia inadecuada"
 	} ]
 };
