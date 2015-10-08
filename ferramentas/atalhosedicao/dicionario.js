33fccb0fb1232a64740f6bcd69671b1d7644f9ba
Ayala <ayala@sustentap.com.py>
2015-10-08 16:07:50 +0000

    traducción

--- ferramentas/atalhosedicao/dicionario.js
+++ ferramentas/atalhosedicao/dicionario.js
@@ -3,56 +3,56 @@ i3GEOF.atalhosedicao.dicionario = {
 	'edicao' : [ {
 		pt : "Edi&ccedil;&atilde;o de mapfile",
 		en : "",
-		es : ""
+		es : "Edici&oacute;n de mapfile"
 	} ],
 	'legenda' : [ {
 		pt : "Legenda",
 		en : "",
-		es : ""
+		es : "Leyenda"
 	} ],
 	'salva' : [ {
 		pt : "Salva mapfile",
 		en : "",
-		es : ""
+		es : "Guardar mapfile"
 	} ],
 	'tabela' : [ {
 		pt : "Tabela de atributos",
 		en : "",
-		es : ""
+		es : "Tabla de atributos"
 	} ],
 	'texto' : [ {
 		pt : "Textos (topon&iacute;mia)",
 		en : "",
-		es : ""
+		es : "Textos (toponimia)"
 	} ],
 	'etiqueta' : [ {
 		pt : "Etiquetas, busca rapida, alias",
 		en : "",
-		es : ""
+		es : "Etiqueta, busqueda r&aacute;pida, alias"
 	} ],
 	'filtro' : [ {
 		pt : "Filtro de atributos",
 		en : "",
-		es : ""
+		es : "Filtro de atributos"
 	} ],
 	'opacidade' : [ {
 		pt : "Opacidade (0 a 100)",
 		en : "",
-		es : ""
+		es : "Opacidad (0 a 100)"
 	} ],
 	'aplicar' : [ {
 		pt : "Aplicar",
 		en : "",
-		es : ""
+		es : "Aplicar"
 	} ],
 	'ativaCache' : [ {
 		pt : "Ativa o cache para melhorar a performance (durante a edi&ccedil;&atilde;o, desmarque essa op&ccedil;&atilde;o)",
 		en : "",
-		es : ""
+		es : "Activa el cache para mejorar la perfomance (durante la edici&oacute;n desmarque esta opci%oacute;n)"
 	} ],
 	'ferramentas' : [ {
 		pt : "Ferramentas com par&acirc;metros que podem ser armazenados no mapfile do tema",
 		en : "",
-		es : ""
+		es : "Herramientas con par&aacute;metros que pueden ser almazenados en el mapfile del tema"
 	} ]
 };
