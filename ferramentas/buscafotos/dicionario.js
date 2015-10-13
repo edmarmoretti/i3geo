ad65093bbdb084bbf270bcab7cfc33c7e3499e30
Ayala <ayala@sustentap.com.py>
2015-10-13 17:06:48 +0000

    traducción

--- ferramentas/buscafotos/dicionario.js
+++ ferramentas/buscafotos/dicionario.js
@@ -3,112 +3,112 @@ i3GEOF.buscaFotos.dicionario = {
 	'ajuda' : [ {
 		pt : "As fotos mostradas aqui s&atilde;o de responsabilidade dos servi&ccedil;os de hospedagem utilizados na busca e podem abranger tem&aacute;ticas diversas.",
 		en : "",
-		es : ""
+		es : "Las fotos mostradas aqu&iacute; son de responsabilidad de los servicios de hospedaje utilizados en la b&uacute;squeda y pueden abarcar tem&aacute;ticas diversas."
 	} ],
 	'ajuda2' : [ {
 		pt : "A busca &eacute; feita apenas para a regi&atilde;o de abrang&ecirc;ncia do mapa atual.",
 		en : "",
-		es : ""
+		es : "La b&uacute;squeda hecha solo para la regi&oacute;n de cobertura del mapa actual"
 	} ],
 	'ajuda3' : [ {
 		pt : "A restri&ccedil;&atilde;o de escala &eacute; necess&aacute;ria para melhorar a performance da busca.",
 		en : "",
-		es : ""
+		es : "La restricci&oacute;n de la escala es necesaria para mejorar la performance de la b&uacute;squeda."
 	} ],
 	'msgForaEscala' : [ {
 		pt : "O mapa atual est&aacute; fora do limite de escala.",
 		en : "",
-		es : ""
+		es : "El mapa actual est&aacute fuera del l&iacute;mite de escala."
 	} ],
 	'ajusta' : [ {
 		pt : "Ajustar",
 		en : "",
-		es : ""
+		es : "Ajustar"
 	} ],
 	'continua' : [ {
 		pt : "Continuar",
 		en : "",
-		es : ""
+		es : "Continuar"
 	} ],
 	'ajudaFlickr' : [ {
 		pt : "Este produto usa a API do Flickr, mas n&atilde;o &eacute; endossado nem certificado pelo Flickr.",
 		en : "",
-		es : ""
+		es : "Este producto usa la API de Flickr, pero no est&aacute; aprobado ni certificado por Flickr."
 	} ],
 	'opcoes' : [ {
 		pt : "Op&ccedil;&otilde;es: Se nenhum par&acirc;metro for definido, ser&atilde;o consideradas apenas as fotos inclu&iacute;das nas &uacute;ltimas 12 horas.",
 		en : "",
-		es : ""
+		es : "Opciones: si ning&acute;n par&aacute;metro es definido, apenas ser&aacute;n consideradas las fotos incluidas en las &uacute;ltimas 12 horas."
 	} ],
 	'maxRegistros' : [ {
 		pt : "De qualquer forma, s&atilde;o mostrados no m&aacute;ximo 250 registros.",
 		en : "",
-		es : ""
+		es : "De cualquier forma, son mostrados como m%aacute;ximo 250 registros."
 	} ],
 	'ajudaAno' : [ {
 		pt : "O ano refere-se a data de inclus&atilde;o da foto no Flickr",
 		en : "",
-		es : ""
+		es : "El a&ntilde; se refiere al dato de inclusi&oacute;n de la foto en el Flickr"
 	} ],
 	'texto' : [ {
 		pt : "Texto",
 		en : "",
-		es : ""
+		es : "Texto"
 	} ],
 	'anoInicial' : [ {
 		pt : "Ano inicial",
 		en : "",
-		es : ""
+		es : "A&ntilde;o inicial"
 	} ],
 	'anoFinal' : [ {
 		pt : "Ano final",
 		en : "",
-		es : ""
+		es : "A&ntilde;o final"
 	} ],
 	'procuraFotos' : [ {
 		pt : "Procurar fotos",
 		en : "",
-		es : ""
+		es : "Buscar fotos"
 	} ],
 	'servidores' : [ {
 		pt : "Servidores",
 		en : "",
-		es : ""
+		es : "Servidores"
 	} ],
 	'msgAguarde' : [ {
 		pt : "Aguarde...",
 		en : "",
-		es : ""
+		es : "Aguarde..."
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ],
 	'erroAcessoDados' : [ {
 		pt : "Problemas no acesso aos dados!",
 		en : "",
-		es : ""
+		es : "Problemas en el acceso a los datos"
 	} ],
 	'erroNadaEncontrado' : [ {
 		pt : "Nada encontrado nessa regi&atilde;o!",
 		en : "",
-		es : ""
+		es : "¡Nada encontrado en esta regi&oacute;n!"
 	} ],
 	'maisFotos' : [ {
 		pt : "mais 15 fotos...",
 		en : "",
-		es : ""
+		es : "m&aacute;s de 15 fotos..."
 	} ],
 	'fotosEncontradas' : [ {
 		pt : "Fotos encontradas na regi&atilde;o vista no mapa",
 		en : "",
-		es : ""
+		es : "Fotos encontradas en la regi&oacute;n viste en el mapa"
 	} ],
 	'erroNadaEncontrado2' : [ {
 		pt : "Nada encontrado nessa regi&atilde;o!",
 		en : "",
-		es : ""
+		es : "Nada encontrado en esta regi&aacute;n "
 	} ],
 	23 : [ {
 		pt : "",
