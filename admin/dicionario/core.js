d0b679068c5bdb875e31b8a349348262a29f1688
Murilo Caixeta <caixetaunb@gmail.com>
2015-10-13 14:13:17 +0000

    Adaptação para dicionário

---
+++ admin/dicionario/core.js
@@ -0,0 +1,59 @@
+//+$trad(1,i3GEOadmin.principal.dicionario)+
+i3GEOadmin.core = {};
+i3GEOadmin.core.dicionario = {
+	'inicio' : [ {
+		pt : "In&iacute;cio",
+		en : "",
+		es : ""
+	} ],
+	'menus' : [ {
+		pt : "Menus",
+		en : "",
+		es : ""
+	} ],
+    'arvore' : [ {
+		pt : "&Aacute;rvore de temas",
+		en : "",
+		es : ""
+	} ],
+    'mapfiles' : [ {
+		pt : "Mapfiles",
+		en : "",
+		es : ""
+	} ],
+    'login' : [ {
+		pt : "Login",
+		en : "",
+		es : ""
+	} ],
+    'arvoreCompleta' : [ {
+		pt : "&Aacute;rvore completa",
+		en : "",
+		es : ""
+	} ],
+    'conexoes' : [ {
+		pt : "Conex&otilde;es",
+		en : "",
+		es : ""
+	} ],
+    'regioes' : [ {
+		pt : "Regi&otilde;es",
+		en : "",
+		es : ""
+	} ],
+    'operacoes' : [ {
+		pt : "Opera&ccedil;&otilde;es",
+		en : "",
+		es : ""
+	} ],
+    'usuarios' : [ {
+		pt : "Usu&aacute;rios",
+		en : "",
+		es : ""
+	} ],
+    'gruposUsuarios' : [ {
+		pt : "Grupos e Usu&aacute;rios",
+		en : "",
+		es : ""
+	} ]
+};
