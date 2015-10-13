4b68363d94f6a60b9b672a4b8246d783f8c9c092
Ayala <ayala@sustentap.com.py>
2015-10-13 17:15:49 +0000

    traducción

--- ferramentas/confluence/dicionario.js
+++ ferramentas/confluence/dicionario.js
@@ -3,7 +3,7 @@ i3GEOF.confluence.dicionario = {
 	'ajuda' : [ {
 		pt : "Navegue pelo mapa para ver o resultado! Clique no link para abrir a p&aacute;gina sobre o ponto.",
 		en : "",
-		es : ""
+		es : "¡Navegue sobre el mapa para ver el resultado! Haga clic en el link para abrir la p&aacute;gina sobre el punto."
 	} ],
 	'msgCoordenada' : [ {
 		pt : "Nenhuma coordenada encontrada.",
