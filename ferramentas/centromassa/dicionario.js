7b5a16ef6066f27ac1d33f8da15abff1f2256a9b
Ayala <ayala@sustentap.com.py>
2015-10-13 17:13:56 +0000

    traducción

--- ferramentas/centromassa/dicionario.js
+++ ferramentas/centromassa/dicionario.js
@@ -3,42 +3,42 @@ i3GEOF.centromassa.dicionario = {
 	'ajuda' : [ {
 		pt : "O centro m&eacute;dio &eacute; uma opera&ccedil;&atilde;o que resulta em um novo tema contendo o ponto que indica o centro de equil&iacute;brio de uma distribui&ccedil;&atilde;o.",
 		en : "",
-		es : ""
+		es : "El centro medio es una operaci&oacute;n que resulta en un nuevo tema que contiene el punto que indica el centro de equilibrio de una distribuci&oacute;n"
 	} ],
 	'ajuda2' : [ {
 		pt : "Para gerar o c&aacute;lculo &eacute; necess&aacute;rio que no mapa exista pelo menos um tema de pontos. Se n&atilde;o houverem elementos selecionados, ser&aacute; considerada a extens&atilde;o geogr&aacute;fica.",
 		en : "",
-		es : ""
+		es : "Para generar el c&aacute;lculo es necesario que en el mapa exista por lo menos un tema de puntos. Si no hubiera elementos seleccionados, ser&aacute; considerada la extensi&oacute;n geogr&aacute;fica."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema contendo os pontos:",
 		en : "",
-		es : ""
+		es : "Tema que contiene los puntos:"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Utilizar o seguinte item para ponderar os valores:",
 		en : "",
-		es : ""
+		es : "Utilizar la siguiente &iacute;tem para ponderar los valores:"
 	} ],
 	'ajuda3' : [ {
 		pt : "O tema com o centro ser&aacute; adicionado ao mapa atual.",
 		en : "",
-		es : ""
+		es : "El tema con el centro ser&aacute; adicionado al mapa actual."
 	} ],
 	'calcula' : [ {
 		pt : "Calcular",
 		en : "",
-		es : ""
+		es : "Calcular"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ],
 	'selecionaTema2' : [ {
 		pt : "Selecione um tema",
 		en : "",
-		es : ""
+		es : "Seleccione un tema"
 	} ],
 	9 : [ {
 		pt : "",
