a7684e25f7989495e856634cd0e3f957a345f33c
Ayala <ayala@sustentap.com.py>
2015-10-13 17:05:27 +0000

    traducción

--- ferramentas/busca/dicionario.js
+++ ferramentas/busca/dicionario.js
@@ -3,67 +3,67 @@ i3GEOF.busca.dicionario = {
 	'digita' : [ {
 		pt : "digite aqui o texto...",
 		en : "",
-		es : ""
+		es : "digite aqu&iacute; el texto..."
 	} ],
 	'procura' : [ {
 		pt : "Procurar",
 		en : "",
-		es : ""
+		es : "Buscar"
 	} ],
 	'busca' : [ {
 		pt : "qualquer lugar do item, ou",
 		en : "",
-		es : ""
+		es : "cualquier lugar del &iacute;tem, o"
 	} ],
 	'busca2' : [ {
 		pt : "a frase exata",
 		en : "",
-		es : ""
+		es : "la frase exacta "
 	} ],
 	'busca3' : [ {
 		pt : "procurar no mapa todo, ou",
 		en : "",
-		es : ""
+		es : "buscar en el mapa todo, o"
 	} ],
 	'busca4' : [ {
 		pt : "na regi&atilde;o atual",
 		en : "",
-		es : ""
+		es : "en la regi&oacute;n actual"
 	} ],
 	'busca5' : [ {
 		pt : "Buscar nos itens:",
 		en : "",
-		es : ""
+		es : "Buscar en los &iacute;tems"
 	} ],
 	'erroTema' : [ {
 		pt : "O tema ja nao existe mais no mapa",
 		en : "",
-		es : ""
+		es : "El tema ya no existe m&aacute;s en el mapa"
 	} ],
 	'erro' : [ {
 		pt : "Ocorreu um erro",
 		en : "",
-		es : ""
+		es : "Ocurri&oacute; un error"
 	} ],
 	'erroTema2' : [ {
 		pt : "O tema ja nao existe mais no mapa",
 		en : "",
-		es : ""
+		es : "El tema ya no existe m&aacute;s en el mapa"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Selecione um item",
 		en : "",
-		es : ""
+		es : "Selecciones un mapa"
 	} ],
 	'semRegistro' : [ {
 		pt : "Nenhum registro encontrado",
 		en : "",
-		es : ""
+		es : "Ning&uacute;n registro encontrado"
 	} ],
 	'localiza' : [ {
 		pt : "localiza",
 		en : "",
-		es : ""
+		es : "localiza"
 	} ],
 	14 : [ {
 		pt : "",
