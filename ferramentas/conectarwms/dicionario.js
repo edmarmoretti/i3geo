0e53ed3f45ff1f03e7448d14ec515cec2b427b2e
Ayala <ayala@sustentap.com.py>
2015-10-13 17:15:09 +0000

    traducción

--- ferramentas/conectarwms/dicionario.js
+++ ferramentas/conectarwms/dicionario.js
@@ -3,26 +3,26 @@ i3GEOF.conectarwms.dicionario = {
 	'erro' : [ {
 		pt : "Oops! Ocorreu um erro",
 		en : "",
-		es : ""
+		es : "¡Oops! Ocurri&oacute; un error"
 	} ],
 	'disponibilidade' : [ {
 		pt : "disponibilidade",
 		en : "",
-		es : ""
+		es : "disponibilidad"
 	} ],
 	'acessos' : [ {
 		pt : "acessos considerados",
 		en : "",
-		es : ""
+		es : "Accesos considerados"
 	} ],
 	'servico' : [ {
 		pt : "Servi&ccedil;o n&atilde;o definido",
 		en : "",
-		es : ""
+		es : "Servicio no definido"
 	} ],
 	5 : [ {
 		pt : "Ooops! Problemas ao acessar o servi&ccedil;o.",
 		en : "",
-		es : ""
+		es : "¡Oops! Problemas para acceder al servicio."
 	} ]
 };
