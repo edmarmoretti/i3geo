c01be0152ef1ebc3de9320619388bc4e94530eb7
Ayala <ayala@sustentap.com.py>
2015-10-13 17:14:10 +0000

    traducción

--- ferramentas/conectargeojson/dicionario.js
+++ ferramentas/conectargeojson/dicionario.js
@@ -2,12 +2,12 @@
 i3GEOF.conectargeojson.dicionario = {
 	'msgNavegador' : [ {
 		pt : "N&atilde;o funciona coretamente no Internet Explorer. Use o Firefox ou outro navegador",
-		en : "",
+		en : "No funciona correctamente el Internet Explorer. Use Firefox u otro navegador",
 		es : ""
 	} ],
 	'insereEndereco' : [ {
 		pt : "Endere&ccedil;o (URL) do GeoJson (ou escolha da lista):",
 		en : "",
-		es : ""
+		es : "Direcci&oacute;n (URL) del GeoJson (o elija de la lista):"
 	} ]
 };
