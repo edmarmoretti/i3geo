ef14e9116c498e7940e55bde361d024571a066d2
Ayala <ayala@sustentap.com.py>
2015-10-13 17:07:59 +0000

    traducción

--- ferramentas/carouseltemas/dicionario.js
+++ ferramentas/carouseltemas/dicionario.js
@@ -3,11 +3,11 @@ i3GEOF.carouseltemas.dicionario = {
 	'adicionaTema' : [ {
 		pt : "Clique na imagem para adicionar o tema ao mapa",
 		en : "",
-		es : ""
+		es : "Haga clic en la imagen para adicionar el tema del mapa "
 	} ],
 	'miniaturas' : [ {
 		pt : "Miniaturas",
 		en : "",
-		es : ""
+		es : "Miniaturas"
 	} ]
 };
