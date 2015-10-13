5b52fa1b9256c1fab9b1287081d3d26d2c7e1749
Ayala <ayala@sustentap.com.py>
2015-10-13 17:13:41 +0000

    traducción

--- ferramentas/centroide/dicionario.js
+++ ferramentas/centroide/dicionario.js
@@ -3,26 +3,26 @@ i3GEOF.centroide.dicionario = {
 	'ajuda' : [ {
 		pt : "Calcula o centro de cada pol&iacute;gono gerado uma nova camada. O c&aacute;lculo baseia-se no pol&iacute;gono envolvente e podem ocorrer casos em que o ponto &eacute; posicionado fora do pol&iacute;gono fonte",
 		en : "",
-		es : ""
+		es : "Calcula el centro de cada pol&iacute;gono generado una nueva camada. El c&aacute;lculo se basa en el pol&iacute;gono envolvente y puede ocurrir casos en que el punto sea posicionado fuera del pol&iacute;gono fuente"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado:",
 		en : "",
-		es : ""
+		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
 	} ],
 	'ajuda2' : [ {
 		pt : "O tema com os pontos ser&aacute; adicionado ao mapa atual.",
 		en : "",
-		es : ""
+		es : "El tema con los puntos ser&aacute; adicionado al mapa actual"
 	} ],
 	'criaCentroide' : [ {
 		pt : "Criar centr&oacute;ides",
 		en : "",
-		es : ""
+		es : "Crear centroide"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ]
 };
