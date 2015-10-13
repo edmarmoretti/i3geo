597597746988b81bbf0498b92ab9daa3347d98bf
Ayala <ayala@sustentap.com.py>
2015-10-13 17:14:27 +0000

    traducción

--- ferramentas/conectargeorss/dicionario.js
+++ ferramentas/conectargeorss/dicionario.js
@@ -3,26 +3,26 @@ i3GEOF.conectargeorss.dicionario = {
 	'msgServico' : [ {
 		pt : "Servi&ccedil;o n&atilde;o definido",
 		en : "",
-		es : ""
+		es : "Servicio indefinido"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Escolha um dos itens para incluir os dados do canal no mapa",
 		en : "",
-		es : ""
+		es : "Elija uno de los &iacute;tems para incluir los datos del canal en el mapa"
 	} ],
 	'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o:",
 		en : "",
-		es : ""
+		es : "Dercripci&oacute;n:"
 	} ],
 	'categoria' : [ {
 		pt : "Categoria:",
 		en : "",
-		es : ""
+		es : "Categor&i;a"
 	} ],
 	'erro' : [ {
 		pt : "Ocorreu um erro",
 		en : "",
-		es : ""
+		es : "Ocurri&oacute un error"
 	} ]
 };
