4631c18350f786405d3d7705c360f07375477b87
Ayala <ayala@sustentap.com.py>
2015-11-04 19:48:52 +0000

    traducción

--- ferramentas/convertews/dicionario.js
+++ ferramentas/convertews/dicionario.js
@@ -3,31 +3,31 @@ i3GEOF.converteMapaWS.dicionario = {
 	'ajudaDesktop' : [ {
 		pt : "Voc&ecirc; pode utilizar o endere&ccedil;o em softwares de geoprocessamento instalados em seu computador, como por exemplo, o",
 		en : "",
-		es : ""
+		es : "Usted puede utilizar la direcci&oacute;n en el software de geoprocesamiento instalado en su ordenador, como por ejemplo: el"
 	} ],
 	'ajudaWms' : [ {
 		pt : "O Web Service criado, utiliza o padr&atilde;o WMS, conforme definido pelo OGC. A disponibilidade do endere&ccedil;o &eacute; tempor&aacute;ria, permanecendo ativa apenas no dia em que foi criado.",
 		en : "",
-		es : ""
+		es : "El Web Service creado, utiliza el padr&oacute;n WMS, conforme lo definido por el OGC. La disponibilidad de la direcci&oacute;n temporal, permanecer&aacute; activa apenas el d&iacute;a en que fue creada."
 	} ],
 	'ajudaWmc' : [ {
 		pt : "O Web Map Context (WMC) pode ou n&atilde;o ser gerado dependendo da configura&ccedil;&atilde;o do servidor onde o i3Geo est&aacute; instalado.",
 		en : "",
-		es : ""
+		es : "El Web Map Context (WMC) puede o no ser generado dependiendo de la configuraci&;oacuten del servidor el  i3GEO est&eacute; instalado."
 	} ],
 	'testa' : [ {
 		pt : "Clique para testar",
 		en : "",
-		es : ""
+		es : "Haga clic para probar"
 	} ],
 	'erroWms' : [ {
 		pt : "Ocorreu um erro ao criar o WMS",
 		en : "",
-		es : ""
+		es : "Ocurri&oacute; un error al crear el WMS"
 	} ],
 	'wmc' : [ {
 		pt : "Clique para obter o Web Map Context",
 		en : "",
-		es : ""
+		es : "Haga clic para obtener el Web Map Context"
 	} ]
 };
