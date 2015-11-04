cb231db6094633ce1a3b95e6c3aae04789de6dfc
Ayala <ayala@sustentap.com.py>
2015-11-04 20:39:28 +0000

    traducción

--- ferramentas/distanciaptpt/dicionario.js
+++ ferramentas/distanciaptpt/dicionario.js
@@ -3,27 +3,27 @@ i3GEOF.distanciaptpt.dicionario = {
 	'ajuda' : [ {
 		pt : "O c&aacute;lculo de dist&acirc;ncias &eacute; feito de um ponto em rela&ccedil;&atilde;o aos mais pr&oacute;ximos existentes no mesmo tema ou em outro tema.",
 		en : "",
-		es : ""
+		es : "El c&aacute;lculo de distancia es realizado de un punto en relaci&oacute;n a los m&aacute;s pr&oacute;ximos existentes en el mismo tema o en otro tema. "
 	} ],
 	'ajuda2' : [ {
 		pt : "O ponto origem, deve estar selecionado em um dos temas existentes no mapa.",
 		en : "",
-		es : ""
+		es : "El punto de origen, debe estar relacionado a uno de los dos temas existentes en el mapa"
 	} ],
 	'ajuda3' : [ {
 		pt : "Os pontos de destino s&atilde;o selecionados em fun&ccedil;&atilde;o de uma dist&acirc;ncia fixa do ponto origem.",
 		en : "",
-		es : ""
+		es : "Los puntos de destino sere&aacute;n seleccionados en funci&oacute;n de una distancia fijada en el punto de origen. "
 	} ],
 	'temaOrigem' : [ {
 		pt : "Tema que cont&eacute;m os pontos de origem (pelo menos um ponto deve estar selecionado):",
 		en : "",
-		es : ""
+		es : "Tema que contienen los puntos de origen (por lo menos un punto debe estar seleccionado):"
 	} ],
 	'temaDestino' : [ {
 		pt : "Tema que cont&eacute;m o(s) ponto(s) de destino:",
 		en : "",
-		es : ""
+		es : "Tema que contiene/n el/los punto/s de destino:  "
 	} ],
 	'distMax' : [ {
 		pt : "Dist&acirc;ncia (metros) m&aacute;xima que ser&aacute; considerada entre os pontos de origem e os pontos de destino:",
@@ -33,36 +33,36 @@ i3GEOF.distanciaptpt.dicionario = {
 	'selecionaAtributos' : [ {
 		pt : "Escolha os itens das tabelas de atributos dos temas de origem e de destino que ser&atilde;o acrescentados ao novo tema que ser&aacute; criado com o resultado do c&aacute;lculo.",
 		en : "",
-		es : ""
+		es : "Seleccione los &iacute;tems de las tablas de atributos de los temas de origen y destino que ser&aacute;n incrementados al nuevo tema que ser&aacute; creado con el resultado del c&aacute;lculo. "
 	} ],
 	'atributoOrigem' : [ {
 		pt : "Para o tema de origem:",
 		en : "",
-		es : ""
+		es : "Para el tema de origen"
 	} ],
 	'atributoDestino' : [ {
 		pt : "Para o tema de destino:",
 		en : "",
-		es : ""
+		es : "Para el tema de destino"
 	} ],
 	'msgTema' : [ {
 		pt : "O tema com o c&aacute;lculo ser&aacute; adicionado ao mapa atual.",
 		en : "",
-		es : ""
+		es : "El tema con el c&aacute;lculo ser&aacute; adicionado al mapa actual"
 	} ],
 	'calcula' : [ {
 		pt : "Calcular",
 		en : "",
-		es : ""
+		es : "Calcular"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ],
 	'erroParametro' : [ {
 		pt : "Algum par&acirc;metro n&atilde;o foi preenchido.",
 		en : "",
-		es : ""
+		es : "Falto completar alg&uacute;n par&aacute;metro."
 	} ]
 };
