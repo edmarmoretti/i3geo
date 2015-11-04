5eae6afbbc8ec42f0357eb7928e9385a1099ac63
Ayala <ayala@sustentap.com.py>
2015-11-04 20:39:12 +0000

    traducción

--- ferramentas/dissolve/dicionario.js
+++ ferramentas/dissolve/dicionario.js
@@ -3,31 +3,31 @@ i3GEOF.dissolve.dicionario = {
 	'descricao' : [ {
 		pt : "Essa ferramenta transforma v&aacute;rios pol&iacute;gonos em um s&oacute; eliminando as divisas entre eles. Caso seja escolhido um item da tabela de atributos, os pol&iacute;gonos que possu&iacute;rem o mesmo valor ser&atilde;o considerados no mesmo grupo e suas divisas eliminadas e caso n&atilde;o tenha sido escolhido nenhum item, todas os pol&iacute;gonos ser&atilde;o agrupados em um s&oacute;. O resultado final ser&aacute; um novo tema com pol&iacute;gonos diferentes dos originais e cuja tabela de atributos conter&aacute; apenas um item.",
 		en : "",
-		es : ""
+		es : "Esta herramienta transforma varios pol&iacute;gonos en uno solo, eliminando las divisiones entre los mismos. En caso que sea seleccionado un &iacute;tem de la tabla de atributos, los pol&iacute;gonos que poseen el mismo valor formaran parte del mismo grupo y sus divisiones ser&aacute;n eliminadas. En caso que ning&uacute;n &iacute;tem sea seleccionado, todos los pol&iacute;gonos serán agrupados en un &uacute;nico pol&iacute;gono. El resultado final ser&aacute; un nuevo tema con pol&iacute;gonos diferentes a los originales y cuya tabla de atributos contendr&aacute; apenas un  &iacute;tem. "
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado:",
 		en : "",
-		es : ""
+		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Escolha o item que ser&aacute; utilizado (opcional)",
 		en : "",
-		es : ""
+		es : "Elija el &iacute;tem que ser&aacute; utilizado (opcional)"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema com as divisas eliminadas ser&aacute; adicionado ao mapa atual.",
 		en : "",
-		es : ""
+		es : "El tema con las divisiones elimindas ser&aacute; adicionado al mapa actual"
 	} ],
 	'dissolve' : [ {
 		pt : "Dissolver",
 		en : "",
-		es : ""
+		es : "Disolver"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "",
-		es : ""
+		es : "Error. La operaci&oacute;n demoro mucho"
 	} ]
 };
