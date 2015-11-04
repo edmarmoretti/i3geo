9f998675349d6baacd2b6a15b2e5d986a4ae5200
Ayala <ayala@sustentap.com.py>
2015-11-04 19:48:19 +0000

    tratraducción

--- ferramentas/convertemapakml/dicionario.js
+++ ferramentas/convertemapakml/dicionario.js
@@ -3,26 +3,26 @@ i3GEOF.converteMapaKml.dicionario = {
 	'ajuda' : [ {
 		pt : "Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar o mapa atual em softwares que aceitam o formato kml,",
 		en : "",
-		es : ""
+		es : "Usted puede utilizar las direcciones para visualizar datos en softwares que acepten el formato kml"
 	} ],
 	'ajuda2' : [ {
 		pt : "O endere&ccedil;o de acesso &eacute; tempor&aacute;rio, ficando dispon&iacute;vel por determinado per&iacute;odo de tempo conforme definido pelo administrador do i3Geo.",
 		en : "",
-		es : ""
+		es : "La direcci&oacute;n de acceso es temporal, queda disponible por un periodo de tiempo determinado por el administrador de i3Geo. "
 	} ],
 	'ajuda3' : [ {
 		pt : "para mais detalhes sobre como usar o link kml no Google Earth.",
 		en : "",
-		es : ""
+		es : "para m&acute;s detalles sobre c&oacute;mo usar el link km en Google Earth."
 	} ],
 	'wms' : [ {
 		pt : "Kml baseado em um servico WMS:",
 		en : "",
-		es : ""
+		es : "Kml basado en un servicio WMS"
 	} ],
 	'ajuda4' : [ {
 		pt : "Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no GoogleEarth (incluindo dados vetoriais)",
 		en : "",
-		es : ""
+		es : "Usted puede tambi&eacute;n utilizar el siguiente link (abajo) para mostrar el &aacute;rbol completo de temas en Google Earth"
 	} ]
 };
