3a6c59388aa6a704cafb2c9e0b7616a7aef89da4
Edmar Moretti <edmar.moretti@gmail.com>
2016-05-04 20:19:59 -0300

    Complementação dos textos da página inicial

--- init/nindex.php
+++ init/nindex.php
@@ -100,7 +100,7 @@ define ( "ONDEI3GEO", ".." );
 					<li><a href="#"><div class="fa" id="bandeiras"></div></a></li>
 				</ul>
 				<!-- template para permitir a traducao -->
-				<div id="menuTpl">
+				<div id="menuTpl" class="hidden">
 					<ul class="nav navbar-nav">
 						<li><a href="#map-o">{{{mapas}}}</a></li>
 					</ul>
@@ -125,8 +125,8 @@ define ( "ONDEI3GEO", ".." );
 	</nav>
 	<div class="container-fluid hidden">
 		<div class="row">
-			<div class="col-sm-12" id="mensagemLogin"></div>
-			<div class="col-sm-12">
+			<div class="col-xs-12" id="mensagemLogin"></div>
+			<div class="col-xs-12">
 				<div class="jumbotron" id="jumbotron">
 					<!-- Template pare mensagem do i3Geo -->
 					<ul class="list-inline">
@@ -141,10 +141,10 @@ define ( "ONDEI3GEO", ".." );
 		</div>
 	</div>
 	<div class="container">
-		<div class="row">
+		<div class="row center-block">
 			<!-- Template para criacao dos quadros ver index.js -->
 			<div id="botoesTpl" class="hidden">
-				<div id="{{{fa}}}" class="col-sm-12" style="width: 260px; min-width: 260px; max-width: 260px;">
+				<div id="{{{fa}}}" class="col-xs-12 center-block" style="width: 260px; min-width: 260px; max-width: 260px;">
 					<div class="panel panel-default">
 						<div class="panel-body" style="height: 250px;">
 							<div class="thumbnail" style="height: 90px;">
@@ -164,14 +164,14 @@ define ( "ONDEI3GEO", ".." );
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-12 hidden" id="tt" style="width: 260px; min-width: 260px; max-width: 260px;">
+			<div class="center-block col-xs-12 hidden" id="tt" style="width: 260px; min-width: 260px; max-width: 260px;">
 				<div class="panel panel-default">
 					<div class="panel-body">
 						<a class="twitter-timeline" href="https://twitter.com/i3geo" data-widget-id="288061915689787392" height="300">Tweets @i3Geo</a>
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-12 hidden" style="width: 260px; min-width: 260px; max-width: 260px;">
+			<div class="center-block col-xs-12 hidden" style="width: 260px; min-width: 260px; max-width: 260px;">
 				<div class="panel panel-default">
 					<div class="panel-body">
 						<a class="twitter-timeline" href="https://twitter.com/hashtag/i3geo" data-widget-id="643417277208133633" height="300">i3geo Tweets</a>
@@ -180,21 +180,21 @@ define ( "ONDEI3GEO", ".." );
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-xs-12 text-center">
 				<script type="text/javascript" src="http://www.openhub.net/p/150688/widgets/project_basic_stats.js"></script>
 			</div>
 		</div>
 
 		<hr />
 
-		<footer class="text-center">
-			<div class="row hidden">
-				<div class="col-sm-6"
+		<footer>
+			<div class="row text-center hidden">
+				<div class="col-xs-6 center-block"
 
 					<a rel="license" href="http://creativecommons.org/licenses/GPL/2.0/legalcode.pt" target="_blank"><img alt="Licen&ccedil;a Creative Commons" style="border-width: 0" src="https://i.creativecommons.org/l/GPL/2.0/88x62.png" /></a><br />O i3Geo est&aacute; licenciado com uma Licen&ccedil;a <a
 						rel="license" href="http://creativecommons.org/licenses/GPL/2.0/legalcode.pt" target="_blank">Creative Commons - Licen&ccedil;a P&uacute;blica Geral GNU (&#34;GNU General Public License&#34;)</a>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-xs-6 center-block">
 					<script type='text/javascript' src='https://www.openhub.net/p/i3geo/widgets/project_users?format=js&style=blue'></script>
 				</div>
 			</div>
@@ -210,7 +210,7 @@ define ( "ONDEI3GEO", ".." );
 	?>
     <script>
     $(document).ready(function(){
-		var template = '<div class="col-sm-12" style="width:260px;min-width:260px;max-width:260px;"><div class="panel panel-default"><div class="panel-body" style="height: 250px;"><div class="thumbnail" style="height:90px;"><img class="img-rounded" style="height: 100%; width: 100%" src="imagens/{{{img}}}" /></div><h4>{{{titulo}}}</h4><div style="overflow:auto;height:200px;"><p>{{{subtitulo}}}</p></div></div><div class="panel-footer text-right" style="border:0px;background-color:white;"><p><a class="btn btn-primary btn-raised" href="{{{href}}}" role="button" target="_blank" >Abrir <i class="fa fa-{{{fa}}}" aria-hidden="true" ></i></a></p></div></div></div>';
+		var template = '<div class="col-xs-12" style="width:260px;min-width:260px;max-width:260px;"><div class="panel panel-default"><div class="panel-body" style="height: 250px;"><div class="thumbnail" style="height:90px;"><img class="img-rounded" style="height: 100%; width: 100%" src="imagens/{{{img}}}" /></div><h4>{{{titulo}}}</h4><div style="overflow:auto;height:200px;"><p>{{{subtitulo}}}</p></div></div><div class="panel-footer text-right" style="border:0px;background-color:white;"><p><a class="btn btn-primary btn-raised" href="{{{href}}}" role="button" target="_blank" >Abrir <i class="fa fa-{{{fa}}}" aria-hidden="true" ></i></a></p></div></div></div>';
 
     	<?php
 			if ($i3geomaster [0] ["usuario"] == "admin" && $i3geomaster [0] ["senha"] == "admin") {
