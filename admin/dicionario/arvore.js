80649700fcca98f84bf663e68dc100751d14a001
Abraham Coiman <abrahamcoiman@gmail.com>
2016-11-14 00:47:58 -0200

    Update 'arvoreTemas', 'txtAjuda'

--- admin/dicionario/arvore.js
+++ admin/dicionario/arvore.js
@@ -3,8 +3,8 @@ i3GEOadmin.arvore = {};
 i3GEOadmin.arvore.dicionario = {
 	'arvoreTemas' : [ {
 		pt : "Administra&ccedil;&atilde;o do i3geo - &Aacute;rvore de temas ",
-		en : "",
-		es : ""
+		en : "i3geo management - Theme catalog ",
+		es : "Administraci&oacute;n de i3geo - &Aacute;rbol de temas "
 	} ],
     'ajuda' : [ {
 		pt : "Ajuda",
@@ -13,8 +13,8 @@ i3GEOadmin.arvore.dicionario = {
 	} ],
     'txtAjuda' : [ {
 		pt : "<p>Este formul&aacute;rio permite editar &aacute;rvore de temas que ser&aacute;utilizada pelo i3Geo.<p>A &aacute;rvore &eacute; composta de menus, grupos, sub-grupos e temas. Vcdeve criar os menus, a lista de grupos e a lista de sub-grupos. Aocriar um menu, esse &eacute; mostrado imediatamente na &aacute;rvore.<p>Menus ou n&oacute;s da &aacute;rvore marcados como n&atilde;o publicados s&atilde;omostrados em vermelho e aparecem apenas para os administradores.<p>Em cada n&oacute; da &aacute;rvore podem ser associados os temas, mesmo naraiz de um dos n&iacute;veis. Os temas s&atilde;o arquivos mapfiles armazenadosem i3geo/temas e registrados no sistema de administra&ccedil;&atilde;o.<p>A cria&ccedil;&atilde;o de mapfiles e a vincula&ccedil;&atilde;o tema-mapfile &eacute; feitapor meio do editor de mapfiles, e consiste na determina&ccedil;&atilde;o de umnome para cada mapfile. Esse nome &eacute; listado ao ser adicionado umtema na &aacute;rvore.",
-		en : "",
-		es : ""
+		en : "<p>This form allows you to edit the theme catalog that will be used by i3Geo.<p>A catalog is made up of menus, groups, subgroups and themes. When you create a menu it is immediately shown on the catalog.<p>Those menus that are labeled as not published on the catalog are shown in red and will be available to developers only.<p>It is possible to associate themes on each catalog node as well as on the root of a given level. Themes are mapfiles stored into i3geo/themes and recorded into the management system.<p>Mapfiles building and theme-mapfile linking are made by the mapfile editor. This involves putting a mapfile name. This name will be shown into the catalog theme list.",
+		es : "<p>Este formulario permite editar el &aacute;rbol de temas que ser&aacute; utilizado por i3Geo.<p>Un &aacute;rbol est&aacute; compuesto de men&uacute;s, grupos, subgrupos y temas. Usted debe crear los men&uacute;s, una lista de grupos y una lista de subgrupos. Cuando se crea un men&uacute; es mostrado inmediatamente en el &aacute;rbol.<p>Los men&uacute;s que son marcados como no publicados en el &aacute;rbol son mostradas en rojo y s&oacute;lo est&aacute;n disponibles para los administradores.<p>Se pueden asociar temas en cada nodo del &aacute;rbol, as&iacute; como en la ra&iacute;z de uno de los niveles. Los temas son archivos mapfiles almacenados en i3geo/temas y registrados en el sistema de administraci&oacute;n.<p>La creaci&oacute;n de mapfiles y la vinculaci&oacute;n tema-mapfiles es hecha a trav&eacute;s del editor mapfiles, y consiste en la colocaci&oacute;n de un nombre a cada mapfile. Este nombre aparecer&aacute; en una lista de temas en el &aacute;rbol."
 	} ],
     'txtCria' : [ {
 		pt : "<p class=paragrafo>Utilize os bot&otilde;es para criar novos nomes de menus, grupos ou sub-grupos, formando listas de itens.<br>Os itens criados podem ser reaproveitados em v&aacute;rios n&oacute;s da &aacute;rvore.</p>",
