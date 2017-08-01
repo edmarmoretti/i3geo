209a9e98123b363cb5a42af454986a0267a12202
edmarmoretti <edmar.moretti@gmail.com>
2017-08-01 18:49:41 -0300

    Adaptação da ferramenta Conexão do catálogo de temas

--- ferramentas/conectarservicos/index.js
+++ ferramentas/conectarservicos/index.js
@@ -6,187 +6,163 @@ if (typeof (i3GEOF) === 'undefined') {
  */
 i3GEOF.conectarservicos =
 {
-	/**
-	 * Template no formato mustache. E preenchido na carga do javascript com o programa dependencias.php
-	 */
-	MUSTACHE : "",
-	/**
-	 * Susbtitutos para o template
-	 */
-	mustacheHash : function() {
-		var dicionario = i3GEO.idioma.objetoIdioma(i3GEOF.conectarservicos.dicionario);
-		dicionario["locaplic"] = i3GEO.configura.locaplic;
-		return dicionario;
-	},
-	/*
-	 * Function: iniciaDicionario (Depreciado na versao 6.0)
-	 *
-	 */
-	iniciaDicionario : function() {
-		i3GEOF.conectarservicos.iniciaJanelaFlutuante();
-	},
-	/*
-	 * Function: inicia
-	 *
-	 * Inicia a ferramenta. &Eacute; chamado por criaJanelaFlutuante
-	 *
-	 * Parametro:
-	 *
-	 * iddiv {String} - id do div que receber&aacute; o conteudo HTML da ferramenta
-	 */
-	inicia : function(iddiv) {
-		if(i3GEOF.conectarservicos.MUSTACHE == ""){
-			$.get(i3GEO.configura.locaplic + "/ferramentas/conectarservicos/template_mst.html", function(template) {
-				i3GEOF.conectarservicos.MUSTACHE = template;
-				i3GEOF.conectarservicos.inicia(iddiv);
-			});
-			return;
-		}
-		$i(iddiv).innerHTML = i3GEOF.conectarservicos.html();
-		new YAHOO.widget.Button("i3GEOFconectarservicosKml", {
-			onclick : {
-				fn : function() {
-					i3GEO.util.scriptTag(i3GEO.configura.locaplic
-						+ "/ferramentas/carregakml/dependencias.php",
-						"i3GEOF.carregakml.iniciaJanelaFlutuante()",
-					"i3GEOF.carregakml_script");
-				}
+		/**
+		 * Template no formato mustache. E preenchido na carga do javascript com o programa dependencias.php
+		 */
+		MUSTACHE : "",
+		/**
+		 * Susbtitutos para o template
+		 */
+		mustacheHash : function() {
+			var dicionario = i3GEO.idioma.objetoIdioma(i3GEOF.conectarservicos.dicionario);
+			dicionario["locaplic"] = i3GEO.configura.locaplic;
+			return dicionario;
+		},
+		/*
+		 * Function: iniciaDicionario (Depreciado na versao 6.0)
+		 *
+		 */
+		iniciaDicionario : function() {
+			i3GEOF.conectarservicos.iniciaJanelaFlutuante();
+		},
+		/*
+		 * Function: inicia
+		 *
+		 * Inicia a ferramenta. &Eacute; chamado por criaJanelaFlutuante
+		 *
+		 * Parametro:
+		 *
+		 * iddiv {String} - id do div que receber&aacute; o conteudo HTML da ferramenta
+		 */
+		inicia : function(iddiv) {
+			if(i3GEOF.conectarservicos.MUSTACHE == ""){
+				$.get(i3GEO.configura.locaplic + "/ferramentas/conectarservicos/template_mst.html", function(template) {
+					i3GEOF.conectarservicos.MUSTACHE = template;
+					i3GEOF.conectarservicos.inicia(iddiv);
+				});
+				return;
 			}
-		});
-		new YAHOO.widget.Button("i3GEOFconectarservicosWms", {
-			onclick : {
-				fn : function() {
-					i3GEO.janela
-					.cria(
-						"400px",
-						"300px",
-						i3GEO.configura.locaplic
-						+ "/ferramentas/conectarwms/index.htm",
-						"",
-						"",
-
-						+ "</div><a class='i3GeoTituloJanelaBs' target=_blank href='"
-						+ i3GEO.configura.locaplic
-						+ "/ajuda_usuario.php?idcategoria=4&idajuda=28' >" + $trad("a4") + "</a>",
-						"i3GEO.conectarwms",
-						false,
-						"hd",
-						"",
-						"",
-						"",
-						true
-					);
-				}
-			}
-		});
-		new YAHOO.widget.Button("i3GEOFconectarservicosWmst", {
-			onclick : {
-				fn : function() {
-					i3GEO.janela
-					.cria(
-						"600px",
-						"400px",
-						i3GEO.configura.locaplic
-						+ "/ferramentas/wmstime/index.htm",
-						"",
-						"",
-						$trad("x46")
-						+ "&nbsp;<a class=ajuda_usuario target=_blank href='"
-						+ i3GEO.configura.locaplic
-						+ "/ajuda_usuario.php?idcategoria=4&idajuda=76' ><b> </b></a>",
-						"i3GEO.conectarwmst",
-						false,
-						"hd",
-						"",
-						"",
-						"",
-						true
-					);
-				}
-			}
-		});
-		new YAHOO.widget.Button("i3GEOFconectarservicosGeorss", {
-			onclick : {
-				fn : function() {
-					i3GEO.janela
-					.cria(
-						"400px",
-						"300px",
-						i3GEO.configura.locaplic
-						+ "/ferramentas/conectargeorss/index.htm",
-						"",
-						"",
-						$trad("x47")
-						+ "&nbsp;<a class=ajuda_usuario target=_blank href='"
-						+ i3GEO.configura.locaplic
-						+ "/ajuda_usuario.php?idcategoria=4&idajuda=29' ><b> </b></a>",
-						"i3GEO.conectargeorss",
-						false,
-						"hd",
-						"",
-						"",
-						"",
-						true
-					);
-				}
-			}
-		});
-		new YAHOO.widget.Button("i3GEOFconectarservicosGeojson", {
-			onclick : {
-				fn : function() {
-					i3GEO.util.scriptTag(i3GEO.configura.locaplic
-						+ "/ferramentas/conectargeojson/dependencias.php",
-						"i3GEOF.conectargeojson.iniciaJanelaFlutuante()",
-					"i3GEOF.conectargeojson_script");
-				}
-			}
-		});
-	},
-	/*
-	 * Function: html
-	 *
-	 * Gera o c&oacute;digo html para apresenta&ccedil;&atilde;o das op&ccedil;&otilde;es da ferramenta
-	 *
-	 * Retorno:
-	 *
-	 * String com o c&oacute;digo html
-	 */
-	html : function() {
-		var ins = Mustache.render(i3GEOF.conectarservicos.MUSTACHE, i3GEOF.conectarservicos.mustacheHash());
-		return ins;
-	},
-	/*
-	 * Function: criaJanelaFlutuante
-	 *
-	 * Cria a janela flutuante para controle da ferramenta.
-	 */
-	iniciaJanelaFlutuante : function() {
-		var cabecalho, janela, divid, titulo;
-		if($i("i3GEOF.conectarservicos")){
-			return;
-		}
-		cabecalho = function() {
-		};
-		// cria a janela flutuante
-		titulo = "</div><div class='i3GeoTituloJanelaBs'>" + $trad("conexao",i3GEOF.conectarservicos.dicionario)+"</div>";
-		janela =
+			$i(iddiv).innerHTML = i3GEOF.conectarservicos.html();
+		},
+		geojson: function(){
+			i3GEO.util.scriptTag(i3GEO.configura.locaplic
+					+ "/ferramentas/conectargeojson/dependencias.php",
+					"i3GEOF.conectargeojson.iniciaJanelaFlutuante()",
+			"i3GEOF.conectargeojson_script");
+		},
+		georss: function(){
 			i3GEO.janela.cria(
-				"230px",
-				"180px",
-				"",
-				"",
-				"",
-				titulo,
-				"i3GEOF.conectarservicos",
-				false,
-				"hd",
-				cabecalho,
-				"",
-				"",
-				true
+					"400px",
+					"300px",
+					i3GEO.configura.locaplic
+					+ "/ferramentas/conectargeorss/index.htm",
+					"",
+					"",
+					$trad("x47")
+					+ "&nbsp;<a class=ajuda_usuario target=_blank href='"
+					+ i3GEO.configura.locaplic
+					+ "/ajuda_usuario.php?idcategoria=4&idajuda=29' ><b> </b></a>",
+					"i3GEO.conectargeorss",
+					false,
+					"hd",
+					"",
+					"",
+					"",
+					true
 			);
-		divid = janela[2].id;
-		$i("i3GEOF.conectarservicos_corpo").style.backgroundColor = "white";
-		i3GEOF.conectarservicos.inicia(divid);
-	}
+		},
+		wmst: function(){
+			i3GEO.janela.cria(
+					"600px",
+					"400px",
+					i3GEO.configura.locaplic
+					+ "/ferramentas/wmstime/index.htm",
+					"",
+					"",
+					$trad("x46")
+					+ "&nbsp;<a class=ajuda_usuario target=_blank href='"
+					+ i3GEO.configura.locaplic
+					+ "/ajuda_usuario.php?idcategoria=4&idajuda=76' ><b> </b></a>",
+					"i3GEO.conectarwmst",
+					false,
+					"hd",
+					"",
+					"",
+					"",
+					true
+			);
+		},
+		wms: function(){
+			i3GEO.janela.cria(
+					"400px",
+					"300px",
+					i3GEO.configura.locaplic
+					+ "/ferramentas/conectarwms/index.htm",
+					"",
+					"",
+					"</div><a class='i3GeoTituloJanelaBs' target=_blank href='"
+					+ i3GEO.configura.locaplic
+					+ "/ajuda_usuario.php?idcategoria=4&idajuda=28' >" + $trad("a4") + "</a>",
+					"i3GEO.conectarwms",
+					false,
+					"hd",
+					"",
+					"",
+					"",
+					true
+			);
+		},
+		kml: function() {
+			i3GEO.util.scriptTag(i3GEO.configura.locaplic
+					+ "/ferramentas/carregakml/dependencias.php",
+					"i3GEOF.carregakml.iniciaJanelaFlutuante()",
+			"i3GEOF.carregakml_script");
+		},
+		/*
+		 * Function: html
+		 *
+		 * Gera o c&oacute;digo html para apresenta&ccedil;&atilde;o das op&ccedil;&otilde;es da ferramenta
+		 *
+		 * Retorno:
+		 *
+		 * String com o c&oacute;digo html
+		 */
+		html : function() {
+			var ins = Mustache.render(i3GEOF.conectarservicos.MUSTACHE, i3GEOF.conectarservicos.mustacheHash());
+			return ins;
+		},
+		/*
+		 * Function: criaJanelaFlutuante
+		 *
+		 * Cria a janela flutuante para controle da ferramenta.
+		 */
+		iniciaJanelaFlutuante : function() {
+			var cabecalho, janela, divid, titulo;
+			if($i("i3GEOF.conectarservicos")){
+				return;
+			}
+			cabecalho = function() {
+			};
+			// cria a janela flutuante
+			titulo = "</div><div class='i3GeoTituloJanelaBs'>" + $trad("conexao",i3GEOF.conectarservicos.dicionario)+"</div>";
+			janela =
+				i3GEO.janela.cria(
+						"230px",
+						"270px",
+						"",
+						"",
+						"",
+						titulo,
+						"i3GEOF.conectarservicos",
+						false,
+						"hd",
+						cabecalho,
+						"",
+						"",
+						true
+				);
+			divid = janela[2].id;
+			$i("i3GEOF.conectarservicos_corpo").style.backgroundColor = "white";
+			i3GEOF.conectarservicos.inicia(divid);
+		}
 };
