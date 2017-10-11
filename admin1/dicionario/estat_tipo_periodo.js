de488d1961ec9a5b07b21ff1f4117540e7c20a14
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-10 23:27:49 -0300

    Tradução de estat_tipo_periodo.js

--- admin1/dicionario/estat_tipo_periodo.js
+++ admin1/dicionario/estat_tipo_periodo.js
@@ -1,12 +1,12 @@
 i3GEOadmin.periodos.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Cadastro dos tipos de per&iacute;odos de tempo",
-		en : "",
-		es : ""
+		en : "Registration of time period types",
+		es : "Registro de los tipos de per&iacute;odos de tiempo"
 	} ],
     'txtDesc' : [ {
 		pt : "Tipos de per&iacute;odos que podem ser escolhidos nos formul&aacute;rios de defini&ccedil;&atilde;o das vari&aacute;veis ",
-		en : "",
-		es : ""
+		en : "Types of periods that can be chosen in the variable definition forms",
+		es : "Tipos de per&iacute;odos que se pueden elegir en los formularios de definici&oacute;n de variables"
 	} ]
 };
