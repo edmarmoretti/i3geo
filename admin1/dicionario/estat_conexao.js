db6be4c87ffab0d1c514a4321a710eb42ac289a9
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-10 21:50:13 -0300

    Tradução estat_conexao.js

--- admin1/dicionario/estat_conexao.js
+++ admin1/dicionario/estat_conexao.js
@@ -1,37 +1,37 @@
 i3GEOadmin.conexoes.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Cadastro de conex&otilde;es com bancos de dados",
-		en : "",
-		es : ""
+		en : "Database connection logging",
+		es : "Registro de conexiones con bases de datos"
 	} ],
     'txtDesc' : [ {
 		pt : "Aqui s&atilde;o cadastradas as conex&otilde;es com os bancos de dados que ser&atilde;o utilizados pelo sistema de metadados estat&iacute;sticos. Por quest&otilde;es de seguran&ccedil;a, a senha de conex&atilde;o n&atilde;o &eacute; mostrada. Para alterar a senha &eacute; necess&aacute;rio editar diretamente o banco de dados de administra&ccedil;&atilde;o do i3Geo ou o arquivo <samp>ms_configura.php</samp>.",
-		en : "",
-		es : ""
+		en : "Here you can register connections into databases that will be used by the statistical metadata system. For security reasons, the connection password is not displayed. To change this password, it is necessary to directly edit the administration database of i3Geo or the <samp>ms_configura.php</samp> file.",
+		es : "Aqu&iacute; se registran las conexiones con las bases de datos que ser&aacute;n utilizadas por el sistema de metadatos estad&iacute;sticos. Por razones de seguridad, no se muestra la contrase&ntilde;a de conexi&oacute;n. Para cambiar la contrase&ntilde;a es necesario editar directamente la base de datos de administraci&oacute;n de i3Geo en el archivo <samp>ms_configura.php</samp>."
 	} ],
     'txtAjuda' : [ {
 		pt : "Os par&acirc;metros de algumas das conex&otilde;es s&atilde;o definidas diretamente no arquivo de configura&ccedil;&atilde;o do i3Geo (ms_configura.php) e outras podem ser registradas no banco de dados de administra&ccedil;&atilde;o. No primeiro caso, n&atilde;o &eacute; poss&iacute;vel editar os par&acirc;metros por esse formul&aacute;rio. Por quest&otilde;es de seguran&ccedil;a, a senha de conex&atilde;o n&atilde;o &eacute; mostrada. Para alterar a senha &eacute; necess&aacute;rio editar diretamente o banco de dados de administra&ccedil;&atilde;o do i3Geo ou o arquivo ms_configura.php.",
-		en : "",
-		es : ""
+		en : "Some connection parameters are defined directly in the configuration file of i3Geo (ms_configura.php). Other parameters can be registered into the administration database. In the former case, you cannot edit the parameters using this form. For security reasons, the connection password is not displayed. To change this password, it is necessary to directly edit the administration database of i3Geo or the ms_configura.php file",
+		es : "Los par&aacute;metros de algunas de las conexiones se definen directamente en el archivo de configuraci&oacute;n de i3Geo (ms_configura.php) y otros se pueden registrar en la base de datos de administraci&oacute;n. En el primer caso, no se puede editar los par&aacute;metros a trav&eacute;s de este formulario. Por razones de seguridad, no se muestra la contrase&ntilde;a de conexi&oacute;n. Para cambiar la contrase&ntilde;a es necesario editar directamente la base de datos de administraci&oacute;n de i3Geo o el archivo ms_configura.php."
 	} ],
     'bancodedadosTxt' : [ {
 		pt : "Nome do banco de dados",
-		en : "",
-		es : ""
+		en : "Database name",
+		es : "Nombre de la base de datos"
 	} ],
     'hostTxt' : [ {
 		pt : "Endere&ccedil;o do servidor",
-		en : "",
-		es : ""
+		en : "Server address",
+		es : "Direcci&oacute;n del servidor"
 	} ],
     'portaTxt' : [ {
 		pt : "Porta",
-		en : "",
-		es : ""
+		en : "Door",
+		es : "Puerta"
 	} ],
     'usuarioTxt' : [ {
 		pt : "Usu&aacute;rio",
-		en : "",
-		es : ""
+		en : "User",
+		es : "Usuario"
 	} ]
 };
