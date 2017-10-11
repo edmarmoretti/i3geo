1ff12d6b6f9fbe4508a9ae7746e4c8b27301dc36
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-10 23:15:24 -0300

    Traduçaõ estat_fontes.js

--- admin1/dicionario/estat_fontes.js
+++ admin1/dicionario/estat_fontes.js
@@ -1,12 +1,12 @@
 i3GEOadmin.fontes.dicionario = {
 	'txtTitulo' : [ {
-		pt : "Cadastro de links para fontes de informações",
-		en : "",
-		es : ""
+		pt : "Cadastro de links para fontes de informa&ccedil;&otilde;es",
+		en : "Registration of links to information sources",
+		es : "Registro de enlaces a fuentes de informaci&oacute;n"
 	} ],
     'txtDesc' : [ {
 		pt : "Links para as fontes originais utilizadas nas vari&aacute;veis",
-		en : "",
-		es : ""
+		en : "Links to original sources used in the variables",
+		es : "Enlaces para las fuentes originales utilizadas en las variables"
 	} ]
 };
