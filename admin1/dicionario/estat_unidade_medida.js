d22e9cb85d1df287910fab2f20f195f7e1bf17e1
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-12 17:04:36 -0300

    Tradução de estat_unidade_medida.js

--- admin1/dicionario/estat_unidade_medida.js
+++ admin1/dicionario/estat_unidade_medida.js
@@ -1,27 +1,27 @@
 i3GEOadmin.unidades.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Cadastro de unidades de medida",
-		en : "",
-		es : ""
+		en : "Regiser of measure units",
+		es : "Registro de unidades de medida"
 	} ],
     'txtDesc' : [ {
 		pt : "Tipos de unidades de medida que podem ser escolhidos nos formul&aacute;rios de defini&ccedil;&atilde;o das vari&aacute;veis",
-		en : "",
-		es : ""
+		en : "Types of measure units that can be chosen from the form for variable definition",
+		es : "Tipos de unidades de medida que pueden ser seleccionados en los formularios de definici&oacute;n de variables"
 	} ],
     'siglaTxt' : [ {
 		pt : "Sigla",
-		en : "",
-		es : ""
+		en : "Abbreviation",
+		es : "Sigla"
 	} ],
     'permitesomaTxt' : [ {
 		pt : "Permite somar os valores",
-		en : "",
-		es : ""
+		en : "Allow sum of values",
+		es : "Permite sumar los valores"
 	} ],
     'permitemediaTxt' : [ {
 		pt : "Permite calcular m&eacute;dia",
-		en : "",
-		es : ""
+		en : "Allow to calculate average",
+		es : "Permite calcular el promedio"
 	} ]
 };
