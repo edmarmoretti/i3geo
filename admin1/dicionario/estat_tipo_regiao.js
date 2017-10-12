e3736016de4a33affe27845188c284376c9ef57d
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-12 00:23:42 -0300

    Tradução de estat_tipo_regiao.js

--- admin1/dicionario/estat_tipo_regiao.js
+++ admin1/dicionario/estat_tipo_regiao.js
@@ -1,107 +1,107 @@
 i3GEOadmin.regioes.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Cadastro de regi&otilde;es geogr&aacute;ficas",
-		en : "",
-		es : ""
+		en : "Register of geographical regions",
+		es : "Registro de regiones geogr&aacute;ficas"
 	} ],
     'txtAjuda' : [ {
 		pt : "As regi&otilde;es s&atilde;o utilizadas para espacializar os dados estat&iacute;sticos. Ao definir uma medida de vari&aacute;vel, deve-se escolher qual a regi&atilde;o que ser&aacute; utilizada para a representa&ccedil;&atilde;o dos dados, ou seja, qual a unidade geogr&aacute;fica correspondente aos dados. Regi&otilde;es podem ter tamb&eacute;m uma hierarquia, que permite agregar dados, por exemplo, uma regi&atilde;o denominada 'munic&iacute;pios' pode ser agregada por outra regi&atilde;o denominada 'estados'. Na pr&aacute;tica, uma regi&atilde;o &eacute; uma tabela no banco de dados que cont&eacute;m uma coluna com a geometria correspondente.",
-		en : "",
-		es : ""
+		en : "Regions are used to spatialize statistical data. When defining a variable measure, you must choose which region will be used to represent the data, that is, which geographical unit corresponds to the data. Regions can also have a hierarchy, which allows you to aggregate data, for example, a region called 'counties' can be aggregated by another region called 'states'. In practice, a region is a table in the database that contains a geometry column.",
+		es : "Las regiones se utilizan para espacializar los datos estad&iacute;sticos. Al definir una medida de variable, se debe elegir qu&eacute; regi&oacute;n se utilizar&aacute; para la representaci&oacute;n de los datos, es decir, cu&aacute;l es la unidad geogr&aacute;fica correspondiente a los datos. Las regiones pueden tener tambi&eacute;n una jerarqu&iacute;a que permite agregar datos, por ejemplo, una regi&oacute;n denominada 'municipios' puede ser agregada por otra regi&oacute;n denominada 'estados'. En la pr&aacute;ctica, una regi&oacute;n es una tabla en la base de datos que contiene una columna con la geometr&iacute;a correspondiente."
 	} ],
     'txtHierarquiaAjuda' : [ {
 		pt : "Regi&otilde;es podem ser agregadas em fun&ccedil;&atilde;o de outra regi&atilde;o de hierarquia superior. Munic&iacute;pios, por exemplo, podem ser agregados em unidades da federa&ccedil;&atilde;o.",
-		en : "",
-		es : ""
+		en : "Regions can be aggregated according to another region of higher hierarchy.For example, counties can be aggregated into states.",
+		es : "Las regiones pueden ser agregadas en funci&oacute;n de otra regi&oacute;n de jerarqu&iacute;a superior. Por ejemplo, los municipios pueden ser agregados en unidades de federaci&oacute;n (estados)."
 	} ],
     'txtDesc' : [ {
-		pt : "Unidades geográficas utilizadas para espacializar os dados estatísticos. Regiões podem compor hierarquias que permitem agregações.",
-		en : "",
-		es : ""
+		pt : "Unidades geogr&aacute;ficas utilizadas para espacializar os dados estat&iacute;sticos. Regi&otilde;es podem compor hierarquias que permitem agrega&ccedil;&otilde;es.",
+		en : "Geographic units used to spatialize statistical data. Regions can compose hierarchies that allow aggregations.",
+		es : "Unidades geogr&aacute;ficas utilizadas para espacializar los datos estad&iacute;sticos. Las regiones pueden componer jerarqu&iacute;as que permiten las agregaciones."
 	} ],
     'txtHierarquiaDesc' : [ {
 		pt : "Cadastro das rela&ccedil;&otilde;es de hierarquia entre tipos de regi&atilde;o. A regi&atilde;o em foco pode ter diferentes regi&otilde;es de n&iacute;vel superior (pai)",
-		en : "",
-		es : ""
+		en : "Register of  hierarchy relationships between region types. A region into focus may have different top-level regions (parent)",
+		es : "Registro de las relaciones de jerarqu&iacute;a entre tipos de regi&oacute;n. La regi&oacute;n en foco puede tener diferentes regiones de nivel superior (padre)"
 	} ],
     'nome_tipo_regiaoTxt' : [ {
 		pt : "Nome do tipo de regi&atilde;o",
-		en : "",
-		es : ""
+		en : "Region type name",
+		es : "Nombre del tipo de regi&oacute;n"
 	} ],
     'descricao_tipo_regiaoTxt' : [ {
 		pt : "Descri&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Description",
+		es : "Descripci&oacute;n"
 	} ],
     'codigo_estat_conexaoTxt' : [ {
 		pt : "C&oacute;digo da conex&atilde;o com o banco de dados",
-		en : "",
-		es : ""
+		en : "Database connection code",
+		es : "C&oacute;digo de conexi&oacute;n con la base de datos"
 	} ],
     'esquemadbTxt' : [ {
 		pt : "Esquema do banco de dados onde est&aacute; a tabela com as regi&otilde;es",
-		en : "",
-		es : ""
+		en : "Database schema where the table with regions is located",
+		es : "Esquema de la base de datos donde est&aacute; la tabla con las regiones"
 	} ],
     'tabelaTxt' : [ {
 		pt : "Nome da tabela com as regi&otilde;es",
-		en : "",
-		es : ""
+		en : "Table name that contains regions",
+		es : "Nombre de la tabla con las regiones"
 	} ],
     'colunageoTxt' : [ {
 		pt : "Coluna que cont&eacute;m as geometrias",
-		en : "",
-		es : ""
+		en : "Column containing geometries",
+		es : "Columna que contiene las geometr&iacute;as"
 	} ],
     'dataTxt' : [ {
 		pt : "Data dos dados",
-		en : "",
-		es : ""
+		en : "Data date",
+		es : "Fecha de los datos"
 	} ],
     'identificadorTxt' : [ {
 		pt : "Coluna que cont&eacute;m os identificadores &uacute;nicos de cada regi&atilde;o",
-		en : "",
-		es : ""
+		en : "Column that contains unique identifiers for each region",
+		es : "Columna que contiene los identificadores &uacute;nicos de cada regi&oacute;n"
 	} ],
     'colunanomeregiaoTxt' : [ {
 		pt : "Coluna com os nomes de cada regi&atilde;o",
-		en : "",
-		es : ""
+		en : "Column containing the names of each region",
+		es : "Columna con los nombres de cada regi&oacute;n"
 	} ],
     'sridTxt' : [ {
 		pt : "C&oacute;digo SRID da proje&ccedil;&atilde;o cartogr&aacute;fica referente &agrave; coluna com a geometria",
-		en : "",
-		es : ""
+		en : "Spatial Reference Identifier (SRID) related to the geometry column",
+		es : "C&oacute;digo SRID de la proyecci&oacute;n cartogr&aacute;fica referente a la columna con la geometr&iacute;a"
 	} ],
     'colunacentroideTxt' : [ {
-		pt : "Coluna do tipo geometria que contém os centróides de cada região (pode ser a mesma coluna anterior)",
-		en : "",
-		es : ""
+		pt : "Coluna do tipo geometria que cont&eacute;m os centr&oacute;ides de cada regi&atilde;o (pode ser a mesma coluna anterior)",
+		en : "Column of type geometry that contains the centroids of each region (It can be the same previous column)",
+		es : "Columna del tipo geometr&iacute;a que contiene los centr&oacute;ides de cada regi&oacute;n (puede ser la misma columna anterior)"
 	} ],
     'colunasvisiveisTxt' : [ {
 		pt : "Colunas vis&iacute;veis, que ser&atilde;o mostradas aos usu&aacute;rios finais",
-		en : "",
-		es : ""
+		en : "Visible columns that will be shown to final users",
+		es : "Columnas visibles que se mostrar&aacute;n a los usuarios finales"
 	} ],
     'apelidosTxt' : [ {
 		pt : "Apelidos (alias) das colunas vis&iacute;veis (separe com v&iacute;rglas)",
-		en : "",
-		es : ""
+		en : "Aliases for visible columns (separate with commas)",
+		es : "Alias de las columnas visibles (separe con comas)"
 	} ],
     'hierarquia' : [ {
 		pt : "hierarquia",
-		en : "",
-		es : ""
+		en : "hierarchy",
+		es : "jerarqu&iacute;a"
 	} ],
     'nome_tipo_regiaoPaiTxt' : [ {
 		pt : "Regi&atilde;o que &eacute; superior a atual (regi&atilde;o pai)",
-		en : "",
-		es : ""
+		en : "Region that is higher than the current one (parent region)",
+		es : "Regi&oacute;n que es superior a la actual (regi&oacute;n padre)"
 	} ],
     'coluna_ligacaoTxt' : [ {
 		pt : "Coluna da regi&atilde;o atual que cont&eacute;m os c&oacute;digos da regi&atilde;o pai (coluna de liga&ccedil;&atilde;o)",
-		en : "",
-		es : ""
+		en : "Column of current region containing codes of parent region  (connection column)",
+		es : "Columna de la regi&oacute;n actual que contiene los c&oacute;digos de la regi&oacute;n principal (columna de conexi&oacute;n)"
 	} ]
 };
