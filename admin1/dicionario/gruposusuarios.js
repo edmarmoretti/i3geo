8ed9ae713f9c711bbb27b292d02731695233d087
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-14 14:25:58 -0300

    Tradução de gruposusuarios.js

--- admin1/dicionario/gruposusuarios.js
+++ admin1/dicionario/gruposusuarios.js
@@ -1,32 +1,32 @@
 i3GEOadmin.gruposusuarios.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Grupos de usu&aacute;rios",
-		en : "",
-		es : ""
+		en : "User groups",
+		es : "Grupos de usuarios"
 	} ],
     'txtDesc' : [ {
-		pt : "Cadastre aqui os grupos e adicione usuários a eles. Cada usu&aacute;rio pode pertencer a um ou mais grupos. Os grupos s&atilde;o utilizados na defini&ccedil;&atilde;o das permiss&otilde;es de usu&aacute;rios para acesso a dados ou mapas.",
-		en : "",
-		es : ""
+		pt : "Cadastre aqui os grupos e adicione usu&aacute;rios a eles. Cada usu&aacute;rio pode pertencer a um ou mais grupos. Os grupos s&atilde;o utilizados na defini&ccedil;&atilde;o das permiss&otilde;es de usu&aacute;rios para acesso a dados ou mapas.",
+		en : "Here you can register groups and add users to them. Each user can belong to one or more groups. Groups are used to set user permissions for data or map access.",
+		es : "Reg&iacute;strate aqu&iacute; los grupos y a&ntilde;ada usuarios a ellos. Cada usuario puede pertenecer a uno o m&aacute;s grupos. Los grupos se utilizan en la definici&oacute;n de permisos de usuario para el acceso a datos o mapas."
 	} ],
     'nome' : [ {
 		pt : "Nome:",
-		en : "",
-		es : ""
+		en : "Name:",
+		es : "Nombre:"
 	} ],
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o:",
-		en : "",
-		es : ""
+		en : "Description:",
+		es : "Descripci&oacute;n:"
 	} ],
     'usuariosv' : [ {
 		pt : "Usu&aacute;rios vinculados",
-		en : "",
-		es : ""
+		en : "Linked users",
+		es : "Usuarios vinculados"
 	} ],
     'grupo' : [ {
 		pt : "Grupo",
-		en : "",
-		es : ""
+		en : "Group",
+		es : "Grupo"
 	} ]
 };
