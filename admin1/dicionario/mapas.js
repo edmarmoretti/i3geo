a5a667639767d351a98785abf50435cc3f0a8c01
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-14 20:34:59 -0300

    Tradução de mapas.js

--- admin1/dicionario/mapas.js
+++ admin1/dicionario/mapas.js
@@ -1,87 +1,87 @@
 i3GEOadmin.mapas.dicionario = {
 	'txtDesc' : [ {
 		pt : "Mapas recebem um identificador &uacute;nico e usam URLs curtas para inicializa&ccedil;&atilde;o. Os mapas podem ter sido salvos (mapfile salvo no banco de dados) por meio da ferramenta 'salvar mapa' ou podem ser apenas par&acirc;metros definidos ao criar um novo mapa nas op&ccedil;&otilde;es abaixo. Na p&aacute;gina principal do i3Geo existe uma op&ccedil;&atilde;o que permite ao usu&aacute;rio abrir os mapas.",
-		en : "",
-		es : ""
+		en : "Maps have a unique identifier and use short URLs for startup. Maps may have been saved (mapfile saved in the database) through 'save map' option or they can be just parameters defined when creating a new map in the options below.",
+		es : "Los mapas reciben un identificador &uacute;nico y utilizan direcciones URL cortas para el inicio. Los mapas pueden haber sido guardados (mapfile guardado en la base de datos) a trav&eacute;s de la herramienta 'Guardar Mapa' o pueden ser solo par&aacute;metros definidos al crear un nuevo mapa en las siguientes opciones. En la p&aacute;gina principal de i3Geo existe una opci&oacute;n que permite al usuario abrir los mapas."
 	} ],
 	'txtTitulo' : [ {
 		pt : "Links para mapas",
-		en : "",
-		es : ""
+		en : "Links to maps",
+		es : "Enlaces a los mapas"
 	} ],
     'txtAjuda' : [ {
 		pt : "No formul&aacute;rio de edi&ccedil;&atilde;o de mapas voc&ecirc; pode definir os par&acirc;metros de um mapa, com temas, imagem etc. H&aacute; ainda a op&ccedil;&atilde;o de incluir um link direto para o mapa, o que vai desprezar todos os demais campos editados.",
-		en : "",
-		es : ""
+		en : "In the form for map editing you can define map parameters using themes, images, etc. There is also an option for including a direct link to the map, which will neglect all other edited fields.",
+		es : "En el formulario de edici&oacute;n de mapas usted puede definir los par&aacute;metros de un mapa, usando temas, im&aacute;genes, etc. Existe una opci&oacute;n para incluir un enlace directo al mapa que omite todos los dem&aacute;s campos editados."
 	} ],
     'ordem' : [ {
 		pt : "ordem",
-		en : "",
-		es : ""
+		en : "order",
+		es : "orden"
 	} ],
     'contemMapfile' : [ {
 		pt : "Contem mapfile embutido",
-		en : "",
-		es : ""
+		en : "It contains embedded mapfile",
+		es : "Contiene mapfile incrustado"
 	} ],
     'ordemMapa' : [ {
 		pt : "Ordem ao listar",
-		en : "",
-		es : ""
+		en : "Order when listing",
+		es : "Orden al hacer un listado"
 	} ],
     'nomeMapa' : [ {
 		pt : "Nome",
-		en : "",
-		es : ""
+		en : "Name",
+		es : "Nombre"
 	} ],
     'publicado' : [ {
 		pt : "Publicado?",
-		en : "",
-		es : ""
+		en : "Published?",
+		es : "&iquest;Publicado?"
 	} ],
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Description",
+		es : "Descripci&oacute;n"
 	} ],
     'extensao' : [ {
 		pt : "Extens&atilde;o geogr&aacute;fica",
-		en : "",
-		es : ""
+		en : "Geographic extent",
+		es : "Extensi&oacute;n geogr&aacute;fica"
 	} ],
     'img' : [ {
 		pt : "URL da imagem miniatura",
-		en : "",
-		es : ""
+		en : "Thumbnail image URL",
+		es : "URL de la imagen miniatura"
 	} ],
     'temas' : [ {
 		pt : "Temas que ser&atilde;o inclu&iacute;dos nesse mapa. Separe por espa&ccedil;os e escolha da lista ao lado.",
-		en : "",
-		es : ""
+		en : "Themes that will be included on this map. Separate with spaces and select from the list on the right side.",
+		es : "Temas que ser&aacute;n incluidos en este mapa. Separe con espacios y seleccione de la lista al lado."
 	} ],
     'temasLigados' : [ {
 		pt : "Temas que ser&atilde;o ligados. Devem constar na lista de temas inclu&iacute;dos",
-		en : "",
-		es : ""
+		en : "Themes that will be linked. They should be included in the list of included themes",
+		es : "Temas que ser&aacute;n vinculados. Deben figurar en la lista de temas incluidos"
 	} ],
     'perfis' : [ {
 		pt : "Perfis que podem ver este mapa. Separe por espa&ccedil;os e escolha da lista ao lado.",
-		en : "",
-		es : ""
+		en : "Profiles that can see this map. Separate with spaces and select from the list on the right side.",
+		es : "Perfiles que pueden ver este mapa. Separe con espacios y seleccione de la lista al lado."
 	} ],
     'parametros' : [ {
 		pt : "Outros par&acirc;metros (separe com '&')",
-		en : "",
-		es : ""
+		en : "Other parameters (separate with '&amp;')",
+		es : "Otros par&aacute;metros (separe con '&amp;')"
 	} ],
     'linkDireto' : [ {
 		pt : "Link direto para abertura do mapa (despreza os outros par&acirc;metros)",
-		en : "",
-		es : ""
+		en : "Direct link to open the map (do not take into account other parameters)",
+		es : "Enlace directo a la apertura del mapa (no toma en cuenta los otros par&aacute;metros)"
 	} ],
     'criaEditaMapa' : [ {
 		pt : "Voc&ecirc; pode tamb&eacute;m criar um novo mapa ou editar um existente utilizando a interface interativa. Essa op&ccedil;&atilde;o permite salvar o mapfile completo no banco de dados, dessa forma &eacute; poss&iacute;vel, por exemplo, alterar a legenda padr&atilde;o das camadas, adicionar camadas e servi&ccedil;os WMS, etc.",
-		en : "",
-		es : ""
+		en : "You can also create a new map or edit an existing one using the interactive interface. This option allows you to save a whole mapfile in the database, so you can change default layer legends, add layers and WMS services, etc.",
+		es : "Usted tambi&eacute;n puede crear un  mapa nuevo o editar uno existente utilizando la interfaz interactiva. Esta opci&oacute;n permite guardar el mapfile completo en la base de datos, en este sentido es posible, por ejemplo, cambiar la leyenda predeterminada de las capas, agregar capas y servicios WMS, etc."
 	} ]
 };
