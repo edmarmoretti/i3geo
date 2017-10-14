65932be545676565794865037fafe2a50421b6d6
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-14 14:08:39 -0300

    Tradução de grupos.js

--- admin1/dicionario/grupos.js
+++ admin1/dicionario/grupos.js
@@ -1,47 +1,47 @@
 i3GEOadmin.grupos.dicionario = {
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o (opcional):",
-		en : "",
-		es : ""
+		en : "Description (optional):",
+		es : "Descripci&oacute;n (opcional):"
 	} ],
     'temasRaizMenu' : [ {
 		pt : "Temas do menu",
-		en : "",
-		es : ""
+		en : "Menu themes",
+		es : "Temas del men&uacute;"
 	} ],
     'txtAjuda' : [ {
 		pt : "Aqui &eacute; poss&iacute;vel criar grupos de camadas ou inserir temas na raiz do menu.",
-		en : "",
-		es : ""
+		en : "Here you can create layer groups or insert themes into the root of menu.",
+		es : "Aqu&iacute; es posible crear grupos de capas o insertar temas en la ra&iacute;z del men&uacute;."
 	} ],
     'perfis' : [ {
 		pt : "Perfis que podem ver. Separe por espa&ccedil;os e escolha da lista ao lado.",
-		en : "",
-		es : ""
+		en : "Profiles that can see. Separate using spaces and choose from the side list",
+		es : "Perfiles que pueden ver. Separe con espacios y seleccione de la lista de al lado"
 	} ],
     'editarSubgrupos' : [ {
 		pt : "subgrupos",
-		en : "",
-		es : ""
+		en : "subgroups",
+		es : "subgrupos"
 	} ],
     'txtDesc' : [ {
 		pt : "Utilize as op&ccedil;&otilde;es abaixo para criar ou editar temas que ficar&atilde;o na raiz do menu e tamb&eacute;m para criar ou editar os grupos vinculados.",
-		en : "",
-		es : ""
+		en : "Use the options below to create or edit themes that will be at the root of the menu, and also to create or edit linked groups.",
+		es : "Utilice las siguientes opciones para crear o editar temas que se encuentren en la ra&iacute;z del men&uacute; y tambi&eacute;n para crear o editar los grupos vinculados."
 	} ],
     'txtGrupos' : [ {
 		pt : "Grupos vinculados a esse menu",
-		en : "",
-		es : ""
+		en : "Groups linked to this menu",
+		es : "Grupos vinculados a este men&uacute;"
 	} ],
     'txtListaDeGrupos' : [ {
 		pt : "Lista controlada de nomes de grupos",
-		en : "",
-		es : ""
+		en : "Controlled list of group names",
+		es : "Lista controlada de nombres de grupos"
 	} ],
     'descListaDeGrupos' : [ {
 		pt : "Os nomes de grupos s&atilde;o utilizados para nomear os n&oacute;s da &aacute;rvore do cat&aacute;logo. Cada nome recebe um identificador, de forma que ao ser modificado a &aacute;rvore refletir&aacute; as edi&ccedil;&otilde;es.",
-		en : "",
-		es : ""
+		en : "Group names are used to identify catalog tree nodes. Each name recieves an identifier, so that editions will be reflected in the modified tree",
+		es : "Los nombres de grupo son utilizados para nombrar los nodos del &aacute;rbol del cat&aacute;logo. Cada nombre recibe un identificador, de forma que al ser modificado el &aacute;rbol reflejar&aacute; las ediciones."
 	} ]
 };
