166cd72a223ec2b7126bef5142ae16885715c8ea
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-14 18:29:38 -0300

    Tradução de identifica.js

--- admin1/dicionario/identifica.js
+++ admin1/dicionario/identifica.js
@@ -1,37 +1,37 @@
 i3GEOadmin.identifica.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Sistemas de identifica&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Identification systems",
+		es : "Sistemas de identificaci&oacute;n"
 	} ],
     'txtDesc' : [ {
 		pt : "A ferramenta de identifica&ccedil;&atilde;o, utilizada nos mapas interativos, lista os dados para o ponto clicado pelo usu&aacute;rio. Os sistemas cadastrados aqui adicionam outras op&ccedil;&otilde;es na janela de identifica&ccedil;&atilde;o, permitindo buscas especiais, que complementam as op&ccedil;&otilde;es padr&atilde;o.",
- 		en : "",
-		es : ""
+ 		en : "The identification tool, used on interactive maps, lists data of a clicked point. Systems registered here add other options in the identification window, allowing for special searches, that complement the default options.",
+		es : "La herramienta de identificaci&oacute;n, utilizada en los mapas interactivos, pone en una lista los datos del punto sobre el cual el usuario hace clic. Los sistemas registrados aqu&iacute; agregan otras opciones en la ventana de identificaci&oacute;n, permitiendo b&uacute;squedas especiales, que complementan las opciones por defecto."
 	} ],
     'programa' : [ {
 		pt : "Programa",
-		en : "",
-		es : ""
+		en : "Program",
+		es : "Programa"
 	} ],
     'abreComo' : [ {
 		pt : "Abrir como?",
-		en : "",
-		es : ""
+		en : "Open as?",
+		es : "&iquest;Abrir como?"
 	} ],
     'programaDesc' : [ {
-		pt : "Ao clicar no mapa ser&aacute; aberto esse programa e adicionados os par&acirc;metros &x e &y no final do endere&ccedil;o",
-		en : "",
-		es : ""
+		pt : "Ao clicar no mapa ser&aacute; aberto esse programa e adicionados os par&acirc;metros &amp;x e &amp;y no final do endere&ccedil;o",
+		en : "When clicking on the map this program will start and &amp;x and &amp;y parameters will be added at the end of the address",
+		es : "Al hacer clic en el mapa se abrir&aacute; este programa y se a&ntilde;adir&aacute;n los par&aacute;metros &amp;x y &amp;y al final de la direcci&oacute;n"
 	} ],
     'mapa' : [ {
 		pt : "dentro do mapa",
-		en : "",
-		es : ""
+		en : "inside the map",
+		es : "dentro del mapa"
 	} ],
     'externo' : [ {
 		pt : "nova aba",
-		en : "",
-		es : ""
+		en : "new tab",
+		es : "pesta&ntilde;a nueva"
 	} ]
 };
