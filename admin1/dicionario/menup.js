9a2db2f8b03dcc248c5c78d17d44846479c83819
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-16 01:30:12 -0200

    Tradução de menup.js

--- admin1/dicionario/menup.js
+++ admin1/dicionario/menup.js
@@ -2,337 +2,337 @@ i3GEOadmin.menup = {};
 i3GEOadmin.menup.dicionario = {
 	'configGeral' : [ {
 		pt : "Ferramentas",
-		en : "Developer tools",
-		es : ""
+		en : "Tools",
+		es : "Herramientas"
 	} ],
 	'abre' : [ {
 		pt : "Abre o i3Geo",
-		en : "",
-		es : ""
+		en : "Open with i3geo",
+		es : "Abrir en i3geo"
 	} ],
 	'contAcesso' : [ {
 		pt : "Usu&aacute;rios",
-		en : "",
-		es : ""
+		en : "Users",
+		es : "Usuarios"
 	} ],
 	'testaInstal' : [ {
 		pt : "Testar a instala&ccedil;&atilde;o do i3geo (testainstal.php)",
-		en : "",
-		es : ""
+		en : "Test i3geo installation (testainstal.php)",
+		es : "Probar la instalaci&oacute;n de i3geo (testainstal.php)"
 	} ],
 	'removeInicia' : [ {
 		pt : "Remover a tela inicial de apresenta&ccedil;&atilde;o do i3Geo",
-		en : "",
-		es : ""
+		en : "Remove the splash screen of i3Geo",
+		es : "Quitar la pantalla de inicio de i3Geo"
 	} ],
 	'controleOperac' : [ {
 		pt : "Controle de opera&ccedil;&otilde;es",
-		en : "",
-		es : ""
+		en : "Operation control",
+		es : "Control de operaciones"
 	} ],
 	'cadastroUsuario' : [ {
 		pt : "Usu&aacute;rios e rela&ccedil;&atilde;o com pap&eacute;is",
-		en : "",
-		es : ""
+		en : "Users and relationship with roles",
+		es : "Usuarios y relaci&oacute;n con los roles"
 	} ],
 	'cadastroGrupos' : [ {
 		pt : "Grupos e rela&ccedil;&atilde;o com usu&aacute;rios",
-		en : "",
-		es : ""
+		en : "Groups and relationship with users",
+		es : "Grupos y relaci&oacute;n con los usuarios"
 	} ],
 	'metadadosEstatisticos' : [ {
 		pt : "Metaestat",
-		en : "",
-		es : ""
+		en : "Metaestat",
+		es : "Metaestat"
 	} ],
 	'documentacao' : [ {
 		pt : "Documenta&ccedil;&atilde;o (baseada no i3GeoSa&uacute;de)",
-		en : "",
-		es : ""
+		en : "Documentation (based on i3GeoSa&uacute;de)",
+		es : "Documentaci&oacute;n (basada en i3GeoSa&uacute;de)"
 	} ],
 	'cadastroVariav' : [ {
 		pt : "Vari&aacute;veis",
-		en : "",
-		es : ""
+		en : "Variables",
+		es : "Variables"
 	} ],
 	'cadastroMapas' : [ {
 		pt : "Mapas para publica&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Maps for publication",
+		es : "Mapas para publicaci&oacute;n"
 	} ],
 	'cadastroMedidas' : [ {
 		pt : "Unidades de medida",
-		en : "",
-		es : ""
+		en : "Units of measurement",
+		es : "Unidades de medida"
 	} ],
 	'cadastroPeriodos' : [ {
 		pt : "Tipos de per&iacute;odos de tempo",
-		en : "",
-		es : ""
+		en : "Type of time period",
+		es : "Tipo de per&iacute;odo de tiempo"
 	} ],
 	'cadastroConexoes' : [ {
 		pt : "Conex&otilde;es com banco de dados",
-		en : "",
-		es : ""
+		en : "Connection to the database",
+		es : "Conexi&oacute;n con la base de datos"
 	} ],
     'cadastroTabelas' : [ {
 		pt : "Tabelas com regi&otilde;es geogr&aacute;ficas",
-		en : "",
-		es : ""
+		en : "Tables containing geographic regions",
+		es : "Tablas con regiones geogr&aacute;ficas"
 	} ],
 	'cadastroFonte' : [ {
 		pt : "Fontes de informa&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Information sources",
+		es : "Fuentes de informaci&oacute;n"
 	} ],
 	'gerenciaBd' : [ {
 		pt : "Gerenciador de banco de dados. Upload de SHP e CSV para armazenamento no Postgis",
-		en : "",
-		es : ""
+		en : "Database manager. Uploading of SHP and CSV files to Postgis storage",
+		es : "Administrador de bases de datos. Carga de SHP y CSV para el almacenamiento en Postgis"
 	} ],
 	'xmlWmsMetaestat' : [ {
 		pt : "XML com a lista de servi&ccedil;os WMS originados do sistema de metadados estat&iacute;sticos",
-		en : "",
-		es : ""
+		en : "XML with the list of WMS services originating from the statistical metadata system",
+		es : "XML con la lista de servicios WMS originados en el sistema de metadatos estad&iacute;sticos"
 	} ],
 	'verRss' : [ {
 		pt : "RSS com a lista de mapas cadastrados",
-		en : "",
-		es : ""
+		en : "RSS with the list of registered maps",
+		es : "RSS con la lista de mapas registrados"
 	} ],
 	'enviarArquivo' : [ {
 		pt : "Upload",
-		en : "",
-		es : ""
+		en : "Upload",
+		es : "Subir"
 	} ],
 	'enviarShp' : [ {
 		pt : "Upload de shapefile",
-		en : "",
-		es : ""
+		en : "Shapefile upload",
+		es : "Carga de shapefile"
 	} ],
 	'uploadSimbolo' : [ {
 		pt : "Imagem para uso como s&iacute;mbolo",
-		en : "",
-		es : ""
+		en : "Image that will be used as symbol",
+		es : "Imagen a ser utilizada como s&iacute;mbolo"
 	} ],
 	'editorMapfile' : [ {
 		pt : "Mapfiles (camadas para os mapas)",
-		en : "",
-		es : ""
+		en : "Mapfiles (layers for maps)",
+		es : "Mapfiles (capas para los mapas)"
 	} ],
 	'editorMenus' : [ {
 		pt : "Menus",
-		en : "",
-		es : ""
+		en : "Menus",
+		es : "Men&uacute;s"
 	} ],
 	'arvoreTemas' : [ {
 		pt : "&Aacute;rvore de temas",
-		en : "",
-		es : ""
+		en : "Theme tree",
+		es : "&Aacute;rbol de temas"
 	} ],
 	'rssArvore' : [ {
 		pt : "RSS com os grupos, subgrupos e temas",
-		en : "",
-		es : ""
+		en : "RSS with groups, subgroups and themes",
+		es : "RSS con grupos, subgrupos y temas"
 	} ],
 	'rssArvoreJson' : [ {
 		pt : "JSON com os grupos, subgrupos e temas",
-		en : "",
-		es : ""
+		en : "JSON with groups, subgroups and themes",
+		es : "JSON con grupos, subgrupos y temas"
 	} ],
 	'rssComentarios' : [ {
 		pt : "RSS com os coment&aacute;rios sobre os temas",
-		en : "",
-		es : ""
+		en : "RSS with comments about themes",
+		es : "RSS con los comentarios sobre los temas"
 	} ],
 	'linkMapas' : [ {
 		pt : "Mapas",
-		en : "",
-		es : ""
+		en : "Maps",
+		es : "Mapas"
 	} ],
     'editaMapfile' : [ {
 		pt : "Cat&aacute;logo",
-		en : "",
-		es : ""
+		en : "Catalog",
+		es : "Cat&aacute;logo"
 	} ],
 	'editaLinks' : [ {
 		pt : "Edi&ccedil;&atilde;o dos links",
-		en : "",
-		es : ""
+		en : "Links editing",
+		es : "Edici&oacute;n de los enlaces"
 	} ],
 	'xmlMapas' : [ {
 		pt : "XML com a lista de mapas",
-		en : "",
-		es : ""
+		en : "XML with the map list",
+		es : "XML con la lista de mapas"
 	} ],
 	'rssMapas' : [ {
 		pt : "RSS com a lista de mapas",
-		en : "",
-		es : ""
+		en : "RSS with the map list",
+		es : "RSS con la lista de mapas"
 	} ],
 	'rssMapasJson' : [ {
 		pt : "RSS com a lista de mapas (JSON)",
-		en : "",
-		es : ""
+		en : "RSS with the map list (RSS)",
+		es : "RSS con la lista de mapas (JSON)"
 	} ],
 	'editaMenus' : [ {
 		pt : "Edi&ccedil;&atilde;o dos menus de adi&ccedil;&atilde;o de temas",
-		en : "",
-		es : ""
+		en : "Editing of menus for adding themes",
+		es : "Edici&oacute;n de men&uacute;s de adici&oacute;n de temas"
 	} ],
 	'editaArvore' : [ {
 		pt : "Editar a &aacute;rvore completa dos menus",
-		en : "",
-		es : ""
+		en : "Edit the whole tree menu",
+		es : "Editar el &aacute;rbol men&uacute;s completo"
 	} ],
 	'editaListaMenus' : [ {
 		pt : "Editar a lista de menus",
-		en : "",
-		es : ""
+		en : "Edit the menu list",
+		es : "Editar la lista de men&uacute;s"
 	} ],
 	'editaTabelasAux' : [ {
 		pt : "Edi&ccedil;&atilde;o das tabelas auxiliares",
-		en : "",
-		es : ""
+		en : "Editing of ancillary tables ",
+		es : "Edici&oacute;n de las tablas auxiliares"
 	} ],
 	'editaPerfil' : [ {
 		pt : "Perfis",
-		en : "",
-		es : ""
+		en : "Profile",
+		es : "Perfil"
 	} ],
 	'editaTags' : [ {
 		pt : "Tags (palavras-chave)",
-		en : "",
-		es : ""
+		en : "Tags (keywords)",
+		es : "Etiquetas (palabras clave)"
 	} ],
     'atlas' : [ {
 		pt : "Atlas",
-		en : "",
-		es : ""
+		en : "Atlas",
+		es : "Atlas"
 	} ],
 	'editaAtlas' : [ {
 		pt : "Edi&ccedil;&atilde;o dos atlas",
-		en : "",
-		es : ""
+		en : "Atlas editing",
+		es : "Edici&oacute;n de los atlas"
 	} ],
 	'webServ' : [ {
 		pt : "Endere&ccedil;os de servi&ccedil;os (utilizados nas ferramentas de conex&atilde;o com dados externos)",
-		en : "",
-		es : ""
+		en : "Service addresses (used for connection tools to external data)",
+		es : "Direcciones de servicios (utilizadas en las herramientas de conexi&oacute;n con datos externos)"
 	} ],
 	'editaWebServ' : [ {
 		pt : "Edi&ccedil;&atilde;o do cadastro",
-		en : "",
-		es : ""
+		en : "Register editing",
+		es : "Edici&oacute;n del registro"
 	} ],
 	'xmlWebServ' : [ {
 		pt : "RSS com a lista de servi&ccedil;os WS (webservices convencionais)",
-		en : "",
-		es : ""
+		en : "RSS with list of WS services (tradition webservices)",
+		es : "RSS con la lista de servicios WS (webservices convencionales)"
 	} ],
 	'xmlWebServJson' : [ {
 		pt : "RSS com a lista de servi&ccedil;os WS (webservices convencionais) em JSON",
-		en : "",
-		es : ""
+		en : "RSS with list of WS services (tradition webservices) in JSON",
+		es : "RSS con la lista de servicios WS (webservices convencionales) en JSON"
 	} ],
 	'xmlWms' : [ {
 		pt : "RSS com a lista de servi&ccedil;os WMS",
-		en : "",
-		es : ""
+		en : "RSS with list of WMS services",
+		es : "RSS con la lista de servicios WMS"
 	} ],
 	'xmlWmsJson' : [ {
 		pt : "RSS com a lista de servi&ccedil;os WMS em JSON",
-		en : "",
-		es : ""
+		en : "RSS with list of WMS services in JSON",
+		es : "RSS con la lista de servicios WMS en JSON"
 	} ],
 	'xmlGeoRss' : [ {
 		pt : "RSS com a lista de servi&ccedil;os Geo RSS",
-		en : "",
-		es : ""
+		en : "RSS with list of Geo RSS services",
+		es : "RSS con la lista de servicios Geo RSS"
 	} ],
 	'xmlGeoRssJson' : [ {
 		pt : "RSS com a lista de servi&ccedil;os Geo RSS em JSON",
-		en : "",
-		es : ""
+		en : "RSS with list of Geo RSS services in JSON",
+		es : "RSS con la lista de servicios Geo RSS en JSON"
 	} ],
 	'sistIdentifica' : [ {
 		pt : "Sistemas especiais de identifica&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Special identification systems",
+		es : "Sistemas especiales de identificaci&oacute;n"
 	} ],
 	'editaIdentifica' : [ {
 		pt : "Edi&ccedil;&atilde;o dos sistemas",
-		en : "",
-		es : ""
+		en : "System editing",
+		es : "Edici&oacute;n de los sistemas"
 	} ],
 	'sistAdiciona' : [ {
 		pt : "Sistemas que adicionam camadas",
-		en : "",
-		es : ""
+		en : "Systems for adding layers",
+		es : "Sistemas de adici&oacute;n de capas"
 	} ],
 	'editaSist' : [ {
 		pt : "Edi&ccedil;&atilde;o dos sistemas",
-		en : "",
-		es : ""
+		en : "System editing",
+		es : "Edici&oacute;n de sistemas"
 	} ],
 	'xmlSistAdiciona' : [ {
 		pt : "XML com a lista de sistemas que adicionam temas especiais",
-		en : "",
-		es : ""
+		en : "XML with a list of systems that add special themes",
+		es : "XML con una lista de sistemas que agregan temas especiales"
 	} ],
 	'outrasOpc' : [ {
 		pt : "Outras op&ccedil;&otilde;es",
-		en : "",
-		es : ""
+		en : "Other Options",
+		es : "Otras opciones"
 	} ],
 	'estat' : [ {
 		pt : "Estat&iacute;sticas",
-		en : "",
-		es : ""
+		en : "Statistics",
+		es : "Estad&iacute;sticas"
 	} ],
 	'descricaoBd' : [ {
 		pt : "Descri&ccedil;&atilde;o do banco padr&atilde;o (SQLITE)",
-		en : "",
-		es : ""
+		en : "Description of the default database (SQLITE)",
+		es : "Descripci&oacute;n de la base de datos predeterminada (SQLITE)"
 	} ],
 	'criaBd' : [ {
 		pt : "Criar banco de dados - apenas usu&aacute;rios cadastrados em ms_configura na vari&aacute;vel $i3geomaster podem realizar essa opera&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Create database - only users registered in ms_configure/$i3geomaster variable have privileges to perform this operation",
+		es : "Crear base de datos - solo los usuarios registrados en ms_configura en la variable $ i3geomaster pueden realizar esta operaci&oacute;n"
 	} ],
 	'geraMiniatura' : [ {
 		pt : "Gera as miniaturas (thumbnails) para os temas existentes no diret&oacute;rio i3geo/temas",
-		en : "",
-		es : ""
+		en : "Create thumbnails for themes in the i3geo/themes directory",
+		es : "Genera las miniaturas (thumbnails) para los temas existentes en el directorio i3geo/temas"
 	} ],
 	'compacta' : [ {
 		pt : "Compacta&ccedil;&atilde;o de c&oacute;digo javascript",
-		en : "",
-		es : ""
+		en : "JScript code compression",
+		es : "Compactaci&oacute;n del c&oacute;digo javascript"
 	} ],
 	'cadastros' : [ {
 		pt : "Cadastros",
-		en : "",
-		es : ""
+		en : "Registers",
+		es : "Registros"
 	} ],
 	'listaDeGrupos' : [ {
 		pt : "Lista de nomes de grupos",
-		en : "",
-		es : ""
+		en : "List of group names",
+		es : "Lista de nombres de grupos"
 	} ],
 	'listaDeSubGrupos' : [ {
 		pt : "Lista de nomes de subgrupos",
-		en : "",
-		es : ""
+		en : "List of subgroup names",
+		es : "Lista de nombres de subgrupos"
 	} ],
 	'shp2pg' : [ {
 		pt : "Importar shapefile para Postgis",
-		en : "",
-		es : ""
+		en : "Import shapefile to Postgis",
+		es : "Importar shapefile a Postgis"
 	} ],
 	'csv2pg' : [ {
 		pt : "Importar CSV para Postgis",
-		en : "",
-		es : ""
+		en : "Import CSV to Postgis",
+		es : "Importar CSV a Postgis"
 	} ]
 };
