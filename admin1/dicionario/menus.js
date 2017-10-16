e4813afde84ebfa57705df7689b1550224a1999a
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-16 19:23:56 -0200

    Tradução de menus.js

--- admin1/dicionario/menus.js
+++ admin1/dicionario/menus.js
@@ -1,47 +1,47 @@
 i3GEOadmin.menus.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Menus utilizados no cat&aacute;logo",
-		en : "",
-		es : ""
+		en : "Menus used in the catalog",
+		es : "Men&uacute;s utilizados en el cat&aacute;logo"
 	} ],
     'txtAjuda' : [ {
 		pt : "<p>Este formul&aacute;rio permite editar a lista de menus que ser&aacute; utilizada pelo i3Geo.</p><p>A &aacute;rvore &eacute; composta de menus, grupos, subgrupos e temas. Voc&ecirc; deve criar os menus, a lista de grupos e a lista de subgrupos. Ao criar um menu, esse &eacute; mostrado imediatamente na &aacute;rvore.</p><p>Os temas podem ser associados em qualquer n&iacute;vel da &aacute;rvore, inclusive na raiz do menu. Os temas s&atilde;o arquivos mapfiles armazenados em i3geo/temas e registrados no sistema de administra&ccedil;&atilde;o.</p><p>A cria&ccedil;&atilde;o de mapfiles e a vincula&ccedil;&atilde;o tema-mapfile &eacute; feita por meio do editor de mapfiles, e consiste na determina&ccedil;&atilde;o de um &lsquo;T&iacute;tulo para o cat&aacute;logo&rsquo; para cada mapfile. Esse nome &eacute; listado ao ser adicionado um tema na &aacute;rvore.</p>",
-		en : "",
-		es : ""
+		en : "<p>This form allows you to edit the list of menus that will be used by i3Geo.</p><p>The tree is made up of menus, groups, subgroups, and themes. You should create menus, group lists, and subgroup lists. When creating a menu, it is immediately displayed on the tree.</p><p>Themes can be associated at any level of the tree, including the root of the menu. Themes are mapfiles files stored in i3geo/themes and registered in the administration system.</p><p>The mapfile building and the theme-mapfile linking  is performed through the mapfile editor, and consists in determining a 'Title for the catalog' for each mapfile. This name is listed when a theme is added to the tree.</p>",
+		es : "<p>Este formulario permite editar la lista de men&uacute;s que utilizar&aacute; i3Geo.</p><p>El &aacute;rbol se compone de men&uacute;s, grupos, subgrupos y temas. Usted debe crear los men&uacute;s, la lista de grupos y la lista de subgrupos. Al crear un men&uacute;, este se muestra inmediatamente en el &aacute;rbol.</p><p>Los temas se pueden asociar en cualquier nivel del &aacute;rbol, incluso en la ra&iacute;z del men&uacute;. Los temas son archivos mapfiles almacenados en i3geo/temas y registrados en el sistema de administraci&oacute;n.</p><p>La creaci&oacute;n de mapfiles y la vinculaci&oacute;n tema-mapfile se realiza a trav&eacute;s del editor de mapfiles, y consiste en la determinaci&oacute;n de un 'T&iacute;tulo para el cat&aacute;logo' para cada mapfile. Este nombre aparece cuando se agrega un tema al &aacute;rbol.</p>"
 	} ],
     'nomeMenu' : [ {
 		pt : "Nome padr&atilde;o do menu",
-		en : "",
-		es : ""
+		en : "Default menu name",
+		es : "Nombre predeterminado del men&uacute;"
 	} ],
     'perfil' : [ {
 		pt : "Perfis",
-		en : "",
-		es : ""
+		en : "Profiles",
+		es : "Perfiles"
 	} ],
     'perfilAjuda' : [ {
 		pt : "Perfis que podem visualizar esse menu, separe por espa&ccedil;o e use a lista ao lado",
-		en : "",
-		es : ""
+		en : "Profiles that can view this menu, separate using spaces and use the list beside",
+		es : "Perfiles que pueden ver este men&uacute;, separar por espacio y usar la lista de al lado"
 	} ],
     'iniciaAberto' : [ {
 		pt : "Inicia aberto?",
-		en : "",
-		es : ""
+		en : "Start open?",
+		es : "&iquest;Inicia abierto?"
 	} ],
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Description",
+		es : "Descripci&oacute;n"
 	} ],
     'txtDesc' : [ {
 		pt : "Aqui s&atilde;o cadastrados os menus. Os menus s&atilde;o os n&iacute;veis superiores da &Aacute;rvore de temas. Utilize o bot&atilde;o 'Grupos' para editar os temas existentes na raiz de um menu e tamb&eacute;m os seus grupos.",
-		en : "",
-		es : ""
+		en : "Here menus are registered. Menus are the top levels of the Theme Tree. Use the 'Groups' button to edit the themes existing in the root of a menu as well as its groups.",
+		es : "Aqu&iacute; se registran los men&uacute;s. Los men&uacute;s son los niveles superiores del &aacute;rbol de temas. Utilice el bot&oacute;n 'Grupos' para editar los temas existentes en la ra&iacute;z de un men&uacute; y tambi&eacute;n sus grupos."
 	} ],
     'txtGrupo' : [ {
 		pt : "grupos",
-		en : "",
-		es : ""
+		en : "groups",
+		es : "grupos"
 	} ]
 };
