f6868537d124f14a1ab26dfeaed78c5b4c6b1a96
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-17 00:40:09 -0200

    Tradução de perfis.js

--- admin1/dicionario/perfis.js
+++ admin1/dicionario/perfis.js
@@ -1,12 +1,12 @@
 i3GEOadmin.perfis.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Perfis de acesso ao cat&aacute;logo",
-		en : "",
-		es : ""
+		en : "Access profiles to the catalog",
+		es : "Perfiles de acceso al cat&aacute;logo"
 	} ],
     'txtDesc' : [ {
 		pt : "Os perfis s&atilde;o utilizados para restringir o acesso a determinados temas, grupos, subgrupos e menus. Ao remover ou modificar um perfil, isso se refletir&aacute; em todas as tabelas onde houver registro do perfil.",
-		en : "",
-		es : ""
+		en : "Profiles are used to restrict access to some themes, groups, subgroups, and menus. When you remove or modify a profile, this will be reflected in all tables where the profile is registered.",
+		es : "Los perfiles se utilizan para restringir el acceso a determinados temas, grupos, subgrupos y men&uacute;s. La eliminaci&oacute;n o modificaci&oacute;n de un perfil se reflejar&aacute; en todas las tablas donde haya un registro del perfil."
 	} ]
 };
