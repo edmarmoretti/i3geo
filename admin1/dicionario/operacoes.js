c9b7c28bce0dca2be9c3f81c7e1e0cdbc3e2c47b
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-17 00:17:07 -0200

    Tradução de operacoes.js

--- admin1/dicionario/operacoes.js
+++ admin1/dicionario/operacoes.js
@@ -1,37 +1,37 @@
 i3GEOadmin.operacoes.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Opera&ccedil;&otilde;es e quem pode execut&aacute;-las (pap&eacute;is)",
-		en : "",
-		es : ""
+		en : "Operations and who can execute them (roles)",
+		es : "Operaciones y qui&eacute;n puede ejecutarlas (roles)"
 	} ],
     'txtDesc' : [ {
 		pt : "Aqui s&atilde;o definidos quais pap&eacute;is podem executar certas funcionalidades. Nessa op&ccedil;&atilde;o o administrador do sistema pode definir uma palavra-chave que deve ser inclu&iacute;da no c&oacute;digo da funcionalidade que se deseja restringir o acesso. As funcionalidades que tenham essa palavra-chave inclu&iacute;da em seu c&oacute;digo (arquivo <samp>exec.php</samp>) ser&atilde;o acess&iacute;veis apenas &agrave;queles usu&aacute;rios cujos pap&eacute;is batam com os v&iacute;nculos exigidos. Administradores podem acessar qualquer funcionalidade.",
-		en : "",
-		es : ""
+		en : "Here you can define which roles can perform some functionalities. Using this option, the system administrator can define a keyword that must be included in the code of the functionality you want to restricted. Functionalities that have this keyword included in its code (exec.php file) will be accessible only to those users whose roles match the required links. Administrators can access any functionality.",
+		es : "Aqu&iacute; se definen qu&eacute; roles pueden realizar ciertas funciones. En esta opci&oacute;n el administrador del sistema puede definir una palabra clave que debe incluirse en el c&oacute;digo de la funcionalidad que se desea restringir el acceso. Las funcionalidades que tengan esa palabra clave incluida en su c&oacute;digo (archivo exec.php) ser&aacute;n accesibles s&oacute;lo a aquellos usuarios cuyos roles coincidan con los v&iacute;nculos requeridos. Los administradores pueden tener acceso a cualquier funcionalidad."
 	} ],
     'txtAjuda' : [ {
 		pt : "Os tipos de pap&eacute;is s&atilde;o predefinidos e s&atilde;o atribu&iacute;dos aos usu&aacute;rios individualmente. Para definir os pap&eacute;is de cada usu&aacute;rio, utilize o editor de usu&aacute;rios.",
-		en : "",
-		es : ""
+		en : "Role types are predefined and assigned individually to users. Use the user editor to define roles for each user.",
+		es : "Los tipos de roles son predefinidos y se asignan a los usuarios individualmente. Para definir los roles de cada usuario, utilice el editor de usuarios."
 	} ],
     'codigo' : [ {
 		pt : "C&oacute;digo:",
-		en : "",
-		es : ""
+		en : "Code:",
+		es : "C&oacute;digo:"
 	} ],
     'operacao' : [ {
 		pt : "Opera&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Operation",
+		es : "Operaci&oacute;n"
 	} ],
     'descricao' : [ {
 		pt : "(opcional) Descri&ccedil;&atilde;o:",
-		en : "",
-		es : ""
+		en : "(optional) Description:",
+		es : "(opcional) Descripci&oacute;n:"
 	} ],
     'papeisv' : [ {
 		pt : "Pap&eacute;is vinculados",
-		en : "",
-		es : ""
+		en : "Related roles",
+		es : "Roles vinculados"
 	} ]
 };
