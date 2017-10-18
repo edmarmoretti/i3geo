7ece05e66401dc3550639c3c5535da495262a3a3
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-18 01:19:13 -0200

    Tradução de prancha.js

--- admin1/dicionario/prancha.js
+++ admin1/dicionario/prancha.js
@@ -4,82 +4,82 @@ if (typeof (i3GEOadmin.prancha) === 'undefined') {
 i3GEOadmin.prancha.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Pranchas",
-		en : "",
-		es : ""
+		en : "Panes",
+		es : "Paneles"
 	} ],
     'txtAjuda' : [ {
 		pt : "Um atlas &eacute; composto de pranchas, que por sua vez, &eacute; uma composi&ccedil;&atilde;o de camadas. O usu&aacute;rio poder&aacute; escolher a prancha que deseja ver, cujos par&acirc;metros s&atilde;o definidos aqui.",
-		en : "",
-		es : ""
+		en : "An atlas is made up of panes which in turn is a layer composition . You can choose the pane that you want to see whose parameters are defined here.",
+		es : "Un atlas se compone de paneles que a su vez es una composici&oacute;n de capas. El usuario podr&aacute; elegir el panel que desea ver cuyos par&aacute;metros se definen aqu&iacute;."
 	} ],
     'editarTema' : [ {
 		pt : "temas",
-		en : "",
-		es : ""
+		en : "themes",
+		es : "temas"
 	} ],
     'codigoTema' : [ {
 		pt : "C&oacute;digo do tema",
-		en : "",
-		es : ""
+		en : "Theme code",
+		es : "C&oacute;digo del tema"
 	} ],
     'temaVisivel' : [ {
 		pt : "Ligado (ao abrir a prancha, esse tema estar&aacute; vis&iacute;vel)?",
-		en : "",
-		es : ""
+		en : "Linked (when opening the pane this theme will become visible)?",
+		es : "&iquest;Vinculado (al abrir el panel el tema se har&aacute; visible)?"
 	} ],
     'titulo' : [ {
 		pt : "T&iacute;tulo",
-		en : "",
-		es : ""
+		en : "Title",
+		es : "T&iacute;tulo"
 	} ],
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Description",
+		es : "Descripci&oacute;n"
 	} ],
     'linkTxt' : [ {
 		pt : "Link para o texto que ser&aacute; mostrado na inicializa&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Link to the text that will be shown at the startup",
+		es : "Enlace del texto que ser&aacute; mostrado en el inicio"
 	} ],
     'largura' : [ {
 		pt : "Largura da janela do texto inicial",
-		en : "",
-		es : ""
+		en : "Width of the initial text window",
+		es : "Ancho de la ventana del texto inicial"
 	} ],
     'altura' : [ {
 		pt : "Altura da janela do texto inicial",
-		en : "",
-		es : ""
+		en : "Hight of the initial text window",
+		es : "Altura de la ventana del texto inicial"
 	} ],
     'icone' : [ {
 		pt : "&Iacute;cone que ser&aacute; utilizado na apresenta&ccedil;&atilde;o da prancha",
-		en : "",
-		es : ""
+		en : "Icon that will be used for pane presentation",
+		es : "Icono que se utilizar&aacute; en la presentaci&oacute;n del panel"
 	} ],
     'extGeo' : [ {
 		pt : "Extens&atilde;o geogr&aacute;fica (xmin ymin xmax ymax)",
-		en : "",
-		es : ""
+		en : "Map extent (xmin ymin xmax ymax)",
+		es : "Extensi&oacute;n geogr&aacute;fica (xmin ymin xmax ymax)"
 	} ],
     'publicado' : [ {
 		pt : "Publicado (os n&atilde;o publicados s&atilde;o mostrados apenas para os usu&aacute;rios administradores)?",
-		en : "",
-		es : ""
+		en : "Published (the unpublished ones are shown for administrator users only)?",
+		es : "&iquest;Publicado (los no publicados se muestran solo para los usuarios administradores)?"
 	} ],
     'ordem' : [ {
 		pt : "Ordem de apresenta&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Presentation order",
+		es : "Orden de presentaci&oacute;n"
 	} ],
     'txtTituloTema' : [ {
 		pt : "Temas que comp&otilde;em a prancha",
-		en : "",
-		es : ""
+		en : "Themes that make up the pane",
+		es : "Temas que conforman el panel"
 	} ],
     'txtDescTema' : [ {
 		pt : "Defina aqui quais os temas que ser&atilde;o mostrados na prancha e suas propriedades. Os temas s&atilde;o os mesmos dispon&iacute;veis no cat&aacute;logo de temas.",
-		en : "",
-		es : ""
+		en : "Define here which themes will be shown on the pane and their properties. These themes are the same as those available in the theme catalog.",
+		es : "Defina aqu&iacute; qu&eacute; temas se mostrar&aacute;n en el panel y sus propiedades. Los temas son los mismos disponibles en el cat&aacute;logo de temas."
 	} ]
 };
