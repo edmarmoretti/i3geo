a3e515bebb70518f40ae5a855daccb8ef9d6ac67
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-18 01:27:08 -0200

    Substituição de table(s) por pane(s)

--- admin1/dicionario/atlas.js
+++ admin1/dicionario/atlas.js
@@ -2,8 +2,8 @@
 i3GEOadmin.atlas.dicionario = {
 	'txtDesc' : [ {
 		pt : "Um atlas utiliza uma interface especial que mostra uma op&ccedil;&atilde;o para que o usu&aacute;rio escolha pranchas. Cada prancha &eacute; formada por camadas, escolhidas do cat&aacute;logo.",
-		en : "An atlas uses a particular interface that shows an option so that users can choose tables. Each table is made up of layers selected from the catalog",
-		es : "Un atlas utiliza una interfaz especial que muestra una opci&oacute;n para que el usuario escoja las tablas. Cada tabla est&aacute; formada por capas seleccionadas del cat&aacute;logo"
+		en : "An atlas uses a particular interface that shows an option so that users can choose panes. Each pane is made up of layers selected from the catalog",
+		es : "Un atlas utiliza una interfaz especial que muestra una opci&oacute;n para que el usuario escoja los paneles. Cada panel est&aacute; formada por capas seleccionadas del cat&aacute;logo"
 	} ],
 	'txtTitulo' : [ {
 		pt : "Atlas ",
@@ -11,19 +11,19 @@ i3GEOadmin.atlas.dicionario = {
 		es : "Atlas "
 	} ],
     'txtAjuda' : [ {
-		pt : "O cadastro de Atlas &eacute; utilizado na interface 'Atlas' do i3Geo. Essa interface pode ser vista em http://localhost/i3geo/atlas</p><p>Cada Atlas possui uma lista de pranchas e cada prancha uma lista de temas e outros par&acirc;metros.</p><p>A montagem de cada Atlas &eacute; feito definindo-se os par&acirc;metros de cada n&iacute;vel hier&aacute;rquico, editando-se uma &aacute;rvore cuja raiz &eacute; cada Atlas, seguido pelas pranchas e temas.</p><p>Para cada Atlas e cada prancha pode ser definido um texto explicativo que ser&aacute; aberto ao iniciar o Atlas ou a prancha.</p><p>Nessa primeira tela &eacute; mostrada a edi&ccedil;&atilde;o de Atlas.",
-		en : "The 'Atlas' interface of i3Geo is where you can record your atlases. You can access to this interface by http://localhost/i3geo/atlas<p>Each atlas contains tables, each table contains a list of themes and other parameters.<p>When you set up each Atlas you must define the parameters of each hierarchical level, editing a tree whose root is each Atlas, followed by tables and themes.<p> For each Atlas and table, you can put an explanatory text that can be visualized when an Atlas or table is initialized.",
-		es : "El registro del Atlas es utilizado en la interfaz 'Atlas' de i3Geo. Esta interfaz puede ser vista en http://localhost/i3geo/atlas<p>Cada Atlas posee una lista de tablas, cada tabla una lista de temas y otros par&aacute;metros.<p> El montaje de cada Atlas se realiza estableciendo los par&aacute;metros de cada nivel jer&aacute;rquico, editando un &aacute;rbol cuya ra&iacute;z es cada Atlas, seguido por las tablas y temas.<p>Para cada Atlas y cada tabla se puede definir un texto explicativo que puede ser abierto al iniciar el Atlas o la tabla."
+		pt : "O cadastro de Atlas &eacute; utilizado na interface 'Atlas' do i3geo. Essa interface pode ser vista em http://localhost/i3geo/atlas<p>Cada Atlas possu&iacute; uma lista de pranchas e cada prancha uma lista de temas e outros par&acirc;metros.<p>A montagem de cada Atlas &eacute; feito definindo-se os par&acirc;metros de cada n&iacute;vel hier&aacute;rquico, editando-se uma &aacute;rvore cuja raiz &eacute; cada Atlas, seguido pelas pranchas e temas.<p>Para cada Atlas e cada prancha pode ser definido um texto explicativo que ser&aacute; aberto ao iniciar o Atlas ou a prancha.",
+		en : "The 'Atlas' interface of i3Geo is where you can record your atlases. You can access to this interface by http://localhost/i3geo/atlas<p>Each atlas contains panes, each pane contains a list of themes and other parameters.<p>When you set up each Atlas you must define the parameters of each hierarchical level, editing a tree whose root is each Atlas, followed by panes and themes.<p> For each Atlas and pane, you can put an explanatory text that can be visualized when an Atlas or pane is initialized.",
+		es : "El registro del Atlas es utilizado en la interfaz 'Atlas' de i3Geo. Esta interfaz puede ser vista en http://localhost/i3geo/atlas<p>Cada Atlas posee una lista de paneles, cada panel tiene una lista de temas y otros par&aacute;metros.<p> El montaje de cada Atlas se realiza estableciendo los par&aacute;metros de cada nivel jer&aacute;rquico, editando un &aacute;rbol cuya ra&iacute;z es cada Atlas, seguido por los paneles y temas.<p>Para cada Atlas y cada panel se puede definir un texto explicativo que puede ser abierto al iniciar el Atlas o el panel."
 	} ],
     'codigoTema' : [ {
 		pt : "C&oacute;digo do tema",
-	    en : "Theme code",
+		en : "Theme code",
 		es : "C&oacute;digo del tema"
 	} ],
     'temaVisivel' : [ {
 		pt : "Ligado (ao abrir a prancha, esse tema estar&aacute; vis&iacute;vel)?",
-		en : "Linked (when opening the table, this theme will become visible)?",
-		es : "&iquest;Conectado (al abrir la tabla, el tema se har&aacute; visible)?"
+		en : "Linked (when opening the pane, this theme will become visible)?",
+		es : "&iquest;Conectado (al abrir el panel, el tema se har&aacute; visible)?"
 	} ],
     'titulo' : [ {
 		pt : "T&iacute;tulo",
@@ -37,23 +37,23 @@ i3GEOadmin.atlas.dicionario = {
 	} ],
     'linkTxt' : [ {
 		pt : "Link para o texto que ser&aacute; mostrado na inicializa&ccedil;&atilde;o",
-		en : "Text link that will be shown at the start",
+		en : "Link to the text that will be shown at the startup",
 		es : "Enlace del texto que ser&aacute; mostrado al inicio"
 	} ],
     'largura' : [ {
 		pt : "Largura da janela do texto inicial",
-		en : "Window width of initial text",
+		en : "Width of the initial text window",
 		es : "Ancho de la ventana del texto inicial"
 	} ],
     'altura' : [ {
 		pt : "Altura da janela do texto inicial",
-		en : "Window height of initial text",
+		en : "Hight of the initial text window",
 		es : "Altura de la ventana del texto inicial"
 	} ],
     'icone' : [ {
 		pt : "&Iacute;cone que ser&aacute; utilizado na apresenta&ccedil;&atilde;o da prancha",
-		en : "Icon that will be used in the table presentation",
-		es : "Icono que ser&aacute; utilizado en la presentaci&oacute;n de la tabla"
+		en : "Icon that will be used for pane presentation",
+		es : "Icono que ser&aacute; utilizado en la presentaci&oacute;n del panel"
 	} ],
     'extGeo' : [ {
 		pt : "Extens&atilde;o geogr&aacute;fica (xmin ymin xmax ymax)",
@@ -61,7 +61,7 @@ i3GEOadmin.atlas.dicionario = {
 		es : "Extensi&oacute;n geogr&aacute;fica (xmin ymin xmax ymax)"
 	} ],
     'mapfileInicia' : [ {
-		pt : "Mapfile inicial (mapfile que ser&aacute; usado como base para montagem do mapa inicial. Se n&atilde;o for definido, ser&aacute; usado o default do i3Geo. Utilize o endere&ccedil;o completo do aquivo mapfile no servidor.)",
+		pt : "Mapfile inicial (mapfile que ser&aacute; usado como base para montagem do mapa inicial. Se n&atilde;o for definido, ser&aacute; usado o default do i3geo. Utilize o endere&ccedil;o completo do aquivo mapfile no servidor.)",
 		en : "Initial mapfile (mapfile that will be used as base for initial map building. If it is not defined, the default i3Geo mapfile will be used. On the server use a mapfile full path)",
 		es : "Mapfile inicial (mapfile que ser&aacute; utilizado como base para el montaje del mapa inicial. Si no es definido, ser&aacute; utilizado &eacute;l predeterminado de i3Geo. Utilice la direcci&oacute;n completa del archivo mapfile en el servidor)"
 	} ],
@@ -71,14 +71,14 @@ i3GEOadmin.atlas.dicionario = {
 		es : "Icono que ser&aacute; utilizado en el montaje de la lista de todos los Atlas"
 	} ],
     'template' : [ {
-		pt : "Template HTML (se n&atilde;o for especificado, ser&aacute; usado o default do i3Geo. Utilize o caminho completo do arquivo html no servidor)",
+		pt : "Template HTML (se n&atilde;o for especificado, ser&aacute; usado o default do i3geo. Utilize o caminho completo do arquivo html no servidor)",
 		en : "HTML template (if not specified, it will be used the i3Geo default. Use full path of HTML file on the server)",
 		es : "Plantilla HTML (si no es especificada, ser&aacute; utilizada la predeterminada de i3Geo. Utilice la ruta completa del archivo HTML en el servidor)"
 	} ],
     'pranchaInicia' : [ {
 		pt : "Prancha inicial (ser&aacute; mostrada quando iniciar o atlas)",
-		en : "Startup table (it will be shown when the atlas starts)",
-		es : "Tabla inicial (ser&aacute; mostrada cuando se inicie el atlas)"
+		en : "Startup pane (it will be shown when the atlas starts)",
+		es : "Panel inicial (ser&aacute; mostrada cuando se inicie el atlas)"
 	} ],
     'tipoGuia' : [ {
 		pt : "Tipo da apresenta&ccedil;&atilde;o das guias",
@@ -97,7 +97,7 @@ i3GEOadmin.atlas.dicionario = {
 	} ],
     'pranchas' : [ {
 		pt : "pranchas",
-		en : "tables",
-		es : "tablas"
+		en : "panes",
+		es : "paneles"
 	} ]
 };
