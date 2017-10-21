e1cfd589a483b29caa522d1d1701650b1c2505c9
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-21 19:03:13 -0200

    Tradução de subgrupos.js

--- admin/dicionario/subgrupos.js
+++ admin/dicionario/subgrupos.js
@@ -1,62 +1,62 @@
 i3GEOadmin.subgrupos.dicionario = {
     'subgrupos' : [ {
 		pt : "Subgrupos vinculados ao grupo",
-		en : "",
-		es : ""
+		en : "Subgroups linked to the group",
+		es : "Subgrupos vinculados al grupo"
 	} ],
     'descricao' : [ {
 		pt : "Descri&ccedil;&atilde;o (opcional):",
-		en : "",
-		es : ""
+		en : "Description (optional):",
+		es : "Descripci&oacute;n (opcional):"
 	} ],
     'nomeIn' : [ {
 		pt : "Nome em ingl&ecirc;s (opcional):",
-		en : "",
-		es : ""
+		en : "Name in English (optional):",
+		es : "Nombre en ingl&eacute;s (opcional):"
 	} ],
     'nomeEs' : [ {
 		pt : "Espanhol (opcional):",
-		en : "",
-		es : ""
+		en : "Spanish (optional):",
+		es : "Espa&ntilde;ol (opcional):"
 	} ],
 	'txtAjuda' : [ {
 		pt : "Aqui &eacute; poss&iacute;vel criar subgrupos de camadas ou inserir temas na raiz do grupo.",
-		en : "",
-		es : ""
+		en : "Here you can create layer subgroups or insert themes in the group root.",
+		es : "Aqu&iacute; se puede crear subgrupos de capas o insertar temas en la ra&iacute;z del grupo."
 	} ],
     'txtDesc' : [ {
-		pt : "Utilize as opções abaixo para criar ou editar temas que ficarão na raiz do grupo e também para criar ou editar os subgrupos vinculados.",
-		en : "",
-		es : ""
+		pt : "Utilize as op&ccedil;&otilde;es abaixo para criar ou editar temas que ficar&atilde;o na raiz do grupo e tamb&eacute;m para criar ou editar os subgrupos vinculados.",
+		en : "Use the options below to create or edit themes that will be at the group root, and also to create or edit linked subgroups.",
+		es : "Utilice las siguientes opciones para crear o editar temas que se encuentren en la ra&iacute;z del grupo y tambi&eacute;n para crear o editar los subgrupos vinculados."
 	} ],
     'temasRaizSubgrupo' : [ {
 		pt : "Temas do grupo",
-		en : "",
-		es : ""
+		en : "Group themes",
+		es : "Temas del grupo"
 	} ],
     'perfis' : [ {
 		pt : "Perfis que podem ver. Separe por espa&ccedil;os e escolha da lista ao lado.",
-		en : "",
-		es : ""
+		en : "Profiles you can see. Separate by spaces and choose from the list beside.",
+		es : "Perfiles que pueden ver. Separe por espacios y elija de la lista de al lado."
 	} ],
     'txtTitulo' : [ {
 		pt : "Grupo",
-		en : "",
-		es : ""
+		en : "Group",
+		es : "Grupo"
 	} ],
     'temas' : [ {
 		pt : "temas",
-		en : "",
-		es : ""
+		en : "themes",
+		es : "temas"
 	} ],
 	'txtListaDeSubGrupos' : [ {
 		pt : "Lista controlada de nomes de subgrupos",
-		en : "",
-		es : ""
+		en : "Controlled list of subgroup names",
+		es : "Lista controlada de nombres de subgrupos"
 	} ],
     'descListaDeSubGrupos' : [ {
 		pt : "Os nomes de subgrupos s&atilde;o utilizados para nomear os n&oacute;s da &aacute;rvore do cat&aacute;logo. Cada nome recebe um identificador, de forma que ao ser modificado a &aacute;rvore refletir&aacute; as edi&ccedil;&otilde;es.",
-		en : "",
-		es : ""
+		en : "Subgroup names are used to name catalog tree nodes. Each name received an identifier, so that the modified tree will show the editings.",
+		es : "Los nombres de los subgrupos se utilizan para darle nombre a los nodos del &aacute;rbol del cat&aacute;logo. Cada nombre recibe un identificador, de forma que al ser modificado el &aacute;rbol se reflejar&aacute;n las ediciones."
 	} ]
 };
