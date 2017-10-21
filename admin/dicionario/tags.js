291c5c8c52476a1cc7af6c20e6ac0d561cad5989
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-21 19:32:51 -0200

    Tradução de tags.js

--- admin/dicionario/tags.js
+++ admin/dicionario/tags.js
@@ -1,12 +1,12 @@
 i3GEOadmin.tags.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Tags",
-		en : "",
-		es : ""
+		en : "Tags",
+		es : "Etiquetas"
 	} ],
     'txtDesc' : [ {
 		pt : "Tags s&atilde;o como palavras-chave que podem ser adicionadas aos temas para a realiza&ccedil;&atilde;o de buscas.",
-		en : "",
-		es : ""
+		en : "Tags are keywords that can be added to the theme for searching.",
+		es : "Las etiquetas son palabras claves que se agregar a los temas para efectuar su b&uacute;squeda."
 	} ]
 };
