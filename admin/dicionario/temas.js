f2dcf6f641d4f4be7bffc30ea7267aeefdf849db
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-21 20:22:42 -0200

    Tradução de temas.js

--- admin/dicionario/temas.js
+++ admin/dicionario/temas.js
@@ -1,17 +1,17 @@
 i3GEOadmin.temas.dicionario = {
     'txtDesc' : [ {
 		pt : "Utilize as op&ccedil;&otilde;es abaixo para incluir ou editar temas que ficar&atilde;o dispon&iacute;veis para o usu&aacute;rio acessar no subgrupo escolhido.",
-		en : "",
-		es : ""
+		en : "Use the options below to include or edit themes that will be available to the user to access the chosen subgroup.",
+		es : "Use las opciones siguientes para incluir o editar temas que estar&aacute;n disponibles para que el usuario acceda al subgrupo elegido."
 	} ],
     'perfis' : [ {
 		pt : "Perfis que podem ver. Separe por espa&ccedil;os e escolha da lista ao lado.",
-		en : "",
-		es : ""
+		en : "Profiles that can see. Separate by spaces and select from the list beside.",
+		es : "Perfiles que pueden ver. Separe con espacios y elija de la lista de al lado."
 	} ],
     'txtTitulo' : [ {
 		pt : "Temas do subgrupo",
-		en : "",
-		es : ""
+		en : "Subgroup themes",
+		es : "Temas del subgrupo"
 	} ]
 };
