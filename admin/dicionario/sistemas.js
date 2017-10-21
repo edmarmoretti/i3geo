57921a206140e312bd8f0da41893bc798c9fab0d
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-21 01:38:08 -0200

    Tradução de sistemas.js

--- admin/dicionario/sistemas.js
+++ admin/dicionario/sistemas.js
@@ -4,67 +4,67 @@ if (typeof (i3GEOadmin.sistemas) === 'undefined') {
 i3GEOadmin.sistemas.dicionario = {
 	'txtDescFuncao' : [ {
 		pt : "As fun&ccedil;&otilde;es s&atilde;o programas que ser&atilde;o abertos em janelas flutuantes dentro do mapa",
-		en : "",
-		es : ""
+		en : "Functions are programs that open in floating windows on the map",
+		es : "Las funciones son programas que se abren en ventanas flotantes dentro del mapa"
 	} ],
 	'txtTituloFuncao' : [ {
 		pt : "Fun&ccedil;&otilde;es do sistema",
-		en : "",
-		es : ""
+		en : "System functions",
+		es : "Funciones del sistema"
 	} ],
 	'txtTitulo' : [ {
 		pt : "Sistemas especiais que adicionam camadas",
-		en : "",
-		es : ""
+		en : "Special systems that add layers",
+		es : "Sistemas especiales que agregan capas"
 	} ],
     'txtDesc' : [ {
 		pt : "Sistemas especiais s&atilde;o utilizados para incluir camadas no i3geo tendo como base programas que interagem com o usu&aacute;rio de alguma forma espec&iacute;fica. Cada sistema pode ter um objetivo espec&iacute;fico e uma interface pr&oacute;pria. Normalmente s&atilde;o utilizados para realizar consultas a bancos de dados ou Web Services e gerar a camada para acrescentar ao mapa. Esses sistemas podem por exemplo, consultar bancos de dados estat&iacute;sticos para gerar cartogramas.",
-		en : "",
-		es : ""
+		en : "Special systems are used to include layers in i3geo based on programs that interact with the user in some specific way. Each system have a specific goal and its own interface. Special systems are typically used to query databases or Web Services and to generate a layer to append to the map. For example, statistical databases can be queried by these systems to create cartograms.",
+		es : "Los sistemas especiales se utilizan para incluir capas en i3geo bas&aacute;ndose en programas que interact&uacute;an con el usuario de alguna forma espec&iacute;fica. Cada sistema puede tener un objetivo espec&iacute;fico y una interfaz propia. Normalmente se utilizan para realizar consultas a las bases de datos o Servicios Web y generar una capa para agregar al mapa. Estos sistemas pueden, por ejemplo, consultar base de datos estad&iacute;sticos para generar cartogramas."
 	} ],
     'sistema' : [ {
 		pt : "Sistema",
-		en : "",
-		es : ""
+		en : "System",
+		es : "Sistema"
 	} ],
     'nome' : [ {
 		pt : "Nome:",
-		en : "",
-		es : ""
+		en : "Name:",
+		es : "Nombre:"
 	} ],
     'perfil' : [ {
 		pt : "Perfis - escolha da lista abaixo:",
-		en : "",
-		es : ""
+		en : "Profiles - choose from the list below:",
+		es : "Perfiles: seleccione de la lista siguiente:"
 	} ],
     'publicado' : [ {
 		pt : "Publicado?",
-		en : "",
-		es : ""
+		en : "Published?",
+		es : "&iquest;Publicado?"
 	} ],
     'programa' : [ {
 		pt : "Programa que ser&aacute; executado:",
-		en : "",
-		es : ""
+		en : "Program that will be executed:",
+		es : "Programa que ser&aacute; ejecutado:"
 	} ],
     'largura' : [ {
 		pt : "Largura da janela onde o programa ser&aacute; aberto:",
-		en : "",
-		es : ""
+		en : "Window width where the program will open",
+		es : "Ancho de la ventana donde se abrir&aacute; el programa:"
 	} ],
     'altura' : [ {
 		pt : "Altura da janela:",
-		en : "",
-		es : ""
+		en : "Window height:",
+		es : "Altura de la ventana:"
 	} ],
     'funcoes' : [ {
 		pt : "fun&ccedil;&otilde;es",
-		en : "",
-		es : ""
+		en : "functions",
+		es : "funciones"
 	} ],
     'perfilAjuda' : [ {
 		pt : "Perfis que podem visualizar esse sistema, separe por espa&ccedil;o e use a lista ao lado",
-		en : "",
-		es : ""
+		en : "Profiles that can view this system, separate by space and use the list beside",
+		es : "Perfiles que pueden ver este sistema, separar con espacios y utilizar la lista de al lado"
 	} ]
 };
