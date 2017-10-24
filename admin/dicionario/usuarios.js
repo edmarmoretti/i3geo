a0e95390f9b0d1bece4bfdca8c62dc125ff06fea
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-24 19:15:54 -0200

    Tradução de usuarios.js

--- admin/dicionario/usuarios.js
+++ admin/dicionario/usuarios.js
@@ -1,62 +1,62 @@
 i3GEOadmin.usuarios.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Usu&aacute;rios e pap&eacute;is operacionais",
-		en : "",
-		es : ""
+		en : "Users and operational roles",
+		es : "Usuarios y roles operativos"
 	} ],
 	'txtDesc' : [ {
 		pt : "Cadastre aqui os usu&aacute;rios que podem fazer login. Quando um usu&aacute;rio &eacute; criado &eacute; necess&aacute;rio vincul&aacute;-lo a um ou mais pap&eacute;is, que ter&atilde;o permiss&otilde;es e responsabilidades diferentes dentro do sistema. Os pap&eacute;is s&atilde;o utilizados para controlar as opera&ccedil;&otilde;es administrativas que o usu&aacute;rio poder&aacute; executar. Usu&aacute;rios podem tamb&eacute;m pertencer a determinados grupos. Os grupos s&atilde;o utilizados para controlar o acesso &agrave;s camadas nos mapas.</p><p>Utilize o editor de grupos para definir de quais grupos os usu&aacute;rios fazem parte.</p>",
-		en : "",
-		es : ""
+		en : "Here, you can register users who are able to login. A newly user must be linked to one or more roles, which will have different permissions and taks on the system. Roles are used to control the administrative operations that a user can perform. Users can also belong to specific groups. Groups are used to control the access to layers on the maps.</p><p>Group editor allows you to define which groups users belong to.</p>",
+		es : "Registre aqu&iacute; los usuarios que pueden entrar. Cuando se crea un usuario es necesario vincularlo con uno o m&aacute;s roles que tendr&aacute;n permisos y responsabilidades diferentes dentro del sistema. Los roles se utilizan para controlar las operaciones administrativas que el usuario puede ejecutar. Los usuarios tambi&eacute;n pueden pertenecer a determinados grupos. Los grupos se utilizan para controlar el acceso a las capas de los mapas.</p><p>Utilice el editor de grupos para definir a cu&aacute;les grupos pertenecen los usuarios.</p>"
 	} ],
     'txtAjuda' : [ {
 		pt : "Para criar um novo usu&aacute;rio clique no &iacute;cone 'Adicionar'. Inclua o nome, login, senha e e-mail e atribua um ou mais pap&eacute;is ao novo usu&aacute;rio.",
-		en : "",
-		es : ""
+		en : "Click on the 'Add' icon to create a new user. Type a name, login, password, email, and assign one or more roles to the new user.",
+		es : "Para crear un usuario nuevo haga clic en el icono 'Agregar'. Incluir el nombre, login, contrase&ntilde;a, correo electr&oacute;nico y asignar uno o m&aacute;s roles al usuario nuevo."
 	} ],
     'enviaSenha' : [ {
 		pt : "enviar senha por e-mail",
-		en : "",
-		es : ""
+		en : "send password by email",
+		es : "enviar contrase&ntilde;a por correo electr&oacute;nico"
 	} ],
     'usuario' : [ {
 		pt : "Usu&aacute;rio",
-		en : "",
-		es : ""
+		en : "User",
+		es : "Usuario"
 	} ],
     'papeisv' : [ {
 		pt : "Pap&eacute;is vinculados",
-		en : "",
-		es : ""
+		en : "Associated roles",
+		es : "Roles vinculados"
 	} ],
     'nome' : [ {
 		pt : "Nome",
-		en : "",
-		es : ""
+		en : "Name",
+		es : "Nombre"
 	} ],
     'sim' : [ {
 		pt : "Sim",
-		en : "",
-		es : ""
+		en : "Yes",
+		es : "S&iacute;"
 	} ],
     'nao' : [ {
 		pt : "N&atilde;o",
-		en : "",
-		es : ""
+		en : "No",
+		es : "No"
 	} ],
     'labelNovaSenha' : [ {
 		pt : "Nova senha",
-		en : "",
-		es : ""
+		en : "New password",
+		es : "Contrase&ntilde;a nueva"
 	} ],
     'labelDataCadastro' : [ {
 		pt : "Data do cadastro",
-		en : "",
-		es : ""
+		en : "Date of registration",
+		es : "Fecha de registro"
 	} ],
     'labelAtivo' : [ {
 		pt : "Ativo?",
-		en : "",
-		es : ""
+		en : "Active?",
+		es : "&iquest;Activo?"
 	} ]
 };
