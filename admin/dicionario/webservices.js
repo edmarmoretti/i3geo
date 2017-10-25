0e632b4797c2ba1b2b7bc6acfcb9782b79bee773
Abraham Coiman <abrahamcoiman@gmail.com>
2017-10-24 22:52:27 -0200

    Tradução de webservices.js

--- admin/dicionario/webservices.js
+++ admin/dicionario/webservices.js
@@ -1,22 +1,22 @@
 i3GEOadmin.webservices.dicionario = {
 	'txtTitulo' : [ {
 		pt : "Cadastro de Webservices",
-		en : "",
-		es : ""
+		en : "Webservice register",
+		es : "Registro de Servicios Web"
 	} ],
     'txtDesc' : [ {
 		pt : "O i3Geo acessa Web Services e servi&ccedil;os para obten&ccedil;&atilde;o de dados em diferentes formatos, organizados por tipo. O cadastro &eacute; utilizado no cat&aacute;logo de temas.",
-		en : "",
-		es : ""
+		en : "I3Geo accesses Web Services and services to obtain data in different formats organized by type. The register is used in the catalog of themes.",
+		es : "I3Geo accede a Servicios Web y servicios para la obtenci&oacute;n de datos en diferentes formatos organizados por tipo. El registro se utiliza en el cat&aacute;logo de temas."
 	} ],
     'autor' : [ {
 		pt : "Autor:",
-		en : "",
-		es : ""
+		en : "Author:",
+		es : "Autor:"
 	} ],
     'endereco' : [ {
 		pt : "Endere&ccedil;o:",
-		en : "",
-		es : ""
+		en : "Address:",
+		es : "Direcci&oacute;n:"
 	} ]
 };
