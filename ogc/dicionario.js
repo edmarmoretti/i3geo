f2b1c2a5c28d65514f108fcca1d66c77fee2c635
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-11 01:46:27 -0200

    Tradução de dicionario.js

--- ogc/dicionario.js
+++ ogc/dicionario.js
@@ -3,107 +3,107 @@ ogc.dicionario =
 {
 		"acesso" : [{
 			pt : "Endere&ccedil;os de acesso:",
-			en : "",
-			es : ""
+			en : "Access Addresses:",
+			es : "Direcciones de acceso:"
 		}],
 		"wstodas" : [{
 			pt : "Webservice com todas as camadas",
-			en : "",
-			es : ""
+			en : "Webservice with all layers",
+			es : "Webservice con todas las capas"
 		}],
 		"wscamada" : [{
 			pt : "Webservice dessa camada",
-			en : "",
-			es : ""
+			en : "Webservice of this layer",
+			es : "Webservice de esta capa"
 		}],
 		"linkpagina" : [{
 			pt : "Link para essa p&aacute;gina",
-			en : "",
-			es : ""
+			en : "Link to this page",
+			es : "Enlace a esta p&aacute;gina"
 		}],
 		"getmap" : [{
 			pt : "Testar getmap",
-			en : "",
-			es : ""
+			en : "Test getmap",
+			es : "Probar getmap"
 		}],
 		"legenda" : [{
 			pt : "Testar getLegendGraphic",
-			en : "",
-			es : ""
+			en : "Test getLegendGraphic",
+			es : "Probar getLegendGraphic"
 		}],
 		"downwfs" : [{
 			pt : "Download shapefile via WFS",
-			en : "",
-			es : ""
+			en : "Download shapefile via WFS",
+			es : "Descargar shapefile a trav&eacute;s de WFS"
 		}],
 		"vOl" : [{
 			pt : "visualizar com openLayers",
-			en : "",
-			es : ""
+			en : "view with openLayers",
+			es : "ver con openLayers"
 		}],
 		"downCgeo" : [{
 			pt : "Download CSV via WFS com geometria",
-			en : "",
-			es : ""
+			en : "Download CSV via WFS with geometry",
+			es : "Descargar CSV a trav&eacute;s de WFS con geometr&iacute;a"
 		}],
 		"downSgeo" : [{
 			pt : "Download CSV via WFS mas sem a geometria",
-			en : "",
-			es : ""
+			en : "Download CSV via WFS without geometry",
+			es : "Descargar CSV a trav&eacute;s de WFS pero sin geometr&iacute;a"
 		}],
 		"kmz" : [{
 			pt : "Download KMZ",
-			en : "",
-			es : ""
+			en : "Download KMZ",
+			es : "Descargar KMZ"
 		}],
 		"kml" : [{
 			pt : "Download KML",
-			en : "",
-			es : ""
+			en : "Download KML",
+			es : "Descargar KML"
 		}],
 		"explore" : [{
 			pt : "Explore a tabela de atributos",
-			en : "",
-			es : ""
+			en : "Explore the attribute table",
+			es : "Explore la tabla de atributos"
 		}],
 		"jumbotron" : [{
-			pt : "Navegue pela &aacute;rvore ao lado para localizar o tema desejado. Clicando-se em um tema, &eacute; mostrado o endere&ccedil;o do servi&ccedil;o OGC. Os servi&ccedil;os s&atilde;o Web Wervices que possibilitam o acesso aos dados dispon&iacute;veis nessa instala&ccedil;&atilde;o do i3Geo. A lista de temas baseia-se nas configura&ccedil;&otilde;es espec&iacute;ficas de cada servidor onde o i3Geoest&aacute; instalado. Voc&ecirc; pode usar um Web Service para acessar os dados configurados nesse servidor por meio de outros softwares de geoprocessamento, como o <a href='http://www.gvsig.gva.es/index.php?id=gvsig&L=0'>gvSIG.</a> Para maiores informa&ccedil;&otilde;es sobre o uso de web services, veja <a href='http://www.opengeospatial.org/standards' target=blank>http://www.opengeospatial.org/standards</a>. <div class='alert alert-info' role='alert'>O i3Geo possu&iacute; alguns par&acirc;metros especiais, que n&atilde;o fazem parte dos padr&otilde;es de servi&ccedil;os OGC. Para ver esses par&acirc;metros acesse <a href='../ogc.php' target='_blank' class='alert-link' >ogc.php</a></div>",
-			en : "",
-			es : ""
+			pt : "Navegue pela &aacute;rvore ao lado para localizar o tema desejado. Clicando-se em um tema, &eacute; mostrado o endere&ccedil;o do servi&ccedil;o OGC. Os servi&ccedil;os s&atilde;o Web Services que possibilitam o acesso aos dados dispon&iacute;veis nessa instala&ccedil;&atilde;o do i3Geo. A lista de temas baseia-se nas configura&ccedil;&otilde;es espec&iacute;ficas de cada servidor onde o i3Geoest&aacute; instalado. Voc&ecirc; pode usar um Web Service para acessar os dados configurados nesse servidor por meio de outros softwares de geoprocessamento, como o <a href='http://www.gvsig.gva.es/index.php?id=gvsig&L=0'>gvSIG.</a> Para maiores informa&ccedil;&otilde;es sobre o uso de web services, veja <a href='http://www.opengeospatial.org/standards' target=blank>http://www.opengeospatial.org/standards</a>. <div class='alert alert-info' role='alert'>O i3Geo possu&iacute; alguns par&acirc;metros especiais, que n&atilde;o fazem parte dos padr&otilde;es de servi&ccedil;os OGC. Para ver esses par&acirc;metros acesse <a href='../ogc.php' target='_blank' class='alert-link' >ogc.php</a></div>",
+			en : "Navigate through the tree to the right to find the desired theme. Clicking on a theme displays the address of the OGC service. The services are Web Services that allow you to access the data available in this i3Geo installation. The list of themes is based on the specific settings of each server where i3Geo is installed. You can use a Web Service to access the data configured on this server through other geoprocessing software such as <a href='http://www.gvsig.gva.es/index.php?id=gvsig&L=0'>gvSIG.</a> For more information on using web services, see <a href='http://www.opengeospatial.org/standards' target=blank>http://www.opengeospatial.org/standards</a>. <div class='alert alert-info' role='alert'>I3Geo has some special parameters that are not part of the OGC service standards. To see these parameters go to <a href='../i3geo/ogc.php' target='_blank' class='alert-link' >ogc.php</a></div>",
+			es : "Dir&iacute;jase hacia el &aacute;rbol (al lado) para localizar el tema deseado. Al hacer clic en un tema se muestra la direcci&oacute;n del servicio OGC. Los servicios son Servicios Web que posibilitan el acceso a los datos disponibles en esa instalaci&oacute;n de i3Geo. La lista de temas se basa en las configuraciones espec&iacute;ficas de cada servidor donde est&aacute; instalado i3Geo. Puede utilizar un Servicio Web para acceder a los datos configurados en este servidor a trav&eacute;s de otros programas de geoprocesamiento como <a href='http://www.gvsig.gva.es/index.php?id=gvsig&amp;L=0'>gvSIG.</a> Para m&aacute;s informaci&oacute;n sobre el uso de servicios web, vea <a href='http://www.opengeospatial.org/standards' target=blank>http://www.opengeospatial.org/standards</a>. <div class='alert alert-info' role='alert'>i3Geo posee algunos par&aacute;metros especiales que no forman parte de los est&aacute;ndares de servicios OGC. Para ver estos par&aacute;metros accede a <a href='../i3geo/ogc.php' target='_blank' class='alert-link' >ogc.php</a></div>"
 		}],
 		"lista" : [{
 			pt : "Lista completa",
-			en : "",
-			es : ""
+			en : "Full list",
+			es : "Lista completa"
 		}],
 		"nomemeta" : [{
 			pt : "Cartogramas",
-			en : "",
-			es : ""
+			en : "Cartograms",
+			es : "Cartogramas"
 		}],
 		"nomeMapasSalvos" : [{
 			pt : "Mapas de usu&aacute;rios",
-			en : "",
-			es : ""
+			en : "User Maps",
+			es : "Mapas de usuarios"
 		}],
 		"map" : [{
 			pt : "Arquivo de configura&ccedil;&atilde;o Mapfile",
-			en : "",
-			es : ""
+			en : "Mapfile configuration file",
+			es : "Archivo de configuraci&oacute;n de Mapfile"
 		}],
 		"sld" : [{
 			pt : "Arquivo de legenda SLD",
-			en : "",
-			es : ""
+			en : "SLD legend file",
+			es : "Archivo de leyenda SLD"
 		}],
 		"filtro" : [{
 			pt : "Filtro",
-			en : "",
-			es : ""
+			en : "Filter",
+			es : "Filtro"
 		}],
 		"txtDesc" : [{
 			pt : "Os servi&ccedil;os OGC possibilitam o acesso &agrave;s camadas p&uacute;blicas configuradas no i3Geo por meio de WebServices. As camadas s&atilde;o organizadas em pastas. Navegue pelos links abaixo ou utilize a op&ccedil;&atilde;o de listagem completa de camadas.",
-			en : "",
-			es : ""
+			en : "OGC services allow you to access open-data layers configured in i3Geo through WebServices. Layers are organized into folders. Browse the links below or use the full layer list option.",
+			es : "Los servicios OGC posibilitan el acceso a las capas de acceso libre configuradas en i3Geo a trav&eacute;s de WebServices. Las capas se organizan en carpetas. Navegue por los siguientes enlaces o utilice la opci&oacute;n de lista completa de capas."
 		}]
 };
