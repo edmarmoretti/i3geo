634fd457c8a6bc852886aa8f656ae48bd6ececa5
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-11 18:15:23 -0200

    Tradução de ferramentas/analisageometrias/dicionario.js

--- ferramentas/analisageometrias/dicionario.js
+++ ferramentas/analisageometrias/dicionario.js
@@ -7,18 +7,18 @@ i3GEOF.analisaGeometrias.dicionario = {
 		}],
 		'retira': [{
 			pt:"Retira",
-			en:"Withdraw",
+			en:"Leave",
 			es:"Retira"
 		}],
 		'inverte': [{
 			pt:"Inverte",
-			en:"Invert",
+			en:"Reverse",
 			es:"Invierte"
 		}],
 		'limpa': [{
 			pt:"Limpa",
 			en:"Clear",
-			es:"Limpia"
+			es:"Limpiar"
 		}],
 		'captura': [{
 			pt:"Capturar",
@@ -27,8 +27,8 @@ i3GEOF.analisaGeometrias.dicionario = {
 		}],
 		'lista': [{
 			pt:"Listar e escolher",
-			en:"List and choose",
-			es:"Listar y elegir"
+			en:"List and select",
+			es:"Crear lista y seleccionar"
 		}],
 		'analisa': [{
 			pt:"Analisar",
@@ -37,142 +37,142 @@ i3GEOF.analisaGeometrias.dicionario = {
 		}],
 		'escolheTema': [{
 			pt:"Escolha o tema que receber&aacute; a sele&ccedil;&atilde;o",
-			en:"Choose the theme that will receive the selection",
-			es:"Elegir el tema que recibir&aacute; la selecci&oacute;n"
+			en:"Select the theme that will receive the selection",
+			es:"Seleccione el tema que recibir&aacute; la selecci&oacute;n"
 		}],
 		'tipoSelecao': [{
 			pt:"Tipo de sele&ccedil;&atilde;o",
-			en:"",
+			en:"Selection type",
 			es:"Tipo de selecci&oacute;n"
 		}],
 		'capturaGeom': [{
 			pt:"Capturar as geometrias selecionadas",
-			en:"",
+			en:"Capture selected geometries",
 			es:"Capturar las geometr&iacute;as seleccionadas"
 		}],
 		'ajuda': [{
 			pt:"Ap&oacute;s escolher o tema, clique no mapa para selecionar os elementos desejados, caso vc j&aacute; n&atilde;o tenha feito isso ou caso deseje alterar a sele&ccedil;&atilde;o. Conclu&iacute;da a sele&ccedil;&atilde;o, clique no bot&atilde;o de captura para obter as geometrias. <br>As geometrias capturadas podem ser vistas na segunda guia.",
-			en:"",
-			es:"Luego de elegir el tema, haga clic en el mapa para seleccionar los elementos deseados, en caso de que usted  a&uacute;n no lo haya hecho o desee alterar la selecci&oacute;n. Concluida la selecci&oacute;n,  haga clic en el bot&oacute;n de captura para obtener las geometr&iacute;as. <br> Las geometr&iacute;as capturadas pueden ser vistas en la segunda gu&iacute;a."
+			en:"After selecting a theme, click on the map to select desired features. Do this if you have not already selected a feature or if you want to change the selection. Once the selection is complete, click the capture button to get geometries.<br>Captured geometries can be seen in the second tab.",
+			es:"En caso de que no la haya hecho o si desea cambiar la selecci&oacute;n, luego de elegir el tema haga clic en el mapa para seleccionar las entidades deseadas. Una vez finalizada la selecci&oacute;n haga clic en el bot&oacute;n de captura para obtener las geometr&iacute;as.<br>Las geometr&iacute;as capturadas se pueden ver en la segunda pesta&ntilde;a."
 		}],
 		'ajuda2': [{
 			pt:"Marque as geometrias para aplicar os processos desejados.",
-			en:"",
-			es:"Marque las geometr&iacute;as para aplicar los procesos deseados"
+			en:"Mark the geometries to apply the desired processes.",
+			es:"Marque las geometr&iacute;as para aplicar los procesos deseados."
 		}],
 		'recalculaGeom': [{
 			pt:"Recalcula a &aacute;rea e o per&iacute;metro das novas geometrias",
-			en:"",
-			es:"Recalcula las &aacute;reas y los per&iacue;metros de las nuevas geometr&iacute;as "
+			en:"Recalculates the area and perimeter of new geometries",
+			es:"Recalcula el &aacute;rea y el per&iacute;metro de las geometr&iacute;as nuevas"
 		}],
 		'operacaoGeom': [{
 			pt:"Opera&ccedil;&otilde;es que obt&eacute;m descri&ccedil;&otilde;es sobre a geometria, retornando valores que s&atilde;o adicionados aos itens da geometria",
-			en:"",
-			es:"Operaciones que optienen descripciones sobre la geometr&iacute;a, retornando valores que son adicionados a los items de la geometr&iacute;a "
+			en:"Operations that get descriptions about geometry, returning values that are added to geometry fields",
+			es:"Operaciones que obtienen descripciones sobre la geometr&iacute;a y generan valores que se agregan a los campos de las geometr&iacute;as"
 		}],
 		'area': [{
 			pt:"&Aacute;rea",
-			en:"",
+			en:"Area",
 			es:"&Aacute;rea"
 		}],
 		'perimetro': [{
 			pt:"Per&iacute;metro",
-			en:"",
+			en:"Perimeter",
 			es:"Per&iacute;metro"
 		}],
 		'aplicar': [{
 			pt:"Aplicar",
-			en:"",
+			en:"Apply",
 			es:"Aplicar"
 		}],
 		'operacaoGeom2': [{
 			pt:"Opera&ccedil;&otilde;es que criam novas geometrias a partir de duas ou mais geometrias",
-			en:"",
-			es:"Operaciones que crean nuevas geometr&iacute;as a partir de dos o m&aacute;s geometr&iacute;as"
+			en:"Operations that create new geometries from two or more geometries",
+			es:"Operaciones que crean geometr&iacute;as nuevas a partir de dos o m&aacute;s geometr&iacute;as"
 		}],
 		'uniao': [{
 			pt:"Uni&atilde;o",
-			en:"",
+			en:"Union",
 			es:"Uni&oacute;n"
 		}],
 		'interseccao': [{
 			pt:"Intersec&ccedil;&atilde;o",
-			en:"",
+			en:"Intersection",
 			es:"Intersecci&oacute;n"
 		}],
 		'diferenca': [{
 			pt:"Diferen&ccedil;a",
-			en:"",
+			en:"Difference",
 			es:"Diferencia"
 		}],
 		'diferencaInversa': [{
 			pt:"Diferen&ccedil;a inversa",
-			en:"",
+			en:"Inverse difference",
 			es:"Diferencia inversa"
 		}],
 		'convexo': [{
 			pt:"Convexo",
-			en:"",
+			en:"Convex",
 			es:"Convexo"
 		}],
 		'operacaoGeom3': [{
 			pt:"Opera&ccedil;&otilde;es que atuam sobre uma &uacute;nica geometria",
-			en:"",
-			es:"Operaciones que act&uacute;an sobre una &uacute;nica geometr&iacute;a"
+			en:"Operations that are executed on a single geometry",
+			es:"Operaciones que act&uacute;an sobre una sola geometr&iacute;a"
 		}],
 		'convexo': [{
 			pt:"Convexo",
-			en:"",
+			en:"Convex",
 			es:"Convexo"
 		}],
 		'entorno': [{
 			pt:"Entorno",
-			en:"",
+			en:"Environment",
 			es:"Entorno"
 		}],
 		'ajuda3': [{
 			pt:"As opera&ccedil;&otilde;es de an&aacute;lise s&atilde;o executadas sobre as geometrias selecionadas.",
-			en:"",
-			es:"Las operaciones de an&aacute;lisis son ejecutadas sobre las geometr&iacute;as seleccionadas"
+			en:"The analysis operations are performed on the selected geometries.",
+			es:"Las operaciones de an&aacute;lisis se ejecutan sobre las geometr&iacute;as seleccionadas."
 		}],
 		'ajuda4': [{
 			pt:"Para maiores informa&ccedil;&otilde;es, veja",
-			en:"",
+			en:"For more information, see",
 			es:"Para mayores informaciones, vea"
 		}],
 		'ajuda5': [{
 			pt:"Algumas fun&ccedil;&otilde;es para operarem corretamente sobre temas pontuais, precisam que antes seja feita a uni&atilde;o dos pontos em uma nova geometria.",
-			en:"",
-			es:"Algunas funciones para que operen correctamente sobre temas puntuales, necesitan que antes sea hecha la uni&oacute;n entre dos puntos en una nueva geometr&iacute;a.  "
+			en:"In order to work correctly on point-based themes, some functions require that the points be merged into a new geometry.",
+			es:"Algunas funciones para que trabajen correctamente sobre temas de puntos necesitan que antes se haga la uni&oacute;n de los puntos en una geometr&iacute;a nueva."
 		}],
 		'nomeGeom': [{
 			pt:"Ecolha um nome para a geometria",
-			en:"",
-			es:"Elija un nombre para la geometr&iacute;a"
+			en:"Select a geometry name",
+			es:"Seleccione un nombre para la geometr&iacute;a"
 		}],
 		'exclui': [{
 			pt:"Excluir marcados",
-			en:"",
+			en:"Delete marked",
 			es:"Excluir marcados"
 		}],
 		'verMapa': [{
 			pt:"Ver no mapa",
-			en:"",
+			en:"See on the map",
 			es:"Ver en el mapa"
 		}],
 		'naoGeom': [{
 			pt:"Nenhuma geometria dispon&iacute;vel",
-			en:"",
+			en:"No geometry available",
 			es:"Ninguna geometr&iacute;a disponible"
 		}],
 		'aguarde': [{
 			pt:"Aguarde...",
-			en:"",
+			en:"Wait...",
 			es:"Aguarde..."
 		}],
 		'naoGeom2': [{
 			pt:"Nenhuma geometria foi marcada",
-			en:"",
+			en:"No geometry was marked",
 			es:"Ninguna geometr&iacute;a fue marcada"
 		}],
 		36: [{
