a1e9d5258bb19b992f62e780ab7c92b3a8943c5f
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-11 15:19:34 -0200

    Tradução de ferramentas/agrupaelementos/dicionario.js

--- ferramentas/agrupaelementos/dicionario.js
+++ ferramentas/agrupaelementos/dicionario.js
@@ -2,48 +2,48 @@
 i3GEOF.agrupaElementos.dicionario = {
 	'descricao' : [ {
 		pt : "Essa ferramenta transforma v&aacute;rios elementos selecionados de um tema em um s&oacute; gerando um pol&iacute;gono para cada grupo (pol&iacute;gonos convexos).",
-		en : "This tool transforms several selected features of a theme into a single feature, generating a polygon for each group (convex polygons).",
-		es : "Esta herramienta transforma varios elementos seleccionados de un tema en un &uacute;nico generando un pol&iacute;gono para cada grupo (pol&iacute;gonos convexos)."
+		en : "This tool transforms selected features of a theme into a single feature, creating a polygon for each group (convex polygons).",
+		es : "Esta herramienta transforma varias entidades seleccionadas de un tema en una entidad &uacute;nica, generando un pol&iacute;gono para cada grupo (pol&iacute;gonos convexos)."
 	} ],
 	'descricao2' : [ {
 		pt : "Para definir quais elementos devem ser unidos uns com os outros &eacute; preciso escolher um item da tabela de atributos do tema. Os elementos que possu&iacute;rem o mesmo valor ser&atilde;o considerados no mesmo grupo. Caso n&atilde;o tenha sido escolhido nenhum item, todas os elementos ser&atilde;o agrupados em um s&oacute;.",
-		en : "You need to choose an item from the table of attribute of a theme to define features that should link each other. Those features that have similar values will be included in the same group. In case you do not choose an item, all features will be grouped into a single group.",
-		es : "Para definir cu&aacute;les elementos deben ser unidos unos con otros es necesario escoger un &iacute;tem de la tabla de atributos del tema. Los elementos que poseen el mismo valor ser&aacute;n considerados en el mismo grupo. En caso que ning&uacute;n &iacute;tem haya sido escogido, todos los elementos ser&aacute;n agrupados en uno solo."
+		en : "In order to define which feature should be linked to each other, you must choose a field from the table of attributes of the theme. Features that have the same value will be group in the same group. If no field is chosen, all features will be grouped into a single one.",
+		es : "Para definir cu&aacute;les entidades deben estar unidas unas con otras es necesario elegir un campo de la tabla de atributos del tema. Las entidades que tengan el mismo valor se considerar&aacute;n en el mismo grupo. Si no se ha seleccionado ninguna entidad todas las entidades se agrupar&aacute;n en una sola."
 	} ],
 	'descricao3' : [ {
 		pt : "O resultado final ser&aacute; um novo tema com elementos diferentes dos originais e cuja tabela de atributos conter&aacute; apenas o item escolhido.",
-		en : "Final result will be a new theme with features different from the originals and whose table of attributes will contain only the selected item.",
-		es : "El resultado final ser&aacute; un nuevo tema con elementos diferentes de los originales y cuya tabla de atributos contendr&aacute; solamente el &iacute;tem escogido."
+		en : "The final result will be a new theme with features different from the original ones and whose attribute table will contain only the selected field.",
+		es : "El resultado final ser&aacute; un tema nuevo con entidades diferentes de las originales y cuya tabla de atributos contendr&aacute; solo el campo seleccionado."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema que cont&eacute;m os elementos (pelo menos um elemento deve estar selecionado)",
-		en : "Theme that contains features (at least one feature should be selected)",
-		es : "Tema que contiene los elementos (al menos un elemento debe estar seleccionado)"
+		en : "Theme containing features (at least one feature must be selected)",
+		es : "Tema que contiene las entidades (al menos una entidad debe estar seleccionada)"
 	} ],
 	'selecionaAtributo' : [ {
 		pt : "Escolha o item da tabela de atributos que ser&aacute; utilizado para agregar. (opcional)",
-		en : "Choose an item from the attribute table that will be used to add. (optional)",
-		es : "Elija el &iacute;tem de la tabla de atributos que ser&aacute; utilizada para adicionar. (opcional)"
+		en : "Select a field from the attribute table that will be used to add. (optional)",
+		es : "Seleccione el campo de la tabla de atributos que se utilizar&aacute; para agregar. (opcional)"
 	} ],
 	'adicionaAgrupamento' : [ {
 		pt : "O tema com o agrupamento ser&aacute; adicionado ao mapa atual.",
-		en : "The theme with which the group will be added to the current map.",
-		es : "El tema con el que el grupo ser&aacute; adicionado al mapa actual."
+		en : "The theme and group will be added to the current map.",
+		es : "El tema con el grupo se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
 		en : "Error. Too long time execution.",
-		es : "Error. La operaci&oacute;n tard&oacute; mucho tiempo."
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ],
 	'erroTema' : [ {
 		pt : "Tema invalido",
-		en : "Invalid theme",
-		es : "Tema invalido"
+		en : "Wrong theme",
+		es : "Tema no v&aacute;lido"
 	} ],
 	'erroItem' : [ {
 		pt : "erro ao ler os itens do tema de origem",
-		en : "error when reading items of themes of origin",
-		es : "error al leer los &iacute;tems del tema de origen"
+		en : "error when reading the fields from the source theme",
+		es : "error al leer los campos del tema de origen"
 	} ],
 	'criaAgrupamento' : [ {
 		pt : "Criar agrupamento",
