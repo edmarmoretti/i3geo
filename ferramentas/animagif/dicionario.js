be8d297634740c57eabc1d117acdc244f6ad169e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-12 18:54:54 -0200

    Tradução de ferramentas/animagif/dicionario.js

--- ferramentas/animagif/dicionario.js
+++ ferramentas/animagif/dicionario.js
@@ -2,112 +2,112 @@
 i3GEOF.animagif.dicionario = {
 	'ativaAoAdic' : [ {
 		pt : "Abre a ferramenta quando a camada &eacute; adicionada ao mapa, permitindo que o usu&aacute;rio escolha os par&acirc;metros",
-		en : "",
-		es : ""
+		en : "Opens the tool when the layer is added to the map, allowing the user to select the parameters",
+		es : "Abre la herramienta cuando la capa se agrega al mapa, permitiendo al usuario elegir los par&aacute;metros"
 	} ],
 	'execAoAdic' : [ {
 		pt : "Gera a anima&ccedil;&atilde;o quando o usu&aacute;rio adiciona a camada ao mapa",
-		en : "",
-		es : ""
+		en : "Creates the animation when the user adds a layer to the map",
+		es : "Crea la animaci&oacute;n cuando el usuario agrega la capa al mapa"
 	} ],
 	'colunat' : [ {
 		pt : "Coluna que ser&aacute; utilizada para obter a lista de datas e que receber&aacute; o filtro de data para cada quadro da anima&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Column that will be used to obtain the list of dates and will be filtered by date for each animation frame",
+		es : "Columna que se utilizar&aacute; para obtener la lista de fechas y que recibir&aacute; el filtro de fecha para cada cuadro de animaci&oacute;n"
 	} ],
 	'tempo' : [ {
 		pt : "Tempo em milisegundos entre cada quadro",
-		en : "",
-		es : ""
+		en : "Time in milliseconds between each frame",
+		es : "Tiempo en milisegundos entre cada cuadro"
 	} ],
 	'w' : [ {
 		pt : "Largura da imagem gif que ser&aacute; gerada",
-		en : "",
-		es : ""
+		en : "Gif image width that will be generated",
+		es : "Ancho de la imagen gif que se generar&aacute;"
 	} ],
 	'cache' : [ {
 		pt : "Reaproveita o arquivo gif em cache",
-		en : "",
-		es : ""
+		en : "Reuse the cached gif file",
+		es : "Reutilice el archivo gif en el cach&eacute;"
 	} ],
 	'extensao' : [ {
 		pt : "Extens&atilde;o geogr&aacute;fica (xmin, ymin, xmax, ymax)",
-		en : "",
-		es : ""
+		en : "Geographic extent (xmin, ymin, xmax, ymax)",
+		es : "Extensi&oacute;n geogr&aacute;fica (xmin, ymin, xmax, ymax)"
 	} ],
 	'legenda' : [ {
 		pt : "Mostra a legenda",
-		en : "",
-		es : ""
+		en : "Show legend",
+		es : "Muestra la leyenda"
 	} ],
 	'transparencia' : [ {
 		pt : "Cada quadro ser&aacute; transparente",
-		en : "",
-		es : ""
+		en : "Each frame will be transparent",
+		es : "Cada cuadro ser&aacute; transparente"
 	} ],
 	'tipocolunat' : [ {
 		pt : "Tipo da coluna com os valores",
-		en : "",
-		es : ""
+		en : "Column type with values",
+		es : "Tipo de columna con valores"
 	} ],
 	'nulos' : [ {
 		pt : "Lista de valores, separados por v&iacute;rgula, que ser&atilde;o exclu&iacute;dos no processo de obten&ccedil;&atilde;o da lista de datas",
-		en : "",
-		es : ""
+		en : "List of values separated by commas that will be deleted in the process of obtaining the list of dates",
+		es : "Lista de valores separados por coma que ser&aacute;n eliminados en el proceso de obtenci&oacute;n de la lista de fechas"
 	} ],
 	'operador' : [ {
-		pt : "Operador de liga&ccedil;&atilde;o entre a coluna e o valor da data, por default utiliza-se '=', mas pode ser definido tamb&eacute;m 'lt' (menor que) ou gt (maior que)",
-		en : "",
-		es : ""
+		pt : "Operador de liga&ccedil;&atilde;o entre a coluna e o valor da data, por default utiliza-se '=', mas pode ser definido tamb&eacute;m 'lt' (menor que) ou 'gt' (maior que)",
+		en : "By default '=' is used as operator for the connection between a column and a date value, but you can also use 'lt' (less than) or 'gt' (greater than)",
+		es : "Se utiliza '=' como operador por defecto de la conexi&oacute;n entre la columna y el valor de la fecha, pero se puede emplear tambi&eacute;n 'lt' (menor que) o 'gt' (mayor que)"
 	} ],
 	'geragif' : [{
 		pt : "Cria o gif animado",
-		en : "",
-		es : ""
+		en : "Create an animated gif",
+		es : "Crea un gif animado"
 	}],
 	'numerico' : [{
 		pt : "Num&eacute;rico",
-		en : "",
-		es : ""
+		en : "Numeric",
+		es : "Num&eacute;rico"
 	}],
 	'texto' : [{
 		pt : "Texto",
-		en : "",
-		es : ""
+		en : "Text",
+		es : "Texto"
 	}],
 	'sim' : [{
 		pt : "Sim",
-		en : "",
-		es : ""
+		en : "Yes",
+		es : "S&iacute;"
 	}],
 	'nao' : [{
 		pt : "N&atilde;o",
-		en : "",
-		es : ""
+		en : "No",
+		es : "No"
 	}],
 	'selecionaCol' : [{
 		pt : "Selecione uma coluna",
-		en : "",
-		es : ""
+		en : "Select a column",
+		es : "Seleccione una columna"
 	}],
 	'salvaParametros' : [ {
 		pt : "Salva par&acirc;metros no mapfile",
-		en : "",
-		es : ""
+		en : "Save parameters in the mapfile",
+		es : "Guarda par&aacute;metros en el mapfile"
 	} ],
 	'removeParametros' : [ {
 		pt : "Remove par&acirc;metros do mapfile",
-		en : "",
-		es : ""
+		en : "Remove parameters from the mapfile",
+		es : "Eliminar par&aacute;metros del mapfile"
 	} ],
 	'removePar' : [ {
 		pt : "Remove os par&acirc;metros armazenados no tema?",
-		en : "",
-		es : ""
+		en : "Do you want to remove the parameters stored in the theme?",
+		es : "&iquest;Se eliminan los par&aacute;metros almacenados en el tema?"
 	} ],
 	'incluiPar' : [ {
 		pt : "Inclui os par&acirc;metros no tema?",
-		en : "",
-		es : ""
+		en : "Do you want to include the parameters in the theme?",
+		es : "&iquest;Se incluyen los par&aacute;metros en el tema?"
 	} ]
 };
