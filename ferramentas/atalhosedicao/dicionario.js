a791cd87992ac50c4a68e9b549b50448a898c261
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-14 00:57:26 -0200

    Tradução de ferramentas/atalhosedicao/dicionario.js

--- ferramentas/atalhosedicao/dicionario.js
+++ ferramentas/atalhosedicao/dicionario.js
@@ -2,82 +2,82 @@
 i3GEOF.atalhosedicao.dicionario = {
 	'edicao' : [ {
 		pt : "Edi&ccedil;&atilde;o de mapfile",
-		en : "",
+		en : "Mapfile edition",
 		es : "Edici&oacute;n de mapfile"
 	} ],
 	'legenda' : [ {
 		pt : "Legenda",
-		en : "",
+		en : "Legend",
 		es : "Leyenda"
 	} ],
 	'salva' : [ {
 		pt : "Salva mapfile",
-		en : "",
+		en : "Save mapfile",
 		es : "Guardar mapfile"
 	} ],
 	'tabela' : [ {
 		pt : "Tabela",
-		en : "",
+		en : "Table",
 		es : "Tabla"
 	} ],
 	'texto' : [ {
 		pt : "Topon&iacute;mia",
-		en : "",
-		es : "Ttoponimia"
+		en : "Toponymy",
+		es : "Toponimia"
 	} ],
 	'etiqueta' : [ {
 		pt : "Etiquetas, busca...",
-		en : "",
-		es : "Etiqueta, busqueda..."
+		en : "Tags, search ...",
+		es : "Etiquetas, b&uacute;squeda ..."
 	} ],
 	'filtro' : [ {
 		pt : "Filtro",
-		en : "",
+		en : "Filter",
 		es : "Filtro"
 	} ],
 	'opacidade' : [ {
 		pt : "Opacidade (0 a 100)",
-		en : "",
+		en : "Opacity (0 - 100)",
 		es : "Opacidad (0 a 100)"
 	} ],
 	'aplicar' : [ {
 		pt : "Aplicar",
-		en : "",
+		en : "Apply",
 		es : "Aplicar"
 	} ],
 	'ativaCache' : [ {
 		pt : "Ativa o cache para melhorar a performance",
-		en : "",
-		es : "Activa el cache para mejorar la perfomance"
+		en : "Enable caching to improve performance",
+		es : "Activa el cach&eacute; para mejorar el rendimiento"
 	} ],
 	'ferramentas' : [ {
 		pt : "Ferramentas com par&acirc;metros que podem ser armazenados no mapfile do tema",
-		en : "",
-		es : "Herramientas con par&aacute;metros que pueden ser almazenados en el mapfile del tema"
+		en : "Tools with parameters that can be stored in the theme mapfile",
+		es : "Herramientas con par&aacute;metros que se pueden almacenar en el mapfile del tema"
 	} ],
 	"temacomgrafico" : [{
 		pt : "Gr&aacute;ficos",
-		en : "",
+		en : "Chart",
 		es : "Gr&aacute;ficos"
 	}],
 	"mostraClasses" : [{
 		pt : "Mostra as classes na legenda",
-		en : "",
-		es : ""
+		en : "Shows classes in legend",
+		es : "Muestra las clases en la leyenda"
 	}],
 	"mostraIdentifica" : [{
 		pt : "Mostra na ferramenta de identifica&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Shows in the identification tool",
+		es : "Muestra en la herramienta de identificaci&oacute;n"
 	}],
 	"removePlugin" : [{
 		pt : "Remove o plugin",
-		en : "",
-		es : ""
+		en : "Remove plugin",
+		es : "Eliminar el plugin"
 	}],
 	"plugins" : [{
 		pt : "Plugins que implementam formas especiais de visualiza&ccedil;&atilde;o. A camada pode ter apenas um plugin.",
-		en : "",
-		es : ""
+		en : "Plugins that implement special ways of visualization. The layer can only have one plugin.",
+		es : "Plugins que implementan formas especiales de visualizaci&oacute;n. La capa solo puede tener un plugin."
 	}]
 };
