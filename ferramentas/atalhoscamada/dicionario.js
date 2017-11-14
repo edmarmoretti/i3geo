8e9160cc8b65165afc0a7a59e52007d91fc2f1e5
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-13 23:51:19 -0200

    Tradução de ferramentas/atalhoscamada/dicionario.js

--- ferramentas/atalhoscamada/dicionario.js
+++ ferramentas/atalhoscamada/dicionario.js
@@ -3,63 +3,63 @@ i3GEOF.atalhoscamada.dicionario = {
 	'opacidade' : [
 		{
 			pt : "Altera a opacidade (de 0 a 100)",
-			en : "",
-			es : "Altera la opacidad"
+			en : "Changes opacity (from 0 to 100)",
+			es : "Cambia la opacidad (de 0 a 100)"
 		}
 	],
 	"temporizador" : [
 		{
 			pt : "Intervalo em segundos ap&oacute;s o qual a camada ser&aacute; atualizada",
 			en : "Interval in seconds after which the layer will be updated",
-			es : "Intervalo en segundos despues del cual la capa ser&aacute; actualizada",
+			es : "Intervalo en segundos despu&eacute;s del cual se actualizar&aacute; la capa",
 			it : "Intervalo em segundos ap&oacute;s o qual a camada ser&aacute; atualizada"
 		}
 	],
 	"temporizadorHelp" : [
   		{
   			pt : "Para ignorar ou cancelar, deixe em branco",
-  			en : "To ignore, leave blank",
-  			es : "Para ignorar, deje en blanco.",
+  			en : "To ignore or cancel, leave blank",
+  			es : "Para ignorar o cancelar, dejar en blanco",
   			it : "Para ignorar, deixe em branco"
   		}
   	],
 	"mudanome" : [
 		{
 			pt : "Muda o nome da camada",
-			en : "Rename layer. Use it for make a better legend of the map",
-			es : "Renombrar el nombre actual del tema. Utilice para mejorar la leyenda del mapa.",
+			en : "Change layer name",
+			es : "Cambia el nombre de la capa",
 			it : "Cambia il nome del tema corrente. Utilizzare per migliorare la legenda della mappa."
 		}
 	],
 	"copia" : [
 		{
 			pt : "C&oacute;pia da camada",
-			en : "",
-			es : "Crea una copia de la capa",
+			en : "Layer copy",
+			es : "Copia de la capa",
 			it : ""
 		}
 	],
 	"contorno" : [
 	   		{
 	   			pt : "Contorno (+-)",
-	   			en : "",
-	   			es : "",
+	   			en : "Outline (+-)",
+	   			es : "Contorno (+-)",
 	   			it : ""
 	   		}
 	   	],
 	"cores" : [
    		{
    			pt : "Esquema de cores",
-   			en : "",
-   			es : "",
+   			en : "Color schema",
+   			es : "Esquema de colores",
    			it : ""
    		}
    	],
 	"juncao" : [
   		{
   			pt : "Jun&ccedil;&atilde;o de camadas",
-  			en : "",
-  			es : "",
+  			en : "Layer union",
+  			es : "Uni&oacute;n de capas",
   			it : ""
   		}
   	]
