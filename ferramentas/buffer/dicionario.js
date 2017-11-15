c5b8b49238b98b5336dc374870bd2fda35523555
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-14 23:43:52 -0200

    Tradução de ferramentas/buffer/dicionario.js

--- ferramentas/buffer/dicionario.js
+++ ferramentas/buffer/dicionario.js
@@ -2,57 +2,57 @@
 i3GEOF.buffer.dicionario = {
 	'descricao' : [ {
 		pt : "O entorno, ou buffer, &eacute; um pol&iacute;gono que circunda um elemento geogr&aacute;fico em uma dist&acirc;ncia fixa.",
-		en : "",
-		es : "El entorno, o el buffer, es un pol&iacute;gono que circunda un elemento geogr&aacute;fico con una distancia fija."
+		en : "A buffer is a polygon that surrounds a geographic feature at a fixed distance.",
+		es : "El entorno o buffer es un pol&iacute;gono que circunda una entidad geogr&aacute;fica a una distancia fija."
 	} ],
 	'descricao2' : [ {
 		pt : "Para gerar o entorno, voc&ecirc; precisa selecionar alguns elementos de um tema. Utilize para isso a op&ccedil;&atilde;o de sele&ccedil;&atilde;o ou a tabela de atributos do tema desejado.",
-		en : "",
-		es : "Para generar el entorno, usted debe seleccionar algunos elementos de un tema. Utilice para esto la opci&oacute;n de selecci&oacute;n o la tabla de atributos del tema deseado."
+		en : "In order to generate a buffer, it is necessary to select some features of a theme. To do this, use the selection option or the attribute table of the selected theme.",
+		es : "Para generar el buffer es necesario seleccionar algunas entidades de un tema. Utilice para ello la opci&oacute;n de selecci&oacute;n o la tabla de atributos del tema deseado."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado",
-		en : "",
-		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
+		en : "Theme, with selection, that will be used",
+		es : "Tema, con selecci&oacute;n, que se utilizar&aacute;"
 	} ],
 	'entorno' : [ {
 		pt : "Dist&acirc;ncia do entorno em metros",
-		en : "",
-		es : "Distancia del entorno en metros"
+		en : "Buffer distance in meters",
+		es : "Distancia del buffer en metros"
 	} ],
 	'agregaElementos' : [ {
 		pt : "Considerar os elementos selecionados como se fossem um s&oacute;?",
-		en : "",
-		es : "Considerar los elementos seleccionados como si fueran uno solo"
+		en : "Do you want to regard the selected features as a single one?",
+		es : "&iquest;Quiere considerar las entidades seleccionadas como si fuesen una sola?"
 	} ],
 	'adicionaTemaEntorno' : [ {
 		pt : "O tema com o entorno ser&aacute; adicionado ao mapa atual",
-		en : "",
-		es : "El tema con el entorno ser&aacute; adicionado al mapa actual"
+		en : "The theme with the buffer will be added to the current map",
+		es : "El tema con el buffer se a&ntilde;adir&aacute; al mapa actual"
 	} ],
 	'criaEntorno' : [ {
 		pt : "Criar entorno",
-		en : "",
-		es : "Crear entorno"
+		en : "Create buffer",
+		es : "Crear buffer"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error. Execution time too long",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho"
 	} ],
 	'erroDistancia' : [ {
 		pt : "Distancia invalida",
-		en : "",
-		es : "Distancia invalida"
+		en : "Invalid distance",
+		es : "Distancia no v&aacute;lida"
 	} ],
 	'entornoAtributo' : [ {
 		pt : "Ou um item da tabela de atributos que cont&eacute;m as dist&acirc;ncias",
-		en : "",
-		es : "O un item de la tabla de atributos que contiene las distancias"
+		en : "Or a field of the attribute table that contains distances",
+		es : "O un campo de la tabla de atributos que contiene las distancias"
 	} ],
 	'multiplica' : [ {
 		pt : "Caso tenha escolhido um item, multiplicar os valores por",
-		en : "",
-		es : "Multiplicado por"
+		en : "If you have selected a field, multiply values by",
+		es : "Si ha elegido un campo, multiplicar los valores por"
 	} ]
 };
