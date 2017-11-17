84215b4304aef64eb9cb1f79db32b051d5e3b26b
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-17 00:03:02 -0200

    Tradução de ferramentas/buscafotos/dicionario.js

--- ferramentas/buscafotos/dicionario.js
+++ ferramentas/buscafotos/dicionario.js
@@ -2,127 +2,127 @@
 i3GEOF.buscaFotos.dicionario = {
 	'ajuda' : [ {
 		pt : "As fotos mostradas aqui s&atilde;o de responsabilidade dos servi&ccedil;os de hospedagem utilizados na busca e podem abranger tem&aacute;ticas diversas.",
-		en : "",
-		es : "Las fotos mostradas aqu&iacute; son de responsabilidad de los servicios de hospedaje utilizados en la b&uacute;squeda y pueden abarcar tem&aacute;ticas diversas."
+		en : "Hosting services used for searching have responsibility for photos shown here. These photos encompass various themes.",
+		es : "Las fotos que aqu&iacute; se muestran son de responsabilidad de los servicios de hospedaje utilizados en la b&uacute;squeda y pueden abarcar tem&aacute;ticas diversas."
 	} ],
 	'ajuda2' : [ {
 		pt : "A busca &eacute; feita apenas para a regi&atilde;o de abrang&ecirc;ncia do mapa atual.",
-		en : "",
-		es : "La b&uacute;squeda hecha solo para la regi&oacute;n de cobertura del mapa actual"
+		en : "Searching will be carried out only on the extent of the current map.",
+		es : "La b&uacute;squeda se realiza solo para el &aacute;rea de cobertura del mapa actual."
 	} ],
 	'ajuda3' : [ {
 		pt : "A restri&ccedil;&atilde;o de escala &eacute; necess&aacute;ria para melhorar a performance da busca.",
-		en : "",
-		es : "La restricci&oacute;n de la escala es necesaria para mejorar la performance de la b&uacute;squeda."
+		en : "Scale constraint is required to improve search performance.",
+		es : "La restricci&oacute;n de escala es necesaria para mejorar el rendimiento de la b&uacute;squeda."
 	} ],
 	'msgForaEscala' : [ {
 		pt : "O mapa atual est&aacute; fora do limite de escala.",
-		en : "",
-		es : "El mapa actual est&aacute fuera del l&iacute;mite de escala."
+		en : "The current map is out of scale range.",
+		es : "El mapa actual est&aacute; fuera del l&iacute;mite de escala."
 	} ],
 	'ajusta' : [ {
 		pt : "Ajustar",
-		en : "",
+		en : "Adjust",
 		es : "Ajustar"
 	} ],
 	'continua' : [ {
 		pt : "Continuar",
-		en : "",
+		en : "Continue",
 		es : "Continuar"
 	} ],
 	'ajudaFlickr' : [ {
 		pt : "Este produto usa APIs de diferentes servi&ccedil;os, mas n&atilde;o &eacute; endossado nem certificado por esses",
-		en : "",
-		es : ""
+		en : "This product uses APIs from different services, but is neither supported nor certified by these services",
+		es : "Este producto utiliza APIs de diferentes servicios, pero no es respaldado ni certificado por estos"
 	} ],
 	'opcoes' : [ {
 		pt : "Op&ccedil;&otilde;es: Se nenhum par&acirc;metro for definido, ser&atilde;o consideradas apenas as fotos inclu&iacute;das nas &uacute;ltimas 12 horas.",
-		en : "",
-		es : "Opciones: si ning&acute;n par&aacute;metro es definido, apenas ser&aacute;n consideradas las fotos incluidas en las &uacute;ltimas 12 horas."
+		en : "Options: If no parameter is set, only photos included in the last 12 hours will be taken into account.",
+		es : "Opciones: Si no se define ning&uacute;n par&aacute;metro, solo ser&aacute;n consideradas las fotos incluidas en las &uacute;ltimas 12 horas."
 	} ],
 	'maxRegistros' : [ {
 		pt : "De qualquer forma, s&atilde;o mostrados no m&aacute;ximo 250 registros.",
-		en : "",
-		es : "De cualquier forma, son mostrados como m%aacute;ximo 250 registros."
+		en : "In any case, a maximum of 250 records will be shown.",
+		es : "En cualquier caso, se mostrar&aacute;n como m&aacute;ximo 250 registros."
 	} ],
 	'ajudaAno' : [ {
 		pt : "O ano refere-se a data de inclus&atilde;o da foto no Flickr",
-		en : "",
-		es : "El a&ntilde; se refiere al dato de inclusi&oacute;n de la foto en el Flickr"
+		en : "The year refers to the upload date of the photo on Flickr",
+		es : "El a&ntilde;o se refiere a la fecha de inclusi&oacute;n de la foto en Flickr"
 	} ],
 	'texto' : [ {
 		pt : "Texto",
-		en : "",
+		en : "Text",
 		es : "Texto"
 	} ],
 	'anoInicial' : [ {
 		pt : "Ano inicial",
-		en : "",
+		en : "Start year",
 		es : "A&ntilde;o inicial"
 	} ],
 	'anoFinal' : [ {
 		pt : "Ano final",
-		en : "",
+		en : "End year",
 		es : "A&ntilde;o final"
 	} ],
 	'procuraFotos' : [ {
 		pt : "Procurar fotos",
-		en : "",
+		en : "Search photos",
 		es : "Buscar fotos"
 	} ],
 	'servidores' : [ {
 		pt : "Servidores",
-		en : "",
+		en : "Servers",
 		es : "Servidores"
 	} ],
 	'msgAguarde' : [ {
 		pt : "Aguarde...",
-		en : "",
+		en : "Wait...",
 		es : "Aguarde..."
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error. Execution time too long.",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ],
 	'erroAcessoDados' : [ {
 		pt : "Problemas no acesso aos dados!",
-		en : "",
-		es : "Problemas en el acceso a los datos"
+		en : "Problems with data access!",
+		es : "&iexcl;Problemas en el acceso a los datos!"
 	} ],
 	'erroNadaEncontrado' : [ {
 		pt : "Nada encontrado nessa regi&atilde;o!",
-		en : "",
-		es : "¡Nada encontrado en esta regi&oacute;n!"
+		en : "Nothing found in this region!",
+		es : "&iexcl;Nada encontrado en esa regi&oacute;n!"
 	} ],
 	'maisFotos' : [ {
 		pt : "mais 15 fotos...",
-		en : "",
-		es : "m&aacute;s de 15 fotos..."
+		en : "plus 15 photos...",
+		es : "m&aacute;s 15 fotos..."
 	} ],
 	'fotosEncontradas' : [ {
 		pt : "Fotos encontradas na regi&atilde;o vista no mapa",
-		en : "",
-		es : "Fotos encontradas en la regi&oacute;n viste en el mapa"
+		en : "Photos found in the region that is visualized on the map",
+		es : "Fotos encontradas en la regi&oacute;n que se visualiza en el mapa"
 	} ],
 	'erroNadaEncontrado2' : [ {
 		pt : "Nada encontrado nessa regi&atilde;o!",
-		en : "",
-		es : "Nada encontrado en esta regi&aacute;n "
+		en : "Nothing found in this region!",
+		es : "&iexcl;Nada encontrado en esa regi&oacute;n!"
 	} ],
 	"fotos" : [ {
 		pt : "Fotos",
-		en : "",
-		es : ""
+		en : "Photos",
+		es : "Fotos"
 	} ],
 	"parflickr" : [ {
 		pt : "Par&acirc;metros que afetam o servi&ccedil;o do Flicker",
-		en : "",
-		es : ""
+		en : "Parameters that affect the Flicker service",
+		es : "Par&aacute;metros que afectan al servicio de Flicker"
 	} ],
 	'msgZoom' : [ {
 		pt : "Voc&ecirc; precisa alterar o zoom para aproximar mais o mapa (no m&iacute;nimo 1:3.000.000)",
-		en : "",
-		es : ""
+		en : "You need to change the zoom to get closer the map (at least 1:3,000,000)",
+		es : "Usted necesita cambiar el zoom para acercar m&aacute;s el mapa (al menos 1:3.000.000)"
 	} ]
 };
