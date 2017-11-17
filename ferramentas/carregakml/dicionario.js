a072612815430028aebcaa1b731ed6564759114f
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-17 00:22:35 -0200

    Tradução de ferramentas/carregakml/dicionario.js

--- ferramentas/carregakml/dicionario.js
+++ ferramentas/carregakml/dicionario.js
@@ -2,7 +2,7 @@
 i3GEOF.carregakml.dicionario = {
 	'insereKml' : [ {
 		pt : "Endere&ccedil;o (URL) do KML (ou escolha da lista)",
-		en : "",
-		es : "Direcci&oacute;n (URL) del KML (o elija de la lista)"
+		en : "KML URL (or select from the list)",
+		es : "Direcci&oacute;n (URL) de KML (o seleccione la lista)"
 	} ]
 };
