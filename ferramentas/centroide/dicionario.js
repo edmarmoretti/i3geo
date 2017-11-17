a5383d61a24b030116dfc43b4c559fc0cf5f1d26
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-17 00:51:54 -0200

    Tradução de ferramentas/centroide/dicionario.js

--- ferramentas/centroide/dicionario.js
+++ ferramentas/centroide/dicionario.js
@@ -2,27 +2,27 @@
 i3GEOF.centroide.dicionario = {
 	'ajuda' : [ {
 		pt : "Calcula o centro de cada pol&iacute;gono gerado uma nova camada. O c&aacute;lculo baseia-se no pol&iacute;gono envolvente e podem ocorrer casos em que o ponto &eacute; posicionado fora do pol&iacute;gono fonte",
-		en : "",
-		es : "Calcula el centro de cada pol&iacute;gono generado una nueva camada. El c&aacute;lculo se basa en el pol&iacute;gono envolvente y puede ocurrir casos en que el punto sea posicionado fuera del pol&iacute;gono fuente"
+		en : "Calculates the center of each polygon. A new layer is created by this operation. The calculation is based on the surrounding polygon technique. There may be cases where a point is placed outside the source polygon",
+		es : "Calcula el centro de cada pol&iacute;gono generando una capa nueva. El c&aacute;lculo se basa en el pol&iacute;gono envolvente y pueden ocurrir casos en los que el punto se coloca fuera del pol&iacute;gono fuente"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado:",
-		en : "",
-		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
+		en : "Theme, with selection, that will be used:",
+		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado:"
 	} ],
 	'ajuda2' : [ {
 		pt : "O tema com os pontos ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : "El tema con los puntos ser&aacute; adicionado al mapa actual"
+		en : "The theme with the points will be added to the current map.",
+		es : "El tema con los puntos se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'criaCentroide' : [ {
 		pt : "Criar centr&oacute;ides",
-		en : "",
-		es : "Crear centroide"
+		en : "Create centroids",
+		es : "Crear centroides"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error.Too long time execution.",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ]
 };
