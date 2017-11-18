3fc6ff28e84acdae52ab759660786e1020989588
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-18 21:34:08 -0200

    Tradução de ferramentas/convertekml/dicionario.js

--- ferramentas/convertekml/dicionario.js
+++ ferramentas/convertekml/dicionario.js
@@ -2,27 +2,27 @@
 i3GEOF.converteKml.dicionario = {
 	'ajuda' : [ {
 		pt : "Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar dados em softwares que aceitam o formato kml, como o",
-		en : "",
-		es : "Usted puede utilizar las direcciones para visualizar datos en softwares que acepten el formato kml, como &eacute;l"
+		en : "You can use the addresses to view data in a software that supports kml format, such as",
+		es : "Usted puede utilizar las direcciones para visualizar datos en programas que aceptan el formato kml, como el"
 	} ],
 	'ajuda2' : [ {
 		pt : "para mais detalhes sobre como usar o link kml no Google Earth.",
-		en : "",
-		es : "para m&aacute;s detalles sobre c&oacute;mo usar el link kml en Google Earth"
+		en : "for more details about using kml links on Google Earth.",
+		es : "para m&aacute;s detalles sobre c&oacute;mo utilizar el enlace kml en Google Earth."
 	} ],
 	'kmlWMS' : [ {
 		pt : "Kml com GroundOverlay baseado em um servico WMS:",
-		en : "",
-		es : "Kml con GroundOverlay basado en un servicio WMS"
+		en : "Kml with GroundOverlay based on a WMS service:",
+		es : "Kml con GroundOverlay basado en un servicio WMS:"
 	} ],
 	'kmzKML' : [ {
 		pt : "Kmz que gera um arquivo kml vetorial:",
-		en : "",
-		es : "Kmz que genera un archivo kml vectorial"
+		en : "Kmz that generates a vector kml file:",
+		es : "Kmz que genera un archivo kml vectorial:"
 	} ],
 	'ajuda3' : [ {
 		pt : "Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no GoogleEarth",
-		en : "",
-		es : "Usted puede tambi&eacute;n utilizar el siguiente link (abajo) para mostrar el &aacute;rbol completo de temas en Google Earth"
+		en : "You can also use the link below to display the whole theme tree on GoogleEarth",
+		es : "Usted puede tambi&eacute;n utilizar el siguiente enlace para ver el &aacute;rbol completo de temas en GoogleEarth"
 	} ]
 };
