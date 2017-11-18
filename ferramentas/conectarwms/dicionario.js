9d7885c0539339d3f20c4569b982f16d5536e097
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-18 20:40:13 -0200

    Tradução de ferramentas/conectarwms/dicionario.js

--- ferramentas/conectarwms/dicionario.js
+++ ferramentas/conectarwms/dicionario.js
@@ -2,42 +2,42 @@
 i3GEOF.conectarwms.dicionario = {
 	'erro' : [ {
 		pt : "Oops! Ocorreu um erro",
-		en : "",
-		es : "¡Oops! Ocurri&oacute; un error"
+		en : "Oops! An error has occurred",
+		es : "&iexcl;Oops! Ocurri&oacute; un error"
 	} ],
 	'disponibilidade' : [ {
 		pt : "disponibilidade",
-		en : "",
+		en : "availability",
 		es : "disponibilidad"
 	} ],
 	'acessos' : [ {
 		pt : "acessos considerados",
-		en : "",
-		es : "Accesos considerados"
+		en : "considered accesses",
+		es : "accesos considerados"
 	} ],
 	'servico' : [ {
 		pt : "Servi&ccedil;o n&atilde;o definido",
-		en : "",
+		en : "Undefined service",
 		es : "Servicio no definido"
 	} ],
 	'listaLayers' : [ {
 		pt : "Lista as camadas do WMS",
-		en : "",
-		es : ""
+		en : "Lists WMS layers",
+		es : "Presenta una lista de las capas WMS"
 	} ],
 	'sel' : [ {
 		pt : "Digite o endere&ccedil;o do servi&ccedil;o ou escolha da lista abaixo",
-		en : "",
-		es : ""
+		en : "Enter the address of the service or choose from the list below",
+		es : "Introduzca la direcci&oacute;n del servicio o seleccione de la lista siguiente"
 	} ],
 	'listasel' : [ {
 		pt : "Lista de servi&ccedil;os cadastrados",
-		en : "",
-		es : ""
+		en : "List of registered services",
+		es : "Lista de servicios registrados"
 	} ],
 	5 : [ {
 		pt : "Ooops! Problemas ao acessar o servi&ccedil;o.",
-		en : "",
-		es : "¡Oops! Problemas para acceder al servicio."
+		en : "Ooops! Problems accessing the service.",
+		es : "&iexcl;Ooops! Problemas al acceder al servicio."
 	} ]
 };
