8a5e157fb1454f1ad3c939dd93ab8ea180a6cc50
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-18 01:12:12 -0200

    Tradução de ferramentas/centromassa/dicionario.js

--- ferramentas/centromassa/dicionario.js
+++ ferramentas/centromassa/dicionario.js
@@ -2,42 +2,42 @@
 i3GEOF.centromassa.dicionario = {
 	'ajuda' : [ {
 		pt : "O centro m&eacute;dio &eacute; uma opera&ccedil;&atilde;o que resulta em um novo tema contendo o ponto que indica o centro de equil&iacute;brio de uma distribui&ccedil;&atilde;o.",
-		en : "",
-		es : "El centro medio es una operaci&oacute;n que resulta en un nuevo tema que contiene el punto que indica el centro de equilibrio de una distribuci&oacute;n"
+		en : "The middle-center is an operation that results in a new theme containing a point that indicates the center of equilibrium of a distribution.",
+		es : "El centro medio es una operaci&oacute;n que resulta en un tema nuevo que contiene un punto que indica el centro de equilibrio de una distribuci&oacute;n."
 	} ],
 	'ajuda2' : [ {
 		pt : "Para gerar o c&aacute;lculo &eacute; necess&aacute;rio que no mapa exista pelo menos um tema de pontos. Se n&atilde;o houverem elementos selecionados, ser&aacute; considerada a extens&atilde;o geogr&aacute;fica.",
-		en : "",
-		es : "Para generar el c&aacute;lculo es necesario que en el mapa exista por lo menos un tema de puntos. Si no hubiera elementos seleccionados, ser&aacute; considerada la extensi&oacute;n geogr&aacute;fica."
+		en : "In order to carry out the calculation, it is necessary that in the map there is at least one point-based theme. If no features are selected the geographic extent will be taken into account for the calculation.",
+		es : "Para efectuar el c&aacute;lculo es necesario que en el mapa exista al menos un tema de puntos. De no haber entidades seleccionadas se considerar&aacute; la extensi&oacute;n geogr&aacute;fica."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema contendo os pontos:",
-		en : "",
+		en : "Theme that contains the points:",
 		es : "Tema que contiene los puntos:"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Utilizar o seguinte item para ponderar os valores:",
-		en : "",
-		es : "Utilizar la siguiente &iacute;tem para ponderar los valores:"
+		en : "Use the following field to weight the values:",
+		es : "Utilice el campo siguiente para ponderar los valores:"
 	} ],
 	'ajuda3' : [ {
 		pt : "O tema com o centro ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : "El tema con el centro ser&aacute; adicionado al mapa actual."
+		en : "The theme with the center of equilibrium will be added to the current map.",
+		es : "El tema con el centro se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'calcula' : [ {
 		pt : "Calcular",
-		en : "",
+		en : "Calculate",
 		es : "Calcular"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error. Too long time execution.",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ],
 	'selecionaTema2' : [ {
 		pt : "Selecione um tema",
-		en : "",
+		en : "Select a theme",
 		es : "Seleccione un tema"
 	} ],
 	9 : [ {
