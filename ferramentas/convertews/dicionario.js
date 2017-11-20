52219ec194c04fda95a60651a2eb8475ae81baca
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-20 01:29:14 -0200

    Tradução de ferramentas/convertews/dicionario.js

--- ferramentas/convertews/dicionario.js
+++ ferramentas/convertews/dicionario.js
@@ -2,32 +2,32 @@
 i3GEOF.converteMapaWS.dicionario = {
 	'ajudaDesktop' : [ {
 		pt : "Voc&ecirc; pode utilizar o endere&ccedil;o em softwares de geoprocessamento instalados em seu computador, como por exemplo, o gvSIG ou QGIS",
-		en : "",
-		es : "Usted puede utilizar la direcci&oacute;n en el software de geoprocesamiento instalado en su ordenador, como por ejemplo: el"
+		en : "You can use the address in a geoprocessing software installed on your computer, such as gvSIG or QGIS",
+		es : "Usted puede utilizar la direcci&oacute;n en programas de geoprocesamiento instalados en su computadora, como por ejemplo gvSIG o QGIS"
 	} ],
 	'ajudaWms' : [ {
 		pt : "O Web Service criado, utiliza o padr&atilde;o WMS, conforme definido pelo OGC. A disponibilidade do endere&ccedil;o &eacute; tempor&aacute;ria, permanecendo ativa apenas no dia em que foi criado.",
-		en : "",
-		es : "El Web Service creado, utiliza el padr&oacute;n WMS, conforme lo definido por el OGC. La disponibilidad de la direcci&oacute;n temporal, permanecer&aacute; activa apenas el d&iacute;a en que fue creada."
+		en : "The newly created Web Service uses the WMS according to OGC standards. The availability of the address is temporary and it only remains active the day it is created.",
+		es : "El servicio Web creado utiliza el est&aacute;ndar WMS seg&uacute;n lo definido por el OGC. La disponibilidad de la direcci&oacute;n es temporal y solo permanece activa el d&iacute;a en que es creada."
 	} ],
 	'ajudaWmc' : [ {
 		pt : "O Web Map Context (WMC) pode ou n&atilde;o ser gerado dependendo da configura&ccedil;&atilde;o do servidor onde o i3Geo est&aacute; instalado.",
-		en : "",
-		es : "El Web Map Context (WMC) puede o no ser generado dependiendo de la configuraci&;oacuten del servidor el  i3GEO est&eacute; instalado."
+		en : "Web Map Context (WMC) may or may not be generated depending on the configuration of the server where i3Geo is installed.",
+		es : "El Web Map Context (WMC) puede o no generarse dependiendo de la configuraci&oacute;n del servidor donde est&aacute; instalado i3Geo."
 	} ],
 	'testa' : [ {
 		pt : "Clique para testar",
-		en : "",
+		en : "Click to test",
 		es : "Haga clic para probar"
 	} ],
 	'erroWms' : [ {
 		pt : "Ocorreu um erro ao criar o WMS",
-		en : "",
-		es : "Ocurri&oacute; un error al crear el WMS"
+		en : "Error creating WMS",
+		es : "Se produjo un error al crear el WMS"
 	} ],
 	'wmc' : [ {
 		pt : "Clique para obter o Web Map Context",
-		en : "",
+		en : "Click to get the Web Map Context",
 		es : "Haga clic para obtener el Web Map Context"
 	} ]
 };
