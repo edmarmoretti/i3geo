79fd857619e7c81cecc783ecf3b54beb1a28d6c0
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-20 00:54:10 -0200

    Tradução de ferramentas/convertemapakml/dicionario.js

--- ferramentas/convertemapakml/dicionario.js
+++ ferramentas/convertemapakml/dicionario.js
@@ -1,28 +1,28 @@
 //+$trad(1,i3GEOF.convertemapaKml.dicionario)+
 i3GEOF.converteMapaKml.dicionario = {
 	'ajuda' : [ {
-		pt : "Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar o mapa atual em softwares que aceitam o formato kml,",
-		en : "",
-		es : "Usted puede utilizar las direcciones para visualizar datos en softwares que acepten el formato kml"
+		pt : "Voc&ecirc; pode utilizar os endere&ccedil;os para visualizar o mapa atual em softwares que aceitam o formato kml",
+		en : "You can use the URLs to view the current map in a software that supports kml format",
+		es : "Usted puede utilizar las direcciones para visualizar el mapa actual en programas que aceptan el formato kml"
 	} ],
 	'ajuda2' : [ {
 		pt : "O endere&ccedil;o de acesso &eacute; tempor&aacute;rio, ficando dispon&iacute;vel por determinado per&iacute;odo de tempo conforme definido pelo administrador do i3Geo.",
-		en : "",
-		es : "La direcci&oacute;n de acceso es temporal, queda disponible por un periodo de tiempo determinado por el administrador de i3Geo. "
+		en : "The entry address is temporary and will be available for a certain period of time defined by the i3Geo administrator.",
+		es : "La direcci&oacute;n de acceso es temporal y estar&aacute; disponible durante un determinado per&iacute;odo de tiempo definido por el administrador de i3Geo."
 	} ],
 	'ajuda3' : [ {
 		pt : "para mais detalhes sobre como usar o link kml no Google Earth.",
-		en : "",
-		es : "para m&acute;s detalles sobre c&oacute;mo usar el link km en Google Earth."
+		en : "for more details on using the kml link on Google Earth.",
+		es : "para m&aacute;s detalles sobre c&oacute;mo utilizar el enlace kml en Google Earth."
 	} ],
 	'wms' : [ {
 		pt : "Kml baseado em um servico WMS:",
-		en : "",
+		en : "Kml based on a WMS service:",
 		es : "Kml basado en un servicio WMS"
 	} ],
 	'ajuda4' : [ {
-		pt : "Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no GoogleEarth (incluindo dados vetoriais)",
-		en : "",
-		es : "Usted puede tambi&eacute;n utilizar el siguiente link (abajo) para mostrar el &aacute;rbol completo de temas en Google Earth"
+		pt : "Voc&ecirc; pode tamb&eacute;m utilizar o link abaixo para mostrar a &aacute;rvore completa de temas no Google Earth (incluindo dados vetoriais)",
+		en : "You can also use the link below to show the whole theme tree on Google Earth (including vector data)",
+		es : "Usted puede tambi&eacute;n utilizar el siguiente enlace para mostrar el &aacute;rbol completo de temas en Google Earth (incluidos los datos vectoriales)"
 	} ]
 };
