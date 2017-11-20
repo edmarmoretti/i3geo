0d23cd1f6faef0a04d516470a0498f75610f464e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-20 02:29:27 -0200

    Tradução de ferramentas/distancia/dicionario.js

--- ferramentas/distancia/dicionario.js
+++ ferramentas/distancia/dicionario.js
@@ -1,18 +1,18 @@
 //+$trad(1,i3GEOF.distancia.dicionario)+
 i3GEOF.distancia.dicionario = {
 		'incorporar' : [{
-			pt : "Incroporar figura ao mapa",
-			en : "",
-			es : ""
+			pt : "Incorporar figura ao mapa",
+			en : "Add shape to map",
+			es : "Agregar figura al mapa"
 		}],
 		'perfilDistancia' : [{
 			pt : "Perfil do relevo",
-			en : "",
-			es : ""
+			en : "Relief profile",
+			es : "Perfil del relieve"
 		}],
 		'inicia' : [{
 			pt : "Clique em diferentes pontos do mapa para tra&ccedil;ar uma linha",
-			en : "",
-			es : ""
+			en : "Click on different points on the map to draw a line",
+			es : "Haga clic en diferentes puntos del mapa para trazar una l&iacute;nea"
 		}]
 };
