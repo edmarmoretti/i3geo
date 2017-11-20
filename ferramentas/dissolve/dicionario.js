eb2f2e4d4f747a6290aefdf9a5907a6eeae04c30
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-20 02:20:27 -0200

    Tradução de ferramentas/dissolve/dicionario.js

--- ferramentas/dissolve/dicionario.js
+++ ferramentas/dissolve/dicionario.js
@@ -2,37 +2,37 @@
 i3GEOF.dissolve.dicionario = {
 	'descricao' : [ {
 		pt : "Essa ferramenta transforma v&aacute;rios pol&iacute;gonos em um s&oacute; eliminando as divisas entre eles. Caso seja escolhido um item da tabela de atributos, os pol&iacute;gonos que possu&iacute;rem o mesmo valor ser&atilde;o considerados no mesmo grupo e suas divisas eliminadas e caso n&atilde;o tenha sido escolhido nenhum item, todas os pol&iacute;gonos ser&atilde;o agrupados em um s&oacute;. O resultado final ser&aacute; um novo tema com pol&iacute;gonos diferentes dos originais e cuja tabela de atributos conter&aacute; apenas um item.",
-		en : "",
-		es : "Esta herramienta transforma varios pol&iacute;gonos en uno solo, eliminando las divisiones entre los mismos. En caso que sea seleccionado un &iacute;tem de la tabla de atributos, los pol&iacute;gonos que poseen el mismo valor formaran parte del mismo grupo y sus divisiones ser&aacute;n eliminadas. En caso que ning&uacute;n &iacute;tem sea seleccionado, todos los pol&iacute;gonos serán agrupados en un &uacute;nico pol&iacute;gono. El resultado final ser&aacute; un nuevo tema con pol&iacute;gonos diferentes a los originales y cuya tabla de atributos contendr&aacute; apenas un  &iacute;tem. "
+		en : "This tool transforms several polygons into a single polygon eliminating boundaries between them. If a field of the attribute table is selected, those polygons with the same value will be considered in the same group and their boundaries eliminated. If no field is selected, all polygons will be grouped into a single one. The final result will be a new theme with polygons different from the original ones and whose attribute table will contain only one field.",
+		es : "Esta herramienta transforma varios pol&iacute;gonos en un pol&iacute;gono &uacute;nico eliminando las divisiones entre ellos. Si se elige un campo de la tabla de atributos, los pol&iacute;gonos que tengan el mismo valor para ese campo ser&aacute;n considerados en el mismo grupo y sus divisiones eliminadas. Si no se selecciona ning&uacute;n campo todos los pol&iacute;gonos se agrupar&aacute;n en uno solo. El resultado final ser&aacute; un tema nuevo con pol&iacute;gonos diferentes a los originales y cuya tabla de atributos contendr&aacute; solo un campo."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, com sele&ccedil;&atilde;o, que ser&aacute; utilizado:",
-		en : "",
-		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado"
+		en : "Theme, with selection, that will be used:",
+		es : "Tema, con selecci&oacute;n, que ser&aacute; utilizado:"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Escolha o item que ser&aacute; utilizado (opcional)",
-		en : "",
-		es : "Elija el &iacute;tem que ser&aacute; utilizado (opcional)"
+		en : "Choose the field that will be used (optional)",
+		es : "Seleccione campo que se utilizar&aacute; (opcional)"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema com as divisas eliminadas ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : "El tema con las divisiones elimindas ser&aacute; adicionado al mapa actual"
+		en : "The theme with dissolved boundaries will be added to the current map.",
+		es : "El tema con las divisiones eliminadas se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'dissolve' : [ {
 		pt : "Dissolver",
-		en : "",
+		en : "Dissolve",
 		es : "Disolver"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error. Too long time execution.",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ],
 	'escolhaTema' : [ {
 		pt : "Escolha um tema",
-		en : "",
-		es : ""
+		en : "Select a theme",
+		es : "Seleccione un tema"
 	} ]
 };
