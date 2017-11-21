b1a11b40e436b8a1cf4b2b0e2ba1101e2978886e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-21 02:05:48 -0200

    Tradução de ferramentas/download/dicionario.js

--- ferramentas/download/dicionario.js
+++ ferramentas/download/dicionario.js
@@ -2,22 +2,22 @@
 i3GEOF.download.dicionario = {
 	'download' : [ {
 		pt : "Clique com o bot&atilde;o da direita do mouse sobre o(s) arquivo(s) abaixo para fazer o download.",
-		en : "",
-		es : "Haga clic con el bot&oacute;n derecho del mouse sobre el/los archivo/s abajo para hacer download. "
+		en : "Click the right mouse button on the file(s) below to download.",
+		es : "Haga clic con el bot&oacute;n derecho del rat&oacute;n sobre el (los) archivo (s) abajo para descargar."
 	} ],
 	'erroTema' : [ {
 		pt : "Ocorreu um erro. O tema n&atilde;o foi encontrado. Pode ser que o c&oacute;digo do tema n&atilde;o existe na defini&ccedil;&atilde;o do mapfile. Informe o administrador do sistema.",
-		en : "",
-		es : "Ocurri&oacute; un error. El tema no fue encontrado. Puede ser que el c&oacute;digo del tema no exista en la definici&oacute;n del mapfile. Informe al administrador del sistema."
+		en : "An error has occurred. Theme not found. The theme code may not exist in the mapfile definition. Please inform your system administrator.",
+		es : "Ocurri&oacute; un error. El tema no fue encontrado. Es posible que el c&oacute;digo del tema no exista en la definici&oacute;n del mapfile. Informe al administrador del sistema."
 	} ],
 	'registros' : [ {
 		pt : "N&uacute;mero de registros",
-		en : "",
+		en : "Number of records",
 		es : "N&uacute;mero de registros"
 	} ],
 	'pagdownload' : [ {
 		pt : "Acesse aqui a p&aacute;gina de download, que cont&eacute;m mais op&ccedil;&otilde;es",
-		en : "",
-		es : ""
+		en : "For more options, access the download page here",
+		es : "Para m&aacute;s opciones, acceda aqu&iacute; a la p&aacute;gina de descarga"
 	} ]
 };
