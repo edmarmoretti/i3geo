f08c3e4c4ed5b462b7ca98d9c5100a53cda88f1b
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-21 11:20:03 -0200

    Tradução de ferramentas/etiqueta/dicionario.js

--- ferramentas/etiqueta/dicionario.js
+++ ferramentas/etiqueta/dicionario.js
@@ -2,17 +2,17 @@
 i3GEOF.etiqueta.dicionario = {
 	'selecionaTema' : [ {
 		pt : "Escolha um tema da lista",
-		en : "",
-		es : ""
+		en : "Select a theme from the list",
+		es : "Seleccione un tema de la lista"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Ative ou desative os itens que ser&atilde;o mostrados na ferramenta tabela. Dos itens ativos, escolha quais ser&atilde;o usados na ferramenta de identifica&ccedil;&atilde;o simples (tip). Opcionalmente, indique os apelidos dos itens ativos e qual o item que ser&aacute; utilizado na ferramenta de busca r&aacute;pida. Links s&atilde;o opcionais e podem receber outros itens como par&acirc;metros de substitui&ccedil;&atilde;o, nesse caso, indique o item com [nomedoitem].",
-		en : "",
-		es : ""
+		en : "Enable or disable fields that will be displayed on the table tool. For active items, select which ones will be used for the simple identification tool (tip). Optionally, indicate alias of active fields and which field will be used for the quick search tool. Links are optional and may receive another field as replacement parameter, in which case, specify the field with [nomedoitem].",
+		es : "Activar o desactivar los campos que se mostrar&aacute;n en la herramienta tabla. De los elementos activos, seleccione los que se utilizar&aacute;n en la herramienta de identificaci&oacute;n simple (tip). Opcionalmente, indique los alias de los campos activos y cu&aacute;l se utilizar&aacute; en la herramienta de b&uacute;squeda r&aacute;pida. Los v&iacute;nculos son opcionales y pueden recibir otros campos como par&aacute;metros de sustituci&oacute;n, en cuyo caso, indique el campo con [nomedoitem]."
 	} ],
 	'desativaEtiquetas' : [ {
 		pt : "Desativar",
-		en : "",
-		es : ""
+		en : "Disable",
+		es : "Desactivar"
 	} ]
 };
