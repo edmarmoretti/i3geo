c5f96e0e7d93219107f37a4067beb336b7016517
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-21 12:14:24 -0200

    Tradução ferramentas/filtraperiodo/dicionario.js

--- ferramentas/filtraperiodo/dicionario.js
+++ ferramentas/filtraperiodo/dicionario.js
@@ -1,47 +1,47 @@
 i3GEOF.filtraperiodo.dicionario = {
 		'filtro' : [ {
 			pt : "Filtro temporal",
-			en : "",
-			es : ""
+			en : "Temporary filter",
+			es : "Filtro temporal"
 		} ],
 		'camada' : [ {
 			pt : "Camada que receber&aacute; o filtro",
-			en : "",
-			es : ""
+			en : "Layer that will be filtered",
+			es : "Capa que recibir&aacute; el filtro"
 		} ],
 		'inicio' : [ {
 			pt : "In&iacute;cio do per&iacute;odo",
-			en : "",
-			es : ""
+			en : "Beginning of period",
+			es : "Inicio del per&iacute;odo"
 		} ],
 		'fim' : [ {
 			pt : "Fim do per&iacute;odo",
-			en : "",
-			es : ""
+			en : "End of period",
+			es : "Fin del per&iacute;odo"
 		} ],
 		'aplicar' : [ {
 			pt : "Aplicar",
-			en : "",
-			es : ""
+			en : "Apply",
+			es : "Aplicar"
 		} ],
 		'remover' : [ {
 			pt : "Remover",
-			en : "",
-			es : ""
+			en : "Remove",
+			es : "Eliminar"
 		} ],
 		'msgIni' : [ {
 			pt : "Ao agregar dados por per&iacute;odo a forma de c&aacute;lculo (soma ou m&eacute;dia) &eacute; mantida conforme o orginal da camada j&aacute; existente",
-			en : "",
-			es : ""
+			en : "When adding data by period, the type of calculation (sum or average) is kept according to the original layer",
+			es : "Al agregar datos por per&iacute;odo, la forma de c&aacute;lculo (suma o media) se mantiene conforme al original de la capa existente"
 		} ],
 		'nomepar' : [ {
 			pt : "Nome do par&acirc;metro",
-			en : "",
-			es : ""
+			en : "Parameter name",
+			es : "Nombre del par&aacute;metro"
 		} ],
 		'valor' : [ {
 			pt : "Valor do par&acirc;metro",
-			en : "",
-			es : ""
+			en : "Parameter Value",
+			es : "Valor del par&aacute;metro"
 		} ]
 };
