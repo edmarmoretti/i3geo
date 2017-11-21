98566c8bc7fe7f551a227d0433dc9fc0b2e9fa84
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-21 10:34:49 -0200

    Tradução de ferramentas/editorsql/dicionario.js

--- ferramentas/editorsql/dicionario.js
+++ ferramentas/editorsql/dicionario.js
@@ -2,22 +2,22 @@
 i3GEOF.editorsql.dicionario = {
 	'valores' : [ {
 		pt : "Valores encontrados:",
-		en : "",
-		es : ""
+		en : "Found values:",
+		es : "Valores encontrados:"
 	} ],
 	'manualSql' : [ {
 		pt : "Veja aqui o manual de fun&ccedil;&otilde;es SQL do Postgis",
-		en : "",
-		es : ""
+		en : "See here Postgis SQL functions manual",
+		es : "Vea aqu&iacute; el manual de funciones SQL de Postgis"
 	} ],
 	'listaAtributos' : [ {
 		pt : "Lista de itens existentes na tabela de atributos do tema (escolha um para ver os valores)",
-		en : "",
-		es : ""
+		en : "List of fields of the theme attribute table (choose one to see values)",
+		es : "Lista de campos de la tabla de atributos del tema (elija uno para ver los valores)"
 	} ],
 	'removeMetadados' : [ {
 		pt : "Remove os metadados que fazem refer&ecirc;ncia as colunas do SQL",
-		en : "",
-		es : ""
+		en : "Removes metadata containing SQL columns",
+		es : "Elimina los metadatos que hacen referencia a las columnas SQL"
 	} ]
 };
