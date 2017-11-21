0a1a813da176aede04217659fc489fbb4cafc352
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-21 01:43:59 -0200

    Traduçao de ferramentas/distanciaptpt/dicionario.js

--- ferramentas/distanciaptpt/dicionario.js
+++ ferramentas/distanciaptpt/dicionario.js
@@ -2,67 +2,67 @@
 i3GEOF.distanciaptpt.dicionario = {
 	'ajuda' : [ {
 		pt : "O c&aacute;lculo de dist&acirc;ncias &eacute; feito de um ponto em rela&ccedil;&atilde;o aos mais pr&oacute;ximos existentes no mesmo tema ou em outro tema.",
-		en : "",
-		es : "El c&aacute;lculo de distancia es realizado de un punto en relaci&oacute;n a los m&aacute;s pr&oacute;ximos existentes en el mismo tema o en otro tema. "
+		en : "Distance calculation is made from a point in relation to the nearest ones in the same theme or in another theme.",
+		es : "El c&aacute;lculo de distancias se hace desde un punto en relaci&oacute;n a los m&aacute;s cercanos existentes en el mismo tema o en otro tema."
 	} ],
 	'ajuda2' : [ {
 		pt : "O ponto origem, deve estar selecionado em um dos temas existentes no mapa.",
-		en : "",
-		es : "El punto de origen, debe estar relacionado a uno de los dos temas existentes en el mapa"
+		en : "The origin point must be selected in one of the themes on the map.",
+		es : "El punto de origen debe estar seleccionado en uno de los temas del mapa."
 	} ],
 	'ajuda3' : [ {
 		pt : "Os pontos de destino s&atilde;o selecionados em fun&ccedil;&atilde;o de uma dist&acirc;ncia fixa do ponto origem.",
-		en : "",
-		es : "Los puntos de destino sere&aacute;n seleccionados en funci&oacute;n de una distancia fijada en el punto de origen. "
+		en : "Destination points are selected based on a fixed distance from the origin point.",
+		es : "Los puntos de destino se seleccionan en funci&oacute;n de una distancia fija desde el punto de origen."
 	} ],
 	'temaOrigem' : [ {
 		pt : "Tema que cont&eacute;m os pontos de origem (pelo menos um ponto deve estar selecionado):",
-		en : "",
-		es : "Tema que contienen los puntos de origen (por lo menos un punto debe estar seleccionado):"
+		en : "Theme containing origin points (at least one point must be selected):",
+		es : "Tema que contiene los puntos de origen (al menos un punto debe estar seleccionado):"
 	} ],
 	'temaDestino' : [ {
 		pt : "Tema que cont&eacute;m o(s) ponto(s) de destino:",
-		en : "",
-		es : "Tema que contiene/n el/los punto/s de destino:  "
+		en : "Theme containing destination points:",
+		es : "Tema que contiene el (los) punto (s) de destino:"
 	} ],
 	'distMax' : [ {
 		pt : "Dist&acirc;ncia (metros) m&aacute;xima que ser&aacute; considerada entre os pontos de origem e os pontos de destino:",
-		en : "",
-		es : ""
+		en : "Max distance (meters) that will be taken into account between origin and destination points:",
+		es : "Distancia m&aacute;xima (metros) que ser&aacute; considerada entre los puntos de origen y destino:"
 	} ],
 	'selecionaAtributos' : [ {
 		pt : "Escolha os itens das tabelas de atributos dos temas de origem e de destino que ser&atilde;o acrescentados ao novo tema que ser&aacute; criado com o resultado do c&aacute;lculo.",
-		en : "",
-		es : "Seleccione los &iacute;tems de las tablas de atributos de los temas de origen y destino que ser&aacute;n incrementados al nuevo tema que ser&aacute; creado con el resultado del c&aacute;lculo. "
+		en : "Select the fields from the attribute tables of the origin and destination themes that will be added to the theme that will be created with the calculation result.",
+		es : "Seleccione los campos de las tablas de atributos de los temas de origen y de destino que se a&ntilde;adir&aacute;n al tema nuevo que se crear&aacute; con el resultado del c&aacute;lculo."
 	} ],
 	'atributoOrigem' : [ {
 		pt : "Para o tema de origem:",
-		en : "",
-		es : "Para el tema de origen"
+		en : "For the origin theme:",
+		es : "Para el tema de origen:"
 	} ],
 	'atributoDestino' : [ {
 		pt : "Para o tema de destino:",
-		en : "",
-		es : "Para el tema de destino"
+		en : "For the destination theme:",
+		es : "Para el tema de destino:"
 	} ],
 	'msgTema' : [ {
 		pt : "O tema com o c&aacute;lculo ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : "El tema con el c&aacute;lculo ser&aacute; adicionado al mapa actual"
+		en : "A theme with calculations will be added to the current map.",
+		es : "El tema con el c&aacute;lculo se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'calcula' : [ {
 		pt : "Calcular",
-		en : "",
+		en : "Calculate",
 		es : "Calcular"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito.",
-		en : "",
-		es : "Error. La operaci&oacute;n demoro mucho"
+		en : "Error. Too long time execution.",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho."
 	} ],
 	'erroParametro' : [ {
 		pt : "Algum par&acirc;metro n&atilde;o foi preenchido.",
-		en : "",
-		es : "Falto completar alg&uacute;n par&aacute;metro."
+		en : "Some parameter missed.",
+		es : "Falt&oacute; completar alg&uacute;n par&aacute;metro."
 	} ]
 };
