09c2a0da34b6fef41b94c5be3664c16007495c19
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-22 11:39:33 -0200

    Tradução de ferramentas/filtro/dicionario.js

--- ferramentas/filtro/dicionario.js
+++ ferramentas/filtro/dicionario.js
@@ -2,97 +2,97 @@
 i3GEOF.filtro.dicionario = {
 	'constroi' : [ {
 		pt : "Construir",
-		en : "",
-		es : ""
+		en : "Build",
+		es : "Construir"
 	} ],
 	'digita' : [ {
 		pt : "Digitar",
-		en : "",
-		es : ""
+		en : "Enter",
+		es : "Introducir"
 	} ],
 	'testa' : [ {
 		pt : "Testar",
-		en : "",
-		es : ""
+		en : "Test",
+		es : "Probar"
 	} ],
 	'inclui' : [ {
 		pt : "Incluir/aplicar",
-		en : "",
-		es : ""
+		en : "Include/apply",
+		es : "Incluir/aplicar"
 	} ],
 	'remove' : [ {
 		pt : "Remover",
-		en : "",
-		es : ""
+		en : "Remove",
+		es : "Eliminar"
 	} ],
 	'item' : [ {
 		pt : "Item",
-		en : "",
-		es : ""
+		en : "Field",
+		es : "Campo"
 	} ],
 	'operador' : [ {
 		pt : "Operador",
-		en : "",
-		es : ""
+		en : "Operator",
+		es : "Operador"
 	} ],
 	'valor' : [ {
 		pt : "Valor",
-		en : "",
-		es : ""
+		en : "Value",
+		es : "Valor"
 	} ],
 	'conector' : [ {
 		pt : "Conector",
-		en : "",
-		es : ""
+		en : "Connector",
+		es : "Conector"
 	} ],
 	'ajuda' : [ {
 		pt : "O resultado da filtragem ser&aacute; mostrado no mapa. Certifique-se que o tema est&aacute; v&iacute;sivel. Antes de aplicar, vc pode testar o filtro definido. Na op&ccedil;&atilde;o IN separe os valores com v&iacute;rgula. Na op&ccedil;&atilde;o like, utilize o sinal de percentual antes e depois da string",
-		en : "",
-		es : ""
+		en : "Filtering result will be shown on the map. Make sure the theme is visible. You can test the filter before applying it. Separate values with a comma for the IN conditional. Use the percent symbol before and after a string for the LIKE operator",
+		es : "El resultado del filtrado se mostrar&aacute; en el mapa. Aseg&uacute;rese de que el tema es visible. Usted puede probar el filtro antes de aplicarlo. En la opci&oacute;n IN separe los valores con coma. En la opci&oacute;n LIKE utilice el signo de porcentaje antes y despu&eacute;s de la cadena"
 	} ],
 	'digitaFiltro' : [ {
 		pt : "Digite o filtro",
-		en : "",
-		es : ""
+		en : "Enter the filter",
+		es : "Introduzca el filtro"
 	} ],
 	'mostraValor' : [ {
 		pt : "mostra valores",
-		en : "",
-		es : ""
+		en : "shows values",
+		es : "muestra valores"
 	} ],
 	'selecionaValor' : [ {
 		pt : "Escolha o valor",
-		en : "",
-		es : ""
+		en : "Select a value",
+		es : "Seleccione el valor"
 	} ],
 	'igual' : [ {
 		pt : "igual",
-		en : "",
-		es : ""
+		en : "equal",
+		es : "igual"
 	} ],
 	'menor' : [ {
 		pt : "menor",
-		en : "",
-		es : ""
+		en : "less",
+		es : "menor"
 	} ],
 	'maior' : [ {
 		pt : "maior",
-		en : "",
-		es : ""
+		en : "greater",
+		es : "mayor"
 	} ],
 	'e' : [ {
 		pt : "e",
-		en : "",
-		es : ""
+		en : "and",
+		es : "y"
 	} ],
 	'ou' : [ {
 		pt : "ou",
-		en : "",
-		es : ""
+		en : "or",
+		es : "o"
 	} ],
 	'nao' : [ {
 		pt : "n&atilde;o",
-		en : "",
-		es : ""
+		en : "not",
+		es : "no"
 	} ]
 };
