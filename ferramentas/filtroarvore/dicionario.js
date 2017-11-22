fceb498b758c70b7e2731d9e9aed3457811301ca
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-22 12:03:50 -0200

    Tradução de ferramentas/filtroarvore/dicionario.js

--- ferramentas/filtroarvore/dicionario.js
+++ ferramentas/filtroarvore/dicionario.js
@@ -2,57 +2,57 @@
 i3GEOF.filtroarvore.dicionario = {
 	'selecionaFiltro' : [ {
 		pt : "Escolha o tipo de filtro",
-		en : "",
-		es : ""
+		en : "Choose filter type",
+		es : "Elija el tipo de filtro"
 	} ],
 	'removeFiltro' : [ {
 		pt : "remover filtro",
-		en : "",
-		es : ""
+		en : "remove filter",
+		es : "eliminar filtro"
 	} ],
 	'ligado' : [ {
 		pt : "ligados",
-		en : "",
-		es : ""
+		en : "connected",
+		es : "conectado"
 	} ],
 	'desligado' : [ {
 		pt : "desligados",
-		en : "",
-		es : ""
+		en : "disconnected",
+		es : "desconectados"
 	} ],
 	'comSelecao' : [ {
 		pt : "com sele&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "with selection",
+		es : "con selecci&oacute;n"
 	} ],
 	'permiteDownload' : [ {
 		pt : "permitem download",
-		en : "",
-		es : ""
+		en : "they allow downloading",
+		es : "Permiten descargas"
 	} ],
 	'wms' : [ {
 		pt : "s&atilde;o do tipo WMS",
-		en : "",
-		es : ""
+		en : "they are of WMS type",
+		es : "son del tipo WMS"
 	} ],
 	'raster' : [ {
 		pt : "s&atilde;o do tipo raster",
-		en : "",
-		es : ""
+		en : "they are of raster type",
+		es : "son del tipo raster"
 	} ],
 	'apenasTexto' : [ {
 		pt : "contem apenas texto",
-		en : "",
-		es : ""
+		en : "contains only text",
+		es : "solo contiene texto"
 	} ],
 	'operacao' : [ {
 		pt : "Opera&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Operation",
+		es : "Operaci&oacute;n"
 	} ],
 	'exclui' : [ {
 		pt : "excluir",
-		en : "",
-		es : ""
+		en : "delete",
+		es : "eliminar"
 	} ]
 };
