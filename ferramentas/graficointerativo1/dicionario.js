0fb9384cb93b94b0e6a09353542f280bb63686f3
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-23 18:34:36 -0200

    Tradução de ferramentas/graficointerativo1/dicionario.js

--- ferramentas/graficointerativo1/dicionario.js
+++ ferramentas/graficointerativo1/dicionario.js
@@ -4,411 +4,411 @@ i3GEOF.graficointerativo1.dicionario =
 		"tipo" : [
 			{
 				pt : "Tipo",
-				en : "",
-				es : ""
+				en : "Type",
+				es : "Tipo"
 			}
 		],
 		"dados" : [
 			{
 				pt : "Dados",
-				en : "",
-				es : ""
+				en : "Data",
+				es : "Datos"
 			}
 		],
 		"grafico" : [
 			{
 				pt : "Gr&aacute;fico",
-				en : "",
-				es : ""
+				en : "Chart",
+				es : "Gr&aacute;fico"
 			}
 		],
 		"escolhatipo" : [
 			{
 				pt : "Escolha o tipo de gr&aacute;fico",
-				en : "",
-				es : ""
+				en : "Choose a chart type",
+				es : "Elija el tipo de gr&aacute;fico"
 			}
 		],
 		"pizza2d" : [
 			{
 				pt : "pizza 2d",
-				en : "",
-				es : ""
+				en : "pie 2d",
+				es : "torta 2d"
 			}
 		],
 		"areasimp" : [
 			{
 				pt : "&aacute;rea simples",
-				en : "",
-				es : ""
+				en : "simple areas",
+				es : "&aacute;reas simples"
 			}
 		],
 		"distpt" : [
 			{
 				pt : "distribui&ccedil;&atilde;o de pontos",
-				en : "",
-				es : ""
+				en : "point distribution",
+				es : "distribuci&oacute;n de puntos"
 			}
 		],
 		"linhassimp" : [
 			{
 				pt : "linhas simples",
-				en : "",
-				es : ""
+				en : "simple lines",
+				es : "l&iacute;neas simples"
 			}
 		],
 		"arvore" : [
 			{
 				pt : "&Aacute;rvore",
-				en : "",
-				es : ""
+				en : "Tree",
+				es : "&Aacute;rbol"
 			}
 		],
 		"barrasv" : [
 			{
 				pt : "barras verticais",
-				en : "",
-				es : ""
+				en : "vertical bars",
+				es : "barras verticales"
 			}
 		],
 		"barrash" : [
 			{
 				pt : "barras horizontais",
-				en : "",
-				es : ""
+				en : "horizontal bars",
+				es : "barras horizontales"
 			}
 		],
 		"configgr" : [
 			{
 				pt : "Configura&ccedil;&atilde;o do gr&aacute;fico",
-				en : "",
-				es : ""
+				en : "Chart configuration",
+				es : "Configuraci&oacute;n del gr&aacute;fico"
 			}
 		],
 		"tema" : [
 			{
 				pt : "Tema",
-				en : "",
-				es : ""
+				en : "Theme",
+				es : "Tema"
 			}
 		],
 		"itemcx" : [
 			{
 				pt : "Item com as classes ou eixo X",
-				en : "",
-				es : ""
+				en : "Field with classes or X-axis",
+				es : "Campo con las clases o eje X"
 			}
 		],
 		"itemcy" : [
 			{
 				pt : "Item(ns) com os valores ou eixo Y",
-				en : "",
-				es : ""
+				en : "Fields with classes or Y-axis",
+				es : "Campo con las clases o eje Y"
 			}
 		],
 		"excluirval" : [
 			{
 				pt : "Excluir o seguinte valor",
-				en : "",
-				es : ""
+				en : "Delete the following value",
+				es : "Eliminar el valor siguiente"
 			}
 		],
 		"geracor" : [
 			{
 				pt : "gera cores aleat&oacute;rias",
-				en : "",
-				es : ""
+				en : "generates random colors",
+				es : "genera colores aleatorios"
 			}
 		],
 		"obterdados" : [
 			{
 				pt : "Obter dados",
-				en : "",
-				es : ""
+				en : "get data",
+				es : "Obtener datos"
 			}
 		],
 		"seescolher" : [
 			{
 				pt : "Se vc escolher para X e Y o mesmo item, ser&aacute; considerada a frequ&ecirc;ncia das ocorr&ecirc;ncias para compor cada parte da pizza. Caso contr&aacute;rio, ser&aacute; feita a soma dos valores existentes em Y para cada ocorr&ecirc;ncia existente em X.",
-				en : "",
-				es : ""
+				en : "If you choose the same field for X and Y, the frequency of occurrence will be used to create each part of the pie. Otherwise, the sum of the existing values in Y will be made for each occurrence in X.",
+				es : "Si se elige el mismo campo para X e Y, se considerar&aacute; la frecuencia de ocurrencia para crear cada parte de la torta. En caso contrario, se har&aacute; la suma de los valores existentes en Y para cada ocurrencia existente en X."
 			}
 		],
 		"inclinax" : [
 			{
 				pt : "Inclina os textos do eixo X",
-				en : "",
-				es : ""
+				en : "Rotates texts of X axis",
+				es : "Gira los textos del eje X"
 			}
 		],
 		"adlinbarras" : [
 			{
 				pt : "Adiciona as linhas em gr&aacute;ficos de barras",
-				en : "",
-				es : ""
+				en : "Adds lines to bar charts",
+				es : "Agrega las l&iacute;neas a los gr&aacute;ficos de barras"
 			}
 		],
 		"utilizvalacum" : [
 			{
 				pt : "Utiliza valores acumulados",
-				en : "",
+				en : "Uses cumulative values",
 				titleFont : 'bold 14px sans-serif',
 				titlePosition : "top",
-				es : ""
+				es : "Utiliza valores acumulados"
 			}
 		],
 		"utilizvalrelat" : [
 			{
 				pt : "Utiliza valores relativos",
-				en : "",
-				es : ""
+				en : "Uses relative values",
+				es : "Utiliza valores relativos"
 			}
 		],
 		"ordenax" : [
 			{
 				pt : "Ordena o eixo X",
-				en : "",
-				es : ""
+				en : "Arrange the X-axis",
+				es : "Ordena el eje X"
 			}
 		],
 		"naoprocessa" : [
 			{
 				pt : "N&atilde;o processa os valores ao obter os dados (mant&eacute;m os dados como est&atilde;o na tabela de atributos) - essa op&ccedil;&atilde;o &eacute; &uacute;til nos gr&aacute;ficos de distribui&ccedil;&atilde;o de pontos",
-				en : "",
-				es : ""
+				en : "Does not process values when gathering data (it maintains data as it is in the attribute table) - this option is useful for point distribution charts",
+				es : "No procesa los valores al obtener los datos (mantiene los datos como est&aacute;n en la tabla de atributos) - esta opci&oacute;n es &uacute;til en los gr&aacute;ficos de distribuci&oacute;n de puntos"
 			}
 		],
 		"soma" : [
 			{
 				pt : "Soma",
-				en : "",
-				es : ""
+				en : "Sum",
+				es : "Suma"
 			}
 		],
 		"media" : [
 			{
 				pt : "M&eacute;dia",
-				en : "",
-				es : ""
+				en : "Average",
+				es : "Promedio"
 			}
 		],
 		"tipoagregay" : [
 			{
 				pt : "Tipo de agrega&ccedil;&atilde;o dos valores do eixo Y",
-				en : "",
-				es : ""
+				en : "Aggregation type of Y-axis values",
+				es : "Tipo de agregaci&oacute;n de los valores del eje Y"
 			}
 		],
 		"fatcalclarg" : [
 			{
 				pt : "Fator de c&aacute;lculo da largura do gr&aacute;fico. O n&uacute;mero de ocorr&ecirc;ncias ser&aacute; multiplicado por esse fator para calcular o tamanho final do gr&aacute;fico em pixels. Deixe como 0 para usar o c&aacute;lculo autom&aacute;tico.",
-				en : "",
-				es : ""
+				en : "Factor used to compute chart width. The number of occurrences will be multiplied by this factor to calculate the final size of the graph in pixels. Leave it as cero (0) to use automatic calculation.",
+				es : "Factor de c&aacute;lculo del ancho del gr&aacute;fico. El n&uacute;mero de ocurrencias se multiplicar&aacute; por este factor para calcular el tama&ntilde;o final del gr&aacute;fico en p&iacute;xeles. Deje el cero (0) para utilizar el c&aacute;lculo autom&aacute;tico."
 			}
 		],
 		"janelasep" : [
 			{
 				pt : "janela separada",
-				en : "",
-				es : ""
+				en : "detached window",
+				es : "ventana separada"
 			}
 		],
 		"limparfiltros" : [
 			{
 				pt : "limpar filtros",
-				en : "",
-				es : ""
+				en : "clean filters",
+				es : "limpiar filtros"
 			}
 		],
 		'selecionaTipoGrafico' : [
 			{
 				pt : "Escolha um tipo de grafico",
-				en : "",
-				es : ""
+				en : "Choose a chart type",
+				es : "Elija el tipo de gr&aacute;fico"
 			}
 		],
 		'erroTemaOrigem' : [
 			{
 				pt : "erro ao ler os itens do tema de origem",
-				en : "",
-				es : ""
+				en : "error reading fields from source theme",
+				es : "error al leer los campos del tema de origen"
 			}
 		],
 		'digitaTituloLegenda' : [
 			{
 				pt : "Digite o t&iacute;tulo para legenda",
-				en : "",
-				es : ""
+				en : "Enter legend title",
+				es : "Introduzca el t&iacute;tulo de la leyenda"
 			}
 		],
 		'selecionaTema' : [
 			{
 				pt : "Escolha um tema",
-				en : "",
-				es : ""
+				en : "Select a theme",
+				es : "Seleccione un tema"
 			}
 		],
 		'selecionaItemX' : [
 			{
 				pt : "Escolha um item para X",
-				en : "",
-				es : ""
+				en : "Select a field for X",
+				es : "Seleccione un campo para X"
 			}
 		],
 		'selecionaItemY' : [
 			{
 				pt : "Escolha um item para Y",
-				en : "",
-				es : ""
+				en : "Select a field for Y",
+				es : "Seleccione un campo para Y"
 			}
 		],
 		'dadosRegiaoMostrada' : [
 			{
 				pt : "Os dados utilizados referem-se apenas &agrave; regi&atilde;o atual mostrada no mapa",
-				en : "",
-				es : ""
+				en : "Processed data encompasses only the current extent shown on the map",
+				es : "Los datos utilizados se refieren solamente a la extensi&oacute;n actual mostrada en el mapa"
 			}
 		],
 		'tabelaGrafico' : [
 			{
 				pt : "Tabela de dados para o gr&aacute;fico. Os valores podem ser editados",
-				en : "",
-				es : ""
+				en : "Table of data for the chart. Values can be edited",
+				es : "Tabla de datos para el gráfico. Los valores se pueden editar"
 			}
 		],
 		39 : [
 			{
 				pt : "nome",
-				en : "",
-				es : ""
+				en : "name",
+				es : "nombre"
 			}
 		],
 		40 : [
 			{
 				pt : "valor",
-				en : "",
-				es : ""
+				en : "value",
+				es : "valor"
 			}
 		],
 		'cor' : [
 			{
 				pt : "cor",
-				en : "",
-				es : ""
+				en : "color",
+				es : "color"
 			}
 		],
 		'exclui' : [
 			{
 				pt : "excluir",
-				en : "",
-				es : ""
+				en : "delete",
+				es : "eliminar"
 			}
 		],
 		'digitaNovoNome' : [
 			{
 				pt : "digite o novo nome",
-				en : "",
-				es : ""
+				en : "enter a new name",
+				es : "ingrese el nombre nuevo"
 			}
 		],
 		44 : [
 			{
 				pt : "digite o novo valor",
-				en : "",
-				es : ""
+				en : "enter a new value",
+				es : "ingrese el valor nuevo"
 			}
 		],
 		'casos' : [
 			{
 				pt : "casos",
-				en : "",
-				es : ""
+				en : "cases",
+				es : "casos"
 			}
 		],
 		'numeroCasos' : [
 			{
 				pt : "n. de casos",
-				en : "",
-				es : ""
+				en : "n. of cases",
+				es : "n. de casos"
 			}
 		],
 		47 : [
 			{
 				pt : "Atualiza o gr&aacute;fico ao navegar pelo mapa",
-				en : "",
-				es : ""
+				en : "Updates the chart as you navigate through the map",
+				es : "Actualiza el gr&aacute;fico al navegar por el mapa"
 			}
 		],
 		"stacked" : [
 			{
 				pt : "Utiliza barras divididas entre os valores (stacked)",
-				en : "",
-				es : ""
+				en : "Uses segmented bars of datasets on top of each other(stacked)",
+				es : "Utiliza barras divididas en segmentos que se colocan unos encima de otros (apilada)"
 			}
 		],
 		"linhasnascols" : [
 			{
 				pt : "As linhas de dados ficam nas colunas",
-				en : "",
-				es : ""
+				en : "ata lines are in columns",
+				es : "Las l&iacute;neas de datos se encuentran en las columnas"
 			}
 		],
 		"titulo" : [
 			{
 				pt : "T&iacute;tulo",
-				en : "",
-				es : ""
+				en : "Title",
+				es : "T&iacute;tulo"
 			}
 		],
 		"titux" : [
 			{
 				pt : "T&iacute;tulo do eixo X",
-				en : "",
-				es : ""
+				en : "Axis X title",
+				es : "T&iacute;tulo del eje X"
 			}
 		],
 		"tituy" : [
 			{
 				pt : "T&iacute;tulo do eixo Y",
-				en : "",
-				es : ""
+				en : "Axis Y title",
+				es : "T&iacute;tulo del eje X"
 			}
 		],
 		"atualizaNavegacao" : [
 			{
 				pt : "Atualiza ao navegar",
-				en : "",
-				es : ""
+				en : "Refresh browser",
+				es : "Refrescar el navegador"
 			}
 		],
 		"idDoGrafico" : [
 			{
 				pt : "Ientificador do grafico",
-				en : "",
-				es : ""
+				en : "Chart identifier",
+				es : "Identificador del gr&aacute;fico"
 			}
 		],
 		"compor" : [
 			{
 				pt : "Compor",
-				en : "",
-				es : "",
+				en : "Compose",
+				es : "Componer",
 				it : ""
 			}
 		],
 		"escolherGrafico" : [
 			{
 				pt : "Escolha as janelas que cont&eacute;m os gr&aacute;ficos que ser&atilde;o compostos com o atual:",
-				en : "",
-				es : "",
+				en : "Select windows that contain the charts that will be built along with the current one:",
+				es : "Seleccione las ventanas que contienen los gr&aacute;ficos que se compondr&aacute;n con el actual:",
 				it : ""
 			}
 		],
 		"GrComposicao" : [
 			{
 				pt : "Lista com os gr&aacute;ficos sobrepostos na atual composi&ccedil;&atilde;o:",
-				en : "",
-				es : "",
+				en : "List with overlapping charts in the current composition:",
+				es : "Lista con los gr&aacute;ficos superpuestos en la composici&oacute;n actual:",
 				it : ""
 			}
 		]
