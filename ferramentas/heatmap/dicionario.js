9a1266b638b303f691a8ae096548c6972cef4181
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-23 20:12:26 -0200

    Tradução de  ferramentas/heatmap/dicionario.js

--- ferramentas/heatmap/dicionario.js
+++ ferramentas/heatmap/dicionario.js
@@ -2,77 +2,77 @@
 i3GEOF.heatmap.dicionario = {
 	'ajuda' : [ {
 		pt : "O mapa de calor mostra a distribui&ccedil;&atilde;o de valores de forma cont&iacute;nua e com cores que identificam classes de maior e menor valor. A fonte para o c&aacute;lculo da distribui&ccedil;&atilde;o &eacute; um tema existente no mapa, podendo ser de pontos ou n&atilde;o.",
-		en : "",
-		es : ""
+		en : "A heatmap shows value distributions continuously and with colors that identify higher and lower value classes. The source for calculating the distribution is a theme on the map, which can be either points or not.",
+		es : "El mapa de calor muestra la distribuci&oacute;n de valores de forma continua y con colores que identifican clases de mayor y menor valor. La fuente para el c&aacute;lculo de la distribuci&oacute;n es un tema existente en el mapa, pudiendo ser de puntos o no."
 	} ],
 	'ajuda2' : [ {
 		pt : "Todos os pontos existente no mapa s&atilde;o considerados. Se voc&ecirc; quiser usar apenas alguns pontos, fa&ccedil;a uma sele&ccedil;&atilde;o e crie um novo tema com esse resultado. Veja a ferramenta de sele&ccedil;&atilde;o para maiores detalhes. Sobre a implementa&ccedil;&atilde;o do Heatmap no i3Geo veja mais detalhes em:<br><a href=http://www.patrick-wied.at/static/heatmapjs/ target=_blank  >http://www.patrick-wied.at/static/heatmapjs/</a>",
-		en : "",
-		es : ""
+		en : "All points on the map will be taken into account. If you want to use only some points, make a selection and create a new theme with the selection result. See selection tool for more details. With regard to Heatmap implementation in i3Geo, get more information at: <br><a href=http://www.patrick-wied.at/static/heatmapjs/ target=_blank  >http://www.patrick-wied.at/static/heatmapjs/</a>",
+		es : "Todos los puntos del mapa ser&aacute;n considerados. Si desea utilizar solo algunos puntos, haga una selecci&oacute;n y cree un tema nuevo con el resultado. Vea la herramienta de selecci&oacute;n para m&aacute;s detalles. En cuanto a la implementaci&oacute;n de Heatmap en i3Geo, vea m&aacute;s detalles en: <br><a href=http://www.patrick-wied.at/static/heatmapjs/ target=_blank  >http://www.patrick-wied.at/static/heatmapjs/</a>"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, que ser&aacute; utilizado",
-		en : "",
-		es : ""
+		en : "Theme that will be used",
+		es : "Tema que ser&aacute; utilizado"
 	} ],
 	'valorPonto' : [ {
 		pt : "Valor de cada ponto:",
-		en : "",
-		es : ""
+		en : "Value of each point:",
+		es : "Valor de cada punto:"
 	} ],
 	5 : [ {
 		pt : "Considerar os elementos selecionados como se fossem um s&oacute;?",
-		en : "",
-		es : ""
+		en : "Do you want to regard selected features as a single one?",
+		es : "&iquest;Usted quiere considerar las entidades seleccionadas como si fuesen una sola?"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema calculado ser&aacute; adicionado ao mapa atual",
-		en : "",
-		es : ""
+		en : "The computed theme will be added to the current map",
+		es : "El tema calculado se a&ntilde;adir&aacute; al mapa actual"
 	} ],
 	'criaCamada' : [ {
 		pt : "Criar camada",
-		en : "",
-		es : ""
+		en : "Create Layer",
+		es : "Crear una capa"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito",
-		en : "",
-		es : ""
+		en : "Error. Too long time execution",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho"
 	} ],
 	'erroDistancia' : [ {
 		pt : "Distancia invalida",
-		en : "",
-		es : ""
+		en : "Invalid distance",
+		es : "Distancia no v&aacute;lida"
 	} ],
 	'selecionaAtributo' : [ {
 		pt : "Ou um item da tabela de atributos que cont&eacute;m os valores:",
-		en : "",
-		es : ""
+		en : "Or a field of the attribute table that contains the values:",
+		es : "O un campo de la tabla de atributos que contiene los valores:"
 	} ],
 	'multiplica' : [ {
 		pt : "Multiplicar por:",
-		en : "",
-		es : ""
+		en : "Multiply by:",
+		es : "Multiplicar por:"
 	} ],
 	'tituloNovaCamada' : [ {
 		pt : "T&iacute;tulo da nova camada:",
-		en : "",
-		es : ""
+		en : "New layer title:",
+		es : "T&iacute;tulo de la nueva capa:"
 	} ],
 	'valorRaio' : [ {
 		pt : "Raio de um ponto em pixels:",
-		en : "",
-		es : ""
+		en : "Radius of a point in pixels:",
+		es : "Radio de un punto en p&iacute;xeles:"
 	} ],
 	'opacidade' : [ {
 		pt : "Opacidade:",
-		en : "",
-		es : ""
+		en : "Opacity:",
+		es : "Opacidad:"
 	} ],
 	'valorMaximoPonto' : [ {
 		pt : "Valor m&aacute;ximo em cada ponto:",
-		en : "",
-		es : ""
+		en : "Maximum value at each point:",
+		es : "Valor m&aacute;ximo en cada punto:"
 	} ]
 };
