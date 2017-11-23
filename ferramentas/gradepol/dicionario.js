0f47cf28bbc876bb4659a85259327ca6682e876c
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-23 12:49:32 -0200

    Tradução de ferramentas/gradepol/dicionario.js

--- ferramentas/gradepol/dicionario.js
+++ ferramentas/gradepol/dicionario.js
@@ -2,77 +2,77 @@
 i3GEOF.gradeDePoligonos.dicionario = {
 	'ajuda' : [ {
 		pt : "A grade &eacute; formada por pol&iacute;gonos espa&ccedil;ados conforme os par&acirc;metros de dx e dy.",
-		en : "",
-		es : ""
+		en : "A grid is made up of polygons spaced according to dx and dy parameters.",
+		es : "La cuadricula est&aacute; formada por pol&iacute;gonos espaciados seg&uacute;n los par&aacute;metros dx y dy."
 	} ],
 	'ajuda2' : [ {
 		pt : "A grade criada &eacute; adicionada como um novo tema no mapa, podendo ser utilizada em opera&ccedil;&otilde;es como o cruzamento entre pontos e pol&iacute;gonos.",
-		en : "",
-		es : ""
+		en : "The created grid is added as a new theme to the map, and can be used in operations such as intersection between points and polygons.",
+		es : "La cuadr&iacute;cula creada se agrega como un tema nuevo en el mapa, pudiendo ser utilizada en operaciones como la intersecci&oacute;n entre puntos y pol&iacute;gonos."
 	} ],
 	'ajuda3' : [ {
 		pt : "O ponto inicial da grade deve ser definido pelo usu&aacute;rio e o ponto final (inferior direito) &eacute; definido em fun&ccedil;&atilde;o da abrang&ecirc;ncia do mapa ou quantidade de c&eacute;lulas.",
-		en : "",
-		es : ""
+		en : "The starting point of the grid must be defined by the user. The end point (bottom right) is defined according to the map extent or the number of cells.",
+		es : "El punto inicial de la cuadr&iacute;cula debe ser definido por el usuario. El punto final (inferior derecho) se define en funci&oacute;n de la extensi&oacute;n del mapa o de la cantidad de celdas."
 	} ],
 	'distancia' : [ {
 		pt : "Dist&acirc;ncia entre c&eacute;lulas",
-		en : "",
-		es : ""
+		en : "Distance between cells",
+		es : "Distancia entre celdas"
 	} ],
 	'grau' : [ {
 		pt : "Grau",
-		en : "",
-		es : ""
+		en : "Degree",
+		es : "Grado"
 	} ],
 	'minuto' : [ {
 		pt : "Minuto",
-		en : "",
-		es : ""
+		en : "Minute",
+		es : "Minuto"
 	} ],
 	'segundo' : [ {
 		pt : "Segundo",
-		en : "",
-		es : ""
+		en : "Second",
+		es : "Segundo"
 	} ],
 	'coord' : [ {
 		pt : "Coordenadas do ponto inicial superior esquerdo (utilize o sinal negativo no grau quando ao sul do equador e a oeste). <b>Voc&ecirc; pode clicar no mapa para pegar o ponto.</b>",
-		en : "",
-		es : ""
+		en : "Coordinates of the upper left starting point (degree values must have a negative sign when the point is located at south of the equator and/or at west of the Greenwich meridian). <b>You can click on the map to fetch the point.</b>",
+		es : "Coordenadas del punto inicial superior izquierdo (utilice el signo negativo en el grado cuando se encuentre al sur del ecuador y/o al oeste del meridano de Greenwich). <b>Usted puede hacer clic en el mapa para tomar el punto.</b>"
 	} ],
 	'numero' : [ {
 		pt : "N&uacute;mero de c&eacute;lulas. Total m&aacute;ximo de 10.000",
-		en : "",
-		es : ""
+		en : "Number of cells. Total maximum 10,000",
+		es : "N&uacute;mero de celdas. Total m&aacute;ximo de 10.000"
 	} ],
 	'adicionaTema' : [ {
 		pt : "A grade ser&aacute; acrescentada como um novo tema no mapa",
-		en : "",
-		es : ""
+		en : "The grid will be added as a new theme to the map",
+		es : "La cuadr&iacute;cula se a&ntilde;adir&aacute; como un tema nuevo en el mapa"
 	} ],
 	'criaGrade' : [ {
 		pt : "Criar grade",
-		en : "",
-		es : ""
+		en : "Create grid",
+		es : "Crear cuadr&iacute;cula"
 	} ],
 	'msg' : [ {
 		pt : "Dist&acirc;ncia entre as celulas n&atilde;o pode ser 0",
-		en : "",
-		es : ""
+		en : "Distance between cells can not be 0",
+		es : "La distancia entre las celdas no puede ser 0"
 	} ],
 	'msg2' : [ {
 		pt : "N&uacute;mero de pontos n&atilde;o pode ser 0",
-		en : "",
-		es : ""
+		en : "Number of points can not be 0",
+		es : "El N&uacute;mero de puntos no puede ser 0"
 	} ],
 	'projMercator' : [ {
 		pt : "Utiliza a proje&ccedil;&atilde;o Mercator na gera&ccedil;&atilde;o da grade",
-		en : "",
-		es : ""
+		en : "Uses the Mercator projection for grid generation",
+		es : "Utiliza la proyecci&oacute;n Mercator para la creaci&oacute;n de la cuadr&iacute;cula"
 	} ],
 	'metros' : [ {
 		pt : "Metros",
-		en : "",
-		es : ""
+		en : "Meters",
+		es : "Metros"
 	} ]
 };
