d565b9c5176d01e925fb73b3b337c81b2352d608
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-23 20:52:09 -0200

    Tradução de ferramentas/identifica/dicionario.js

--- ferramentas/identifica/dicionario.js
+++ ferramentas/identifica/dicionario.js
@@ -2,147 +2,147 @@
 i3GEOF.identifica.dicionario = {
 	'selecionaTema' : [ {
 		pt : "Escolha um tema da lista",
-		en : "",
-		es : ""
+		en : "Select a theme from the list",
+		es : "Seleccione un tema de la lista"
 	} ],
 	'msgEtiquetas' : [ {
 		pt : "Esse tema n&atilde;o permite etiquetas",
-		en : "",
-		es : ""
+		en : "This theme does not allow labels",
+		es : "Este tema no permite etiquetas"
 	} ],
 	'temasVisiveis' : [ {
 		pt : "Temas vis&iacute;veis",
-		en : "",
-		es : ""
+		en : "Visible themes",
+		es : "Temas visibles"
 	} ],
 	'todos' : [ {
 		pt : "Todos",
-		en : "",
-		es : ""
+		en : "All",
+		es : "Todos"
 	} ],
 	'etiquetas' : [ {
 		pt : "Etiquetas",
-		en : "",
-		es : ""
+		en : "Labels",
+		es : "Etiquetas"
 	} ],
 	'propriedades' : [ {
 		pt : "Propriedades",
-		en : "",
-		es : ""
+		en : "Properties",
+		es : "Propiedades"
 	} ],
 	'msgAguarde' : [ {
 		pt : "Aguarde",
-		en : "",
-		es : ""
+		en : "Wait",
+		es : "Espere"
 	} ],
 	'resolucaoBusca' : [ {
 		pt : "Resolu&ccedil;&atilde;o de busca - n&uacute;mero de pixels, no entorno do ponto clicado no mapa, que ser&aacute; utilizado na busca de dados",
-		en : "",
-		es : ""
+		en : "Search resolution - number of pixels around the clicked point on the map that will be used for searching data",
+		es : "Resoluci&oacute;n de b&uacute;squeda - n&uacute;mero de p&iacute;xeles alrededor del punto sobre el que se ha hecho clic en el mapa que se utilizar&aacute; en la b&uacute;squeda de datos"
 	} ],
 	'mostraEtiquetas' : [ {
 		pt : "As etiquetas s&atilde;o mostradas quando o mouse &eacute; estacionado sobre um elemento.",
-		en : "",
-		es : ""
+		en : "Labels are displayed when you hover the mouse over a feature.",
+		es : "Las etiquetas se muestran cuando el rat&oacute;n se detiene sobre una entidad."
 	} ],
 	'configuraEtiqueta' : [ {
 		pt : "Configurar etiquetas",
-		en : "",
-		es : ""
+		en : "Configure labels",
+		es : "Configurar etiquetas"
 	} ],
 	'valorPonto' : [ {
 		pt : "Valores para o ponto indicado no mapa",
-		en : "",
-		es : ""
+		en : "Values of the point indicated on the map",
+		es : "Valores del punto indicado en el mapa"
 	} ],
 	'criaBuffer' : [ {
 		pt : "Criar entorno (buffer)",
-		en : "",
-		es : ""
+		en : "Create buffer",
+		es : "Crear entorno (buffer)"
 	} ],
 	'verDados' : [ {
 		pt : "Clique no tema para ver os dados",
-		en : "",
-		es : ""
+		en : "Click on the theme to see the data",
+		es : "Haga clic en el tema para ver los datos"
 	} ],
 	'msgNadaEncontrado' : [ {
 		pt : "Nada encontrado",
-		en : "",
-		es : ""
+		en : "Nothing found",
+		es : "Nada encontrado"
 	} ],
 	'todas' : [ {
 		pt : "todas",
-		en : "",
-		es : ""
+		en : "all",
+		es : "todas"
 	} ],
 	'mostraOcorrencia' : [ {
 		pt : "Mostra a ocorr&ecirc;ncia",
-		en : "",
-		es : ""
+		en : "Shows the occurrence",
+		es : "Muestra la ocurrencia"
 	} ],
 	'msgNadaEncontrado2' : [ {
 		pt : "Nada encontrado",
-		en : "",
-		es : ""
+		en : "Nothing found",
+		es : "Nada encontrado"
 	} ],
 	'adicionaPonto' : [ {
 		pt : "Clique aqui para adicionar um ponto",
-		en : "",
-		es : ""
+		en : "Click here to add a pont",
+		es : "Haga clic aqu&iacute; para a&ntilde;adir un punto"
 	} ],
 	'apagaRegistro' : [ {
 		pt : "Apagar registro",
-		en : "",
-		es : ""
+		en : "Delete record",
+		es : "Borrar registro"
 	} ],
 	'removeRegistroBD' : [ {
 		pt : "Remove o registro do banco de dados?",
-		en : "",
-		es : ""
+		en : "Do you want to remove the record from the database?",
+		es : "&iquest;Quiere eliminar el registro de la base de datos?"
 	} ],
 	'sim' : [ {
 		pt : "Sim",
-		en : "",
-		es : ""
+		en : "Yes",
+		es : "S&iacute;"
 	} ],
 	'cancela' : [ {
 		pt : "Cancela",
-		en : "",
-		es : ""
+		en : "Cancel",
+		es : "Cancelar"
 	} ],
 	'move' : [ {
 		pt : "Mover",
-		en : "",
-		es : ""
+		en : "Move",
+		es : "Mover"
 	} ],
 	'cliqueNovaPosicao' : [ {
 		pt : "Clique no mapa para indicar a nova posi&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Click on the map to indicate the new position",
+		es : "Haga clic en el mapa para indicar la posici&oacute;n nueva"
 	} ],
 	'etiquetaAtiva' : [ {
 		pt : "Etiqueta est&aacute; ativa",
-		en : "",
-		es : ""
+		en : "Label is active",
+		es : "Etiqueta est&aacute; activa"
 	} ],
 	'filtraValor' : [ {
 		pt : "Filtrar por valor",
-		en : "",
-		es : ""
+		en : "Filter by value",
+		es : "Filtrar por valor"
 	} ],
 	'removeFiltro' : [ {
 		pt : "Remove filtro",
-		en : "",
-		es : ""
+		en : "Remove filter",
+		es : "Quitar el filtro"
 	} ],
 	'atualizaJanela' : [ {
 		pt : "Atualiza essa janela",
-		en : "",
-		es : ""
+		en : "Refreshes this window",
+		es : "Actualiza esta ventana"
 	} ],
 	'resultado' : [ {
 		pt : "Resultado",
-		en : "",
-		es : ""
+		en : "Result",
+		es : "Resultado"
 	} ]
 };
