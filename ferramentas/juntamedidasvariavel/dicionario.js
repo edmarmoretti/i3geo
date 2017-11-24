770ccb4d959d8d73804ee78ba6c2b7aa806d4521
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-24 17:32:23 -0200

    Tradução de ferramentas/juntamedidasvariavel/dicionario.js

--- ferramentas/juntamedidasvariavel/dicionario.js
+++ ferramentas/juntamedidasvariavel/dicionario.js
@@ -1,47 +1,47 @@
 i3GEOF.juntamedidasvariavel.dicionario = {
 	'titulo' : [ {
 		pt : "Jun&ccedil;&atilde;o de tabelas",
-		en : "",
-		es : ""
+		en : "Table join",
+		es : "Uni&oacute;n de tablas"
 	} ],
 	'ajuda' : [ {
 		pt : "Os c&aacute;lculos s&atilde;o f&oacute;rmulas que utilizam as colunas que forem juntadas e que ser&atilde;o inclu&iacute;das na nova camada. Cada uma dessas colunas receber&aacute; um nome sequencial, por exemplo <i>valortema0,valortema1, valortema2...</i><br><br>As colunas calculadas podem utilizar esses nomes como entrada em f&oacute;rmulas. Cada f&oacute;rmula ir&aacute; gerar uma nova coluna na tabela e deve utilizar a sintaxe SQL (<a href='http://www.postgresql.org/docs/9.3/static/functions-math.html' target=_blank >SQL no Postgres</a>)<br><br>Exemplos de uso:<br><br>coluna de nome: <b>soma</b><br>valortema0 + valortema1 <br><br>coluna de nome: <b>taxa</b><br>(valortema0 * 100) / valortema1",
-		en : "",
-		es : ""
+		en : "Calculations are formulas that use joined columns and will be included in the new layer. Each of these columns will receive a sequential name, for example valortema0, valortema1, valortema2 ...</i><br><br>Computed columns can use these names as input into formulas. Each formula will generate a new column in the table and must use a SQL syntax (SQL in Postgres)(<a href='http://www.postgresql.org/docs/9.3/static/functions-math.html' target=_blank >SQL no Postgres</a>)<br><br>Examples of use:<br><br>column name: <b>sum</b><br>valuetema0 + valuetema1<br><br>column name: <b>rate</b><br>(valortema0 * 100) / valortema1",
+		es : "Los c&aacute;lculos son f&oacute;rmulas que utilizan las columnas enlazadas y que se incluir&aacute;n en la capa nueva. Cada una de estas columnas recibir&aacute; un nombre secuencial, por ejemplo valortema0, valortema1, valortema2 ...</i><br><br>Las columnas calculadas pueden utilizar estos nombres como entrada de f&oacute;rmulas. Cada f&oacute;rmula generar&aacute; una columna nueva en la tabla y debe utilizar la sintaxis SQL (SQL en Postgres)(<a href='http://www.postgresql.org/docs/9.3/static/functions-math.html' target=_blank >SQL no Postgres</a>)<br><br>Ejemplos de uso:<br><br>columna de nombre: <b>suma</b><br>valortema0 + valortema1<br><br>columna de nombre: <b>tasa</b><br>(valortema0 * 100) / valortema1"
 	} ],
 	'escolha' : [ {
 		pt : "Escolha as camadas de mesmo tipo e regi&atilde;o",
-		en : "",
-		es : ""
+		en : "Select layers of the same type and region",
+		es : "Seleccione las capas del mismo tipo y regi&oacute;n"
 	} ],
 	'nome' : [ {
 		pt : "Nome da nova camada",
-		en : "",
-		es : ""
+		en : "New layer name",
+		es : "Nombre de la capa nueva"
 	} ],
 	'aplicar' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'colunas' : [ {
 		pt : "Colunas calculadas (opcional)",
-		en : "",
-		es : ""
+		en : "Computed columns (optional)",
+		es : "Columnas calculadas (opcional)"
 	} ],
 	'addno' : [ {
 		pt : "Adiciona entrada de formulario",
-		en : "",
-		es : ""
+		en : "Add form entry",
+		es : "Agregar entrada de formulario"
 	} ],
 	'tituloform' : [ {
 		pt : "Nome (apenas letras e n&uacute;meros)",
-		en : "",
-		es : ""
+		en : "Name (only letters and numbers)",
+		es : "Nombre (solo letras y n&uacute;meros)"
 	} ],
 	'titulosql' : [ {
 		pt : "Digite a f&oacute;rmula (padr&atilde;o sql)",
-		en : "",
-		es : ""
+		en : "Enter the formula (sql standard)",
+		es : "Introduzca la f&oacute;rmula (est&aacute;ndar sql)"
 	} ]
 };
