3e81fec14ccc6f2058439d22a1998c5ba45ba7e0
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-24 16:43:27 -0200

    Tradução de ferramentas/inserexy2/dicionario.js

--- ferramentas/inserexy2/dicionario.js
+++ ferramentas/inserexy2/dicionario.js
@@ -2,177 +2,177 @@
 i3GEOF.inserexy.dicionario = {
 	'tema' : [ {
 		pt : "Tema",
-		en : "",
-		es : ""
+		en : "Theme",
+		es : "Tema"
 	} ],
 	'digita' : [ {
 		pt : "Digitar",
-		en : "",
-		es : ""
+		en : "Enter",
+		es : "Introducir"
 	} ],
 	'cola' : [ {
 		pt : "Colar",
-		en : "",
-		es : ""
+		en : "Paste",
+		es : "Pegar"
 	} ],
 	'converte' : [ {
 		pt : "Converter",
-		en : "",
-		es : ""
+		en : "Transform",
+		es : "Convertir"
 	} ],
 	'coord' : [ {
 		pt : "Coordenadas",
-		en : "",
-		es : ""
+		en : "Coordinates",
+		es : "Coordenadas"
 	} ],
 	'projCoord' : [ {
 		pt : "Proje&ccedil;&atilde;o das coordenadas inseridas",
-		en : "",
-		es : ""
+		en : "Projection of inserted coordinates",
+		es : "Proyecci&oacute;n de las coordenadas insertadas"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Escolha o tema edit&aacute;vel para inserir os pontos",
-		en : "",
-		es : ""
+		en : "Select an editable theme to insert the points",
+		es : "Seleccione un tema editable para insertar los puntos"
 	} ],
 	'criaTemaEditavel' : [ {
 		pt : "Criar um tema edit&aacute;vel",
-		en : "",
-		es : ""
+		en : "Create an editable theme",
+		es : "Crear un tema editable"
 	} ],
 	'graficoPerfil' : [ {
 		pt : "Gr&aacute;fico de perfil",
-		en : "",
-		es : ""
+		en : "Profile graphic",
+		es : "Gr&aacute;fico de perfil"
 	} ],
 	'selecionaItem' : [ {
 		pt : "Se vc quiser, escolha um item existente no tema e o valor que ser&aacute; inclu&iacute;do quando o ponto for adicionado. Vc pode mudar o valor quando desejar",
-		en : "",
-		es : ""
+		en : "If you like, you can select an existing field of the theme and a value that will be added when a point is added. You can change this value whenever you want",
+		es : "Si lo desea, elija un campo existente en el tema y el valor que se incluir&aacute; cuando se agregue el punto. Usted puede cambiar ese valor cuando lo desee"
 	} ],
 	'ajuda2' : [ {
 		pt : "Ap&oacute;s selecionar ou criar um tema, clique no mapa para inserir os pontos ou use as op&ccedil;&otilde;es das guias acima.",
-		en : "",
-		es : ""
+		en : "After selecting or creating a theme, click on the map to insert the points or use the options of the tabs above.",
+		es : "Despu&eacute;s de seleccionar o crear un tema, haga clic en el mapa para insertar los puntos o utilice las opciones de las pesta&ntilde;as de arriba."
 	} ],
 	'ajuda3' : [ {
 		pt : "Para inserir pontos, &eacute; necess&aacute;rio a exist&ecirc;ncia de um tema edit&aacute;vel dispon&iacute;vel no mapa.",
-		en : "",
-		es : ""
+		en : "In order to insert points, you must have an editable theme available on the map.",
+		es : "Para insertar puntos es necesario la existencia de un tema editable disponible en el mapa."
 	} ],
 	'ajuda4' : [ {
 		pt : "Utilize o bot&atilde;o <i>criar um novo</i> se n&atilde;o existir nenhum tema desse tipo no mapa.",
-		en : "",
-		es : ""
+		en : "Use the <i>create new</i> button if there is no this kind of theme on the map.",
+		es : "Utilice el bot&oacute;n <i>crear uno nuevo</i>  si no hay ning&uacute;n tema de este tipo en el mapa."
 	} ],
 	'direcao' : [ {
 		pt : "&Acirc;ngulo (grau, minuto e segundo)",
-		en : "",
-		es : ""
+		en : "Angle (degrees, minutes and seconds)",
+		es : "&Aacute;ngulo (grados, minutos y segundos)"
 	} ],
 	'distancia' : [ {
 		pt : "Dist&acirc;ncia em Km",
-		en : "",
-		es : ""
+		en : "Distance in Km",
+		es : "Distancia en Km"
 	} ],
 	'insere' : [ {
 		pt : "Insere",
-		en : "",
-		es : ""
+		en : "Insert",
+		es : "Insertar"
 	} ],
 	'ajuda5' : [ {
 		pt : "O pr&oacute;ximo ponto ser&aacute; inclu&iacute;do em rela&ccedil;&atilde;o ao &uacute;ltimo ponto existente no tema, considerando-se apenas os pontos vis&iacute;veis na extens&atilde;o do mapa atual.",
-		en : "",
-		es : ""
+		en : "The next point will be inserted in relation to the last existing point of the theme. It only will take into account visible points in the current map extent.",
+		es : "El punto siguiente ser&aacute; incluido en relaci&oacute;n al &uacute;ltimo punto existente en el tema, considerando solo los puntos visibles en la extensi&oacute;n del mapa actual."
 	} ],
 	'anguloDistancia' : [ {
 		pt : "Digite o &acirc;ngulo em rela&ccedil;&atilde;o ao Norte e a dist&acirc;ncia em Km.",
-		en : "",
-		es : ""
+		en : "Enter an angle in relation to the North and a distance in Km.",
+		es : "Introduzca el &aacute;ngulo en relaci&oacute;n al Norte y la distancia en Km."
 	} ],
 	'tipoEntrada' : [ {
 		pt : "Tipo de entrada",
-		en : "",
-		es : ""
+		en : "Input type",
+		es : "Tipo de entrada"
 	} ],
 	'mascara' : [ {
 		pt : "M&aacute;scara",
-		en : "",
-		es : ""
+		en : "Mask",
+		es : "M&aacute;scara"
 	} ],
 	'campoUnico' : [ {
 		pt : "Campo &uacute;nico",
-		en : "",
-		es : ""
+		en : "Unique field",
+		es : "Campo &uacute;nico"
 	} ],
 	'ajuda6' : [ {
 		pt : "Digite as coordenadas do ponto desejado e clique em <i>Insere</i>.",
-		en : "",
-		es : ""
+		en : "Enter the coordinates of the desired point and click <i>Insert</i>.",
+		es : "Introduzca las coordenadas del punto deseado y haga clic en <i>Insertar</i>."
 	} ],
 	'ajuda7' : [ {
 		pt : "A coordenada X corresponde a longitude (leste-oeste) e a coordenada Y corresponde a latitude (norte-sul).",
-		en : "",
-		es : ""
+		en : "X coordinate corresponds to longitude (east-west) and  Y coordinate corresponds to latitude (north-south).",
+		es : "La coordenada X corresponde a la longitud (este-oeste) y la coordenada Y corresponde a la latitud (norte-sur)."
 	} ],
 	'ajuda8' : [ {
-		pt : "Os valores devem sempre estar no formato Grau, Minuto e Segundo, sendo que no caso da op&ccedil;&atilde;o de campo &uacute;nico, os valores devem estar separados por espa&ccedil;os. Caso as coordenadas estejam em d&eacute;cimos de grau, utilize a guia <i>Colar</i>.'",
-		en : "",
-		es : ""
+		pt : "Os valores devem sempre estar no formato Grau, Minuto e Segundo, sendo que no caso da op&ccedil;&atilde;o de campo &uacute;nico, os valores devem estar separados por espa&ccedil;os. Caso as coordenadas estejam em d&eacute;cimos de grau, utilize a guia <i>Colar</i>.",
+		en : "Values must always be in Degrees, Minutes and Seconds. In the case of unique field option, values must be separated by spaces. If coordinates are in decimal degrees use the <i>Paste</i> tab.",
+		es : "Los valores deben siempre deben estar en el formato de Grados, Minutos y Segundos. En el caso de la opci&oacute;n de campo &uacute;nico los valores deben estar separados por espacios. Si las coordenadas est&aacute;n en grados decimales utilice la pesta&ntilde;a <i>Pegar</i>."
 	} ],
 	'ajuda9' : [ {
 		pt : "O valor correspondente ao Grau deve receber o sinal de negativo(-) nos casos de longitude oeste e nos casos de latitude Sul.",
-		en : "",
-		es : ""
+		en : "Degree values should have a negative sign (-) in the case of west longitude and/or South latitude.",
+		es : "El valor correspondiente al Grado debe recibir el signo negativo (-) en los casos de longitud oeste y/o latitud Sur."
 	} ],
 	'insereListaCoords' : [ {
 		pt : "Cole ou digite a lista de coordenadas",
-		en : "",
-		es : ""
+		en : "Paste or enter the list of coordinates",
+		es : "Pegue o introduzca la lista de coordenadas"
 	} ],
 	'insereXY' : [ {
 		pt : "Cole os valores de X e Y, em d&eacute;cimos de grau, com sinal de negativo para oeste e sul, exemplo:",
-		en : "",
-		es : ""
+		en : "Paste X and Y values in decimal degrees. They should have a negative sign to the west and south, for example:",
+		es : "Pegue los valores de X e Y en grados decimales. Deben tener signo de negativo hacia el oeste y el sur, por ejemplo:"
 	} ],
 	'listaWkt' : [ {
 		pt : "Lista wkt",
-		en : "",
-		es : ""
+		en : "wkt list",
+		es : "Lista wkt"
 	} ],
 	'criaTemaPoligono' : [ {
 		pt : "Cria tema poligonal",
-		en : "",
-		es : ""
+		en : "Create a polygon-based theme",
+		es : "Crear un tema de pol&iacute;gonos"
 	} ],
 	'criaTemaLinha' : [ {
 		pt : "Cria tema linear",
-		en : "",
-		es : ""
+		en : "Create a line-based theme",
+		es : "Crear un tema de l&iacute;neas"
 	} ],
 	'tituloNovoTema' : [ {
 		pt : "T&iacute;tulo do novo tema",
-		en : "",
-		es : ""
+		en : "New theme title",
+		es : "T&iacute;tulo del tema nuevo"
 	} ],
 	'pontosInseridos' : [ {
 		pt : "Pontos inseridos ",
-		en : "",
-		es : ""
+		en : "Inserted points ",
+		es : "Puntos insertados"
 	} ],
 	'msgNenhumTemaDefinido' : [ {
 		pt : "Nenhum tema definido para editar",
-		en : "",
-		es : ""
+		en : "No theme set to edit",
+		es : "Ning&uacute;n tema definido para editar"
 	} ],
 	'msgNenhumPontoEncontrado' : [ {
 		pt : "Nenhum ponto encontrado",
-		en : "",
-		es : ""
+		en : "No points found",
+		es : "Ning&uacute;n punto encontrado"
 	} ],
 	'ajuda' : [ {
 		pt : "Os pontos s&atilde;o armazenados temporariamente no servidor e s&atilde;o perdidos quando o mapa &eacute; fechado. Para reutilizar os dados criados fa&ccedil;a o download do arquivo shapefile.",
-		en : "",
-		es : ""
+		en : "Points are temporarily stored on the server and will get lost when you close the map. To reuse this data download it as a shapefile.",
+		es : "Los puntos se almacenan temporalmente en el servidor y se pierden cuando se cierra el mapa. Para reutilizar los datos creados desc&aacute;rguelos como un archivo shapefile."
 	} ]
 };
