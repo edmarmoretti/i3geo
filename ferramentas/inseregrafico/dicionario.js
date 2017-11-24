c068f10067bf46c0535bf472f23661ee636af51e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-23 23:27:21 -0200

    Tradução de ferramentas/inseregrafico/dicionario.js

--- ferramentas/inseregrafico/dicionario.js
+++ ferramentas/inseregrafico/dicionario.js
@@ -2,47 +2,47 @@
 i3GEOF.insereGrafico.dicionario = {
 	'fonteDados' : [ {
 		pt : "Fonte dos dados",
-		en : "",
-		es : ""
+		en : "Data source",
+		es : "Fuente de los datos"
 	} ],
 	'propriedades' : [ {
 		pt : "Propriedades",
-		en : "",
-		es : ""
+		en : "Properties",
+		es : "Propiedades"
 	} ],
 	'msgAguarde' : [ {
 		pt : "Aguarde",
-		en : "",
-		es : ""
+		en : "Wait",
+		es : "Espere"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Escolha o tema para ver a lista de itens",
-		en : "",
-		es : ""
+		en : "Select the theme to see the list of fields",
+		es : "Seleccione el tema para ver la lista de campos"
 	} ],
 	'selecionaItens' : [ {
 		pt : "Marque os itens para compor as partes do gr&aacute;fico: Edite os valores de cor (R,G,B) conforme o desejado. Ap&oacute;s escolher os itens, clique no elemento do mapa para inserir o gr&aacute;fico.",
-		en : "",
-		es : ""
+		en : "Check the fields to build the chart. Edit color values (R, G, B) as desired. After selecting the fields, click on the map feature to insert the chart.",
+		es : "Marque los campos para construir las partes del gr&aacute;fico. Edite los valores de color (R, G, B) como desee. Despu&eacute;s de elegir los campos, haga clic en la entidad del mapa para insertar el gr&aacute;fico."
 	} ],
 	'inclinacaoCirculo' : [ {
 		pt : "Inclina&ccedil;&atilde;o do c&iacute;rculo",
-		en : "",
-		es : ""
+		en : "Circle tilt",
+		es : "Inclinaci&oacute;n del c&iacute;rculo"
 	} ],
 	'tamanhoSombra' : [ {
 		pt : "Tamanho da sombra",
-		en : "",
-		es : ""
+		en : "Shadow size",
+		es : "Tama&ntilde;o de la sombra"
 	} ],
 	'semTemaDefinido' : [ {
 		pt : "Nenhum tema definido para pegar os dados",
-		en : "",
-		es : ""
+		en : "No theme set to gather data",
+		es : "Ning&uacute;n tema definido para obtener datos"
 	} ],
 	'semItemEscolhido' : [ {
 		pt : "Nenhum item foi escolhido",
-		en : "",
-		es : ""
+		en : "No fields were chosen",
+		es : "No se ha seleccionado ning&uacute;n campo"
 	} ]
 };
