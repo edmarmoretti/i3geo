f8aaf75b4a493fa59c449cbb1ba510b48dde462e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-25 19:07:09 -0200

    Tradução de ferramentas/markercluster/dicionario.js

--- ferramentas/markercluster/dicionario.js
+++ ferramentas/markercluster/dicionario.js
@@ -2,77 +2,77 @@
 i3GEOF.markercluster.dicionario = {
 	'ajuda' : [ {
 		pt : "O agrupamento de pontos &eacute; feito conforme a proximidade entre as coordenadas originais e a escala do mapa, sendo o raio de busca definido em pixels. Cada cluster (grupo) receber&aacute; um s&iacute;mbolo e um n&uacute;mero indicando o total de pontos agrupados. Os pontos fora dos grupos s&atilde;o mostrados com um s&iacute;mbolo individual.",
-		en : "",
-		es : ""
+		en : "Point clustering is done according to the proximity between original coordinates and the map scale. The search radius is in pixels. Each cluster will have a symbol and a number indicating the total number of clustered points. Points outside a cluster are shown with a particular symbol.",
+		es : "La agrupaci&oacute;n de puntos se realiza seg&uacute;n la proximidad entre las coordenadas originales y la escala del mapa, en donde el radio de b&uacute;squeda es definido en p&iacute;xeles. Cada cl&uacute;ster (grupo) tendr&aacute; un s&iacute;mbolo y un n&uacute;mero que indica el total de puntos agrupados. Los puntos fuera de los grupos se muestran con un s&iacute;mbolo individual."
 	} ],
 	'ajuda2' : [ {
 		pt : "A camada adicionada ao mapa &eacute; controlada por elementos inclu&iacute;dos no navegador web, por isso nem todas as funcionalidades do i3Geo ir&atilde;o operar corretamente na nova camada inserida no mapa.",
-		en : "",
-		es : ""
+		en : "The layer added to the map is controlled by the web browser, some i3Geo functionalities won't perform correctly on the new layer added to the map.",
+		es : "La capa agregada al mapa es controlada a trav&eacute;s navegador web, por lo que no todas las funcionalidades del i3Geo se desempe&ntilde;ar&aacute;n correctamente en la capa nueva insertada en el mapa."
 	} ],
 	'selecionaTema' : [ {
 		pt : "Tema, que ser&aacute; utilizado",
-		en : "",
-		es : ""
+		en : "Theme that will be used",
+		es : "Tema que ser&aacute; utilizado"
 	} ],
 	4 : [ {
 		pt : "Valor de cada ponto:",
-		en : "",
-		es : ""
+		en : "Value of each point:",
+		es : "Valor de cada punto:"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema calculado ser&aacute; adicionado ao mapa atual",
-		en : "",
-		es : ""
+		en : "The computed theme will be added to the current map",
+		es : "El tema calculado se a&ntilde;adir&aacute; al mapa actual"
 	} ],
 	'criaCamada' : [ {
 		pt : "Criar camada",
-		en : "",
-		es : ""
+		en : "Create Layer",
+		es : "Crear una capa"
 	} ],
 	'erroTempo' : [ {
 		pt : "Erro. A opera&ccedil;&atilde;o demorou muito",
-		en : "",
-		es : ""
+		en : "Error. Too long time execution",
+		es : "Error. La operaci&oacute;n tard&oacute; mucho"
 	} ],
 	9 : [ {
 		pt : "Distancia invalida",
-		en : "",
-		es : ""
+		en : "Invalid distance",
+		es : "Distancia no v&aacute;lida"
 	} ],
 	10 : [ {
 		pt : "Ou um item da tabela de atributos que cont&eacute;m os valores:",
-		en : "",
-		es : ""
+		en : "Or a field of the attribute table that contains values:",
+		es : "O un campo de la tabla de atributos que contenga los valores:"
 	} ],
 	'tituloNovaCamada' : [ {
 		pt : "T&iacute;tulo da nova camada:",
-		en : "",
-		es : ""
+		en : "New layer title:",
+		es : "T&iacute;tulo de la capa nueva:"
 	} ],
 	'raio' : [ {
 		pt : "Raio de um ponto em pixels:",
-		en : "",
-		es : ""
+		en : "Radius of a point in pixels:",
+		es : "Radio de un punto en p&iacute;xeles:"
 	} ],
 	'opacidade' : [ {
 		pt : "Opacidade:",
-		en : "",
-		es : ""
+		en : "Opacity:",
+		es : "Opacidad:"
 	} ],
 	'color' : [ {
 		pt : "Cor do fundo:",
-		en : "",
-		es : ""
+		en : "Background Color:",
+		es : "Color del fondo:"
 	} ],
 	'strokecolor' : [ {
 		pt : "Cor do contorno:",
-		en : "",
-		es : ""
+		en : "Outline color:",
+		es : "Color del contorno:"
 	} ],
 	'textcolor' : [ {
 		pt : "Cor do texto:",
-		en : "",
-		es : ""
+		en : "Text color:",
+		es : "Color del texto:"
 	} ]
 };
