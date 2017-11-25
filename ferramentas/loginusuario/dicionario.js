f8d1ff4f0f66eb7df5b4b0fd1e447bbb4c1789ab
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-25 16:36:10 -0200

    Tradução de ferramentas/loginusuario/dicionario.js

--- ferramentas/loginusuario/dicionario.js
+++ ferramentas/loginusuario/dicionario.js
@@ -2,12 +2,12 @@
 i3GEOF.loginusuario.dicionario = {
 	"tentativas" : [ {
 		pt : "Ap&oacute;s 3 tentativas, tente mais tarde",
-		en : "",
-		es : ""
+		en : "After 3 attempts, try again later",
+		es : "Despu&eacute;s de 3 ingresos fallidos, int&eacute;ntelo m&aacute;s tarde"
 	} ],
 	"cabecalhoForm" : [ {
 		pt : "Digite o usu&aacute;rio e a senha para acessar o sistema de administra&ccedil;&atilde;o bem como outras funcionalidades de uso restrito",
-		en : "",
-		es : ""
+		en : "Enter the username and password to access the management system as well as other restricted functionalities",
+		es : "Introduzca el usuario y la contrase&ntilde;a para acceder al sistema de administraci&oacute;n y a otras funcionalidades de uso restringido"
 	} ]
 };
