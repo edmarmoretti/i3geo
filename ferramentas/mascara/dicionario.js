dbd2ee50ac940ffd300fa409ec78c6e8efd698ca
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-25 19:16:30 -0200

    Tradução de  ferramentas/mascara/dicionario.js

--- ferramentas/mascara/dicionario.js
+++ ferramentas/mascara/dicionario.js
@@ -2,22 +2,22 @@
 i3GEOF.mascara.dicionario = {
 	'selecionaTema' : [ {
 		pt : "Tema que ir&aacute; mascarar os demais",
-		en : "",
-		es : ""
+		en : "Theme that will mask other themes",
+		es : "Tema que enmascarar&aacute; a los dem&aacute;s"
 	} ],
 	'listaTemas' : [ {
 		pt : "Temas que ser&atilde;o afetados pela m&aacute;scara",
-		en : "",
-		es : ""
+		en : "Themes that will be affected by the mask",
+		es : "Temas que ser&aacute;n afectados por la m&aacute;scara"
 	} ],
 	'criaMascara' : [ {
 		pt : "Aplicar m&aacute;scara",
-		en : "",
-		es : ""
+		en : "Apply mask",
+		es : "Aplicar m&aacute;scara"
 	} ],
 	'tema' : [ {
 		pt : "Escolha um tema",
-		en : "",
-		es : ""
+		en : "Select a theme",
+		es : "Seleccione un tema"
 	} ]
 };
