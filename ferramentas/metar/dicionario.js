aeeefc8c1c2edf612415e1345e69cef09667b0d5
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-26 02:13:35 -0200

    Tradução de ferramentas/metar/dicionario.js

--- ferramentas/metar/dicionario.js
+++ ferramentas/metar/dicionario.js
@@ -2,62 +2,62 @@
 i3GEOF.metar.dicionario = {
 	'ajuda' : [ {
 		pt : "Navegue no mapa para atualizar a lista de resultados (s&atilde;o mostradas apenas as 10 primeiras esta&ccedil;&otilde;es encontradas)",
-		en : "",
-		es : ""
+		en : "Browse the map to update the list of results (only the first 10 stations are shown)",
+		es : "Navegue por el mapa para actualizar la lista de resultados (solo se muestran las 10 primeras estaciones encontradas)"
 	} ],
 	'estacao' : [ {
 		pt : "Esta&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Station",
+		es : "Estaci&oacute;n"
 	} ],
 	'temperatura' : [ {
 		pt : "temperatura",
-		en : "",
-		es : ""
+		en : "temperature",
+		es : "temperatura"
 	} ],
 	'condicao' : [ {
 		pt : "condi&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "condition",
+		es : "condici&oacute;n"
 	} ],
 	'observacao' : [ {
 		pt : "observa&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "observation",
+		es : "observaci&oacute;n"
 	} ],
 	'nuvens' : [ {
 		pt : "nuvens",
-		en : "",
-		es : ""
+		en : "clouds",
+		es : "nubes"
 	} ],
 	'direcaoVento' : [ {
 		pt : "dire&ccedil;&atilde;o do vento",
-		en : "",
-		es : ""
+		en : "wind direction",
+		es : "direcci&oacute;n del viento"
 	} ],
 	'pontoOrvalho' : [ {
 		pt : "ponto de orvalho",
-		en : "",
-		es : ""
+		en : "dew point",
+		es : "punto de roc&iacute;o"
 	} ],
 	'velocidadeVento' : [ {
 		pt : "velocidade do vento",
-		en : "",
-		es : ""
+		en : "wind speed",
+		es : "velocidad del viento"
 	} ],
 	'humidade' : [ {
 		pt : "humidade",
-		en : "",
-		es : ""
+		en : "humidity",
+		es : "humedad"
 	} ],
 	'data' : [ {
 		pt : "data",
-		en : "",
-		es : ""
+		en : "date",
+		es : "fecha"
 	} ],
 	'pressao' : [ {
 		pt : "press&atilde;o",
-		en : "",
-		es : ""
+		en : "pressure",
+		es : "presi&oacute;n"
 	} ]
 };
