87b92729292bdab9e8879a5cdffca727ab8905ae
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-26 22:48:13 -0200

    Tradução de ferramentas/mmscale/dicionario.js

--- ferramentas/mmscale/dicionario.js
+++ ferramentas/mmscale/dicionario.js
@@ -2,22 +2,22 @@
 i3GEOF.mmscale.dicionario = {
 	'minscale' : [ {
 		pt : "Denominador da escala m&iacute;nima (ao aproximar o mapa, o tema ser&aacute; mostrado se o denominador da escala do mapa for maior que esse valor)",
-		en : "",
-		es : ""
+		en : "Minimum Scale Denominator (when you zoom in on the map, a theme will be shown if the map scale denominator is greater than this value)",
+		es : "Denominador de escala m&iacute;nima (al aproximar el mapa el tema se visualizar&aacute; si el denominador de escala del mapa es mayor que ese valor)"
 	} ],
 	'maxscale' : [ {
 		pt : "Denominador da escala m&aacute;xima (ao aproximar o mapa, o tema ser&aacute; mostrado se o denominador da escala do mapa for menor que esse valor)",
-		en : "",
-		es : ""
+		en : "Maximum Scale Denominator (when you zoom in on the map, a theme will be shown if the map scale denominator is smaller than this value)",
+		es : "Denominador de escala m&aacute;xima (al aproximar el mapa el tema se visualizar&aacute; si el denominador de escala del mapa es menor que ese valor)"
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'cancelar' : [ {
 		pt : "Para cancelar qualquer dos efeitos, utilize o valor 0, -1 ou vazio",
-		en : "",
-		es : ""
+		en : "Use 0, -1 or empty to cancel any effect",
+		es : "Para cancelar cualquier efecto utilice el valor 0, -1 o vac&iacute;o"
 	} ]
 };
