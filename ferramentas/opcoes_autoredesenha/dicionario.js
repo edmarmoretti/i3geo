6cf7934c463fdae9463a73f81dad59eea14b80ed
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-27 01:07:49 -0200

    Tradução de  ferramentas/opcoes_autoredesenha/dicionario.js

--- ferramentas/opcoes_autoredesenha/dicionario.js
+++ ferramentas/opcoes_autoredesenha/dicionario.js
@@ -2,17 +2,17 @@
 i3GEOF.opcoesTempo.dicionario = {
 	'ajuda' : [ {
 		pt : "Tempo em segundos ap&oacute;s o qual o mapa ser&aacute; redesenhado automaticamente",
-		en : "",
-		es : ""
+		en : "Time in seconds after which the map will be automatically re-rendered",
+		es : "Tiempo en segundos despu&eacute;s del cual el mapa se cargar&aacute; nuevamente de manera autom&aacute;tica"
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'para' : [ {
 		pt : "Parar",
-		en : "",
-		es : ""
+		en : "Stop",
+		es : "Parar"
 	} ]
 };
