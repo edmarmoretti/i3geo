68b1717ef6b1cedb76bb3dc966548bd9eb6567f5
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-27 00:19:42 -0200

    Tradução de ferramentas/nptpol/dicionario.js

--- ferramentas/nptpol/dicionario.js
+++ ferramentas/nptpol/dicionario.js
@@ -2,42 +2,42 @@
 i3GEOF.nptpol.dicionario = {
 	'ajuda' : [ {
 		pt : "Ponto em pol&iacute;gono &eacute; uma opera&ccedil;&atilde;o que resulta em um novo tema contendo o cruzamento entre um tema com pontos e outro com pol&iacute;gonos considerando-se a extens&atilde;o geogr&aacute;fica do mapa atual.",
-		en : "",
-		es : ""
+		en : "Point in polygon is an operation that results in a new theme that contains the intersection between a point-based theme and a polygon-based theme. This operation is carried out on the geographical extent of the current map.",
+		es : "El punto en pol&iacute;gono es una operaci&oacute;n que resulta en un tema nuevo que contiene la intersecci&oacute;n entre un tema de puntos y otros de pol&iacute;gonos considerando la extensi&oacute;n geogr&aacute;fica del mapa actual."
 	} ],
 	'ajuda2' : [ {
 		pt : "O resultado ser&aacute; um novo tema do tipo poligonal, sendo que a tabela de atributos conter&aacute; o total de pontos que se sobrep&otilde;em ao pol&iacute;gono.",
-		en : "",
-		es : ""
+		en : "The result will be a new polygon theme whose attribute table will contain the total points that overlap the polygon.",
+		es : "El resultado ser&aacute; un tema nuevo de pol&iacute;gono en el que la tabla de atributos contendr&aacute; el total de puntos que se superponen al pol&iacute;gono."
 	} ],
 	'ajuda3' : [ {
 		pt : "Para gerar o cruzamento &eacute; necess&aacute;rio que no mapa exista pelo menos um tema poligonal e um com os pontos.",
-		en : "",
-		es : ""
+		en : "In order to make the intersection, it is necessary that on the map there is at least one polygon-based theme and another one with points.",
+		es : "Para efectuar la intersecci&oacute;n es necesario que en el mapa exista a menos un tema de pol&iacute;gono y uno con los puntos."
 	} ],
 	'selecionaTemaPonto' : [ {
 		pt : "Tema contendo os pontos",
-		en : "",
-		es : ""
+		en : "Theme containing points",
+		es : "Tema que contiene los puntos"
 	} ],
 	'selecionaTemaPoligono' : [ {
 		pt : "Tema contendo os pol&iacute;gonos",
-		en : "",
-		es : ""
+		en : "Theme containing polygons",
+		es : "Tema que contiene los pol&iacute;gonos"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema com o cruzamento ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : ""
+		en : "A theme with the intersection will be added to the current map.",
+		es : "El tema con la intersecci&oacute;n se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'calcula' : [ {
 		pt : "Calcular",
-		en : "",
-		es : ""
+		en : "Calculate",
+		es : "Calcular"
 	} ],
 	'selecionaItemSoma' : [ {
 		pt : "(Opcional) Item da tabela que ser&aacute; somado",
-		en : "",
-		es : ""
+		en : "(Optional) Field of the table that will be added up",
+		es : "(Opcional) Campo de la tabla que ser&aacute; sumado"
 	} ]
 };
