65dd6842c3d94bdee5e02dbf3d2c8426fe8b056c
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-26 23:13:02 -0200

    Tradução de ferramentas/mostraexten/dicionario.js

--- ferramentas/mostraexten/dicionario.js
+++ ferramentas/mostraexten/dicionario.js
@@ -2,43 +2,43 @@
 i3GEOF.mostraExten.dicionario = {
 	'ajuda' : [ {
 		pt : "Extens&atilde;o geogr&aacute;fica atual, em d&eacute;cimos de grau. As coordenadas correspondem a menor longitude, menor latitude, maior longitude e maior latitude",
-		en : "",
-		es : ""
+		en : "Current geographical extent in decimal degrees. Coordinates correspond to the min longitude, min latitude, max longitude and max latitude",
+		es : "Extensi&oacute;n geogr&aacute;fica actual en grados decimales. Las coordenadas corresponden a longitud menor m&iacute;nima, latitud m&iacute;nima, longitud m&aacute;xima y latitud m&aacute;xima"
 	} ],
 	'digitaCoord' : [ {
 		pt : "Digite as coordenadas referentes a nova extens&atilde;o geogr&aacute;fica desejada para o mapa. Utilize coordenadas (graus) negativos para indicar a longitude como oeste e latitude como sul",
-		en : "",
-		es : ""
+		en : "Enter coordinates for the geographical extent you want to display on the map. Use negative (degrees) coordinates for west longitude and south latitude",
+		es : "Introduzca las coordenadas de la extensi&oacute;n geogr&aacute;fica que desea visualizar en el mapa. Utilice coordenadas (grados) negativas para longitud oeste y latitud sur"
 	} ],
 	'longW' : [ {
 		pt : "Longitude oeste",
-		en : "",
-		es : ""
+		en : "West longitude",
+		es : "Longitud oeste"
 	} ],
 	'latS' : [ {
 		pt : "Latitude sul",
-		en : "",
-		es : ""
+		en : "South latitude",
+		es : "Latitud sur"
 	} ],
 	'longE' : [ {
 		pt : "Longitude leste",
-		en : "",
-		es : ""
+		en : "East longitude",
+		es : "Longitud este"
 	} ],
 	'latN' : [ {
 		pt : "Latitude norte",
-		en : "",
-		es : ""
+		en : "North latitude",
+		es : "Latitud norte"
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar a nova extens&atilde;o",
-		en : "",
-		es : ""
+		en : "Apply new extension",
+		es : "Aplicar la nueva extensi&oacute;n"
 	} ],
 	'msgCoordValida' : [ {
 		pt : "Digite coordenadas v&aacute;lidas",
-		en : "",
-		es : ""
+		en : "Please enter valid coordinates",
+		es : "Ingrese coordenadas v&aacute;lidas"
 	} ],
 	9 : [ {
 		pt : "",
