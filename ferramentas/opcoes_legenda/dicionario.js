7600611a01508ab9265aaca531fdc9b122dd4130
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-28 18:52:06 -0200

    Tradução de ferramentas/opcoes_legenda/dicionario.js

--- ferramentas/opcoes_legenda/dicionario.js
+++ ferramentas/opcoes_legenda/dicionario.js
@@ -1,98 +1,98 @@
 //+$trad(1,i3GEOF.opcoesLegenda.dicionario)+
 i3GEOF.opcoesLegenda.dicionario = {
 	'msg' : [ {
-		pt : "Essa operacao afeta apenas a legenda utilizada na ferramenta de impressao do mapa",
-		en : "",
-		es : ""
+		pt : "Essa opera&ccedil;&atilde;o afeta apenas a legenda utilizada na ferramenta de impress&atilde;o do mapa",
+		en : "This operation only affects the legend used for the map printing tool",
+		es : "Esta operaci&oacute;n solo afecta a la leyenda utilizada en la herramienta de impresi&oacute;n del mapa"
 	} ],
 	'incluiNoMapa' : [ {
 		pt : "Inclui no corpo do mapa?",
-		en : "",
-		es : ""
+		en : "Do you want to add it to the map body?",
+		es : "&iquest;Se Incluye en el cuerpo del mapa?"
 	} ],
 	'corFundo' : [ {
 		pt : "Cor do fundo",
-		en : "",
-		es : ""
+		en : "Background color",
+		es : "Color del fondo"
 	} ],
 	'contornoSimbolos' : [ {
 		pt : "Contorno dos s&iacute;mbolos",
-		en : "",
-		es : ""
+		en : "Symbol outline",
+		es : "Contorno de los símbolos"
 	} ],
 	'posicao' : [ {
 		pt : "Posi&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Position",
+		es : "Posici&oacute;n"
 	} ],
 	'supEsquerdo' : [ {
 		pt : "sup. esquerdo",
-		en : "",
-		es : ""
+		en : "upper left",
+		es : "sup. izquierdo"
 	} ],
 	'supCentro' : [ {
 		pt : "sup. centro",
-		en : "",
-		es : ""
+		en : "upper center",
+		es : "sup. centro"
 	} ],
 	'supDireito' : [ {
 		pt : "sup. direito",
-		en : "",
-		es : ""
+		en : "upper right",
+		es : "sup. derecho"
 	} ],
 	'infEsquerdo' : [ {
 		pt : "inf. esquerdo",
-		en : "",
-		es : ""
+		en : "lower left",
+		es : "inf. izquierdo"
 	} ],
 	'infCentro' : [ {
 		pt : "inf. centro",
-		en : "",
-		es : ""
+		en : "lower center",
+		es : "inf. centro"
 	} ],
 	'infDireito' : [ {
 		pt : "inf. direito",
-		en : "",
-		es : ""
+		en : "lower right",
+		es : "inferior derecho"
 	} ],
 	'espacamentoY' : [ {
 		pt : "Espa&ccedil;amento em Y",
-		en : "",
-		es : ""
+		en : "Spacing in Y",
+		es : "Espaciado en Y"
 	} ],
 	'espacamentoX' : [ {
 		pt : "Espa&ccedil;amento em X",
-		en : "",
-		es : ""
+		en : "Spacing in X",
+		es : "Espaciado en X"
 	} ],
 	'tamanhoSimboloX' : [ {
 		pt : "Tamanho do s&iacute;mb. Y",
-		en : "",
-		es : ""
+		en : "Size of Y symbol",
+		es : "Tama&ntilde;o del s&iacute;mbolo Y"
 	} ],
 	'tamanhoSimboloX' : [ {
 		pt : "Tamanho do s&iacute;mb. X",
-		en : "",
-		es : ""
+		en : "Size of X symbol",
+		es : "Tama&ntilde;o del s&iacute;mbolo X"
 	} ],
 	'tamanhoTexto' : [ {
 		pt : "Tamanho do texto",
-		en : "",
-		es : ""
+		en : "Text Size",
+		es : "Tama&ntilde;o del texto"
 	} ],
 	'fonte' : [ {
 		pt : "Fonte tipogr&aacute;fica",
-		en : "",
-		es : ""
+		en : "Typographical font",
+		es : "Fuente tipogr&aacute;fica"
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'testa' : [ {
 		pt : "Testar",
-		en : "",
-		es : ""
+		en : "Test",
+		es : "Probar"
 	} ]
 };
