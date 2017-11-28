41c93ee73e5b51bf8d5fd9e0a1ea3015ef3a4855
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-28 19:09:44 -0200

    Tradução de ferramentas/opcoes_tamanho/dicionario.js

--- ferramentas/opcoes_tamanho/dicionario.js
+++ ferramentas/opcoes_tamanho/dicionario.js
@@ -2,32 +2,32 @@
 i3GEOF.opcoesTamanho.dicionario = {
 	'largura' : [ {
 		pt : "Largura",
-		en : "",
-		es : ""
+		en : "Width",
+		es : "Ancho"
 	} ],
 	'altura' : [ {
 		pt : "Altura",
-		en : "",
-		es : ""
+		en : "Height",
+		es : "Altura"
 	} ],
 	'valor' : [ {
 		pt : "Valores em pixels",
-		en : "",
-		es : ""
+		en : "Values in pixels",
+		es : "Valores en p&iacute;xeles"
 	} ],
 	'testa' : [ {
 		pt : "Testar",
-		en : "",
-		es : ""
+		en : "Test",
+		es : "Probar"
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'msg' : [ {
-		pt : "Essa operacao afeta apenas a legenda utilizada na ferramenta de impressao do mapa",
-		en : "",
-		es : ""
+		pt : "Essa opera&ccedil;&atilde;o afeta apenas a legenda utilizada na ferramenta de impress&atilde;o do mapa",
+		en : "This operation only affects the legend used for the map printing tool",
+		es : "Esta operaci&oacute;n solo afecta a la leyenda utilizada en la herramienta de impresi&oacute;n del mapa"
 	} ]
 };
