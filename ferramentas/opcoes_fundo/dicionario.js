4e07ece0e30441d22ff42c5d0f6cdb3bb7a97405
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-28 00:07:38 -0200

    Tradução de ferramentas/opcoes_fundo/dicionario.js

--- ferramentas/opcoes_fundo/dicionario.js
+++ ferramentas/opcoes_fundo/dicionario.js
@@ -2,17 +2,17 @@
 i3GEOF.opcoesFundo.dicionario = {
 	'ajuda' : [ {
 		pt : "Essa operacao afeta apenas algumas funcionalidades, como a ferramenta de impressao do mapa",
-		en : "",
-		es : ""
+		en : "This operation only affects certain functionalities, such as the map printing tool",
+		es : "Esta operaci&oacute;n afecta solo a algunas funcionalidades, como la herramienta de impresi&oacute;n del mapa"
 	} ],
 	'aplica' : [ {
 		pt : "Aplica",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'cor' : [ {
 		pt : "Escolha a cor desejada",
-		en : "",
-		es : ""
+		en : "Select a color",
+		es : "Seleccione el color deseado"
 	} ]
 };
