20754ceca51d654a7b0eab814374bf1888f84dc0
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-29 00:00:31 -0200

    Tradução de ferramentas/pontoempoligono/dicionario.js

--- ferramentas/pontoempoligono/dicionario.js
+++ ferramentas/pontoempoligono/dicionario.js
@@ -2,48 +2,48 @@
 i3GEOF.pontoEmPoligono.dicionario = {
 	'ajuda' : [ {
 		pt : "Ponto em pol&iacute;gono &eacute; uma opera&ccedil;&atilde;o que resulta em um novo tema contendo o cruzamento entre um tema com pontos e outros com pol&iacute;gonos ou raster.",
-		en : "",
-		es : ""
+		en : "Point in polygon is an operation that results in a new theme that contains the intersection between a point-based theme and polygon-based themes or raster.",
+		es : "Punto en pol&iacute;gono es una operaci&oacute;n que resulta en un nuevo tema que contiene la intersecci&oacute;n entre un tema de puntos y otros de pol&iacute;gonos o raster."
 	} ],
 	'ajuda2' : [ {
 		pt : "As informa&ccedil;&otilde;es do tema poligonal ser&atilde;o inseridas na tabela de atributos de um novo tema do tipo pontual.",
-		en : "",
-		es : ""
+		en : "Information about a polygon-based theme will be inserted into the attribute table of a new point-based theme.",
+		es : "La informaci&oacute;n del tema de polígono se inserta en la tabla de atributos de un tema nuevo de puntos."
 	} ],
 	'ajuda3' : [ {
 		pt : "Para gerar o cruzamento &eacute; necess&aacute;rio que no mapa exista pelo menos um tema poligonal e um com os pontos.",
-		en : "",
-		es : ""
+		en : "In order to create an intersection, it is necessary that on the map there is at least one polygon-based theme and one point-based theme.",
+		es : "Para efectuar la intersecci&oacute;n es necesario que en el mapa exista al menos un tema de pol&iacute;gono y uno de puntos."
 	} ],
 	'selecionaTemaPontos' : [ {
 		pt : "Escolha o tema que cont&eacute;m os pontos",
-		en : "",
-		es : ""
+		en : "Select a theme that contains points",
+		es : "Seleccione el tema que contenga los puntos"
 	} ],
 	'msgSelecionaTema' : [ {
 		pt : "Voc&ecirc; precisa escolher um tema com os pontos.",
-		en : "",
-		es : ""
+		en : "You need to select a theme with points.",
+		es : "Usted necesita elegir un tema con los puntos."
 	} ],
 	'SelecionaTemaCruzamento' : [ {
 		pt : "Escolha os temas que ser&atilde;o utilizados para cruzar com os pontos",
-		en : "",
-		es : ""
+		en : "Select the themes that will be intersected with points",
+		es : "Seleccione los temas que se intersectar&aacute;n con los puntos"
 	} ],
 	'adicionaTema' : [ {
 		pt : "O tema resultante ser&aacute; adicionado ao mapa atual.",
-		en : "",
-		es : ""
+		en : "The resulting theme will be added to the current map.",
+		es : "El tema resultante se a&ntilde;adir&aacute; al mapa actual."
 	} ],
 	'msgRasterPoligono' : [ {
 		pt : "Escolha um tema raster ou poligonal pelo menos",
-		en : "",
-		es : ""
+		en : "Select at least a raster or a polygon-based theme",
+		es : "Seleccione al menos un tema raster o de pol&iacute;gono"
 	} ],
 	'colunas' : [ {
 		pt : "Colunas",
-		en : "",
-		es : ""
+		en : "Columns",
+		es : "Columnas"
 	} ],
 	10 : [ {
 		pt : "",
