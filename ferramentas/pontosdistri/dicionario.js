44f1b19c8ed46d17c51762fb981a2069d73fa90e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-11-29 00:48:47 -0200

    Tradução de ferramentas/pontosdistri/dicionario.js

--- ferramentas/pontosdistri/dicionario.js
+++ ferramentas/pontosdistri/dicionario.js
@@ -2,87 +2,87 @@
 i3GEOF.pontosDistri.dicionario = {
 	'calculo' : [ {
 		pt : "C&aacute;lculo",
-		en : "",
-		es : ""
+		en : "Calculation",
+		es : "C&aacute;lculo"
 	} ],
 	'propriedades' : [ {
 		pt : "Propriedades",
-		en : "",
-		es : ""
+		en : "Properties",
+		es : "Propiedades"
 	} ],
 	'selecionaTema' : [ {
 		pt : "Escolha o tema que ser&aacute; utilizado (no caso de linhas ou pol&iacute;gonos, ser&aacute; calculado o centr&oacute;ide):",
-		en : "",
-		es : ""
+		en : "Select a theme that will be used (in the case of lines or polygons the centroid will be computed):",
+		es : "Seleccione el tema que se utilizar&aacute; (en el caso de l&iacute;neas o pol&iacute;gonos se calcular&aacute; el centroide):"
 	} ],
 	'restringeAnalise' : [ {
 		pt : "Restringe a an&aacute;lise aos limites de abrang&ecirc;ncia dos pontos?",
-		en : "",
-		es : ""
+		en : "Do you want to bound the analysis to the spatial extent of the points?",
+		es : "&iquest;Se restringe el an&aacute;lisis a la extensi&oacute;n espacial de los puntos?"
 	} ],
 	'extendeAbrangencia' : [ {
 		pt : "Extende os limites de abrang&ecirc;ncia em ",
-		en : "",
-		es : ""
+		en : "Enlarges the extent by ",
+		es : "Ampliar la extensi&oacute;n en "
 	} ],
 	'densidadePontos' : [ {
 		pt : "Densidade de pontos",
-		en : "",
-		es : ""
+		en : "Point density",
+		es : "Densidad de puntos"
 	} ],
 	'distanciaPontos' : [ {
 		pt : "Dist&acirc;ncia entre pontos",
-		en : "",
-		es : ""
+		en : "Distance between points",
+		es : "Distancia entre puntos"
 	} ],
 	'desvioPadrao' : [ {
 		pt : "sigma (para kernel)",
-		en : "",
-		es : ""
+		en : "sigma (for kernel)",
+		es : "sigma (para kernel)"
 	} ],
 	'relatorio' : [ {
 		pt : "Relat&oacute;rio",
-		en : "",
-		es : ""
+		en : "Summary",
+		es : "Reporte"
 	} ],
 	'opcoesCoresClasses' : [ {
 		pt : "Op&ccedil;&otilde;es de cores e classes do tema resultante:",
-		en : "",
-		es : ""
+		en : "Classes and color options for the resulting theme:",
+		es : "Opciones de colores y clases del tema resultante:"
 	} ],
 	'numeroClasses' : [ {
 		pt : "N&uacute;mero de classes:",
-		en : "",
-		es : ""
+		en : "Number of classes:",
+		es : "N&uacute;mero de clases:"
 	} ],
 	'corInicial' : [ {
 		pt : "Cor inicial:",
-		en : "",
-		es : ""
+		en : "Initial color:",
+		es : "Color inicial:"
 	} ],
 	'corFinal' : [ {
 		pt : "Cor final:",
-		en : "",
-		es : ""
+		en : "Final color:",
+		es : "Color final:"
 	} ],
 	'verCor' : [ {
 		pt : "Ver cores",
-		en : "",
-		es : ""
+		en : "View colors",
+		es : "Ver colores"
 	} ],
 	'nenhumTemaSelecionado' : [ {
 		pt : "Nenhum tema de pontos com sele&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "No point-based theme with selection",
+		es : "Ning&uacute;n tema de puntos con selecci&oacute;n"
 	} ],
 	'seleciona' : [ {
 		pt : "Selecionar...",
-		en : "",
-		es : ""
+		en : "Select...",
+		es : "Seleccionar..."
 	} ],
 	'selecionaUmTema' : [ {
 		pt : "Escolha um tema",
-		en : "",
-		es : ""
+		en : "Select a theme",
+		es : "Seleccionar un tema"
 	} ]
 };
