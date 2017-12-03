371eec7816a7a59ab386708891497f7a6899f53e
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-03 01:33:29 -0200

    Correção do dicionário

--- ferramentas/animacao/dicionario.js
+++ ferramentas/animacao/dicionario.js
@@ -23,6 +23,6 @@ i3GEOF.animacao.dicionario = {
 	'ajuda' : [ {
 		pt : "Marque as camadas que ser&atilde;o inclu&iacute;das na anima&ccedil;&atilde;o e depois clique no bot&atilde;o <i>inicia</i>",
 		en : "Check the layers that will be included in the animation and then click the <i>start</i> button",
-		es : "Marque las capas que se incluir&aacute;n en la animaci&oacute;n y, a continuaci&oacute;n, haga clic en el bot&oacute;n de <i>inicio</i>"
+		es : "Marque las capas que se incluir&aacute;n en la animaci&oacute;n y luego haga clic en el bot&oacute;n de <i>inicio</i>"
 	} ]
 };
