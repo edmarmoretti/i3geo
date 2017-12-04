32e15d7f1c15b5eb969b474c0631931906bb0e85
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-04 11:18:37 -0200

    Tradução de ferramentas/salvamapa/dicionario.js

--- ferramentas/salvamapa/dicionario.js
+++ ferramentas/salvamapa/dicionario.js
@@ -2,52 +2,52 @@
 i3GEOF.salvaMapa.dicionario = {
 	'salvaMapa' : [ {
 		pt : "Salvar o mapa",
-		en : "",
-		es : ""
+		en : "Save the map",
+		es : "Guardar en el mapa"
 	} ],
 	'listaMapas' : [ {
 		pt : "Ver a lista de mapas",
-		en : "",
-		es : ""
+		en : "See the list of maps",
+		es : "Ver la lista de mapas"
 	} ],
 	'baixaArquivo' : [ {
 		pt : "Baixar o arquivo",
-		en : "",
-		es : ""
+		en : "Download file",
+		es : "Descargar el archivo"
 	} ],
 	'msgMapaSalvo' : [ {
 		pt : "Mapa salvo",
-		en : "",
-		es : ""
+		en : "Map saved",
+		es : "Mapa guardado"
 	} ],
 	'ajuda' : [ {
 		pt : "Salvando o mapa atual, voc&ecirc; poder&aacute; carreg&aacute;-lo novamente. Gr&aacute;ficos e desenhos tamb&eacute;m s&atilde;o salvos.",
-		en : "",
-		es : ""
+		en : "When saving the current map, it can be uploaded again. Graphics and drawings can also be saved.",
+		es : "Si usted guarda el mapa actual, usted puede volver a cargarlo. Gr&aacute;ficos y dibujos tambi&eacute;n se pueden guardar."
 	} ],
 	'ajuda3' : [ {
-		pt : "Armazene o arquivo de configura&ccedil;&atilde;o do mapa em seu computador,",
-		en : "",
-		es : ""
+		pt : "Armazene o arquivo de configura&ccedil;&atilde;o do mapa em seu computador.",
+		en : "Store the map configuration file on your computer",
+		es : "Almacene el archivo de configuraci&oacute;n del mapa en su equipo"
 	} ],
 	'ajuda4' : [ {
 		pt : "fazendo o download. Isso permitir&aacute; que voc&ecirc; fa&ccedil;a o upload desse mesmo arquivo, restaurando o mapa.",
-		en : "",
-		es : ""
+		en : "downloading it. This will allow you to upload the same file and restore the map.",
+		es : "haciendo la descarga. Esto le permitir&aacute; cargar el mismo archivo y restaurar el mapa."
 	} ],
 	'salvaArquivo' : [ {
 		pt : "Clique no link abaixo com o bot&atilde;o direito do mouse e salve o arquivo em seu computador. Para carregar o mapa salvo utilize a op&ccedil;&atilde;o de carregar mapa.",
-		en : "",
-		es : ""
+		en : "Right click the link below and save the file to your computer. To load a saved map, use the upload map option.",
+		es : "Haga clic en el siguiente enlace con el bot&oacute;n derecho del rat&oacute;n y guarde el archivo en su ordenador. Para cargar el mapa guardado utilice la opci&oacute;n cargar mapa."
 	} ],
 	'salvaPreferencias' : [ {
 		pt : "As prefer&ecirc;ncias definidas atualmente ser&atilde;o salvas tamb&eacute;m.",
-		en : "",
-		es : ""
+		en : "All preferences you have set will be saved as well.",
+		es : "Las preferencias definidas actualmente se guardar&aacute;n tambi&eacute;n."
 	} ],
 	'veArquivo' : [ {
 		pt : "Clique aqui para ver o arquivo",
-		en : "",
-		es : ""
+		en : "Click here to see the file",
+		es : "Haga clic aqu&iacute; para ver el archivo"
 	} ]
 };
