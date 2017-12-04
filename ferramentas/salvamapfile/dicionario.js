1315b6a74b87ceeba1ac5e086f2f115570936020
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-04 11:24:32 -0200

    Tradução de ferramentas/salvamapfile/dicionario.js

--- ferramentas/salvamapfile/dicionario.js
+++ ferramentas/salvamapfile/dicionario.js
@@ -2,32 +2,32 @@
 i3GEOF.salvamapfile.dicionario = {
 	'naoExisteMapfile' : [ {
 		pt : "N&atilde;o existe um arquivo mapfile cadastrado para essa camada. Utilize o sistema de administra&ccedil;&atilde;o para criar um",
-		en : "",
-		es : ""
+		en : "There is no mapfile file registered for this layer. Use the management system to create a mapfile",
+		es : "No existe un archivo mapfile registrado para esta capa. Utilice el sistema de administraci&oacute;n para crear un mapfile"
 	} ],
 	'ajuda' : [ {
 		pt : "Apenas usu&aacute;rios cadastrados como editores podem salvar um mapfile. O tema ativo possui um mapfile j&aacute; criado. Ao salvar, as defini&ccedil;&otilde;es originais  de legenda, filtros e outras, ser&atilde;o substitu&iacute;das pelas atuais",
-		en : "",
-		es : ""
+		en : "Only users registered as editors can save a mapfile. The active theme has a mapfile already created. Original definitions of legend, filters and others, will be replaced by the current ones when you save these definitions",
+		es : "Solo los usuarios registrados como editores pueden guardar un mapfile. El tema activo tiene un mapfile creado. Las definiciones originales de leyenda, filtros y otras; ser&aacute;n reemplazadas por las actuales cuando se guarden dichas definiciones"
 	} ],
 	'concluido' : [ {
 		pt : "Conclu&iacute;do!",
-		en : "",
-		es : ""
+		en : "Completed!",
+		es : "&iexcl;Concluido!"
 	} ],
 	'salva' : [ {
 		pt : "Salvar",
-		en : "",
-		es : ""
+		en : "Save",
+		es : "Guardar"
 	} ],
 	'testa' : [ {
 		pt : "Testar",
-		en : "",
-		es : ""
+		en : "Test",
+		es : "Probar"
 	} ],
 	'naosalva' : [ {
 		pt : "N&atilde;o &eacute; poss&iacute;vel salvar esse tipo de camada",
-		en : "",
-		es : ""
+		en : "Unable to save this type of layer",
+		es : "No se puede guardar este tipo de capa"
 	} ]
 };
