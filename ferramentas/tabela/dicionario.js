3cad636922415d6f6d9bf4916ef44afeacbf3d78
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-05 19:07:51 -0200

    Tradução de ferramentas/tabela/dicionario.js

--- ferramentas/tabela/dicionario.js
+++ ferramentas/tabela/dicionario.js
@@ -2,252 +2,252 @@
 i3GEOF.tabela.dicionario = {
 	'tabela' : [ {
 		pt : "Tabela",
-		en : "",
-		es : ""
+		en : "Table",
+		es : "Tabla"
 	} ],
 	'erroTemaOrigem' : [ {
 		pt : "erro ao ler os itens do tema de origem",
-		en : "",
-		es : ""
+		en : "error reading fields from source theme",
+		es : "error al leer los campos del tema de origen"
 	} ],
 	"registros" : [ {
 		pt : "Registros",
-		en : "",
-		es : ""
+		en : "Records",
+		es : "Registros"
 	} ],
 	"estatisticas" : [ {
 		pt : "Estat&iacute;sticas",
-		en : "",
-		es : ""
+		en : "Statistics",
+		es : "Estadísticas"
 	} ],
 	"relat" : [ {
 		pt : "Relat&oacute;rios",
-		en : "",
-		es : ""
+		en : "Reports",
+		es : "Informes"
 	} ],
 	6 : [ {
 		pt : "Considerar na listagem a regi&atilde;o mostrada no mapa (se essa op&ccedil;&atilde;o for desmarcada, ser&aacute; considerada a extens&atilde;o geogr&aacute;fica do mapa inicial)",
-		en : "",
-		es : ""
+		en : "The area shown on the map will be included in the list (the geographic extent of the initial map will be used if this option is disabled)",
+		es : "Considerar en la lista el &aacute;rea que se muestra en el mapa (si esta opci&oacute;n es desactivada se considerar&aacute; la extensi&oacute;n geogr&aacute;fica del mapa inicial)"
 	} ],
 	7 : [ {
 		pt : "Atualiza a tabela ap&oacute;s navegar no mapa",
-		en : "",
-		es : ""
+		en : "Refreshes the table after browsing the map",
+		es : "Actualiza la tabla despu&eacute;s de navegar en el mapa"
 	} ],
 	"atuatab" : [ {
 		pt : "Mostrar na listagem apenas os selecionados",
-		en : "",
-		es : ""
+		en : "Show selected only",
+		es : "Mostrar solo los seleccionados"
 	} ],
 	"colleg" : [ {
 		pt : "Mostrar coluna com a legenda da classe",
-		en : "",
-		es : ""
+		en : "Show column with class legend",
+		es : "Mostrar columna con la leyenda de la clase"
 	} ],
 	"sellinhas" : [ {
 		pt : "Seleciona no mapa as linhas marcadas",
-		en : "",
-		es : ""
+		en : "Selects marked lines on the map",
+		es : "Selecciona en el mapa las l&iacute;neas marcadas"
 	} ],
 	"atvsel" : [ {
 		pt : "Selecionar",
-		en : "",
-		es : ""
+		en : "Select",
+		es : "Seleccionar"
 	} ],
 	"limpselmap" : [ {
 		pt : "Limpa a sele&ccedil;&atilde;o do mapa",
-		en : "",
-		es : ""
+		en : "Clear selection from map",
+		es : "Limpia la selecci&oacute;n del mapa"
 	} ],
 	"limpsel" : [ {
 		pt : "Limpar",
-		en : "",
-		es : ""
+		en : "Clear",
+		es : "Limpiar"
 	} ],
 	"criatemasel" : [ {
 		pt : "Cria um novo tema no mapa contendo a sele&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Creates a new theme on the map containing the selection",
+		es : "Crea un tema nuevo en el mapa que contiene la selecci&oacute;n"
 	} ],
 	"criatema" : [ {
 		pt : "Criar tema",
-		en : "",
-		es : ""
+		en : "Create theme",
+		es : "Crear tema"
 	} ],
 	"de" : [ {
 		pt : "de",
-		en : "",
-		es : ""
+		en : "from",
+		es : "de"
 	} ],
 	"ate" : [ {
 		pt : "at&eacute;",
-		en : "",
-		es : ""
+		en : "until",
+		es : "hasta"
 	} ],
 	"atuareg" : [ {
 		pt : "Atualiza a lista de registros",
-		en : "",
-		es : ""
+		en : "Updates record list",
+		es : "Actualiza la lista de registros"
 	} ],
 	"listar" : [ {
 		pt : "Listar",
-		en : "",
-		es : ""
+		en : "List",
+		es : "Crear lista"
 	} ],
 	"itemcalc" : [ {
 		pt : "Item para aplicar os c&aacute;lculos",
-		en : "",
-		es : ""
+		en : "Field to apply calculations",
+		es : "Campo para aplicar los c&aacute;lculos"
 	} ],
 	"excluicalc" : [ {
 		pt : "Excluir dos c&aacute;lculos o valor",
-		en : "",
-		es : ""
+		en : "Exclude from calculations the value",
+		es : "Excluir de los c&aacute;lculos el valor"
 	} ],
 	"calc" : [ {
 		pt : "Calcular",
-		en : "",
-		es : ""
+		en : "Calculate",
+		es : "Calcular"
 	} ],
 	"paraqueas" : [ {
 		pt : "Para que as opera&ccedil;&otilde;es atuem apenas sobre os elementos selecionados ative a sele&ccedil;&atilde;o no mapa, para isso utilize, na guia <i>Registros</i> a op&ccedil;&atilde;o <i>Ativa a sele&ccedil;&atilde;o</i>",
-		en : "",
-		es : ""
+		en : "In order that operations execute only on selected features, enable selection on the map. To do that use <i>Enable selection</i> option on the <i>Record</i> tab",
+		es : "Para que las operaciones act&uacute;en solo sobre las entidades seleccionadas, active la selecci&oacute;n en el mapa, para ello utilice la opci&oacute;n <i>Activa la selecci&oacute;n</i> en la pesta&ntilde;a <i>Registros</i>"
 	} ],
 	"itensrel" : [ {
 		pt : "Escolha os itens que dever&atilde;o constar no relat&oacute;rio. Voc&ecirc; pode alterar os nomes originais das colunas para melhorar o cabe&ccedil;alho do relat&oacute;rio e tamb&eacute;m definir a ordem das colunas no relat&oacute;rio",
-		en : "",
-		es : ""
+		en : "Select fields that should appear in the report. You can change column order and names to improve the report header",
+		es : "Seleccione los campos que deben estar presente en el informe. Usted puede cambiar los nombres originales de las columnas para mejorar el encabezado del informe y cambiar tambi&eacute;n el orden de las columnas en el informe"
 	} ],
 	"itensagr" : [ {
 		pt : "Escolha o item que voc&ecirc; quer utilizar para agrupar valores (opcional)",
-		en : "",
-		es : ""
+		en : "Select a field you want to use to group values (optional)",
+		es : "Seleccione el campo que desea utilizar para agrupar valores (opcional)"
 	} ],
 	"incarea" : [ {
 		pt : "Incluir c&aacute;lculo de &aacute;rea",
-		en : "",
-		es : ""
+		en : "Include area calculation",
+		es : "Incluir c&aacute;lculo de &aacute;rea"
 	} ],
 	"incestat" : [ {
 		pt : "Incluir c&aacute;lculo de estat&iacute;sticas",
-		en : "",
-		es : ""
+		en : "Include statistics calculation",
+		es : "Incluir c&aacute;lculo de estad&iacute;sticas"
 	} ],
 	"excvalor" : [ {
 		pt : "Excluir das estat&iacute;sticas o valor",
-		en : "",
-		es : ""
+		en : "Delete value from statistics",
+		es : "Excluir de las estad&iacute;sticas el valor"
 	} ],
 	"relattext" : [ {
 		pt : "Relat&oacute;rio no formato texto",
-		en : "",
-		es : ""
+		en : "Report in text format",
+		es : "Informe en formato de texto"
 	} ],
 	'temaNaoExisteMais' : [ {
-		pt : "O tema ja nao existe mais no mapa",
-		en : "",
-		es : ""
+		pt : "O tema j&aacute; n&atilde;o existe mais no mapa",
+		en : "El tema ya no está disponible en el mapa",
+		es : "El tema ya no existe en el mapa"
 	} ],
 	'ordena' : [ {
 		pt : "ordena",
-		en : "",
-		es : ""
+		en : "sort",
+		es : "ordenar"
 	} ],
 	'erroTemaOrigem2' : [ {
 		pt : "erro ao ler os itens do tema de origem",
-		en : "",
-		es : ""
+		en : "error reading fields from source theme",
+		es : "error al leer los campos del tema de origen"
 	} ],
 	33 : [ {
 		pt : "A representa&ccedil;&atilde;o gr&aacute;fica dos dados tabulares utiliza todos os elementos da tabela ou os selecionados, se estiverem ativos no mapa",
-		en : "",
-		es : ""
+		en : "Graphical representation of tabular data uses all or selected fields if they are active on the map",
+		es : "La representaci&oacute;n gr&aacute;fica de los datos en tablas utiliza todos los campos de la tabla o los seleccionados si est&aacute;n activos en el mapa"
 	} ],
 	34 : [ {
 		pt : "Os gr&aacute;ficos s&atilde;o gerados com o software R,</a> mas vc pode optar por usar a ferramenta de gr&aacute;ficos interativos que possuem outras op&ccedil;&otilde;es e geram gr&aacute;ficos em Flash.",
-		en : "",
-		es : ""
+		en : "Charts are generated with R software</a>, but you can use the interactive chart tool that has other options and allows you to create Flash charts.",
+		es : "Los gr&aacute;ficos se generan con el software R</a>, pero usted puede optar por usar la herramienta de gr&aacute;ficos interactivos que tienen otras opciones que crean gr&aacute;ficos en Flash."
 	} ],
 	35 : [ {
 		pt : "Se voc&ecirc; optar por usar o R, nas telas seguintes defina o tipo e as op&ccedil;&otilde;es do gr&aacute;fico",
-		en : "",
-		es : ""
+		en : "If you choose to use R, in the following screens define a chart type and options",
+		es : "Si opta por utilizar R, en las siguientes pantallas defina el tipo y las opciones del gr&aacute;fico"
 	} ],
 	36 : [ {
 		pt : "abrir em uma janela separada",
-		en : "",
-		es : ""
+		en : "open in a detached window",
+		es : "abrir en una ventana separada"
 	} ],
 	37 : [ {
 		pt : "Marque uma coluna para vincular com outras tabelas. Marque uma linha para localizar em todas as tabelas abertas",
-		en : "",
-		es : ""
+		en : "Check a column to link to other tables. Check one line to search in all open tables",
+		es : "Marque una columna para enlazar con otras tablas. Marque una l&iacute;nea para buscar en todas las tablas abiertas"
 	} ],
 	'msgDadosRegiaoAtual' : [ {
 		pt : "Os dados utilizados referem-se apenas &agrave; regi&atilde;o atual mostrada no mapa",
-		en : "",
-		es : ""
+		en : "Data in use is only related to the current region shown on the map",
+		es : "Los datos utilizados se relacionan solo con la regi&oacute;n actual mostrada en el mapa"
 	} ],
 	39 : [ {
 		pt : "Marque uma coluna primeiro",
-		en : "",
-		es : ""
+		en : "Check first a column",
+		es : "Marque una columna primero"
 	} ],
 	"vertodos" : [ {
 		pt : "Ver todos",
-		en : "",
-		es : ""
+		en : "See all",
+		es : "Ver todos"
 	} ],
 	'atualizaAoNavegar' : [ {
 		pt : "Atualiza a listagem ao navegar",
-		en : "",
-		es : ""
+		en : "Refresh listing when browsing",
+		es : "Actualiza la lista al navegar"
 	} ],
 	"grafico" : [ {
 		pt : "Gr&aacute;fico",
-		en : "",
-		es : ""
+		en : "Chart",
+		es : "Gr&aacute;fico"
 	} ],
 	"vinctab" : [ {
 		pt : "V&iacute;nculos entre as tabelas abertas",
-		en : "",
-		es : ""
+		en : "Links between open tables",
+		es : "V&iacute;nculos entre las tablas abiertas"
 	} ],
 	"vinculos" : [ {
 		pt : "V&iacute;nculos",
-		en : "",
-		es : ""
+		en : "Links",
+		es : "V&iacute;nculos"
 	} ],
 	"selecionaTabela" : [ {
 		pt : "Escolha uma tabela",
-		en : "",
-		es : ""
+		en : "Select a table",
+		es : "Seleccione una tabla"
 	} ],
 	"selecionaSegundaTabela" : [ {
 		pt : "Escolha uma segunda tabela",
-		en : "",
-		es : ""
+		en : "Select a second table",
+		es : "Seleccione una segunda tabla"
 	} ],
 	"criaVinculo" : [ {
 		pt : "Criar v&iacute;nculo",
-		en : "",
-		es : ""
+		en : "Create link",
+		es : "Crear v&iacute;nculo"
 	} ],
 	'colunaLigacao' : [ {
 		pt : "Coluna de liga&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Connection column",
+		es : "Columna de conexi&oacute;n"
 	} ],
 	'idDaTabela' : [ {
 		pt : "Identificador da tabela",
-		en : "",
-		es : ""
+		en : "Table identifier",
+		es : "Identificador de tabla"
 	} ],
 	'selUmReg' : [ {
 		pt : "Selecione pelo menos um registro",
-		en : "",
-		es : ""
+		en : "Select at least one record",
+		es : "Seleccione al menos un registro"
 	} ]
 };
