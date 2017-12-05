dc773bf4a30964dd844cd3f1e8152ba7f11b9127
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-05 19:44:04 -0200

    Tradução de ferramentas/tipoimagem/dicionario.js

--- ferramentas/tipoimagem/dicionario.js
+++ ferramentas/tipoimagem/dicionario.js
@@ -2,43 +2,43 @@
 i3GEOF.tipoimagem.dicionario = {
 	'selecionaFiltro' : [ {
 		pt : "Escolha um ou mais filtros de cores. Vc pode editar manualmente a lista de filtros.",
-		en : "",
-		es : ""
+		en : "Select one or more color filters. You can edit the filter list manually.",
+		es : "Seleccione uno o m&aacute;s filtros de color. Puede editar manualmente la lista de filtros."
 	} ],
 	'aplica' : [ {
 		pt : "Aplicar",
-		en : "",
-		es : ""
+		en : "Apply",
+		es : "Aplicar"
 	} ],
 	'nenhum' : [ {
 		pt : "nenhum",
-		en : "",
-		es : ""
+		en : "none",
+		es : "ninguno"
 	} ],
 	'tonsCinza' : [ {
 		pt : "tons de cinza",
-		en : "",
-		es : ""
+		en : "grayscale",
+		es : "tonos de grises"
 	} ],
 	'sepiaClara' : [ {
 		pt : "s&eacute;pia clara (funciona apenas com o tipo de imagem png1)",
-		en : "",
-		es : ""
+		en : "bright sepia (it only works with png1 image type)",
+		es : "sepia clara (solo funciona con el tipo de imagen png1)"
 	} ],
 	'sepiaNormal' : [ {
 		pt : "s&eacute;pia normal (funciona apenas com o tipo de imagem png1)",
-		en : "",
-		es : ""
+		en : "regular sepia (it only works with png1 image type)",
+		es : "sepia normal (solo funciona con el tipo de imagen png1)"
 	} ],
 	'negativo' : [ {
 		pt : "negativo",
-		en : "",
-		es : ""
+		en : "negative",
+		es : "negativo"
 	} ],
 	'detectaBordas' : [ {
 		pt : "detecta bordas",
-		en : "",
-		es : ""
+		en : "detects edges",
+		es : "detecta bordes"
 	} ],
 	9 : [ {
 		pt : "",
