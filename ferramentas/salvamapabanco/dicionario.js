6b9cbb92781843c981de5d05756061f2b079e34c
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-04 23:44:22 -0200

    Tradução de ferramentas/salvamapabanco/dicionario.js

--- ferramentas/salvamapabanco/dicionario.js
+++ ferramentas/salvamapabanco/dicionario.js
@@ -2,37 +2,37 @@
 i3GEOF.salvaMapaBanco.dicionario = {
 	'salvaMapa' : [ {
 		pt : "Salvar o mapa",
-		en : "",
-		es : ""
+		en : "Save map",
+		es : "Guardar en el mapa"
 	} ],
 	'msgMapaSalvo' : [ {
 		pt : "Mapa salvo",
-		en : "",
-		es : ""
+		en : "Saved map",
+		es : "Mapa guardado"
 	} ],
 	'atualizaIdMapa' : [ {
 		pt : "ID do mapa que ser&aacute; atualizado (opcional).<br>Se for um mapa novo, deixe em branco",
-		en : "",
-		es : ""
+		en : "ID of the map that will be updated (optional).<br>If it is a new map, leave blank ",
+		es : "ID del mapa que ser&aacute; actualizado (opcional).<br>Si es un mapa nuevo, deje en blanco "
 	} ],
 	'tituloMapa' : [ {
 		pt : "T&iacute;tulo do mapa",
-		en : "",
-		es : ""
+		en : "Map Title",
+		es : "T&iacute;tulo del mapa"
 	} ],
 	'ajuda' : [ {
 		pt : "Mantenha essa janela aberta para poder salvar o mapa. Utilize as op&ccedil;&otilde;es do i3Geo para montar seu mapa.",
-		en : "",
-		es : ""
+		en : "Keep this window open so that you can save the map. Use i3Geo options to build your map.",
+		es : "Mantenga esta ventana abierta para poder guardar el mapa. Utilice las opciones de i3Geo para crear el mapa."
 	} ],
 	'salvaPreferencias' : [ {
 		pt : "As prefer&ecirc;ncias definidas atualmente ser&atilde;o salvas tamb&eacute;m.",
-		en : "",
-		es : ""
+		en : "Currently set preferences will be saved as well.",
+		es : "Las preferencias definidas actualmente se guardar&aacute;n tambi&eacute;n."
 	} ],
 	'remove' : [ {
 		pt : "Remover",
-		en : "",
-		es : ""
+		en : "Remove",
+		es : "Eliminar"
 	} ]
 };
