e79919ae231e32d3db13d3cd03f9e6a52a9e06f6
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-07 00:54:18 -0200

    Tradução de ferramentas/upload/dicionario.js

--- ferramentas/upload/dicionario.js
+++ ferramentas/upload/dicionario.js
@@ -2,52 +2,52 @@
 i3GEOF.upload.dicionario = {
 	'pastaArmazenamento' : [ {
 		pt : "Pasta no servidor onde os dados ser&atilde;o armazenados. Apenas pastas localizadas no mesmo local onde o i3Geo est&aacute; instalado s&atilde;o permitidas.  Exemplo, no caso da instala&ccedil;&atilde;o ser /var/www e existir a pasta /var/www/data, digite apenas data",
-		en : "",
-		es : ""
+		en : "Folder on the server where data will be stored. Only folders located in the same location where i3Geo is installed are allowed. For example, if the installation is at /var /www/ and there is a folder /var/www/data/, enter only data",
+		es : "Carpeta en el servidor donde se almacenan los datos. Solo est&aacute; permitido en las carpetas ubicadas en el mismo lugar donde est&aacute; instalado i3Geo. Por ejemplo, si la instalaci&oacute;n es /var/www/ y existe la carpeta /var/www/data/, ingrese solo data"
 	} ],
 	'tipoGeom' : [ {
 		pt : "Tipo de geometria",
-		en : "",
-		es : ""
+		en : "Geometry type",
+		es : "Tipo de geometr&iacute;a"
 	} ],
 	'naoConhecido' : [ {
 		pt : "n&atilde;o conhecido",
-		en : "",
-		es : ""
+		en : "unknown",
+		es : "desconocido"
 	} ],
 	'pontual' : [ {
 		pt : "pontual",
-		en : "",
-		es : ""
+		en : "point",
+		es : "de puntos"
 	} ],
 	'poligonal' : [ {
 		pt : "poligonal",
-		en : "",
-		es : ""
+		en : "polygon",
+		es : "de pol&iacute;gono"
 	} ],
 	'linear' : [ {
 		pt : "linear",
-		en : "",
-		es : ""
+		en : "line",
+		es : "de l&iacute;nea"
 	} ],
 	'projecao' : [ {
 		pt : "Proje&ccedil;&atilde;o (apenas ser&aacute; utilizado se n&atilde;o for feito o upload de um arquivo .prj)",
-		en : "",
-		es : ""
+		en : "Projection (it will only be used if a .prj file is not uploaded)",
+		es : "Proyecci&oacute;n (solo se utilizar&aacute; si no se carga el archivo. prj)"
 	} ],
 	'envia' : [ {
 		pt : "Enviar",
-		en : "",
-		es : ""
+		en : "Send",
+		es : "Enviar"
 	} ],
 	'ajudaCaracter' : [ {
 		pt : "N&atilde;o utilize _ no nome do arquivo. Apenas letras e n&uacute;meros s&atilde;o aceitos!!!",
-		en : "",
-		es : ""
+		en : "Do not use _ in  file names. Only letters and numbers are allowed!!!",
+		es : "No utilice _ en el nombre de archivo. &iexcl;Solo letras y n&uacute;meros son aceptados!!!"
 	} ],
 	'criaMapfile' : [ {
 		pt : "Marque para criar o arquivo de configura&ccedil;&atilde;o (mapfile) e visualizar os dados no mapa interativo (voc&ecirc; poder&aacute; editar esse arquivo posteriormente no editor de mapfiles)",
-		en : "",
-		es : ""
+		en : "Check to create the configuration file (mapfile) and visualize the data on an interactive map (you can edit this file later using the mapfile editor)",
+		es : "Marque para crear el archivo de configuraci&oacute;n (mapfile) y visualizar los datos en el mapa interactivo (usted puede editar este archivo m&aacute;s adelante en el editor de mapfiles)"
 	} ]
 };
