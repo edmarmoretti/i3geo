fe6e3f12bc1e2d0364a0027a7837387d47ce44ea
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-08 00:20:34 -0200

    Tradução de ferramentas/uploadkml/dicionario.js

--- ferramentas/uploadkml/dicionario.js
+++ ferramentas/uploadkml/dicionario.js
@@ -2,32 +2,32 @@
 i3GEOF.uploadkml.dicionario = {
 	'ajuda' : [ {
 		pt : "N&atilde;o use arquivos ou nome de layer com espa&ccedil;os em branco ou acentua&ccedil;&atilde;o",
-		en : "",
-		es : ""
+		en : "Do not use files or layer names with spaces or accents",
+		es : "No utilice archivos o nombres de capas con espacios en blanco o acentuaci&oacute;n"
 	} ],
 	'arquivoKml' : [ {
 		pt : "Arquivo kml:",
-		en : "",
-		es : ""
+		en : "kml file:",
+		es : "Archivo kml:"
 	} ],
 	'nomeLayerKml' : [ {
 		pt : "Nome do layer KML:",
-		en : "",
-		es : ""
+		en : "KML layer name:",
+		es : "Nombre de la capa KML:"
 	} ],
 	'projecao' : [ {
 		pt : "Proje&ccedil;&atilde;o:",
-		en : "",
-		es : ""
+		en : "Projection:",
+		es : "Proyecci&oacute;n:"
 	} ],
 	'criaCamada' : [ {
 		pt : "Criar camada",
-		en : "",
-		es : ""
+		en : "Create layer",
+		es : "Crear capa"
 	} ],
 	'ajuda2' : [ {
 		pt : "O nome do layer corresponde ao elemento folder->name existente no arquivo KML. Para descobrir esse valor, abra o arquivo KML em um editor de textos comum.",
-		en : "",
-		es : ""
+		en : "The name of layer corresponds to the folder->name element in a KML file. Open the KML file with a plain text editor to find out this value.",
+		es : "El nombre del layer corresponde al elemento folder->name existente en el archivo KML. Para ver este valor abra el archivo KML con un editor de texto plano."
 	} ]
 };
