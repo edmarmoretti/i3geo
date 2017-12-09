0d39821ba53a75c5dd1d4eed0c5de38542fb0108
Abraham Coiman <abrahamcoiman@gmail.com>
2017-12-09 01:10:13 -0200

    Traduçao de ferramentas/wmstime/dicionario.js

--- ferramentas/wmstime/dicionario.js
+++ ferramentas/wmstime/dicionario.js
@@ -2,87 +2,87 @@
 i3GEOF.wmstime.dicionario = {
 	1 : [ {
 		pt : "Escolha o servi&ccedil;o de fornecimento de dados (WMS-T)",
-		en : "",
-		es : ""
+		en : "Select a data delivery service (WMS-T)",
+		es : "Seleccione el servicio de suministro de datos (WMS-T)"
 	} ],
 	2 : [ {
 		pt : "Ano inicial:",
-		en : "",
-		es : ""
+		en : "Starting year:",
+		es : "A&ntilde;o inicial:"
 	} ],
 	3 : [ {
 		pt : "Ano final:",
-		en : "",
-		es : ""
+		en : "Final year:",
+		es : "A&ntilde;o final:"
 	} ],
 	4 : [ {
 		pt : "M&ecirc;s inicial:",
-		en : "",
-		es : ""
+		en : "Starting month:",
+		es : "Mes inicial:"
 	} ],
 	5 : [ {
 		pt : "M&ecirc;s final:",
-		en : "",
-		es : ""
+		en : "Final month:",
+		es : "Mes final:"
 	} ],
 	6 : [ {
 		pt : "Dia inicial:",
-		en : "",
-		es : ""
+		en : "Starting day:",
+		es : "D&iacute;a inicial:"
 	} ],
 	7 : [ {
 		pt : "Dia final:",
-		en : "",
-		es : ""
+		en : "Final day:",
+		es : "D&iacute;a final:"
 	} ],
 	8 : [ {
 		pt : "Obter uma imagem por:",
-		en : "",
-		es : ""
+		en : "Get an image through:",
+		es : "Obtener una imagen por:"
 	} ],
 	9 : [ {
 		pt : "Continuar",
-		en : "",
-		es : ""
+		en : "Continue",
+		es : "Continuar"
 	} ],
 	10 : [ {
 		pt : "Ano",
-		en : "",
-		es : ""
+		en : "Year",
+		es : "A&ntilde;o"
 	} ],
 	11 : [ {
 		pt : "M&ecirc;s",
-		en : "",
-		es : ""
+		en : "Month",
+		es : "Mes"
 	} ],
 	12 : [ {
 		pt : "Dia",
-		en : "",
-		es : ""
+		en : "Day",
+		es : "D&iacute;a"
 	} ],
 	13 : [ {
 		pt : "clique para adicionar ao mapa",
-		en : "",
-		es : ""
+		en : "click to add to the map",
+		es : "haga clic para a&ntilde;adir al mapa"
 	} ],
 	14 : [ {
 		pt : "exclu&iacute;do",
-		en : "",
-		es : ""
+		en : "deleted",
+		es : "excluido"
 	} ],
 	15 : [ {
 		pt : "Imagem exclu&iacute;da",
-		en : "",
-		es : ""
+		en : "Deleted image",
+		es : "Imagen excluida"
 	} ],
 	16 : [ {
 		pt : "clique para adicionar ao mapa",
-		en : "",
-		es : ""
+		en : "click to add to the map",
+		es : "haga clic para a&ntilde;adir al mapa"
 	} ],
     17 : [ {
         pt : "Imagens",
-        en : "",
-        es : ""
+        en : "Images",
+        es : "Im&aacute;genes"
     } ]
 };
