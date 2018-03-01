3129df14b672bde5aaf8ea4aff504b80f5d668bb
Edmar Moretti <edmar.moretti@gmail.com>
2018-03-01 18:34:51 -0300

    i3geo/i3geo#88 Adicionar tooltip nos botões de edição (atalhos edição) https://trello.com/c/o9eqehw3

--- ferramentas/atalhosedicao/dicionario.js
+++ ferramentas/atalhosedicao/dicionario.js
@@ -10,31 +10,61 @@ i3GEOF.atalhosedicao.dicionario = {
 		en : "Legend",
 		es : "Leyenda"
 	} ],
+    'legendaTip' : [ {
+        pt : "Altera a simbologia e as classes",
+        en : "",
+        es : ""
+    } ],
 	'salva' : [ {
 		pt : "Salva mapfile",
 		en : "Save mapfile",
 		es : "Guardar mapfile"
 	} ],
+    'salvaTip' : [ {
+        pt : "Salva permanentemente as edi&ccedil;&otilde;es feitas",
+        en : "",
+        es : ""
+    } ],
 	'tabela' : [ {
 		pt : "Tabela",
 		en : "Table",
 		es : "Tabla"
 	} ],
+    'tabelaTip' : [ {
+        pt : "Mostra a tabela de atributos associada aos elementos do tema",
+        en : "",
+        es : ""
+    } ],
 	'texto' : [ {
 		pt : "Topon&iacute;mia",
 		en : "Toponymy",
 		es : "Toponimia"
 	} ],
+    'textoTip' : [ {
+        pt : "Altera a apresenta&ccedil;&atilde;o de labels vinculados aos elementos do tema",
+        en : "",
+        es : ""
+    } ],
 	'etiqueta' : [ {
 		pt : "Etiquetas, busca...",
 		en : "Tags, search ...",
 		es : "Etiquetas, b&uacute;squeda ..."
 	} ],
+    'etiquetaTip' : [ {
+        pt : "Define como os dados s&atilde;o mostrados em eventos do mouse",
+        en : "",
+        es : ""
+    } ],
 	'filtro' : [ {
 		pt : "Filtro",
 		en : "Filter",
 		es : "Filtro"
 	} ],
+    'filtroTip' : [ {
+        pt : "Altera os par&acirc;metros de filtragem de elementos do tema",
+        en : "",
+        es : ""
+    } ],
 	'opacidade' : [ {
 		pt : "Opacidade (0 a 100)",
 		en : "Opacity (0 - 100)",
@@ -60,6 +90,11 @@ i3GEOF.atalhosedicao.dicionario = {
 		en : "Chart",
 		es : "Gr&aacute;ficos"
 	}],
+    'temacomgraficoTip' : [ {
+        pt : "Define gr&aacute;ficos que s&atilde;o mostrados em cada elemento do tema",
+        en : "",
+        es : ""
+    } ],
 	"mostraClasses" : [{
 		pt : "Mostra as classes na legenda",
 		en : "Shows classes in legend",
@@ -80,9 +115,54 @@ i3GEOF.atalhosedicao.dicionario = {
 		en : "Plugins that implement special ways of visualization. The layer can only have one plugin.",
 		es : "Plugins que implementan formas especiales de visualizaci&oacute;n. La capa solo puede tener un plugin."
 	}],
-	"funcaojstip" : [{
+	"funcaojsTitulo" : [{
 		pt : "Fun&ccedil;&atilde;o JS",
 		en : "",
 		es : ""
-	}]
+	}],
+    'funcaojsTip' : [ {
+        pt : "Configura links que s&atilde;o mostrados nos bal&otilde;es de informa&ccedil;&atilde;o (identifica)",
+        en : "",
+        es : ""
+    } ],
+    "tmeTip" : [{
+        pt : "Gera um KML para visualiza&ccedil;&atilde;o de dados estat&iacute;sticos no Google Earth",
+        en : "",
+        es : ""
+    }],
+    "storymapTip" : [{
+        pt : "Mostra os dados da camada na forma de uma hist&oacute;ria com textos e mapas",
+        en : "",
+        es : ""
+    }],
+    "animagifTip" : [{
+        pt : "Cria uma imagem no formato GIF que mostra a evolu&ccedil;&atilde;o dos dados no tempo",
+        en : "",
+        es : ""
+    }],
+    "timelineTip" : [{
+        pt : "Mostra um gr&aacute;fico do tipo linha do tempo com os eventos armazenados na tabela de atributos",
+        en : "",
+        es : ""
+    }],
+    "parametrossqlTip" : [{
+        pt : "Define par&acirc;metros que o usu&aacute;rio poder&aacute; escolher quando o tema for adicionado ao mapa",
+        en : "",
+        es : ""
+    }],
+    "layerkmlTip" : [{
+        pt : "Permite que o tema utilize como fonte de dados um arquivo KML definido em uma URI",
+        en : "",
+        es : ""
+    }],
+    "clusterTip" : [{
+        pt : "Modifica a forma de apresenta&ccedil;&atilde;o do tema utilizando a estrat&eacute;gia de agrupamentos por proximidade",
+        en : "",
+        es : ""
+    }],
+    "heatmapTip" : [{
+        pt : "Configura o tema para ser mostrado na forma de um mapa de calor",
+        en : "",
+        es : ""
+    }]
 };
