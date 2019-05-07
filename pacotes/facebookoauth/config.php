046a519578c1e143190ad7e6dfd7cecf308afb5d
Edmar Moretti <edmar.moretti@gmail.com>
2019-05-07 19:55:37 -0300

    Remoção de bibliotecas não utilizadas

--- pacotes/facebookoauth/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-include(dirname(__FILE__)."/../../ms_configura.php");
-define('FACEBOOK_APP_ID', $facebookoauth["consumerkey"]);
-define('FACEBOOK_SECRET', $facebookoauth["consumersecret"]);
-?>
