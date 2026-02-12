aa8c594ffbb81072b5cddaedf1ad4a066cb141a4
edmarmoretti <edmar.moretti@gmail.com>
2026-02-12 16:09:00 -0300

    Atualização das funções de inclusão de sistemas adicionais na ferramenta de identificação. Remoção de xml.php

--- classesphp/classe_temas.php
+++ classesphp/classe_temas.php
@@ -667,7 +667,7 @@ class Temas
             $this->layer->setmetaData("cache", "");
             $this->layer->set("opacity", 100);
             $this->layer->updateFromString('LAYER COMPOSITE OPACITY '.$valor.' END END');
-            
+
             return ("ok");
     }
 
@@ -1207,51 +1207,15 @@ class Temas
      */
     function fonteTema($tema)
     {
-        include_once ($this->locaplic . "/classesphp/xml.php");
-        require ($this->locaplic . "/classesphp/classe_menutemas.php");
-        $menutemas = new Menutemas("", "", "", $this->locaplic);
+        require ($this->locaplic . "/classesphp/classe_arvore.php");
+        $arvore = new Arvore($this->locaplic);
+        $tema = $arvore->pegaTema($tema);
         $linkfonte = "erro";
-        $tipo = "";
-        $this->xml = "";
-        foreach ($menutemas->pegaListaDeMenus() as $menu) {
-            if (! isset($menu["url"])) {
-                $menu["url"] = "";
-            } // para efeitos de compatibilidade entre vers&otilde;es do i3geo
-            $ondexml = $menu["arquivo"];
-            if ($menu["url"] != "") {
-                $ondexml = $menu["url"];
-            }
-            $verificaXml = false;
-            if ($ondexml != "") {
-                $verificaXml = simplexml_load_file($ondexml);
-                if ($verificaXml)
-                    $this->xml[] = $verificaXml;
-            } else // pega o xml do sistema de administra&ccedil;&atilde;o
-            {
-                $verificaXml = simplexml_load_string(geraXmlMenutemas(implode(" ", $this->perfil), $menu["idmenu"], $tipo, $this->locaplic));
-                if ($verificaXml)
-                    $this->xml[] = $verificaXml;
-            }
-            if (! $verificaXml)
-                $this->xml[] = simplexml_load_string(geraXmlMenutemas(implode(" ", $this->perfil), $menu["idmenu"], $tipo, $this->locaplic));
-        }
-        foreach ($this->xml as $xml) {
-            $subgrupo = array();
-            foreach ($xml->GRUPO as $grupo) {
-                foreach ($grupo->SGRUPO as $sgrupo) {
-                    foreach ($sgrupo->TEMA as $t) {
-                        $link = ixml($t, "TLINK");
-                        $tid = ixml($t, "TID");
-                        if ($tid == $tema) {
-                            $linkfonte = $link;
-                        }
-                    }
-                }
-            }
+        if (isset($tema["link"])) {
+            $linkfonte = $tema["link"];
         }
         return ($linkfonte);
     }
-
     /*
      * function: zoomSel
      *
